1590a0c065e15ef254a2fde0747efb0f82052eab
eriohl <eriohl@gmail.com>
2012-09-04 15:11:21 +0200

    Entity comparison.

--- src/Starcounter/Mock/Entity.cs
+++ src/Starcounter/Mock/Entity.cs
@@ -21,6 +21,60 @@ namespace Starcounter
     public abstract class Entity : DbObject, IObjectView
     {
 
+        /// <summary>
+        /// Specialized comparison operator for database objects. The
+        /// framework-provided comparison operator for objects compares them by
+        /// reference only, and for entity object's we want the comparison
+        /// to be value based. This means that even if two objects are
+        /// different in memory, the can still be considered equal if they only
+        /// reference the same kernel database object.
+        /// </summary>
+        /// <param name="obj1">
+        /// The first object in the comparison
+        /// </param>
+        /// <param name="obj2">
+        /// The second object in the comparison
+        /// </param>
+        /// <returns>
+        /// True if both references are null or if both references are not null
+        /// and references the same kernel database object. False otherwise.
+        /// </returns>
+        public static bool operator == (Entity obj1, Entity obj2)
+        {
+            if (object.ReferenceEquals(obj1, null))
+            {
+                return object.ReferenceEquals(obj2, null);
+            }
+            return obj1.Equals(obj2);
+        }
+
+        /// <summary>
+        /// Specialized comparison operator for database objects. The
+        /// framework-provided comparison operator for objects compares them by
+        /// reference only, and for entity object's we want the comparison
+        /// to be value based. This means that even if two objects are
+        /// different in memory, the can still be considered equal if they only
+        /// reference the same kernel database object.
+        /// </summary>
+        /// <param name="obj1">
+        /// The first object in the comparison
+        /// </param>
+        /// <param name="obj2">
+        /// The second object in the comparison
+        /// </param>
+        /// <returns>
+        /// False if both references are null or if both references are not null
+        /// and references the same kernel database object. True otherwise.
+        /// </returns>
+        public static bool operator != (Entity obj1, Entity obj2)
+        {
+            if (object.ReferenceEquals(obj1, null))
+            {
+                return !object.ReferenceEquals(obj2, null);
+            }
+            return !obj1.Equals(obj2);
+        }
+        
         private TypeBinding typeBinding_;
 
         public Entity(Sc.Server.Internal.Uninitialized u) { }
@@ -33,6 +87,53 @@ namespace Starcounter
         public void Delete()
         {
             throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Overrides the .NET frameworks Object's Equals method, that will
+        /// compare for object reference equality. For a database object, the
+        /// equals function uses value equality logic rather than reference
+        /// equality logic; that means, that even if two instances are
+        /// different in memory, they can still be considered the same if they
+        /// reference the same database kernel object.
+        /// </summary>
+        /// <param name="obj">
+        /// The object to compare this instance to.
+        /// </param>
+        /// <returns>
+        /// True if obj is not null, is an entity object and references the
+        /// same kernel object as this; false otherwise.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity);
+        }
+
+        public bool Equals(Entity obj)
+        {
+            if (obj != null)
+            {
+                if (obj.ThisRef.ObjectID == ThisRef.ObjectID)
+                {
+                    return (GetType() == obj.GetType());
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// The GetHashCode implementation will return the hash-code of the
+        /// unique object ID of a given object.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return ThisRef.ObjectID.GetHashCode();
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return string.Format("{0}({1})", GetType().Name, ThisRef.ObjectID.ToString());
         }
 
         internal void Attach(ObjectRef objectRef, TypeBinding typeBinding)
