3659121feac2ce38face01543699d3fbb4cf285b
eriohl <eriohl@gmail.com>
2012-09-04 16:02:27 +0200

    Updated test project adding result scan to test IObjectView implementation.

--- src/hello/Program.cs
+++ src/hello/Program.cs
@@ -19,7 +19,8 @@ namespace hello
             Db.Transaction(() =>
             {
                 MyMusic.Mucho m;
-                m = (MyMusic.Mucho)Db.SQL("SELECT m FROM MyMusic.Mucho m WHERE m.Number = ?", 7).First;
+//                m = (MyMusic.Mucho)Db.SQL("SELECT m FROM MyMusic.Mucho m WHERE m.Number = ?", 7).First;
+                m = (MyMusic.Mucho)Db.SQL("SELECT m FROM MyMusic.Mucho m WHERE m.Number = ? AND m.Name = ?", 7, "Nisse").First;
                 if (m == null)
                 {
                     m = new MyMusic.Mucho();
@@ -29,7 +30,8 @@ namespace hello
                 m = null;
 
                 MyMusic.Album a;
-                a = (MyMusic.Album)Db.SQL("SELECT m FROM MyMusic.Album m WHERE m.Name = ?", "Nisse").First;
+//                a = (MyMusic.Album)Db.SQL("SELECT a FROM MyMusic.Album a WHERE a.Name = ?", "Nisse").First;
+                a = (MyMusic.Album)Db.SQL("SELECT a FROM MyMusic.Album a WHERE a.Name = ? AND a.Label = ?", "Nisse", "Nisse").First;
                 if (a == null)
                 {
                     a = new MyMusic.Album("Nisse", "Nisse", DateTime.Now);
