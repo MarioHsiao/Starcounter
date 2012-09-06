41f116da7b73840c3cdfa69e4e53ad21c894e12c
eriohl <eriohl@gmail.com>
2012-09-06 13:43:47 +0200

    Removed schema creation code from sample program.

--- src/hello/Program.cs
+++ src/hello/Program.cs
@@ -13,8 +13,6 @@ namespace hello
         {
             Console.WriteLine("Hello world (on database thread in database process)!");
 
-            UpdateSchema();
-
 #if false
             Db.Transaction(() =>
             {
@@ -39,112 +37,6 @@ namespace hello
                 a = null;
             });
 #endif
-        }
-
-        private static void UpdateSchema()
-        {
-            UpdateSchema_Album();
-            UpdateSchema_Mucho();
-
-            Starcounter.Internal.Fix.ResetTheQueryModule();
-        }
-
-        private static void UpdateSchema_Album()
-        {
-            TableDef t = null;
-
-            Db.Transaction(() =>
-            {
-                t = Db.LookupTable("MyMusic.Album");
-            });
-
-#if false
-            if (t != null)
-            {
-                Db.DropTable(t.TableId);
-                t = null;
-            }
-#endif
-
-            if (t == null)
-            {
-                t = new TableDef(
-                    "MyMusic.Album",
-                    new ColumnDef[] {
-                        new ColumnDef("Label", DbTypeCode.String, true),
-                        new ColumnDef("Name", DbTypeCode.String, true),
-                        new ColumnDef("Published", DbTypeCode.DateTime, true)
-                        }
-                    );
-                Db.CreateTable(t);
-
-                Db.Transaction(() =>
-                {
-                    t = Db.LookupTable("MyMusic.Album");
-                });
-
-                Db.CreateIndex(t.DefinitionAddr, "Album1", 1); // Index on Album.Name
-            }
-
-            TypeDef typeDef = new TypeDef(
-                "MyMusic.Album",
-                new PropertyDef[] {
-                    new PropertyDef("Label", DbTypeCode.String, true, 0),
-                    new PropertyDef("Name", DbTypeCode.String, true, 1),
-                    new PropertyDef("Published", DbTypeCode.DateTime, true, 2)
-                    },
-                new TypeLoader(AppDomain.CurrentDomain.BaseDirectory + "MyMusic.dll", "MyMusic.Album"),
-                t
-                );
-            Bindings.RegisterTypeDef(typeDef);
-        }
-
-        private static void UpdateSchema_Mucho()
-        {
-            TableDef t = null;
-
-            Db.Transaction(() =>
-            {
-                t = Db.LookupTable("MyMusic.Mucho");
-            });
-
-#if false
-            if (t != null)
-            {
-                Db.DropTable(t.TableId);
-                t = null;
-            }
-#endif
-
-            if (t == null)
-            {
-                t = new TableDef(
-                    "MyMusic.Mucho",
-                    new ColumnDef[] {
-                        new ColumnDef("Name", DbTypeCode.String, true),
-                        new ColumnDef("Number", DbTypeCode.Int64, false)
-                        }
-                    );
-                Db.CreateTable(t);
-
-                Db.Transaction(() =>
-                {
-                    t = Db.LookupTable("MyMusic.Mucho");
-                });
-
-                Db.CreateIndex(t.DefinitionAddr, "Mucho1", 1); // Index on Mucho.Number
-            }
-
-            TypeDef typeDef = new TypeDef(
-                "MyMusic.Mucho",
-                new PropertyDef[] {
-                    new PropertyDef("Name", DbTypeCode.String, true, 0),
-                    new PropertyDef("Number", DbTypeCode.Int64, false, 1)
-                    },
-                new TypeLoader(AppDomain.CurrentDomain.BaseDirectory + "MyMusic.dll", "MyMusic.Mucho"),
-                t
-                );
-            Bindings.RegisterTypeDef(typeDef);
         }
     }
 }
