cf0c6fc65557cf4203a21225c6802e53bb2915cd
Per Samuelsson <per@starcounter.com>
2012-09-11 10:41:20 +0200

    Added class TransactionConflictException, representing a transaction conflict.

---
+++ src/Starcounter/TransactionConflictException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Starcounter {
+    [Serializable]
+    public class TransactionConflictException : TransactionAbortedException, ITransactionConflictException {
+
+        internal TransactionConflictException(UInt32 errorCode, String message, Exception innerException) : base(errorCode, message, innerException) { }
+
+        protected TransactionConflictException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context) {
+        }
+    }
+}
