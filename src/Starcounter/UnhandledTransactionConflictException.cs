55e7a604e8dfcd7dafe7011cb37fb7d49e97f59a
Per Samuelsson <per@starcounter.com>
2012-09-11 10:30:34 +0200

    Added class UnhandledTransactionConflictException and made sure it produced a proper error message, based on error message factoring.

---
+++ src/Starcounter/UnhandledTransactionConflictException.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Runtime.Serialization;
+
+namespace Starcounter {
+    
+    public class UnhandledTransactionConflictException : DbException {
+
+        /// <summary>
+        /// For internal use, not to be used in user code (used in generated
+        /// transaction scopes).
+        /// </summary>
+        public UnhandledTransactionConflictException(Exception innerException)
+            : base(
+                Error.SCERRUNHANDLEDTRANSACTCONFLICT,
+                Starcounter.ErrorCode.ToMessage(Error.SCERRUNHANDLEDTRANSACTCONFLICT),
+                innerException
+                ) { }
+
+        internal UnhandledTransactionConflictException(UInt32 errorCode, String message, Exception innerException) : base(errorCode, message, innerException) { }
+
+        protected UnhandledTransactionConflictException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context) {
+        }
+    }
+}
