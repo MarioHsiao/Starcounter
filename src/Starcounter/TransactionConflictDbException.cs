e730ab769533e3fa2fe56c1cdff48eb3c30c34ab
Per Samuelsson <per@starcounter.com>
2012-09-11 10:23:57 +0200

    Added class TransactionConflictDbException, representing a transaction conflict.

---
+++ src/Starcounter/TransactionConflictDbException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Starcounter {
+    /// <summary>
+    /// Variant of <see cref="DbException" /> thrown when it wraps a
+    /// <see cref="ITransactionConflictException" />. Used to make sure that
+    /// automatic restarts and such are triggered because of conflicts detected
+    /// in hooks.
+    /// </summary>
+    [Serializable]
+    public class TransactionConflictDbException : DbException, ITransactionConflictException {
+        internal TransactionConflictDbException(UInt32 errorCode, String message, Exception innerException) : base(errorCode, message, innerException) { }
+
+        protected TransactionConflictDbException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context) {
+        }
+    }
+}
