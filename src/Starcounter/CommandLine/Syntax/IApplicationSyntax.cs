b802a11623d11fa2d533f3caecf01554c77ffe6f
Per Samuelsson <per@starcounter.com>
2012-09-11 12:26:55 +0200

    Incorporated and adapted the core classes for the CommandLine implementation we specified to be used in all our end-user programs.

---
+++ src/Starcounter/CommandLine/Syntax/IApplicationSyntax.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Starcounter.CommandLine.Syntax
+{
+    public interface IApplicationSyntax
+    {
+        bool RequiresCommand { get; }
+        string DefaultCommand { get; }
+        string ProgramDescription { get; }
+        OptionInfo[] Properties { get; }
+        OptionInfo[] Flags { get; }
+        ICommandSyntax[] Commands { get; }
+    }
+}
---
+++ src/Starcounter/CommandLine/Syntax/OptionAttributes.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace Starcounter.CommandLine.Syntax
+{
+    /// <summary>
+    /// Defines a set of constant flags that can be use to attribute
+    /// an option.
+    /// </summary>
+    [Flags] public enum OptionAttributes
+    {
+        /// <summary>
+        /// Specifies the default option attributes are to be used.
+        /// </summary>
+        /// <remarks>
+        /// The default is a property that is not required and can
+        /// only appear once.
+        /// </remarks>
+        Default = 0,
+
+        /// <summary>
+        /// Specifies the option to which the current attribute
+        /// applies is required.
+        /// </summary>
+        Required = 1,
+
+        /// <summary>
+        /// Specifies the option to which the current attribute
+        /// applies is a flag, and hence no value can be expected.
+        /// </summary>
+        Flag = 2
+    }
+}
