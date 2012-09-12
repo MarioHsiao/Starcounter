ccd4ff1a048036e9ed5c33729831c70c7b876e28
eriohl <eriohl@gmail.com>
2012-09-12 21:08:24 +0200

    Handle inherited tables on table upgrade.

--- src/Starcounter/Binding/TableDef.cs
+++ src/Starcounter/Binding/TableDef.cs
@@ -17,6 +17,51 @@ namespace Starcounter.Binding
     public sealed class TableDef
     {
 
+        internal unsafe static TableDef ConstructTableDef(ulong definitionAddr, sccoredb.Mdb_DefinitionInfo definitionInfo)
+        {
+            string name = new String(definitionInfo.table_name);
+            ushort tableId = definitionInfo.table_id;
+            uint columnCount = definitionInfo.column_count;
+            string baseName = null;
+
+            int b = 1;
+
+            if (definitionInfo.inherited_definition_addr != sccoredb.INVALID_DEFINITION_ADDR)
+            {
+                b = sccoredb.Mdb_DefinitionToDefinitionInfo(definitionInfo.inherited_definition_addr, out definitionInfo);
+                if (b != 0)
+                {
+                    baseName = new String(definitionInfo.table_name);
+                }
+            }
+
+            if (b != 0)
+            {
+                ColumnDef[] columns = new ColumnDef[columnCount];
+                for (ushort i = 0; i < columns.Length; i++)
+                {
+                    sccoredb.Mdb_AttributeInfo attributeInfo;
+                    b = sccoredb.Mdb_DefinitionAttributeIndexToInfo(definitionAddr, i, out attributeInfo);
+                    if (b != 0)
+                    {
+                        columns[i] = new ColumnDef(
+                            new string(attributeInfo.PtrName),
+                            BindingHelper.ConvertScTypeCodeToDbTypeCode(attributeInfo.Type),
+                            (attributeInfo.Flags & sccoredb.MDB_ATTRFLAG_NULLABLE) != 0,
+                            (attributeInfo.Flags & sccoredb.MDB_ATTRFLAG_DERIVED) != 0
+                            );
+                    }
+                    else
+                    {
+                        throw ErrorCode.ToException(sccoredb.Mdb_GetLastError());
+                    }
+                }
+                return new TableDef(name, baseName, columns, tableId, definitionAddr);
+            }
+
+            throw ErrorCode.ToException(sccoredb.Mdb_GetLastError());
+        }
+
         public string Name;
 
         public string BaseName;
