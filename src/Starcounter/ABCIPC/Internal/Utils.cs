5e831b5dd5370714945ce5328741e5d6af08aca3
Per Samuelsson <per@starcounter.com>
2012-09-14 14:31:30 +0200

    Added utility code that can be used to invoke a ABCIPC server from a command prompt using a simple text based input method and to see the response on the console.

---
+++ src/Starcounter/ABCIPC/Internal/Utils.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Starcounter.ABCIPC.Internal {
+    
+    public static class Utils {
+
+        public static class PromptHelper {
+
+            /// <summary>
+            /// Can be installed in servers that want to offer a simple
+            /// text based input/output alternative, using the console.
+            /// </summary>
+            /// <remarks>
+            /// Install when creating new server, like new Server(ReadTextRequestFromConsole, WriteTextResponseToConsole);
+            /// </remarks>
+            /// <returns></returns>
+            public static string ReadTextRequestFromConsole() {
+                while (true) {
+                    Console.Write(">");
+                    string read = Console.ReadLine();
+
+                    // Never return anything until we have properly parsed it!
+                    // And remember, we can write to the console here to!
+                    // TODO:
+
+                    read = read.Trim();
+                    if (read.Equals(string.Empty)) {
+                        // Make shutdown request.
+                        // TODO;
+                        Console.Beep();
+                        continue;
+                    }
+
+                    int indexOfFirstSpace = read.IndexOf(" ");
+                    if (indexOfFirstSpace == -1) {
+                        // Implements: client.Send(string)
+                        var r = Request.Protocol.MakeRequestStringWithoutParameters(read);
+                        ToConsoleWithColor("(" + r + ")", ConsoleColor.DarkGray);
+                        return r;
+                    }
+
+                    Console.Beep();
+                }
+            }
+
+            public static void WriteTextResponseToConsole(string response) {
+                ToConsoleWithColor("<" + response, ConsoleColor.Yellow);
+            }
+
+            static void ToConsoleWithColor(string text, ConsoleColor color) {
+                try {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(text);
+                } finally {
+                    Console.ResetColor();
+                }
+            }
+        }
+	}
+}
