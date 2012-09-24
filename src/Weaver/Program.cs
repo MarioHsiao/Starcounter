a1c94abe32d2dddba822224f080aaa06b7631558
Per Samuelsson <per@starcounter.com>
2012-09-24 14:54:55 +0200

    Added weaver program skeleton code.

--- src/Weaver/Program.cs
+++ src/Weaver/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Starcounter;
+using Starcounter.CommandLine;
+using Starcounter.CommandLine.Syntax;
 
 namespace Weaver {
+
     class Program {
+
         static void Main(string[] args) {
+            ApplicationArguments arguments;
+
+            try {
+                if (TryGetProgramArguments(args, out arguments))
+                    ExecuteCommand(arguments);
+            } finally {
+                Console.ResetColor();
+            }
+        }
+
+        static void ExecuteCommand(ApplicationArguments arguments) {
+            // Implement.
+            // TODO:
+        }
+
+        static bool TryGetProgramArguments(string[] args, out ApplicationArguments arguments) {
+            arguments = null;
+            return false;
         }
     }
 }
