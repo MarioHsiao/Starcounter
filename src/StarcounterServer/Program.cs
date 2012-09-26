e864e9ff6144787e74ba592648b05add70fb00ed
Per Samuelsson <per@starcounter.com>
2012-09-26 14:32:51 +0200

    Implemented the skeleton bootstrap code in the server and made sure it can be executed both as a standalone program and a Windows service.

--- src/StarcounterServer/Program.cs
+++ src/StarcounterServer/Program.cs
@@ -3,12 +3,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.ServiceProcess;
 
 namespace StarcounterServer {
 
-    class Program {
+    class Server : ServiceBase {
+        static Server server;
+        
         static void Main(string[] args) {
+            server = new Server();
+
+            if (Environment.UserInteractive) {
+                // The server is executed either as a console program, or a
+                // Windows application, depending on how it was built.
+                Console.CancelKeyPress += Console_CancelKeyPress;
+                server.OnStart(args);
+                Console.WriteLine("Press CTRL+C to exit...");
+                Thread.Sleep(Timeout.Infinite);
+
+            } else {
+                // The server is ran as a service.
+                ServiceBase.Run(server);
+            }
+        }
+
+        protected override void OnStart(string[] args) {
+            // The server is starting.
+        }
+
+        protected override void OnStop() {
+            // The server is stopping.
+        }
+
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e) {
+            server.OnStop();
+            Environment.Exit(0);
         }
     }
 }
