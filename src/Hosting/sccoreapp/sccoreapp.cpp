75f10e08416bfbf4cf25512f2757538629fc0c9f
eriohl <eriohl@gmail.com>
2012-09-27 19:10:37 +0200

    Updated sccoreapp to link and include sccorelib.

--- src/Hosting/sccoreapp/sccoreapp.cpp
+++ src/Hosting/sccoreapp/sccoreapp.cpp
@@ -2,15 +2,12 @@
 #include "internal.h"
 
 
-COALMINE_STANDBY coalmine_standby;
-
-
 extern "C" uint32_t __stdcall sccoreapp_standby(void* hsched, CM2_TASK_DATA* ptask_data)
 {
 	for (;;)
 	{
 		uint32_t e;
-		e = coalmine_standby(hsched, ptask_data);
+		e = cm2_standby(hsched, ptask_data);
 		if (e == 0)
 		{
             switch (ptask_data->Type)
