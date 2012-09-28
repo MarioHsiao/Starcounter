a99f8ee643cf97e1c537f61433de1340a06b3970
Per Samuelsson <per@starcounter.com>
2012-09-28 06:19:24 +0200

    Added the ServerNode class, representing the running server inside a host program.

---
+++ src/StarcounterServer/ServerNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter.Configuration;
+
+namespace StarcounterServer {
+
+    /// <summary>
+    /// Representing the running server, hosted in a server program.
+    /// </summary>
+    internal class ServerNode {
+
+        public readonly ServerConfiguration Configuration;
+        
+            public ServerNode(ServerConfiguration configuration) {
+            this.Configuration = configuration;
+        }
+    }
+}
