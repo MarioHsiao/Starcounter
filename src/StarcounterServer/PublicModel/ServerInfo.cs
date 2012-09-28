6ceaeaad37bae76ebec2781900d3eb2585ecb93a
Per Samuelsson <per@starcounter.com>
2012-09-28 09:19:47 +0200

    Added the first two classes in the new StarcounterServer.PublicModel namespace, and an About.txt file describing the intention of said namespace.

---
+++ src/StarcounterServer/PublicModel/ServerInfo.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Xml.Serialization;
+using Starcounter.Configuration;
+
+namespace StarcounterServer.PublicModel {
+
+    /// <summary>
+    /// Represents a snapshot of the public state of a server.
+    /// </summary>
+    internal sealed class ServerInfo {
+
+        /// <summary>
+        /// Initializes a <see cref="ServerInfo"/> message object.
+        /// </summary>
+        public ServerInfo() {
+        }
+
+        /// <summary>
+        /// Gets the URI of the server.
+        /// </summary>
+        /// <remarks>
+        /// The URI is a logical URI of type <see cref="ScUriKind.Server"/>
+        /// </remarks>
+        public string Uri {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Configuration of the server.
+        /// </summary>
+        public ServerConfiguration Configuration {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets a value indicating if monitoring is supported
+        /// by this server.
+        /// </summary>
+        public bool IsMonitoringSupported {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The servers default maximum image size, used when creating
+        /// databases if no maximum size is explicitly given.
+        /// </summary>
+        public long DefaultMaxImageSize {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The servers default transaction log size, used when creating
+        /// databases if no maximum size is explicitly given.
+        /// </summary>
+        public long DefaultTransactionLogSize {
+            get;
+            set;
+        }
+    }
+}
