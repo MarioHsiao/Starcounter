3535c60de74652af02c7d059947a8f34ab4a1847
Per Samuelsson <per@starcounter.com>
2012-09-28 09:16:26 +0200

    Added the Database class, designed to hold the server-internal state of a database maintained by a given server.

---
+++ src/StarcounterServer/Database.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter.Configuration;
+
+namespace StarcounterServer {
+
+    /// <summary>
+    /// A database maintained by a certain server (represented by
+    /// the <see cref="Database.Server"/> property).
+    /// </summary>
+    internal sealed class Database {
+        
+        /// <summary>
+        /// The server to which this database belongs.
+        /// </summary>
+        internal readonly ServerNode Server;
+
+        /// <summary>
+        /// The configuration of this <see cref="Database"/>.
+        /// </summary>
+        internal readonly DatabaseRuntimeConfiguration Configuration;
+
+        /// <summary>
+        /// Intializes a <see cref="Database"/>.
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="configuration"></param>
+        internal Database(ServerNode server, DatabaseRuntimeConfiguration configuration) {
+            this.Server = server;
+            this.Configuration = configuration;
+        }
+    }
+}
