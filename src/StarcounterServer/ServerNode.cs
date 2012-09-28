8d7b2905f4ae4a3fb63142f868d91911b20d3269
Per Samuelsson <per@starcounter.com>
2012-09-28 09:17:33 +0200

    Extended the ServerNode with a couple of core properties and stubs for Setup, Start and Stop.

--- src/StarcounterServer/ServerNode.cs
+++ src/StarcounterServer/ServerNode.cs
@@ -1,21 +1,52 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Starcounter.Configuration;
+using Starcounter;
 
 namespace StarcounterServer {
 
     /// <summary>
     /// Representing the running server, hosted in a server program.
     /// </summary>
-    internal class ServerNode {
+    internal sealed class ServerNode {
 
-        public readonly ServerConfiguration Configuration;
-        
-            public ServerNode(ServerConfiguration configuration) {
+        /// <summary>
+        /// Gets the server configuration.
+        /// </summary>
+        internal readonly ServerConfiguration Configuration;
+
+        /// <summary>
+        /// Gets the URI of this server.
+        /// </summary>
+        internal readonly string Uri;
+
+        /// <summary>
+        /// Gets the dictionary with databases maintained by this server,
+        /// keyed by their name.
+        /// </summary>
+        internal Dictionary<string, Database> Databases { get; private set; }
+
+        /// <summary>
+        /// Initializes a <see cref="ServerNode"/>.
+        /// </summary>
+        /// <param name="configuration"></param>
+        internal ServerNode(ServerConfiguration configuration) {
             this.Configuration = configuration;
+            this.Uri = ScUri.MakeServerUri(ScUri.GetMachineName(), configuration.Name);
+            this.Databases = new Dictionary<string, Database>();
+        }
+
+        internal void Setup() {
+        }
+
+        internal void Start() {
+        }
+
+        internal void Stop() {
         }
     }
 }
