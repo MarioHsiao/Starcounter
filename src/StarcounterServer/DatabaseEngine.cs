edfc7bc781896ae047a3a964bd95609bb34cdd4c
Per Samuelsson <per@starcounter.com>
2012-09-29 20:56:39 +0200

    Introduced the DatabaseEngine class, an adapter pattern type that shields the database engine and allows callers to get process commands without having to know all the plumbing of the underlying command lines.

---
+++ src/StarcounterServer/DatabaseEngine.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Starcounter.Server {
+
+    /// <summary>
+    /// Encapsulates and abstracts the database engine, i.e. letting code
+    /// using this class act on the database engine without having to know
+    /// the exact underlying details about how to actually start it or
+    /// what exact input to use.
+    /// </summary>
+    internal sealed class DatabaseEngine {
+        string installationDirectory;
+
+        internal const string DatabaseExeFileName = "scpmm.exe";
+        internal const string WorkerProcessExeFileName = "boot.exe";
+
+        /// <summary>
+        /// Gets the server that has instantiated this engine.
+        /// </summary>
+        internal readonly ServerNode Server;
+
+        /// <summary>
+        /// Gets the full path to the database executable.
+        /// </summary>
+        internal string DatabaseExePath {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the full path to the worker process executable.
+        /// </summary>
+        internal string WorkerProcessExePath {
+            get;
+            private set;
+        }
+
+        internal DatabaseEngine(ServerNode server) {
+            this.Server = server;
+        }
+
+        internal void Setup() {
+            this.installationDirectory = Path.GetDirectoryName(typeof(DatabaseEngine).Assembly.Location);
+            var databaseExe = Path.Combine(this.installationDirectory, DatabaseExeFileName);
+            if (!File.Exists(databaseExe)) {
+                throw ErrorCode.ToException(Error.SCERRUNSPECIFIED, string.Format("Database engine executable not found: {0}", databaseExe));
+            }
+            var workerProcExe = Path.Combine(this.installationDirectory, WorkerProcessExeFileName);
+            if (!File.Exists(workerProcExe)) {
+                throw ErrorCode.ToException(Error.SCERRUNSPECIFIED, string.Format("Worker process executable not found: {0}", databaseExe));
+            }
+
+            this.DatabaseExePath = databaseExe;
+            this.WorkerProcessExePath = workerProcExe;
+        }
+
+        internal ProcessStartInfo GetDatabaseStartInfo(Database database) {
+            throw new NotImplementedException();
+        }
+
+        internal ProcessStartInfo GetWorkerProcessStartInfo(Database database) {
+            throw new NotImplementedException();
+        }
+    }
+}
