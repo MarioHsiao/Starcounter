c0b848b62ed4699f3b4f7ef14aed760eb1e1e939
Per Samuelsson <per@starcounter.com>
2012-10-01 12:40:05 +0200

    Added interface IResponseSerializer, hiding the implementation of a handler response serializer.

---
+++ src/StarcounterServer/PublicModel/Serialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Server.PublicModel {
+
+    /// <summary>
+    /// Interface of a response serializer.
+    /// </summary>
+    public interface IResponseSerializer {
+        string SerializeReponse(object response);
+    }
+}
