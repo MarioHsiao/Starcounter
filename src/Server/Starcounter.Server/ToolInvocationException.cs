2815290346fb8f84c053abb0c416c91cfd369243
Per Samuelsson <per@starcounter.com>
2012-10-05 10:22:49 +0200

    Added helper code to easily start/run external tool processes from the server.

---
+++ src/Server/Starcounter.Server/ToolInvocationException.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+namespace Starcounter.Server {
+    /// <summary>
+    /// Exception thrown by <see cref="ToolInvocationHelper"/> when an external process
+    /// dot not complete successfully.
+    /// </summary>
+    public class ToolInvocationException : Exception {
+        /// <summary>
+        /// Initializes a new <see cref="ToolInvocationException"/>
+        /// and builds the error message from a <see cref="ToolInvocationResult"/>.
+        /// </summary>
+        /// <param name="result"></param>
+        internal ToolInvocationException(ToolInvocationResult result)
+            : this(string.Format("Error invoking \"\"{0}\" {1}\": exit code is 0x{2:x}. Tool output:\n\n {3}",
+                                 result.FileName, result.Arguments, result.ExitCode, string.Join(Environment.NewLine, result.GetOutput()))) {
+            this.ExitCode = result.ExitCode;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="ToolInvocationException"/>.
+        /// </summary>
+        public ToolInvocationException() {
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="ToolInvocationException"/> and specifies the
+        /// exception message.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        public ToolInvocationException(string message)
+            : base(message) {
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="ToolInvocationException"/> and specifies
+        /// the exception message and an inner <see cref="Exception"/>.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        /// <param name="inner">The inner <see cref="Exception"/>.</param>
+        public ToolInvocationException(string message, Exception inner)
+            : base(message, inner) {
+        }
+
+        /// <summary>
+        /// Gets the process exit code.
+        /// </summary>
+        public int ExitCode {
+            get;
+            private set;
+        }
+    }
+}
