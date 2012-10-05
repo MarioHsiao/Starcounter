9d26ebeaf6ac2b43bd7f6fcf4c93c9acb7fa0728
Per Samuelsson <per@starcounter.com>
2012-10-05 09:35:34 +0200

    Made ExecAppCommand public and extended it to be able to allow a database name to be given in the command line, or use the assembly name, if not explicitly set.

--- src/Server/Starcounter.Server/PublicModel/Commands/ExecAppCommand.cs
+++ src/Server/Starcounter.Server/PublicModel/Commands/ExecAppCommand.cs
@@ -1,29 +1,56 @@
 ﻿
 using System;
 using System.IO;
+using System.Linq;
+using Starcounter.Apps.Bootstrap;
 
 namespace Starcounter.Server.PublicModel.Commands {
 
     /// <summary>
     /// A command representing the request to start an executable.
     /// </summary>
-    internal sealed class ExecAppCommand : ServerCommand {
+    public sealed class ExecAppCommand : ServerCommand {
         /// <summary>
         /// Gets the path to the assembly file requesting to start.
         /// </summary>
-        internal readonly string AssemblyPath;
+        public readonly string AssemblyPath;
 
         /// <summary>
         /// Gets the path to the directory the requesting executable
         /// has given as it's working directory;
         /// </summary>
-        internal readonly string WorkingDirectory;
+        public readonly string WorkingDirectory;
 
         /// <summary>
         /// Gets the arguments with which the requesting executable
         /// was started with.
         /// </summary>
-        internal readonly string[] Arguments;
+        public readonly string[] Arguments;
+
+        /// <summary>
+        /// Gets or sets the name of the database the App should load
+        /// into.
+        /// </summary>
+        public string DatabaseName {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets all arguments targeting Starcounter.
+        /// </summary>
+        internal string[] ArgumentsToStarcounter {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets all arguments targeting the about-to-be started application.
+        /// </summary>
+        internal string[] ArgumentsToApplication {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// Initializes an instance of <see cref="ExecAppCommand"/>.
@@ -33,7 +60,7 @@ namespace Starcounter.Server.PublicModel.Commands {
         /// <param name="assemblyPath">Path to the assembly requesting to start.</param>
         /// <param name="workingDirectory">Working directory the executable has requested to run in.</param>
         /// <param name="arguments">Arguments as passed to the requesting executable.</param>
-        internal ExecAppCommand(ServerEngine engine, string assemblyPath, string workingDirectory, string[] arguments)
+        public ExecAppCommand(ServerEngine engine, string assemblyPath, string workingDirectory, string[] arguments)
             : base(engine, "Starting {0}", Path.GetFileName(assemblyPath)) {
             if (string.IsNullOrEmpty(assemblyPath)) {
                 throw new ArgumentNullException("assemblyPath");
@@ -44,6 +71,29 @@ namespace Starcounter.Server.PublicModel.Commands {
             }
             this.WorkingDirectory = workingDirectory;
             this.Arguments = arguments;
+        }
+
+        /// </inheritdoc>
+        internal override void GetReadyToEnqueue() {
+            string[] scargs;
+            string[] appargs;
+
+            AppProcess.ParseArguments(this.Arguments, out scargs, out appargs);
+            this.ArgumentsToStarcounter = scargs;
+            this.ArgumentsToApplication = appargs;
+            
+            if (string.IsNullOrEmpty(this.DatabaseName)) {
+                string databaseArg;
+                databaseArg = scargs.FirstOrDefault<string>(delegate(string candidate) {
+                    return candidate != null && candidate.StartsWith("Db=");
+                });
+                if (databaseArg != null) {
+                    databaseArg = databaseArg.Substring(databaseArg.IndexOf("=") + 1);
+                    this.DatabaseName = databaseArg;
+                } else {
+                    this.DatabaseName = Path.GetFileNameWithoutExtension(this.AssemblyPath);
+                }
+            }
         }
     }
 }
