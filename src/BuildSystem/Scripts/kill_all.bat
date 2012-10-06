4bf87e787b5c9bdda77715e7824b3426444b43d1
alemoi <alexey.moiseenko@starcounter.com>
2012-10-06 21:29:43 +0200

    Modified start_all.bat and kill_all.bat for faster cycle.

--- src/BuildSystem/Scripts/kill_all.bat
+++ src/BuildSystem/Scripts/kill_all.bat
@@ -4,22 +4,22 @@ TASKKILL /f /t /im Scpmm.exe
 TASKKILL /f /t /im ScGateway.exe
 TASKKILL /f /t /im ScConnMonitor.exe
 
-TASKKILL /f /t /im DaemonObserver.exe
-TASKKILL /f /t /im Postsharp*
-TASKKILL /f /t /im Starcounter*
-TASKKILL /f /t /im Scdbs*
-TASKKILL /f /t /im x86_64-w64-mingw32-gcc.exe
-TASKKILL /f /t /im ActivityMonitorServer.exe
-TASKKILL /f /t /im ServerLogTail.exe
-TASKKILL /f /t /im LoadAndLatencyClient.exe
-TASKKILL /f /t /im PolePositionClient.exe
-TASKKILL /f /t /im SQLTestClient.exe
-TASKKILL /f /t /im SQLTest1Client.exe
-TASKKILL /f /t /im SQLTest2Client.exe
-TASKKILL /f /t /im SQLTest3Client.exe
-TASKKILL /f /t /im ScCode.exe
-TASKKILL /f /t /im ScData.exe
-TASKKILL /f /t /im ScMonitor.exe
-TASKKILL /f /t /im ScServer.exe
+:: TASKKILL /f /t /im DaemonObserver.exe
+:: TASKKILL /f /t /im Postsharp*
+:: TASKKILL /f /t /im Starcounter*
+:: TASKKILL /f /t /im Scdbs*
+:: TASKKILL /f /t /im x86_64-w64-mingw32-gcc.exe
+:: TASKKILL /f /t /im ActivityMonitorServer.exe
+:: TASKKILL /f /t /im ServerLogTail.exe
+:: TASKKILL /f /t /im LoadAndLatencyClient.exe
+:: TASKKILL /f /t /im PolePositionClient.exe
+:: TASKKILL /f /t /im SQLTestClient.exe
+:: TASKKILL /f /t /im SQLTest1Client.exe
+:: TASKKILL /f /t /im SQLTest2Client.exe
+:: TASKKILL /f /t /im SQLTest3Client.exe
+:: TASKKILL /f /t /im ScCode.exe
+:: TASKKILL /f /t /im ScData.exe
+:: TASKKILL /f /t /im ScMonitor.exe
+:: TASKKILL /f /t /im ScServer.exe
 
 IF NOT "%SC_RUNNING_ON_BUILD_SERVER%"=="" EXIT 0
