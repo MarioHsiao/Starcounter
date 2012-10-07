35d93b611e59d75469d400f0ffef1247d70952b1
Per Samuelsson <per@starcounter.com>
2012-10-07 21:41:10 +0200

    Introduced the CreateDatabase command and a stub for the default server implementation of it's processor.

---
+++ src/Server/Starcounter.Server/Commands/Processors/CreateDatabaseCommandProcessor.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter.Server.PublicModel.Commands;
+
+namespace Starcounter.Server.Commands {
+    
+    [CommandProcessor(typeof(CreateDatabaseCommand))]
+    internal sealed class CreateDatabaseCommandProcessor : CommandProcessor {
+        /// <summary>
+        /// Initializes a new <see cref="CreateDatabaseCommandProcessor"/>.
+        /// </summary>
+        /// <param name="server">The server in which the processor executes.</param>
+        /// <param name="command">The <see cref="CreateDatabaseCommand"/> the
+        /// processor should exeucte.</param>
+        public CreateDatabaseCommandProcessor(ServerEngine server, ServerCommand command)
+            : base(server, command) {
+        }
+
+        /// </inheritdoc>
+        protected override void Execute() {
+            throw new NotImplementedException();
+        }
+    }
+}
