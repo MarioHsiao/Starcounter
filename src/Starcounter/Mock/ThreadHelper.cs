9407c64f3d797d7ae1de7244525802e359bb98ec
eriohl <eriohl@gmail.com>
2012-10-08 12:26:22 +0200

    Implementation ThreadHelper.SetYieldBlock and ThreadHelper.ReleaseYieldBlock.

--- src/Starcounter/Mock/ThreadHelper.cs
+++ src/Starcounter/Mock/ThreadHelper.cs
@@ -1,12 +1,25 @@
 ﻿
+using Starcounter.Internal;
+using System;
+
 namespace Starcounter
 {
     
     public static class ThreadHelper
     {
 
-        public static void SetYieldBlock() { }
+        public static void SetYieldBlock()
+        {
+            uint r = sccorelib.cm3_set_yblk((IntPtr)0);
+            if (r == 0) return;
+            throw ErrorCode.ToException(r);
+        }
 
-        public static void ReleaseYieldBlock() { }
+        public static void ReleaseYieldBlock()
+        {
+            uint r = sccorelib.cm3_rel_yblk((IntPtr)0);
+            if (r == 0) return;
+            ExceptionManager.HandleInternalFatalError(r);
+        }
     }
 }
