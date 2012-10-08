9742a48b22a85f2bf6f2bb4e873aa4394d11a142
Per Samuelsson <per@starcounter.com>
2012-10-08 02:02:50 +0200

    Added the stub for the server-side StopDatabaseCommandProcessor, implementing the default processor the the StopDatabaseCommand.

---
+++ src/Server/Starcounter.Server/Commands/Processors/StopDatabaseCommandProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter.Server.PublicModel.Commands;
+
+namespace Starcounter.Server.Commands.Processors {
+    
+    [CommandProcessor(typeof(StopDatabaseCommand))]
+    internal sealed class StopDatabaseCommandProcessor : CommandProcessor {
+        /// <summary>
+        /// Initializes a new <see cref="StopDatabaseCommandProcessor"/>.
+        /// </summary>
+        /// <param name="server">The server in which the processor executes.</param>
+        /// <param name="command">The <see cref="StopDatabaseCommand"/> the
+        /// processor should exeucte.</param>
+        public StopDatabaseCommandProcessor(ServerEngine server, ServerCommand command)
+            : base(server, command) {
+        }
+
+        /// </inheritdoc>
+        protected override void Execute() {
+            throw new NotImplementedException();
+        }
+    }
+}
