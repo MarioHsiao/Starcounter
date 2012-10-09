a317fd75581e7042cf1b849d4a0eb90b1d623721
peteria <peter@starcounter.com>
2012-10-09 17:03:15 +0200

    Support of dynamic schema changes in Query module.

--- src/Starcounter/Query/_QueryModule.cs
+++ src/Starcounter/Query/_QueryModule.cs
@@ -7,55 +7,59 @@ using System.Diagnostics;
 
 namespace Starcounter.Query
 {
-/// <summary>
-/// Configuration, initiation and termination of query module.
-/// </summary>
-public static class QueryModule
-{
-    // Configuration of query module.
-    static readonly String processFolder = StarcounterEnvironment.SystemDirectory + "\\32BitComponents\\";
-    const String processFileName = "StarcounterSQL.exe";
-    const String processVersion = "111208";
-    static Int32 processPort;
-    static readonly String schemaFilePath = AppDomain.CurrentDomain.BaseDirectory + "\\schema.pl";
-    const Int32 maxQueryLength = 3000;
-    const Int32 maxQueryRetries = 10;
-    const Int32 maxVerifyRetries = 100;
-    const Int32 timeBetweenVerifyRetries = 100; // [ms]
-
-    private static int configuredSQLProcessPort;
-
-    public static void Configure(int sqlProcessPort)
-    {
-        configuredSQLProcessPort = sqlProcessPort;
-    }
-
     /// <summary>
-    /// Initiates query module. Called during start-up.
+    /// Configuration, initiation and termination of query module.
     /// </summary>
-    internal static void Initiate(Boolean newSchema)
+    public static class QueryModule
     {
-        // Connect managed and native Sql functions.
-        UInt32 errCode = SqlConnectivity.InitSqlFunctions();
+        // Configuration of query module.
+        static readonly String processFolder = StarcounterEnvironment.SystemDirectory + "\\32BitComponents\\";
+        const String processFileName = "StarcounterSQL.exe";
+        //const String processVersion = "111208";
+        const String processVersion = "121002";
+        static Int32 processPort;
+        static Int32 configuredProcessPort = 0;
+        //static readonly String schemaFilePath = AppDomain.CurrentDomain.BaseDirectory + "\\schema.pl";
+        static readonly String schemaFolder = AppDomain.CurrentDomain.BaseDirectory + "\\";
+        const Int32 maxQueryLength = 3000;
+        const Int32 maxQueryRetries = 10;
+        const Int32 maxVerifyRetries = 100;
+        const Int32 timeBetweenVerifyRetries = 100; // [ms]
 
-        // Checking for error code and translating it.
-        if (errCode != 0)
-            SqlConnectivity.ThrowConvertedServerError(errCode);
+        public static void Configure(Int32 sqlProcessPort)
+        {
+            configuredProcessPort = sqlProcessPort;
+        }
 
-        // Export database schema and index information, and start Prolog-process.
-        processPort = configuredSQLProcessPort;
-        if (processPort == 0)
-            processPort = StarcounterEnvironment.DefaultPorts.SQLProlog;
-        PrologManager.Initiate(newSchema, processFolder, processFileName, processVersion, processPort, schemaFilePath, 
-            maxQueryLength, maxQueryRetries, maxVerifyRetries, timeBetweenVerifyRetries);
+        /// <summary>
+        /// Initiates query module. Called during start-up.
+        /// </summary>
+        internal static void Initiate(Boolean notInUse)
+        {
+            // TEMP
+            System.Diagnostics.Debugger.Break();
+
+            // Connect managed and native Sql functions.
+            UInt32 errCode = SqlConnectivity.InitSqlFunctions();
+
+            // Checking for error code and translating it.
+            if (errCode != 0)
+                SqlConnectivity.ThrowConvertedServerError(errCode);
+
+            // Start external SQL process (Prolog-process).
+            processPort = configuredProcessPort;
+            if (processPort == 0)
+                processPort = StarcounterEnvironment.DefaultPorts.SQLProlog;
+            PrologManager.Initiate(notInUse, processFolder, processFileName, processVersion, processPort, schemaFolder,
+                maxQueryLength, maxQueryRetries, maxVerifyRetries, timeBetweenVerifyRetries);
+        }
+
+        /// <summary>
+        /// Terminates query module. Called during shut-down.
+        /// </summary>
+        internal static void Terminate()
+        {
+            PrologManager.Terminate();
+        }
     }
-
-    /// <summary>
-    /// Terminates query module. Called during shut-down.
-    /// </summary>
-    internal static void Terminate()
-    {
-        PrologManager.Terminate();
-    }
-}
 }
