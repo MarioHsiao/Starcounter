2b95e0eefe24e0a71699644dca0dae59aa6a92fc
eriohl <eriohl@gmail.com>
2012-10-09 21:55:12 +0200

    Added SysTable entity class.

---
+++ src/Starcounter/Metadata/SysTable.cs
@@ -0,0 +1,17 @@
+﻿
+using Starcounter.Internal;
+
+namespace Starcounter.Metadata
+{
+    
+    public sealed class SysTable : Entity
+    {
+
+        public SysTable(Uninitialized u) : base(u) { }
+
+        public string Name
+        {
+            get { return DbState.ReadString(this, 1); }
+        }
+    }
+}
