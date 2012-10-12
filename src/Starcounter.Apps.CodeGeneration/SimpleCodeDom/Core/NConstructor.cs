d34d9e64aa96dfa94cc38111b6d27dda76b1cef5
Christian Holmstrand <christian.holmstrand@starcounter.com>
2012-10-12 16:23:29 +0200

    Added constructor and inputbinding to DOM and updated codegeneration to include inputbindings. Work in progress. Only Handle-methods declared in the same app as the property is working at the moment.

---
+++ src/Starcounter.Apps.CodeGeneration/SimpleCodeDom/Core/NConstructor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Starcounter.Internal.Application.CodeGeneration
+{
+    /// <summary>
+    /// Represents a constructor
+    /// </summary>
+    public class NConstructor : NBase
+    {
+        
+    }
+}
