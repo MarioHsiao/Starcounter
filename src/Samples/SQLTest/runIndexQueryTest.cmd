8873c0103770b5ecd2653ab7b37956a20bb2b283
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-10-15 14:17:38 +0200

    SQLTest: added indexes creation and dropping. Added test of creating data and indexes. Added scripts to run.

---
+++ src/Samples/SQLTest/runIndexQueryTest.cmd
@@ -0,0 +1,4 @@
+pushd C:\GitRepositories\Starcounter\Level1\bin\Debug
+Weaver.exe s\SQLTest\SQLTest.exe --FLAG:tocache
+boot SQLTEST --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --CompilerPath=MinGW\bin\x86_64-w64-mingw32-gcc.exe
+popd
