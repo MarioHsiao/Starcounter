4fc0b8e6479f8e11f9be0be41358374b9e702f30
Christian Holmstrand <christian.holmstrand@starcounter.com>
2012-10-15 16:55:43 +0200

    Refactored inputhandling code to support all different types. Only String and int is implemented currently though.

--- src/Starcounter.Apps/Templates/IntProperty.cs
+++ src/Starcounter.Apps/Templates/IntProperty.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System;
+using Starcounter.Internal;
 using Starcounter.Templates.Interfaces;
 #if CLIENT
 namespace Starcounter.Client.Template {
@@ -8,13 +9,19 @@ namespace Starcounter.Client.Template {
 namespace Starcounter.Templates {
 #endif
 
-    public class IntProperty : Property
+    public class IntProperty : Property<int>
 #if IAPP
         , IIntTemplate
 #endif
     {
 
         private int _DefaultValue = 0;
+
+        public override void ProcessInput(App app, byte[] rawValue)
+        {
+            int v = (int)Utf8Helper.IntFastParseFromAscii(rawValue, 0, (uint)rawValue.Length);
+            ProcessInput(app, v);
+        }
 
         public int DefaultValue {
             get { return _DefaultValue; }
