6b1c756f5af4a17b836584f29f44b0f4bf88131a
Christian Holmstrand <christian.holmstrand@starcounter.com>
2012-10-16 10:50:31 +0200

    Bugfix for boolean values and metadataclasses in generated code.

--- src/Starcounter.Apps.CodeGeneration/SimpleCodeDom/NPrimitiveType.cs
+++ src/Starcounter.Apps.CodeGeneration/SimpleCodeDom/NPrimitiveType.cs
@@ -1,22 +1,30 @@
-﻿
-
-using System;
+﻿using System;
 using Starcounter.Templates;
 
-namespace Starcounter.Internal.Application.CodeGeneration {
-
-    public class NPrimitiveType : NValueClass {
-        public override string Inherits {
+namespace Starcounter.Internal.Application.CodeGeneration
+{
+    public class NPrimitiveType : NValueClass
+    {
+        public override string Inherits
+        {
             get { throw new NotImplementedException(); }
         }
 
-        public override string ClassName {
-            get {
+        public override string ClassName
+        {
+            get
+            {
                 if (NTemplateClass.Template is ActionProperty)
                     return "Action";
+
                 var type = NTemplateClass.Template.InstanceType;
-                if (type == typeof(Int32)) {
+                if (type == typeof(Int32))
+                {
                     return "int";
+                }
+                else if (type == typeof(Boolean))
+                {
+                    return "bool";
                 }
                 return type.Name;
             }
