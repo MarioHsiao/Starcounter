48615301754bc6db4e7c3982fafcd11cad7c41db
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-10-16 11:15:10 +0200

    Starcounter: added few methods used in SQL regression test.

--- src/Starcounter/Binding/ITypeBinding.cs
+++ src/Starcounter/Binding/ITypeBinding.cs
@@ -12,6 +12,11 @@ namespace Starcounter.Binding
         string Name { get; }
 
         /// <summary>
+        /// Returns number of properties bindings.
+        /// </summary>
+        int PropertyCount { get; }
+
+        /// <summary>
         /// Gets the property binding for the property with the specified name.
         /// </summary>
         /// <returns>
@@ -19,5 +24,15 @@ namespace Starcounter.Binding
         /// name exists.
         /// </returns>
         IPropertyBinding GetPropertyBinding(string name);
+
+        /// <summary>
+        /// Gets the property binding for the property at the specified index.
+        /// </summary>
+        /// <param name="index">Index of the property binding</param>
+        /// <returns>
+        /// A property binding. Returns null is no property with the specified
+        /// name exists.
+        /// </returns>
+        IPropertyBinding GetPropertyBinding(int index);
     }
 }
