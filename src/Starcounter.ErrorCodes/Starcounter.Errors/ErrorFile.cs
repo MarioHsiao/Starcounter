cabae54b1a5e0ed635ad34e4b84c187f7b23d801
Kostis <Kostis@starcounter.com>
2012-10-17 18:16:33 +0200

    Commenting.

--- src/Starcounter.ErrorCodes/Starcounter.Errors/ErrorFile.cs
+++ src/Starcounter.ErrorCodes/Starcounter.Errors/ErrorFile.cs
@@ -1,12 +1,35 @@
-﻿
+﻿// ***********************************************************************
+// Assembly         : Starcounter.Errors
+// Author           : Starcounter AB
+// Created          : 10-17-2012
+//
+// Last Modified By : Starcounter AB
+// Last Modified On : 10-17-2012
+// ***********************************************************************
+// <copyright file="ErrorFile.cs" company="Starcounter AB">
+//     . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
 using System.Collections.Generic;
 
 namespace Starcounter.Errors
 {
+    /// <summary>
+    /// Class ErrorFile
+    /// </summary>
     public sealed class ErrorFile
     {
+        /// <summary>
+        /// The error codes
+        /// </summary>
         public readonly IList<ErrorCode> ErrorCodes;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorFile" /> class.
+        /// </summary>
+        /// <param name="codes">The codes.</param>
         internal ErrorFile(IList<ErrorCode> codes)
         {
             this.ErrorCodes = codes;
--- src/Starcounter.ErrorCodes/Starcounter.Errors/Facility.cs
+++ src/Starcounter.ErrorCodes/Starcounter.Errors/Facility.cs
@@ -1,10 +1,32 @@
-﻿
+﻿// ***********************************************************************
+// Assembly         : Starcounter.Errors
+// Author           : Starcounter AB
+// Created          : 10-17-2012
+//
+// Last Modified By : Starcounter AB
+// Last Modified On : 10-17-2012
+// ***********************************************************************
+// <copyright file="Facility.cs" company="Starcounter AB">
+//     . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
 using System;
 
 namespace Starcounter.Errors
 {
+    /// <summary>
+    /// Class Facility
+    /// </summary>
     public sealed class Facility
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Facility" /> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="code">The code.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">code;Not a valid 12-bit value: 0x</exception>
         internal Facility(string name, uint code)
         {
             if (code >> 12 != 0)
@@ -14,12 +36,20 @@ namespace Starcounter.Errors
             this.Code = code;
         }
 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
         public string Name
         {
             get;
             set;
         }
-        
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        /// <value>The code.</value>
         public uint Code
         {
             get;
