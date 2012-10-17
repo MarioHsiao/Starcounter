6b64d4b13fd1a13abc9b5cdc74dca35edb5b440a
Per Samuelsson <per@starcounter.com>
2012-10-17 10:19:43 +0200

    Added the ServerFactory utility class, designed to expose just a minimum API to ABCIPC consumers that want to utilize the extensions we provide on top of the core.

---
+++ src/Starcounter.Bootstrap/ABCIPC/Internal/ServerFactory.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Starcounter.ABCIPC.Internal {
+
+    /// <summary>
+    /// Exposes a set of factory methods for creation of servers.
+    /// </summary>
+    public static class ServerFactory {
+
+        /// <summary>
+        /// Creates a server based on named pipes, exposed to clients
+        /// using the given <paramref name="pipeName"/>.
+        /// </summary>
+        /// <param name="pipeName">The name of the servers pipe.</param>
+        /// <returns>A <see cref="Server"/> that reads requests from, and
+        /// writes replies to, a named pipe.</returns>
+        public static Server CreateUsingNamedPipes(string pipeName) {
+            return new InternalNamedPipeServer(pipeName);
+        }
+    }
+}
