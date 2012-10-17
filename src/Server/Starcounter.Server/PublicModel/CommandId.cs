a1c8a2525f01ce89cd8d405222e4ad2fd5a2456e
Per Samuelsson <per@starcounter.com>
2012-10-17 21:59:04 +0200

    Added a Parse method to CommandId, allowing instances to be recreated based on the string represented by a previous CommandId instance.

--- src/Server/Starcounter.Server/PublicModel/CommandId.cs
+++ src/Server/Starcounter.Server/PublicModel/CommandId.cs
@@ -34,6 +34,18 @@ namespace Starcounter.Server.PublicModel {
             return new CommandId(Guid.NewGuid());
         }
 
+        /// <summary>
+        /// Reconstructs a <see cref="CommandId"/> based on the
+        /// given value.
+        /// </summary>
+        /// <param name="value">The value of a previously created
+        /// command.</param>
+        /// <returns>A new <see cref="CommandId"/> based on the
+        /// given value.</returns>
+        public static CommandId Parse(string value) {
+            return new CommandId(Guid.Parse(value));
+        }
+
         /// <inheritdoc />
         public bool Equals(CommandId other) {
             return other.Value == this.Value;
