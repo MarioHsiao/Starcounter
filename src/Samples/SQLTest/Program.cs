e97ec0e96eb8268d355235a65d9b4355c74c7475
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-10-18 11:44:09 +0200

    SQLTest: framework and program is completed.

--- src/Samples/SQLTest/Program.cs
+++ src/Samples/SQLTest/Program.cs
@@ -9,8 +9,8 @@ namespace SQLTest
         static void Main(string[] args)
         {
             Console.WriteLine("Started SQLTest.");
-            Test1();
             Test2();
+            Test1();
             Test3();
             Console.WriteLine("Finished SQLTest.");
         }
@@ -26,8 +26,10 @@ namespace SQLTest
 
         static Boolean Test2()
         {
+            TestRunner.Initialize("SqlTest2", false, true, false);
             SQLTest.EmployeeDb.EmployeeData.CreateIndexes();
             SQLTest.EmployeeDb.EmployeeData.CreateData();
+            TestRunner.RunTest();
             SQLTest.EmployeeDb.EmployeeData.DeleteData();
             SQLTest.EmployeeDb.EmployeeData.DropIndexes();
             return true;
@@ -35,8 +37,10 @@ namespace SQLTest
         
         static Boolean Test3()
         {
+            TestRunner.Initialize("SqlTest3", false, true, false);
             SQLTest.PointDb.PointData.CreateIndexes();
             SQLTest.PointDb.PointData.CreateData();
+            TestRunner.RunTest();
             SQLTest.PointDb.PointData.DeleteData();
             SQLTest.PointDb.PointData.DropIndexes();
             return true;
