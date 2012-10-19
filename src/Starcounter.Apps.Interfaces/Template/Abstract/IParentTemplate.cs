1347f83d46d5f7063329b17dcf6b18b003434f02
Kostis <Kostis@starcounter.com>
2012-10-19 11:51:30 +0200

    Commenting Starcounter.Apps.Interfaces

--- src/Starcounter.Apps.Interfaces/Template/Abstract/IParentTemplate.cs
+++ src/Starcounter.Apps.Interfaces/Template/Abstract/IParentTemplate.cs
@@ -1,5 +1,13 @@
-﻿
+﻿// ***********************************************************************
+// <copyright file="IParentTemplate.cs" company="Starcounter AB">
+//     Copyright (c) Starcounter AB.  All rights reserved.
+// </copyright>
+// ***********************************************************************
+
 namespace Starcounter.Templates.Interfaces {
+    /// <summary>
+    /// Interface IParentTemplate
+    /// </summary>
     public interface IParentTemplate : IStatefullTemplate {
 
 
