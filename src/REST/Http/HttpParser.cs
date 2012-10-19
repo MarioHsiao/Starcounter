363b73526aa80c8f2df31364b1ca5417f555262f
Kostis <Kostis@starcounter.com>
2012-10-19 18:10:36 +0200

    Commenting Starcounter.REST

--- src/REST/Http/HttpParser.cs
+++ src/REST/Http/HttpParser.cs
@@ -1,3 +1,9 @@
+// ***********************************************************************
+// <copyright file="HttpParser.cs" company="Starcounter AB">
+//     Copyright (c) Starcounter AB.  All rights reserved.
+// </copyright>
+// ***********************************************************************
+
 /*
  * Parser a byte buffer to a structured Http request
  * 
--- src/REST/RequestHandler/AstTree/AstElseIfList.cs
+++ src/REST/RequestHandler/AstTree/AstElseIfList.cs
@@ -1,16 +1,37 @@
-﻿using System.Text;
+﻿// ***********************************************************************
+// <copyright file="AstElseIfList.cs" company="Starcounter AB">
+//     Copyright (c) Starcounter AB.  All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using System.Text;
 
 namespace Starcounter.Internal.Uri {
+    /// <summary>
+    /// Class AstElseIfList
+    /// </summary>
     internal class AstElseIfList : AstVerifier {
 
+        /// <summary>
+        /// Gets or sets the parse node.
+        /// </summary>
+        /// <value>The parse node.</value>
         internal ParseNode ParseNode { get; set; }
 
+        /// <summary>
+        /// Gets a value indicating whether [indent children].
+        /// </summary>
+        /// <value><c>true</c> if [indent children]; otherwise, <c>false</c>.</value>
         internal override bool IndentChildren {
             get {
                 return false;
             }
         }
 
+        /// <summary>
+        /// Gets the debug string.
+        /// </summary>
+        /// <value>The debug string.</value>
         internal override string DebugString {
             get {
                 return "// ifelseif";
