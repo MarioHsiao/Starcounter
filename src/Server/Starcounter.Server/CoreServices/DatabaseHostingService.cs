de177dbe20374177b633ddae5e6c34713a2889ed
Per Samuelsson <per@starcounter.com>
2012-10-22 11:42:20 +0200

    Added class DatabaseHostingService to the server engine library. This service will take on the responsibility to provide a single source of retreival of hosting facades to the server in which it is created.
    The first/current implementation creates a new facade on every call, using named pipes to communicate with host processes.

---
+++ src/Server/Starcounter.Server/CoreServices/DatabaseHostingService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter.ABCIPC;
+using Starcounter.ABCIPC.Internal;
+
+namespace Starcounter.Server.CoreServices {
+    /// <summary>
+    /// Implements the functionality used by the server to interact with
+    /// the the database host.
+    /// </summary>
+    internal sealed class DatabaseHostingService {
+        /// <summary>
+        /// Gets the server that has instantiated this service.
+        /// </summary>
+        readonly ServerEngine engine;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatabaseHostingService"/>
+        /// class.
+        /// </summary>
+        /// <param name="engine">The <see cref="ServerEngine"/> in which the
+        /// service will live.</param>
+        internal DatabaseHostingService(ServerEngine engine) {
+            this.engine = engine;
+        }
+
+        /// <summary>
+        /// Executes setup of the current <see cref="DatabaseHostingService"/>.
+        /// </summary>
+        internal void Setup() {
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Client"/> representing the local hosting
+        /// interface of the given <see cref="Database"/>.
+        /// </summary>
+        /// <param name="database">The <see cref="Database"/> whose hosting
+        /// interface is to be retreived.</param>
+        /// <returns>A <see cref="Client"/> that can be used to send
+        /// management commands to the host.</returns>
+        internal Client GetHostingInterface(Database database) {
+            var pipeName = string.Format("sc//{0}/{1}/{2}", Environment.MachineName, engine.Name, database.Name);
+            pipeName = pipeName.ToLowerInvariant();
+            return ClientServerFactory.CreateClientUsingNamedPipes(pipeName);
+        }
+    }
+}
