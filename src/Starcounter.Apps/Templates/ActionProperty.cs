5115ef29369e8a8dd3acf146c726a2ddd3cca614
Christian Holmstrand <christian.holmstrand@starcounter.com>
2012-10-23 13:56:10 +0200

    Added inputhandling for ActionProperty types in Apps (specified as null in the json-file).

--- src/Starcounter.Apps/Templates/ActionProperty.cs
+++ src/Starcounter.Apps/Templates/ActionProperty.cs
@@ -4,6 +4,8 @@
 // </copyright>
 // ***********************************************************************
 
+using System;
+using System.Collections.Generic;
 using Starcounter.Templates.Interfaces;
 #if CLIENT
 namespace Starcounter.Client.Template {
@@ -14,11 +16,21 @@ namespace Starcounter.Templates {
     /// <summary>
     /// Class ActionProperty
     /// </summary>
-    public class ActionProperty : Template
+    public class ActionProperty : Property
 #if IAPP
         , IActionTemplate
 #endif
     {
+        /// <summary>
+        /// The custom input event creator
+        /// </summary>
+        public Func<App, Property, Input> CustomInputEventCreator = null;
+     
+        /// <summary>
+        /// The custom input handlers
+        /// </summary>
+        public List<Action<App, Input>> CustomInputHandlers = new List<Action<App, Input>>();
+
         /// <summary>
         /// Gets a value indicating whether this instance has instance value on client.
         /// </summary>
@@ -48,6 +60,36 @@ namespace Starcounter.Templates {
         /// <returns>System.Object.</returns>
         public override object CreateInstance(AppNode parent) {
             return false;
+        }
+
+        /// <summary>
+        /// Adds the handler.
+        /// </summary>
+        /// <param name="createInputEvent">The create input event.</param>
+        /// <param name="handler">The handler.</param>
+        public void AddHandler(
+            Func<App, Property, Input> createInputEvent = null,
+            Action<App, Input> handler = null) {
+            this.CustomInputEventCreator = createInputEvent;
+            this.CustomInputHandlers.Add(handler);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="rawValue"></param>
+        public override void ProcessInput(App app, byte[] rawValue) {
+            Input input = null;
+
+            if (CustomInputEventCreator != null)
+                input = CustomInputEventCreator.Invoke(app, this);
+
+            if (input != null) {
+                foreach (var h in CustomInputHandlers) {
+                    h.Invoke(app, input);
+                }
+            } 
         }
     }
 }
