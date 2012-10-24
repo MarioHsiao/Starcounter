a22443b82d89ce4ad86074bbbc0da14c3757cf43
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-10-24 11:01:16 +0200

    SQLTest: updated tests to test dropping indexes.

--- src/Samples/SQLTest/Program.cs
+++ src/Samples/SQLTest/Program.cs
@@ -10,8 +10,7 @@ namespace SQLTest
         {
             Console.WriteLine("Started SQLTest.");
 #if true
-            Test2();
-            Test1();
+            Test1and2();
             Test3();
 #else
             TempTest();
@@ -19,31 +18,24 @@ namespace SQLTest
             Console.WriteLine("Finished SQLTest.");
         }
 
-        static Boolean Test1()
-        {
-            TestRunner.Initialize("SqlTest1", false, true, false);
-            SQLTest.EmployeeDb.EmployeeData.CreateData();
-            TestRunner.RunTest();
-            SQLTest.EmployeeDb.EmployeeData.DeleteData();
-            return true;
-        }
-
-        static Boolean Test2()
+        static Boolean Test1and2()
         {
             TestRunner.Initialize("SqlTest2", false, true, false);
             SQLTest.EmployeeDb.EmployeeData.CreateIndexes();
             SQLTest.EmployeeDb.EmployeeData.CreateData();
             TestRunner.RunTest();
-            SQLTest.EmployeeDb.EmployeeData.DeleteData();
             SQLTest.EmployeeDb.EmployeeData.DropIndexes();
+            TestRunner.Initialize("SqlTest1", false, true, false);
+            TestRunner.RunTest();
+            SQLTest.EmployeeDb.EmployeeData.DeleteData();
             return true;
         }
-        
+
         static Boolean Test3()
         {
             TestRunner.Initialize("SqlTest3", false, true, false);
-            SQLTest.PointDb.PointData.CreateIndexes();
             SQLTest.PointDb.PointData.CreateData();
+            SQLTest.PointDb.PointData.CreateIndexes();
             TestRunner.RunTest();
             SQLTest.PointDb.PointData.DeleteData();
             SQLTest.PointDb.PointData.DropIndexes();
