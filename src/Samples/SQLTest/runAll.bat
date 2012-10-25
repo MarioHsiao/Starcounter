b9d960cae56f580cb4ce04d8cc422d32b27bb87d
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-10-25 14:50:46 +0200

    SQLTest: fixed the script for build server that no paths are excepted due to bug in --AutoStartExePath

--- src/Samples/SQLTest/runAll.bat
+++ src/Samples/SQLTest/runAll.bat
@@ -1,34 +1,34 @@
 :: @ECHO OFF
 
-IF "%1"=="" (
-ECHO Error: Please specify the path to Starcounter bin directory.
-GOTO ERROR
-)
+::IF "%1"=="" (
+::ECHO Error: Please specify the path to Starcounter bin directory.
+::GOTO ERROR
+::)
 
 :: clean up the database and processes
-CMD /C %1\kill_all.bat 2> NUL
+CMD /C kill_all.bat 2> NUL
 IF EXIST .db (
-RMDIR %1\.db /S /Q
-RMDIR %1\.db.output /S /Q
+RMDIR .db /S /Q
+RMDIR .db.output /S /Q
 )
-IF EXIST %1\SQLTest RMDIR %1\SQLTest /S /Q
+IF EXIST SQLTest RMDIR SQLTest /S /Q
 :: create the database
-MKDIR %1\.db
-MKDIR %1\.db.output
-CMD /C %1\scdbc.exe -ip .db -lp .db SqlTest
+MKDIR .db
+MKDIR .db.output
+CMD /C scdbc.exe -ip .db -lp .db SqlTest
 :: start servers
-START %1\ScConnMonitor PERSONAL .db.output
-START %1\scpmm SQLTEST SqlTest .db.output
+START ScConnMonitor PERSONAL .db.output
+START scpmm SQLTEST SqlTest .db.output
 :: start the program
-CMD /C %1\Weaver.exe %1\s\SQLTest\SQLTest.exe --FLAG:tocache
-%1\boot SQLTEST --DatabaseDir=%1\.db --OutputDir=%1\.db.output --TempDir=%1\.db.output --CompilerPath=%1\MinGW\bin\x86_64-w64-mingw32-gcc.exe --AutoStartExePath="s\SQLTest\.starcounter\SQLTest.exe" --FLAG:UseConsole  --FLAG:NoNetworkGateway
+CMD /C Weaver.exe s\SQLTest\SQLTest.exe --FLAG:tocache
+boot SQLTEST --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --CompilerPath=MinGW\bin\x86_64-w64-mingw32-gcc.exe --AutoStartExePath="s\SQLTest\.starcounter\SQLTest.exe" --FLAG:UseConsole  --FLAG:NoNetworkGateway
 :: clean up
-CMD /C %1\kill_all.bat 2> NUL
-RMDIR %1\.db /S /Q
-RMDIR %1\.db.output /S /Q
-RMDIR %1\SQLTest /S /Q
+CMD /C kill_all.bat 2> NUL
+RMDIR .db /S /Q
+RMDIR .db.output /S /Q
+RMDIR SQLTest /S /Q
 rem return exit code
-IF NOT EXIST %1\s\Starcounter\failedTest EXIT 0
+IF NOT EXIST s\Starcounter\failedTest EXIT 0
 
 DEL %1\s\Starcounter\failedTest
 ECHO Error: The regression test is failed!
