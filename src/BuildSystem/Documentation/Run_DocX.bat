413314a95d47f5286460ff97243401bf3d1b94ad
Kostis <Kostis@starcounter.com>
2012-10-26 14:02:58 +0200

    Add error handling in the documentation bat file

--- src/BuildSystem/Documentation/Run_DocX.bat
+++ src/BuildSystem/Documentation/Run_DocX.bat
@@ -10,13 +10,25 @@ set SRVpassword=showme
 set DocumentationParentFolder=Internal
 set DocumentationResultFolder=build
 
+
 :: Run DocumentX build
 "%DocumentX%" ".\%DocumentationProjectPath%\%DocumentationProjectName%" [/buildconfiguration="%DocumentationBuildConf%"]
+if %errorlevel% neq 0 goto FAILED
+
 
 :: SCTESTSRV01
 net use Q: %SRVwebappsPath% /user:%SRVusername% %SRVpassword%
+if %errorlevel% neq 0 goto FAILED
 rd /s /q "Q:\%DocumentationParentFolder%"
+:: "rd" doesn't return errorlevel value
 robocopy .\%DocumentationProjectPath%\%DocumentationResultFolder%\ Q: /e
+if %errorlevel% neq 0 goto FAILED
 net use Q: /delete
+if %errorlevel% neq 0 goto FAILED
 
+
+:: Success
 exit 0
+:: Failed
+:FAILED
+exit 1
