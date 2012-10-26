20706d4a797757f27e9ba8180ee4e027e9d63f77
Anders Wahlgren <anders.wahlgren@societyobjects.com>
2012-10-26 17:02:59 +0200

    Remove the 'select2' (Combobox)
    Added <typeahead/> directive.

---
+++ src/Samples/HelloMcD/js/starcounter-directives.js
@@ -0,0 +1,43 @@
+﻿
+/*
+ * starcounter.directives v0.1.1
+ * (c) 2012 Starcounter. http://www.starcounter.com
+ */
+angular.module('starcounter.directives', [])
+ 
+.directive('typeahead', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            bindModel: '=',
+            resultlist: '&'
+        },
+        template: '<input type="text" ng-model="bindModel"/>',
+        link: function(scope, element, attrs) {
+            $(element).typeahead({
+                source: function(query , process) { 
+
+                // Quick and dirty, TODO: Fix this
+                var list = scope.resultlist();
+                var newArray = new Array();
+                for (var i = 0; i < list.length; i++) {
+                    newArray[i] = list[i].Description;
+                }
+                return newArray;
+                },
+                updater: function(item) {
+                    scope.$apply(read(item));
+                    return item;
+                }
+            });
+
+            function read(value) {
+                scope.bindModel = value;
+            }
+        }
+    };
+})
+
+
+
