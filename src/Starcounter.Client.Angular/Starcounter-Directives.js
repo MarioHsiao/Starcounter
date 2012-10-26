50cc8c89bcd1420d0d4e8f94397c35bc73c195ee
Anders Wahlgren <anders.wahlgren@societyobjects.com>
2012-10-26 13:48:41 +0200

    Added Angular Client files ( AppContext, ServerScope, Standalone and Directives )

---
+++ src/Starcounter.Client.Angular/Starcounter-Directives.js
@@ -0,0 +1,43 @@
+﻿
+/*
+ * StarcounterDirectives v0.1.0
+ * (c) 2012 Starcounter. http://www.starcounter.com
+ */
+angular.module('StarcounterDirectives', [])
+ 
+.directive('typeahead', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            bindModel: '=',
+            resultlist: '&'
+        },
+        template: '<input type="text" ng-model="bindModel"/>',
+        link: function(scope, element, attrs) {
+            $(element).typeahead({
+                source: function(query , process) { 
+
+                // Quick and dirty, TODO: Fix this
+                var list = scope.resultlist();
+                var newArray = new Array();
+                for (var i = 0; i < list.length; i++) {
+                    newArray[i] = list[i].Description;
+                }
+                return newArray;
+                },
+                updater: function(item) {
+                    scope.$apply(read(item));
+                    return item;
+                }
+            });
+
+            function read(value) {
+                scope.bindModel = value;
+            }
+        }
+    };
+})
+
+
+
