679ecd00c588497f0b848c42db106aa8c4f70c1e
Christian Holmstrand <christian.holmstrand@starcounter.com>
2012-10-29 17:18:13 +0100

    Updated visualstudio template for Starcounter Apps.

---
+++ src/Development/Starcounter.VS.CSAppsProjectTemplate/Master.json.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Starcounter;
+
+namespace $safeprojectname$ {
+    partial class Master : App {
+        static void Main(string[] args) {
+            GET("/master", () => {
+                return new Master(){ 
+                    View = "Master.html", 
+                    SomeNo = 146,
+                    Message= "Click the button!"
+                };
+            });
+
+            GET("/empty", () => {
+                return "empty";
+            });
+
+        }
+
+        void Handle(Input.TheButton input) {
+            this.Message = "I clicked the button!";
+        }
+    }
+}
