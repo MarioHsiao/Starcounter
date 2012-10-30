550dc64640c083182080e22b4e034a074910fcec
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-10-30 13:43:38 +0100

    SQLParser: added managed part of SQL Parser.

---
+++ src/Starcounter/Query/RawParserAnalyzer/ParserAnalyzerErrors.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Starcounter;
+
+namespace Sc.Query.RawParserAnalyzer
+{
+    internal partial class RawParserAnalyzer
+    {
+        /// <summary>
+        /// Checks the error code if an error was returned by parser. If so the error 
+        /// information is read from unmanaged parser and a Starcounter exception is
+        /// thrown.
+        /// </summary>
+        /// <param name="scerrorcode">the code returned by the unmanaged parser. 
+        /// 0 means no error.</param>
+        internal unsafe void RawParserError(int scerrorcode)
+        {
+            Debug.Assert(IsOpenParserThread, "Raw parser error management requires an open parser.");
+            if (scerrorcode > 0)
+            {
+                // Unmanaged parser returned an error, thus throwing an exception.
+                unsafe
+                {
+                    ScError* scerror = UnmanagedParserInterface.GetScError();
+                    // Throw Starcounter exception for parsing error
+                    String message = new String(scerror->scerrmessage);
+                    if (scerror->scerrposition >= 0)
+                        message += " Position " + scerror->scerrposition + " in the query \"" + Query + "\"";
+                    else
+                        message += " in the query \"" + Query + "\"";
+                    if (scerror->tocken != null)
+                        message += "The error is near or at: " + new String(scerror->tocken);
+                    throw ErrorCode.ToException((uint)scerror->scerrorcode, message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generates a string reporting position and token in the given query for an error.
+        /// </summary>
+        /// <param name="node">Node of unmanaged tree where error happened.</param>
+        /// <returns></returns>
+        internal unsafe String LocationMessageForError(Node* node)
+        {
+            return LocationMessageForError(node, new String(UnmanagedParserInterface.StrVal(node)));
+        }
+
+        internal unsafe String LocationMessageForError(Node* node, String token)
+        {
+            return "Position " + UnmanagedParserInterface.Location(node) + " in the query \"" + Query + "\"" +
+                "The error is near or at: " + token;
+        }
+
+        // Proper error should be returned from here.
+        internal unsafe void UnknownNode(Node* node)
+        {
+            throw ErrorCode.ToException(Error.SCERRSQLNOTIMPLEMENTED, "");
+        }
+
+        /// <summary>
+        /// Has to be called to assert if temporal assumption holds.
+        /// If not Debug.Assert is called and an exception is thrown to catch in the parent code and do different condition.
+        /// ONLY FOR DEVELOPMENT PURPOSE
+        /// </summary>
+        /// <param name="condition">The condition to check</param>
+        internal void SQLParserAssert(bool condition)
+        {
+            Debug.Assert(condition);
+            if (!condition)
+                throw new SQLParserAssertException();
+        }
+
+        /// <summary>
+        /// Has to be called to assert if temporal assumption holds.
+        /// If not Debug.Assert is called and an exception is thrown to catch in the parent code and do different condition.
+        /// ONLY FOR DEVELOPMENT PURPOSE
+        /// </summary>
+        /// <param name="condition">The condition to check</param>
+        /// <param name="message">Adds message to Debug.Assert</param>
+        internal void SQLParserAssert(bool condition, string message)
+        {
+            Debug.Assert(condition, message);
+            if (!condition)
+                throw new SQLParserAssertException();
+        }
+    }
+
+    /// <summary>
+    /// Exception class used during development to trigger that this parser cannot be used.
+    /// </summary>
+    internal class SQLParserAssertException : Exception
+    {
+    }
+}
---
+++ src/Starcounter/Query/RawParserAnalyzer/RawParserAnalyzer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Starcounter;
+using Starcounter.Query.Execution;
+using Starcounter.Query.Optimization;
+
+namespace Sc.Query.RawParserAnalyzer
+{
+    /// <summary>
+    /// Contains methods to analyze raw parsed tree and generate necessary structures for current optimizer.
+    /// </summary>
+    internal partial class RawParserAnalyzer
+    {
+        /// <summary>
+        /// Keeps knowledge if an open parser exists in this thread. It is important to have maximum one open parser per thread.
+        /// </summary>
+        [ThreadStatic]
+        private static bool IsOpenParserThread = false;
+
+        internal String Query { get; private set; }
+
+        /// <summary>
+        /// Contains joins of relations. It is constructed from all clauses of select statement.
+        /// It consists of relations mentioned in FROM clause and path expressions - in all clauses.
+        /// </summary>
+        internal IOptimizationNode JoinTree { get; private set; }
+
+        /// <summary>
+        /// Contains logical condition of the query. Cannot be null for optimizer. At least TRUE.
+        /// </summary>
+        internal ConditionDictionary WhereCondition { get; private set; }
+
+        internal INumericalExpression FetchNumExpr { get; private set; }
+        internal IBinaryExpression FetchOffsetKeyExpr { get; private set; }
+        internal HintSpecification HintSpec { get; private set; }
+        internal VariableArray VarArray { get; private set; }
+
+        internal IExecutionEnumerator OptimizedPlan { get; private set; }
+
+        /// <summary>
+        /// Calls unmanaged bison-based parser and then managed analyzer for the query.
+        /// Generates all necessary structures for original optimizer and fills the values to 
+        /// corresponding class properties.
+        /// </summary>
+        /// <param name="query">Query to process</param>
+        internal unsafe void ParseAndAnalyzeQuery(string query)
+        {
+            IsOpenParserThread = true; // Important to avoid destroying global variables in unmanaged parser.
+            Query = query;
+            // Reset variables with structures for optimizer
+            JoinTree = null;
+            WhereCondition = null;
+            FetchNumExpr = null;
+            FetchOffsetKeyExpr = null;
+            HintSpec = null;
+            VarArray = null;
+            // Call parser
+            int scerrorcode = 0;
+            unsafe
+            {
+                // The result error code. If 0 then parsing was successful.
+                // Calls unmanaged parser, which returns the parsed tree
+                Sc.Query.RawParserAnalyzer.List* parsedTree = UnmanagedParserInterface.ParseQuery(query, &scerrorcode);
+                try
+                {
+                    // Throw exception if error
+                    RawParserError(scerrorcode);
+                    // Call analyzer, which can throw exception for errors
+                    AnalyzeParseTree(parsedTree);
+                }
+                finally
+                {
+                    UnmanagedParserInterface.CleanMemoryContext(); // Otherwise memory leaks
+                    IsOpenParserThread = false; // Important to allow calling parser again
+                }
+            }
+        }
+
+        internal unsafe void ParseQuery(string query)
+        {
+            IsOpenParserThread = true; // Important to avoid destroying global variables in unmanaged parser.
+            int scerrorcode = 0;
+            unsafe
+            {
+                // The result error code. If 0 then parsing was successful.
+                // Calls unmanaged parser, which returns the parsed tree
+                Sc.Query.RawParserAnalyzer.List* parsedTree = UnmanagedParserInterface.ParseQuery(query, &scerrorcode);
+                UnmanagedParserInterface.CleanMemoryContext(); // Otherwise memory leaks
+            }
+            IsOpenParserThread = false; // Important to allow calling parser again
+        }
+
+        /// <summary>
+        /// Entry point of analyzer.
+        /// </summary>
+        /// <param name="parsedTree">Parsed tree produced by the unmanaged bison-based parser.</param>
+        internal unsafe void AnalyzeParseTree(Sc.Query.RawParserAnalyzer.List* parsedTree)
+        {
+            Debug.Assert(parsedTree != null, "Parsed tree should not be null");
+            Debug.Assert(parsedTree->type == NodeTag.T_List, "Parsed tree should be of T_List, but was " + parsedTree->type.ToString());
+            if (parsedTree->length > 1) // Parser can parse several statements in a string and thus produce list of trees. See test example.
+                throw ErrorCode.ToException(Error.SCERRSQLINCORRECTSYNTAX, "The query should contain only one statement." +
+                    LocationMessageForError((Node*)parsedTree->tail->data.ptr_value));
+            Node* stmt = (Node*)parsedTree->head->data.ptr_value;
+            switch (stmt->type)
+            {
+                case NodeTag.T_SelectStmt: AnalyzeSelectStmt((SelectStmt*)stmt);
+                    break;
+                default: UnknownNode(stmt);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Calls original optimizer on results of analyzer.
+        /// </summary>
+        internal void Optimize()
+        {
+            Debug.Assert(JoinTree != null && WhereCondition != null && HintSpec != null, "Query should parsed and analyzed before optimization");
+            OptimizedPlan = Optimizer.Optimize(JoinTree, WhereCondition, FetchNumExpr, FetchOffsetKeyExpr, HintSpec);
+        }
+    }
+}
