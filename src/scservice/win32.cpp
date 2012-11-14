cca438999591190fa7f3cda0d27078c55e15e8ba
eriohl <eriohl@gmail.com>
2012-11-14 10:40:25 +0100

    Recursive cleanup of child processes when killing a managed process.

--- src/scservice/win32.cpp
+++ src/scservice/win32.cpp
@@ -7,6 +7,7 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <TlHelp32.h>
 
 
 _STATIC_ASSERT(sizeof(HANDLE) == sizeof(void *));
@@ -76,17 +77,21 @@ uint32_t _wait(void **handles, uint32_t count, uint32_t *psignaled_index)
 	return (uint32_t)dr;
 }
 
+
+static void __kill_and_cleanup_children(DWORD process_id);
+
+
 void _kill_and_cleanup(void *handle)
 {
 	HANDLE hprocess;
-	BOOL terminated;
-	DWORD dr;
 	BOOL br;
+	DWORD dr;
+	DWORD process_id;
 
 	hprocess = (HANDLE)handle;
 
-	terminated = TerminateProcess(hprocess, 1); // TODO: Exit code
-	if (!terminated)
+	br = TerminateProcess(hprocess, 1); // TODO: Exit code
+	if (!br)
 	{
 		dr = GetLastError();
 		if (dr == ERROR_ACCESS_DENIED)
@@ -101,6 +106,57 @@ void _kill_and_cleanup(void *handle)
 		dr = GetLastError();
 	}
 
+	// We kill the child processes after we killed the parent process to make
+	// sure that it stopped spawning new processes. 
+	//
+	// As long as the handle is opened we can still get the process id
+	// regardless of the process is dead or not. The child processes will also
+	// still reference the process id as the parent process.
+
+	process_id = GetProcessId(handle);
+	__kill_and_cleanup_children(process_id);
+
 	br = GetExitCodeProcess(hprocess, &dr);
 	br = CloseHandle(hprocess);
+}
+
+
+void __kill_and_cleanup_children(DWORD process_id)
+{
+	HANDLE hsnapshot;
+	PROCESSENTRY32 pe;
+	HANDLE hchild_proc;
+	DWORD dr;
+
+	hsnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	memset(&pe, 0, sizeof(PROCESSENTRY32));
+	pe.dwSize = sizeof(PROCESSENTRY32);
+
+	if (Process32First(hsnapshot, &pe))
+	{
+		do
+		{
+			if (pe.th32ParentProcessID == process_id)
+			{
+				hchild_proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe.th32ProcessID);
+				if (hchild_proc)
+				{
+					_kill_and_cleanup(hchild_proc);
+				}               
+				else
+				{
+					dr = GetLastError();
+					if (dr == ERROR_INVALID_PARAMETER)
+					{
+						// The referenced process was terminated after we
+						// created to snapshot.
+
+						__kill_and_cleanup_children(pe.th32ProcessID);
+					}
+				}
+			}
+		}
+		while (Process32Next(hsnapshot, &pe));
+	}
 }
