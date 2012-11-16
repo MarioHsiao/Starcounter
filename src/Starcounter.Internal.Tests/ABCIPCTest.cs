41045792e526140a539f0964a6e2771df9f4cd1d
Per Samuelsson <per@starcounter.com>
2012-11-16 13:36:32 +0100

    Added the Starcounter.Internal.Tests project and configured it properly, setting up references etc.

---
+++ src/Starcounter.Internal.Tests/ABCIPCTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Starcounter.Internal.Tests {
+    
+    /// <summary>
+    /// Expose a set of test methods allowing unit testing of the
+    /// ABCIPC in Starcounter.Internal.
+    /// </summary>
+    [TestFixture]
+    public class ABCIPCTest {
+        
+        /// <summary>
+        /// Placeholder: Runs test 1.
+        /// </summary>
+        [Test]
+        public void Test1() {
+        }
+    }
+}
