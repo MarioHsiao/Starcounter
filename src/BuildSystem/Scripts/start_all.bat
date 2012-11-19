d2926d02eb5d31bc31c912c62f381a130760fbda
alemoi <alexey.moiseenko@starcounter.com>
2012-11-19 01:39:23 -0800

    Scripts refactoring.

--- src/BuildSystem/Scripts/start_all.bat
+++ src/BuildSystem/Scripts/start_all.bat
@@ -23,16 +23,16 @@ MKDIR .db.output
 sccreatedb.exe -ip .db -lp .db %1
 
 :: Starting database memory management process.
-START "scdata" scdata.exe %1 %1 .db.output
+START scdata.exe %1 %1 .db.output
 
 :: Starting connection monitor on PERSONAL server.
-START "scipcmonitor" scipcmonitor.exe PERSONAL .db.output
+START scipcmonitor.exe PERSONAL .db.output
 
 :: Starting the specific database.
-::START "sccode" sccode.exe %1 --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --CompilerPath=MinGW\bin\x86_64-w64-mingw32-gcc.exe --AutoStartExePath=C:\sc\Level1\src\Samples\MySampleApp\bin\debug\mysampleapp.exe
+::START sccode.exe %1 --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --CompilerPath=MinGW\bin\x86_64-w64-mingw32-gcc.exe --AutoStartExePath=C:\sc\Level1\src\Samples\MySampleApp\bin\debug\mysampleapp.exe
 
 :: Auto-start example.
 ::sccode.exe NETWORKIOTEST --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --CompilerPath=MinGW\bin\x86_64-w64-mingw32-gcc.exe --AutoStartExePath=c:\github\Orange\bin\Debug\NetworkIoTest\NetworkIoTest.exe
 
 :: Starting Network Gateway.
-START "scnetworkgateway" scnetworkgateway.exe PERSONAL scnetworkgateway.xml .db.output
+START scnetworkgateway.exe PERSONAL scnetworkgateway.xml .db.output
