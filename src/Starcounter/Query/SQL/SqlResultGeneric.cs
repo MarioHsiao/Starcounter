66fcb9fe39c87f131f7876b0f04d5522dbec44a4
eriohl <eriohl@gmail.com>
2012-11-21 21:55:51 +0100

    Interface improvements SqlResult and SqlEnumerator.

--- src/Starcounter/Query/SQL/SqlResultGeneric.cs
+++ src/Starcounter/Query/SQL/SqlResultGeneric.cs
@@ -128,17 +128,54 @@ namespace Starcounter
     /// <summary>
     /// 
     /// </summary>
-    public class SqlResult<T> : SqlResult, IEnumerable<T>
-    {
+    public class SqlResult<T> : SqlResult, IEnumerable<T> {
         // Creating SQL result with query parameters all given at once.
         internal SqlResult(UInt64 transactionId, String query, Boolean slowSQL, params Object[] sqlParamsValues)
             : base(transactionId, query, slowSQL, sqlParamsValues) {
         }
 
+        // We hide the base First property to return an instance of T instead
+        // of a dynamic in case the property is accessed from generic SqlResult
+        // instance in order to not polute the calling code with dynamic code
+        // when you explicitly specify the type.
+
         /// <summary>
-        /// 
+        /// Obtaining only the first hit/result and disposing the enumerator.
         /// </summary>
-        /// <returns></returns>
+        /// <value></value>
+        new public T First {
+            get {
+                // Note that we needed to copy the code because if simply
+                // calling base.First the CLR refused to inline this method.
+
+                IExecutionEnumerator execEnum = null;
+                dynamic current = null;
+
+                try {
+                    execEnum = GetExecutionEnumerator();
+
+                    current = default(T);
+
+                    // Setting the fetch first only flag.
+                    execEnum.SetFirstOnlyFlag();
+
+                    if (execEnum.MoveNext())
+                        current = execEnum.Current;
+                }
+                finally {
+                    if (execEnum != null) execEnum.Dispose();
+                }
+
+                return current;
+            }
+        }
+
+        // Getting enumerator for manual use.
+        /// <summary>
+        /// Gets the enumerator.
+        /// </summary>
+        /// <returns>SqlEnumerator.</returns>
+        /// <exception cref="Starcounter.SqlException">Literal in query is not supported. Use variable and parameter instead.</exception>
         new public SqlEnumerator<T> GetEnumerator() {
             return new SqlEnumerator<T>(GetExecutionEnumerator());
         }
