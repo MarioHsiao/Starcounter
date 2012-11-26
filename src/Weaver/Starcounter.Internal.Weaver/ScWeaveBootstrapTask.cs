02e8500233e27ad429fe0ab1995a343c2a8b6fc5
Per Samuelsson <per@starcounter.com>
2012-11-26 13:49:54 +0100

    Added the ScWeaveBootstrapTask PostSharp task to support the future weaving of executables to allow them to be executed from the OS shell.

---
+++ src/Weaver/Starcounter.Internal.Weaver/ScWeaveBootstrapTask.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PostSharp.Sdk.Extensibility;
+
+namespace Starcounter.Internal.Weaver {
+    
+    /// <summary>
+    /// Implements the weaver that injects the proper calls to allow
+    /// shell bootstraping of Starcounter executables.
+    /// </summary>
+    public class ScWeaveBootstrapTask : Task {
+
+        /// <summary>
+        /// Executes the weaver, effectively injecting the code into
+        /// the entrypoint type to support bootstraping.
+        /// </summary>
+        /// <returns></returns>
+        public override bool Execute() {
+            throw new NotImplementedException("The bootstrap weaver is not yet implemented");
+        }
+    }
+}
