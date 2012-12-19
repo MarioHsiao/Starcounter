8e469dc1845be13cfb73b942849af564d57cdc52
eriohl <eriohl@gmail.com>
2012-12-19 23:27:11 +0100

    Changed assembly company and assembly copyright to Starcounter AB where set to Microsoft.

--- src/Starcounter.Rest/Properties/AssemblyInfo.cs
+++ src/Starcounter.Rest/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Starcounter.Internal.Interfaces")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Starcounter AB")]
 [assembly: AssemblyProduct("Starcounter.Internal.Interfaces")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Starcounter AB 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
