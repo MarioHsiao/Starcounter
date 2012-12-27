73d9bd19f31693486eb4a09efb3b51669162bde9
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2012-12-27 11:21:40 +0100

    SQLParser: removed downcase_identifier.

--- src/Starcounter.SqlParser/Parser/scansup.h
+++ src/Starcounter.SqlParser/Parser/scansup.h
@@ -15,11 +15,6 @@
 #ifndef SCANSUP_H
 #define SCANSUP_H
 
-extern char *downcase_identifier(const char *ident, int len,
-							 bool warn);
-
-//extern void truncate_identifier(char *ident, int len, bool warn);
-
 extern bool scanner_isspace(char ch);
 
 #endif   /* SCANSUP_H */
