d6e09a9795c30bad095dea229fbb8ec89902c969
alemoi <alexey.moiseenko@starcounter.com>
2013-01-07 16:37:59 +0100

    Fixed wide characters usage in scservice (including path values from UTF8 XML config files).
    Reading server Logs directory from server config and passing it to gateway/monitor.

--- src/scservice/config.cpp
+++ src/scservice/config.cpp
@@ -1,82 +1,165 @@
 
 #include "internal.h"
 
+#include <windows.h>
 #include <malloc.h>
 #include <stdio.h>
 #include <stdint.h>
 #include "rapidxml.hpp"
 
-
-uint32_t _read_config(const char *name, char **pserver_dir)
+uint32_t _read_service_config(const wchar_t *name, wchar_t **pserver_dir)
 {
-	using namespace rapidxml;
+    using namespace rapidxml;
 
-	uint32_t r;
-	char *file_name;
-	FILE *file;
-	char *config_data;
-	xml_document<> *doc; // Character type defaults to char.
+    uint32_t r;
+    wchar_t *file_name;
+    FILE *file;
+    char *config_data;
+    xml_document<> *doc; // Character type defaults to char.
+    size_t str_num_chars;
+    size_t str_size_bytes;
 
-	r = SCERRBADSERVICECONFIG;
+    r = SCERRBADSERVICECONFIG;
 
-	file_name = 0;
-	file = 0;
-	config_data = 0;
-	doc = 0;
+    file_name = 0;
+    file = 0;
+    config_data = 0;
+    doc = 0;
 
-	file_name = (char *)malloc(strlen(name) + 4 + 1);
-	if (!file_name) goto end;
-#pragma warning (disable: 4996)
-	sprintf(file_name, "%s.xml", name);
-#pragma warning (default: 4996)
+    str_num_chars = wcslen(name) + 4 + 1;
+    str_size_bytes = str_num_chars * sizeof(wchar_t);
 
-	errno_t errno;
-	int32_t file_size;
-	size_t read;
+    file_name = (wchar_t *)malloc(str_size_bytes);
+    if (!file_name) goto end;
 
-	errno = fopen_s(&file, file_name, "r");
-	if (errno) goto end;
-	fseek(file, 0, SEEK_END);
-	file_size = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	config_data = (char *)malloc(file_size + 1);
-	if (!config_data) goto end;
-	read = fread(config_data, 1, file_size, file);
-	config_data[read] = 0;
-	fclose(file);
-	file = 0;
+    swprintf(file_name, str_num_chars, L"%s.xml", name);
 
-	// TODO: Handle rapidxml error (exception thrown).
+    errno_t errno;
+    int32_t file_size;
+    size_t read;
 
-	doc = new xml_document<>; // Character type defaults to char.
-	if (!doc) goto end;
+    errno = _wfopen_s(&file, file_name, L"r");
+    if (errno) goto end;
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
 
-	try
-	{
-		doc->parse<0>(config_data); // 0 means default parse flags.
-	}
-	catch (parse_error)
-	{
-		goto end;
-	}
+    config_data = (char *)malloc(file_size + 1);
+    if (!config_data) goto end;
 
-	xml_node<> *root_elem = doc->first_node("service");
-	if (!root_elem) goto end;
+    read = fread(config_data, 1, file_size, file);
+    config_data[read] = 0;
+    fclose(file);
+    file = 0;
 
-	xml_node<> *server_dir_elem = root_elem->first_node("server-dir");
-	if (!server_dir_elem) goto end;
+    // TODO: Handle rapidxml error (exception thrown).
 
-	*pserver_dir = (char *)malloc(server_dir_elem->value_size() + 1);
-#pragma warning (disable: 4996)
-	strcpy(*pserver_dir, server_dir_elem->value());
-#pragma warning (default: 4996)
+    doc = new xml_document<>; // Character type defaults to char.
+    if (!doc) goto end;
 
-	r = 0;
+    try
+    {
+        doc->parse<0>(config_data); // 0 means default parse flags.
+    }
+    catch (parse_error)
+    {
+        goto end;
+    }
+
+    xml_node<> *root_elem = doc->first_node("service");
+    if (!root_elem) goto end;
+
+    xml_node<> *server_dir_elem = root_elem->first_node("server-dir");
+    if (!server_dir_elem) goto end;
+
+    str_num_chars = server_dir_elem->value_size() + 1;
+    str_size_bytes = str_num_chars * sizeof(wchar_t);
+    *pserver_dir = (wchar_t *)malloc(str_size_bytes);
+    if (!*pserver_dir) goto end;
+
+    // Converting server directory from UTF-8 to wchar_t.
+    int32_t num_chars_converted = MultiByteToWideChar(CP_UTF8, 0, server_dir_elem->value(), -1, *pserver_dir, (int)str_num_chars);
+    if (0 == num_chars_converted) goto end;
+
+    r = 0;
 
 end:
-	if (doc) delete doc;
-	if (config_data) free(config_data);
-	if (file) fclose(file);
-	if (file_name) free(file_name);
-	return r;
+    if (doc) delete doc;
+    if (config_data) free(config_data);
+    if (file) fclose(file);
+    if (file_name) free(file_name);
+    return r;
+}
+
+uint32_t _read_server_config(const wchar_t *server_config_path, wchar_t **pserver_logs_dir)
+{
+    using namespace rapidxml;
+
+    uint32_t r;
+    FILE *file;
+    char *config_data;
+    xml_document<> *doc; // Character type defaults to char.
+    size_t str_num_chars;
+    size_t str_size_bytes;
+
+    r = SCERRBADSERVERCONFIG;
+
+    file = 0;
+    config_data = 0;
+    doc = 0;
+
+    errno_t errno;
+    int32_t file_size;
+    size_t read;
+
+    errno = _wfopen_s(&file, server_config_path, L"r");
+    if (errno) goto end;
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    config_data = (char *)malloc(file_size + 1);
+    if (!config_data) goto end;
+
+    read = fread(config_data, 1, file_size, file);
+    config_data[read] = 0;
+    fclose(file);
+    file = 0;
+
+    // TODO: Handle rapidxml error (exception thrown).
+
+    doc = new xml_document<>; // Character type defaults to char.
+    if (!doc) goto end;
+
+    try
+    {
+        doc->parse<0>(config_data); // 0 means default parse flags.
+    }
+    catch (parse_error)
+    {
+        goto end;
+    }
+
+    xml_node<> *root_elem = doc->first_node("Server");
+    if (!root_elem) goto end;
+
+    xml_node<> *server_log_dir_elem = root_elem->first_node("LogDirectory");
+    if (!server_log_dir_elem) goto end;
+
+    str_num_chars = server_log_dir_elem->value_size() + 1;
+    str_size_bytes = str_num_chars * sizeof(wchar_t);
+    *pserver_logs_dir = (wchar_t *)malloc(str_size_bytes);
+    if (!*pserver_logs_dir) goto end;
+
+    // Converting server directory from UTF-8 to wchar_t.
+    int32_t num_chars_converted = MultiByteToWideChar(CP_UTF8, 0, server_log_dir_elem->value(), -1, *pserver_logs_dir, (int)str_num_chars);
+    if (0 == num_chars_converted) goto end;
+
+    r = 0;
+
+end:
+    if (doc) delete doc;
+    if (config_data) free(config_data);
+    if (file) fclose(file);
+    return r;
 }
