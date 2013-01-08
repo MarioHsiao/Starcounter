1da05579b4e0fe8d4e9c1ade6664fc57febbc04e
Per Samuelsson <per@starcounter.com>
2013-01-08 09:20:04 +0100

    Introduced the SynonymousToAttribute class in the Starcounter assembly.

---
+++ src/Starcounter/SynonymousToAttribute.cs
@@ -0,0 +1,29 @@
+
+using System;
+
+namespace Starcounter {
+
+    /// <summary>
+    /// Attribute to allow entity fields to indicate they are mearly synonyms, or "casts",
+    /// of an allready defined field, declared in the same class or one of the base-classes
+    /// to the class declaring the synonym.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class SynonymousToAttribute : Attribute {
+
+        /// <summary>
+        /// Initializes a <see cref="SynonymousToAttribute"/> instance.
+        /// </summary>
+        public SynonymousToAttribute(string target) {
+            if (string.IsNullOrEmpty(target)) {
+                throw new ArgumentNullException("target");
+            }
+            this.Target = target;
+        }
+
+        /// <summary>
+        /// The name of the field this one is synonymous to.
+        /// </summary>
+        public string Target { get; set; }
+    }
+}
