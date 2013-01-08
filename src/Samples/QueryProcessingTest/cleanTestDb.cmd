901ce7d142d83fde2dde7985eedb8e0e452e8f21
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-01-08 16:02:13 +0100

    Testing: implementing tests for type bindings with short names.

---
+++ src/Samples/QueryProcessingTest/cleanTestDb.cmd
@@ -0,0 +1,5 @@
+pushd C:\GitRepositories\Starcounter\Level1\bin\Debug
+call kill_all.bat
+rm .db/BindingTest*
+rm -r .db.output/*BINDINGTEST*
+popd
--- src/Starcounter/Binding/Bindings.cs
+++ src/Starcounter/Binding/Bindings.cs
@@ -6,6 +6,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("QueryProcessingTest")]
 
 namespace Starcounter.Binding
 {
