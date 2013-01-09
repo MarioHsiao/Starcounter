60fe63765bbfda0fd9872d3196227add3f4ba083
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-01-09 15:00:10 +0100

    Testing: added tests of getting type binding for classes with and without namespaces in lower or original cases using full or short names.

--- src/Samples/QueryProcessingTest/BindingTestDirect.cs
+++ src/Samples/QueryProcessingTest/BindingTestDirect.cs
@@ -4,6 +4,24 @@ namespace QueryProcessingTest {
     public static class BindingTestDirect {
         public static void DirectBindingTest() {
             Starcounter.Binding.Bindings.GetTypeBinding("QueryProcessingTest.CamelNameSpace.CommonClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("QueryProcessingTest.CamelNameSpace.LongNameSpace.CommonClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("CommonClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("commonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("CommonClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("lowercasecommonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("lowercasenamespace.lowercasecommonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("lowercasecommonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("nonamespaceclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("QueryProcessingTest.CamelNameSpace.lowercasecommonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("lowercasenamespace.commonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("CamelClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("QueryProcessingTest.CamelNameSpace.LongNameSpace.lowercasecommonclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("QueryProcessingTest.CamelNameSpace.CamelClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("CamelClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("lowercasenamespace.lowercaseclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("LongNameCamelClass");
+            Starcounter.Binding.Bindings.GetTypeBinding("lowercaseclass");
+            Starcounter.Binding.Bindings.GetTypeBinding("QueryProcessingTest.CamelNameSpace.LongNameSpace.LongNameCamelClass");
         }
     }
 }
