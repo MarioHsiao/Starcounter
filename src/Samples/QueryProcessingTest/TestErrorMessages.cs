798ef9c94b3c5881627e5097b0d3b953d3750c4f
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-01-11 15:53:17 +0100

    SQLParser: added files to test error messages.

---
+++ src/Samples/QueryProcessingTest/TestErrorMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Starcounter;
+using Starcounter.Query.RawParserAnalyzer;
+
+namespace QueryProcessingTest {
+    public static class TestErrorMessages {
+        public static void RunTestErrorMessages() {
+            ParserAnalyzer analyzer = new ParserAnalyzer();
+            analyzer.ParseAndAnalyzeQuery("DELETE FROM Account");
+        }
+    }
+}
