adac9614e7466ae373b454c6a008e6bc022d3af9
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-01-18 13:22:37 +0100

    InheritedIndexes: adding test model.

---
+++ src/Samples/QueryProcessingTest/InheritedIndexDataModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Starcounter;
+
+namespace QueryProcessingTest {
+    public class Person : Entity {
+        String Name;
+        DateTime Birthday;
+        Int64 Gender; // 0 - man, 1 -female (standard)
+    }
+
+    public class Student : Person {
+        University Place;
+        String Program;
+        Int64 StartYear;
+    }
+
+    public class Employee : Person {
+        Employer Company;
+        Manager Boss;
+        DateTime StartDate;
+        Decimal Salary;
+    }
+
+    public class Manager : Employee {
+
+    }
+
+    public class Teacher : Employee {
+    }
+
+    public class Professor : Teacher {
+        String Subject;
+    }
+
+    public class Employer : Entity {
+        Employer Director;
+        String Address;
+    }
+
+    public class University : Employer {
+
+    }
+}
