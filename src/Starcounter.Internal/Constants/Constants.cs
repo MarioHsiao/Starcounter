48902530b8d5764c508cf708eb0b46b649b144ae
alemoi <alexey.moiseenko@starcounter.com>
2013-01-22 13:13:24 +0100

    Moved Bootstrap command line options to Starcounter.Internal because of Starcounter.VisualStudio 32-bit code.

--- src/Starcounter.Internal/Constants/Constants.cs
+++ src/Starcounter.Internal/Constants/Constants.cs
@@ -49,5 +49,99 @@ namespace Starcounter.Internal
             /// </summary>
             public const string ScSqlParser = "scsqlparser";
         }
+
+        /// <summary>
+        /// Defines the commands this program accepts.
+        /// </summary>
+        public static class BootstrapCommandNames
+        {
+            /// <summary>
+            /// Defines the name of the Start command.
+            /// </summary>
+            /// <remarks>
+            /// The Start command is the default command and can hence
+            /// be omitted on the command line.
+            /// </remarks>
+            public const string Start = "Start";
+        }
+
+        /// <summary>
+        /// Defines the options the "Start" command accepts.
+        /// </summary>
+        public static class BootstrapOptionNames
+        {
+            /// <summary>
+            /// Specifies the database directory to use.
+            /// </summary>
+            public const string DatabaseDir = "DatabaseDir";
+
+            /// <summary>
+            /// Specifies the output directory to use.
+            /// </summary>
+            public const string OutputDir = "OutputDir";
+
+            /// <summary>
+            /// Specifies the temporary directory to use.
+            /// </summary>
+            public const string TempDir = "TempDir";
+
+            /// <summary>
+            /// Specifies the path to the compiler to use when generating code.
+            /// </summary>
+            public const string CompilerPath = "CompilerPath";
+
+            /// <summary>
+            /// Specifies the name of Starcounter server which started the database.
+            /// </summary>
+            public const string ServerName = "ServerName";
+
+            /// <summary>
+            /// Specifies the total number of chunks used for shared memory communication.
+            /// </summary>
+            public const string ChunksNumber = "ChunksNumber";
+
+            /// <summary>
+            /// Specifies TCP/IP port to be used by SQL parser.
+            /// </summary>
+            public const string SQLProcessPort = "SQLProcessPort";
+
+            /// <summary>
+            /// Specifies the number of schedulers.
+            /// </summary>
+            public const string SchedulerCount = "SchedulerCount";
+
+            /// <summary>
+            /// Gets the string to use to apply the switch that tells the host process
+            /// not to connect to the database nor utilize the SQL engine.
+            /// </summary>
+            public const string NoDb = "NoDb";
+
+            /// <summary>
+            /// Indicates if this host Apps is not utilizing the network gateway.
+            /// </summary>
+            public const string NoNetworkGateway = "NoNetworkGateway";
+
+            /// <summary>
+            /// Gets the string we support as a flag on the command-line to allow
+            /// the host process to accept management input on standard streams/console
+            /// rather than named pipes (with named pipes being the default).
+            /// </summary>
+            public const string UseConsole = "UseConsole";
+
+            /// <summary>
+            /// Specifies the path to executable that should be run on startup.
+            /// </summary>
+            public const string AutoStartExePath = "AutoStartExePath";
+
+            /// <summary>
+            /// User command line arguments.
+            /// </summary>
+            public const string UserArguments = "UserArguments";
+
+            /// <summary>
+            /// Explicit working directory.
+            /// </summary>
+            public const string WorkingDir = "WorkingDir";
+        }
     }
 }
