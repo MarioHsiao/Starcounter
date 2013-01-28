0a4aff22c2583baddabd6ce6a6b490513f2a4618
andthy <andreas.thyrhaug@starcounter.com>
2013-01-28 20:03:55 +0100

    IPC: Constructing the event names not_full and not_empty, etc.

--- src/Chunks/common/macro_definitions.hpp
+++ src/Chunks/common/macro_definitions.hpp
@@ -13,8 +13,17 @@
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
 ///********************************************************************************************
+/// Defining IPC_REPLACE_IPC_SYNC_IN_THE_SHARED_CHUNK_POOL
+/// to synchronize access to the shared_chunk_pool using a spinlock and Windows Events.
+#define IPC_REPLACE_IPC_SYNC_IN_THE_SHARED_CHUNK_POOL
+
+#if defined (IPC_REPLACE_IPC_SYNC_IN_THE_SHARED_CHUNK_POOL)
+#else // !defined (IPC_REPLACE_IPC_SYNC_IN_THE_SHARED_CHUNK_POOL)
+#endif // defined (IPC_REPLACE_IPC_SYNC_IN_THE_SHARED_CHUNK_POOL)
+
 /// Defining IPC_HANDLE_CHANNEL_OUT_BUFFER_FULL
 /// to activate the use of one overflow queue per channel, based on linked lists.
+/// Warning: There is a bug causing access violation if this macro is defined.
 //#define IPC_HANDLE_CHANNEL_OUT_BUFFER_FULL
 
 /// Define CONNECTIVITY_MONITOR_SHOW_ACTIVITY in order for the connectivity monitor
--- src/Chunks/server/initialize.cpp
+++ src/Chunks/server/initialize.cpp
@@ -166,7 +166,7 @@ is_system, uint32_t chunks_total_number) try {
 	sizeof(shared_chunk_pool_type));
 
 	shared_chunk_pool_type* shared_chunk_pool = new(p) shared_chunk_pool_type
-	(chunks_total_number, shared_chunk_pool_alloc_inst);
+	(segment_name, chunks_total_number, shared_chunk_pool_alloc_inst);
 	
 	// Initialize the shared_chunk_pool by pushing in chunk_indexes.
 	// These chunk_indexes represents free chunks.
