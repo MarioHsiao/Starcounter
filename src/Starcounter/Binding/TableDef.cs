105dbc4fd37d1cb2424dcebe6fa66a60ae3621c2
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-01-29 09:59:21 +0100

    QueryProcessing: added tableId field to IndexInfo.

--- src/Starcounter/Binding/TableDef.cs
+++ src/Starcounter/Binding/TableDef.cs
@@ -329,7 +329,7 @@ namespace Starcounter.Binding
                         break;
                 }
             }
-            return new IndexInfo(pii->handle, name, columnDefs, sortOrderings);
+            return new IndexInfo(pii->handle, TableId, name, columnDefs, sortOrderings);
         }
     }
 }
