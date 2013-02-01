74e150f8bd17221cda299ad07967b3e4092e8d84
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-02-01 17:36:40 +0100

    QueryProcessingTest: added a test to reproduce bug on fetch variable in the case of sorting on non-index property.

---
+++ src/Samples/QueryProcessingTest/SqlBugsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Starcounter;
+
+namespace QueryProcessingTest {
+    public static class SqlBugsTest {
+        public static void QueryTests() {
+            // Test query with FETCH and ORDER BY
+            decimal amounts = 0;
+            var accounts = Db.SQL("select a from account a order by a.updated desc fetch ?", 10);
+            foreach (Account a in Db.SQL("select a from account a order by a.updated desc fetch ?", 10))
+                amounts += a.Amount;
+        }
+    }
+}
