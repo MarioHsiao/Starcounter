b9fbc4cb513ed1012fe781478e3988808550eb51
Anders Wahlgren <anders.wahlgren@societyobjects.com>
2013-02-03 19:05:18 +0100

    Preparation for log Window (not yet working)

--- src/Starcounter.Administrator/Model.cs
+++ src/Starcounter.Administrator/Model.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Sc.Tools.Logging;
 using Starcounter;
 
 namespace StarcounterApps3 {
@@ -6,5 +7,24 @@ namespace StarcounterApps3 {
         public string Field1;
     }
 
+    public class LogItem : Entity {
+        public long SeqNumber; // TODO: Should be ulong
+        public long ActivityID;
+        public string Category;
+        public DateTime DateTime;
 
+        public string DateTimeStr {
+            get { return DateTime.ToString(); }
+        }
+
+        public string MachineName;
+        public string Message;
+        public string ServerName;
+        public string Source;
+        public EntryType Type;
+        public string TypeStr {
+            get { return Type.ToString(); }
+        }
+        public string UserName;
+    }
 }
