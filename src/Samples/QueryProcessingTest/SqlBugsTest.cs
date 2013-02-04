3f2b1ff7e1126ad5c951da4ef63eebe4ced32212
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-02-04 12:23:04 +0100

    QueryProcessing: added storing fetch data in sort and tested for exception. The result is incorrect, since fetch data are not used during execution.

--- src/Samples/QueryProcessingTest/SqlBugsTest.cs
+++ src/Samples/QueryProcessingTest/SqlBugsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Starcounter;
 
 namespace QueryProcessingTest {
@@ -6,10 +7,20 @@ namespace QueryProcessingTest {
         public static void QueryTests() {
             // Test query with FETCH and ORDER BY
             decimal amounts = 0;
+            int nrs = 0;
             var accounts = Db.SQL("select a from account a order by a.updated desc fetch ?", 10);
             Account acc = accounts.First;
-            foreach (Account a in Db.SQL("select a from account a order by a.updated desc fetch ?", 10))
+            foreach (Account a in Db.SQL("select a from account a order by a.updated desc fetch ?", 10)) {
                 amounts += a.Amount;
+                nrs++;
+            }
+            Trace.Assert(nrs == 10);
+            nrs = 0;
+            foreach (Account a in Db.SQL("select a from account a order by a.updated desc fetch ?", 20)) {
+                amounts += a.Amount;
+                nrs++;
+            }
+            Trace.Assert(nrs == 20);
         }
     }
 }
