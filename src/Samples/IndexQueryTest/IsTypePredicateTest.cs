30323effbe28c97f7a3b376f9cca19dddc4f32ff
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-02-06 11:01:19 +0100

    Query processing: added more tests of IS type predicate. A bug in Peter's code.

--- src/Samples/IndexQueryTest/IsTypePredicateTest.cs
+++ src/Samples/IndexQueryTest/IsTypePredicateTest.cs
@@ -19,13 +19,38 @@ namespace IndexQueryTest {
             foreach (Professor e in Db.SQL("select p from professor p  where p IS Student"))
                 nrObjects++;
             Trace.Assert(nrObjects == 0);
-            // Type short and full names
             // Object expression is subtype to type identifier
+            nrObjects = 0;
+            InheritedIndexTest.PrintQueryPlan("select p from Teacher p  where p IS Employee"); // use index
+            foreach (Teacher e in Db.SQL("select p from Teacher p  where p IS Employee"))
+                nrObjects++;
+            Trace.Assert(nrObjects == InheritedIndexTest.TotalTeachers);
             // Object expression is equal to type identifier
+            nrObjects = 0;
+            InheritedIndexTest.PrintQueryPlan("select p from Employee p  where p IS Employee"); // use index
+            foreach (Employee e in Db.SQL("select p from Employee p  where p IS Employee"))
+                nrObjects++;
+            Trace.Assert(nrObjects == InheritedIndexTest.TotalEmployees);
             // Object expression is supertype to type identifier
+            nrObjects = 0;
+            InheritedIndexTest.PrintQueryPlan("select p from Employee p  where p IS Manager"); // use index
+            foreach (Employee e in Db.SQL("select p from Employee p  where p IS Manager"))
+                nrObjects++;
+            Trace.Assert(nrObjects == InheritedIndexTest.nrManagers);
             // Object variable and type identifier
+            nrObjects = 0;
+            InheritedIndexTest.PrintQueryPlan("select p from Employee p  where ? is Employer"); // use index
+            foreach (Employee e in Db.SQL("select p from Employee p  where ? is Employer", company))
+                nrObjects++;
+            Trace.Assert(nrObjects == InheritedIndexTest.nrManagers);
             // Object expression and type variable
             // Object variable and type variable
+            // Several IS type expressions
+            nrObjects = 0;
+            InheritedIndexTest.PrintQueryPlan("select p from Employee p  where p IS Manager and ? is Employer"); // use index
+            foreach (Employee e in Db.SQL("select p from Employee p  where p IS Manager and ? is Employer", company))
+                nrObjects++;
+            Trace.Assert(nrObjects == InheritedIndexTest.nrManagers);
         }
     }
 }
