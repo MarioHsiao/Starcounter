dca4a5d653537c245be15262d070c1256d24241a
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-02-06 09:02:13 +0100

    Query Processing: added a test of IS type predicate. Fixed bugs in BuildString and in AssertEquals.

---
+++ src/Samples/IndexQueryTest/IsTypePredicateTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using IndexQueryTest.InheritedIndex;
+using Starcounter;
+
+namespace IndexQueryTest {
+    public static class IsTypePredicateTest {
+        public static void RunIsTypePredicateTest() {
+            // Object expression and type identifier to true
+            int nrObjects = 0;
+            InheritedIndexTest.PrintQueryPlan("select p from professor p  where p IS IndexQueryTest.InheritedIndex.Professor"); // use index
+            foreach (Professor e in Db.SQL("select p from professor p  where p IS IndexQueryTest.InheritedIndex.Professor"))
+                nrObjects++;
+            Trace.Assert(nrObjects == InheritedIndexTest.nrProfessors);
+            // Object expression and type identifier to false
+            // Type short and full names
+            // Object expression is subtype to type identifier
+            // Object expression is equal to type identifier
+            // Object expression is supertype to type identifier
+            // Object variable and type identifier
+            // Object expression and type variable
+            // Object variable and type variable
+        }
+    }
+}
