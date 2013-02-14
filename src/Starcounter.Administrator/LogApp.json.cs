d45b055fd43c6cb76752e2cda32374b4980108e1
eriohl <eriohl@gmail.com>
2013-02-14 22:33:43 +0100

    New log reader. Updated logging output administrator.

--- src/Starcounter.Administrator/LogApp.json.cs
+++ src/Starcounter.Administrator/LogApp.json.cs
@@ -9,59 +9,25 @@ using Starcounter;
 namespace StarcounterApps3 {
     partial class LogApp : App {
 
-        static List<LogEntry> GlobalLogEntries = new List<LogEntry>();
+        static string directoryPath;
 
         static public void Setup(string directory) {
-            ThreadPool.QueueUserWorkItem(LogListenerThread, directory);
-        }
-
-        static private void LogListenerThread(object state) {
-
-            String directory = state as String;
-
-            LogFilter lf;
-            LogReader lr;
-
-            if (!Directory.Exists(directory)) {
-                Console.WriteLine("Specified directory does not exist.");
-                return;
-            }
-
-            try {
-
-                lf = null;
-                lr = new LogReader(directory, lf, (4096 * 256));
-                lr.Open();
-                //int i = 0;
-                LogEntry le;
-                for (; ; ) {
-                    le = lr.Read(true);
-                    if (le == null) {
-                        break;
-                    }
-
-                    LogApp.GlobalLogEntries.Add(le);
-                }
-                lr.Close();
-            } catch (Exception) {
-            }
-
-
+            directoryPath = directory;
         }
 
         void Handle(Input.RefreshList action) {
             this.RefreshLogEntriesList();
         }
 
-        void Handler(Input.FilterNotice action) {
+        void Handle(Input.FilterNotice action) {
             this.RefreshLogEntriesList();
         }
 
-        void Handler(Input.FilterWarning action) {
+        void Handle(Input.FilterWarning action) {
             this.RefreshLogEntriesList();
         }
 
-        void Handler(Input.FilterError action) {
+        void Handle(Input.FilterError action) {
             this.RefreshLogEntriesList();
         }
 
@@ -69,86 +35,47 @@ namespace StarcounterApps3 {
             this.RefreshLogEntriesList();
         }
 
-
         public void RefreshLogEntriesList() {
-
             this.LogEntries.Clear(); // Clearlist
 
-            this.ResetSummary();
+            int limit = 30;   // Limith the result
 
-            int limith = 30;   // Limith the result
+            var lr = new LogReader();
+            var i = 0;
+            lr.Open(directoryPath, (64 * 1024));
+            for (; ; ) {
+                var le = lr.Next();
+                if (le == null) break;
 
-
-            LogEntryApp logEntryApp;
-            for (int i = (GlobalLogEntries.Count - 1); i > 0; i--) {
-
-                LogEntry le = GlobalLogEntries[i];
-
-                // Summerize
-                if (le.Type == EntryType.Debug) {
-                    this.Summary.Debug++;
-                }
-
-                if (le.Type == EntryType.Notice) {
-                    this.Summary.Notice++;
-                }
-                if (le.Type == EntryType.Warning) {
-                    this.Summary.Warning++;
-                }
-
-                if (le.Type == EntryType.Error || le.Type == EntryType.FailureAudit || le.Type == EntryType.Critical) {
-                    this.Summary.Errors++;
-                }
-
-
-                // Filer out
-                if (this.FilterDebug == false && le.Type == EntryType.Debug) {
+                if (this.FilterDebug == false && le.Severity == Severity.Debug) {
                     continue;
                 }
 
-                if (this.FilterWarning == false && le.Type == EntryType.Warning) {
+                if (this.FilterWarning == false && le.Severity == Severity.Warning) {
                     continue;
                 }
 
-                if (this.FilterNotice == false && (le.Type == EntryType.Notice || le.Type == EntryType.SuccessAudit)) {
+                if (this.FilterNotice == false && (le.Severity == Severity.Notice || le.Severity == Severity.SuccessAudit)) {
                     continue;
                 }
 
-                if (this.FilterError == false && (le.Type == EntryType.Error || le.Type == EntryType.FailureAudit || le.Type == EntryType.Critical)) {
+                if (this.FilterError == false && (le.Severity == Severity.Error || le.Severity == Severity.FailureAudit || le.Severity == Severity.Critical)) {
                     continue;
                 }
 
+                if (++i > limit) break;
 
-                // Create and Add entry
-                if (this.LogEntries.Count < limith) {
-                    logEntryApp = new LogEntryApp() {
-                        ActivityID = le.ActivityID,
-                        Category = le.Category ?? "",
+                LogEntries.Add(
+                    new LogEntryApp() {
                         DateTimeStr = le.DateTime.ToString(),
-                        MachineName = le.MachineName ?? "",
-                        Message = le.Message ?? "",
-                        SeqNumber = (long)le.Number,   // TODO: Number is a ulong
-                        ServerName = le.ServerName ?? "",
-                        Source = le.Source ?? "",
-                        TypeStr = le.Type.ToString(),
-                        UserName = le.UserName ?? ""
-                    };
-                    this.LogEntries.Add(logEntryApp);
-                }
-
+                        TypeStr = le.Severity.ToString(),
+                        HostName = le.HostName,
+                        Source = le.Source,
+                        Message = le.Message
+                    }
+                    );
             }
-
+            lr.Close();
         }
-
-        private void ResetSummary() {
-            this.Summary.Debug = 0;
-            this.Summary.Notice = 0;
-            this.Summary.Warning = 0;
-            this.Summary.Errors = 0;
-        }
-
     }
-
-
-
 }
