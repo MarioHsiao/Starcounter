96ecdc37e42c05257615daa6ea404085d5bc15c8
Joachim Wester <joachimwester@mac.com>
2013-02-15 11:23:14 +0100

    Introducing the Message template

---
+++ src/Starcounter.Apps/Messages/TMessage.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Starcounter.Templates {
+
+    /// <summary>
+    /// Defines the schema (properties) for a Message object.
+    /// </summary>
+    public class TMessage : TObj {
+
+        /// <summary>
+        /// Creates a new Message using the schema defined by this template
+        /// </summary>
+        /// <param name="parent">The parent for the new message (if any)</param>
+        /// <returns>The new message</returns>
+        public override object CreateInstance(Container parent) {
+            return new Message() { Template = this, Parent = parent };
+        }
+    }
+}
