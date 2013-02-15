e02702941476490ed46a4248f56c85a3bceb13b9
Joachim Wester <joachimwester@mac.com>
2013-02-15 11:23:33 +0100

    Fixed bug in code generation (wrong name for integer metadata)

--- src/Starcounter.Apps/ValueMetaData/LongMetadata.cs
+++ src/Starcounter.Apps/ValueMetaData/LongMetadata.cs
@@ -11,14 +11,14 @@ namespace Starcounter.Templates
     /// <summary>
     /// Class IntMetadata
     /// </summary>
-	public class IntMetadata : ValueMetadata
+	public class LongMetadata : ValueMetadata
 	{
         /// <summary>
-        /// Initializes a new instance of the <see cref="IntMetadata" /> class.
+        /// Initializes a new instance of the <see cref="LongMetadata" /> class.
         /// </summary>
         /// <param name="app">The app.</param>
         /// <param name="prop">The prop.</param>
-		public IntMetadata(Obj app, Template prop) : base(app, prop) { }
+		public LongMetadata(Obj app, Template prop) : base(app, prop) { }
 
 	}
 }
