e4c5f8aed10352cbd710a817b27376490321568e
Per Samuelsson <per@starcounter.com>
2013-02-20 17:14:09 +0100

    Added class ConsoleUtil to star.exe project. It expose utility methods to use when writing to the console.

---
+++ src/Server/sccli/ConsoleUtil.cs
@@ -0,0 +1,30 @@
+﻿using Starcounter.CommandLine.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star {
+
+    internal static class ConsoleUtil {
+
+        internal static void ToConsoleWithColor(string text, ConsoleColor color) {
+            try {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+            } finally {
+                Console.ResetColor();
+            }
+        }
+
+        internal static void ToConsoleWithColor(Action action, ConsoleColor color) {
+            try {
+                Console.ForegroundColor = color;
+                action();
+            } finally {
+                Console.ResetColor();
+            }
+        }
+    }
+}
