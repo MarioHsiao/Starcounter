ec4955686235780f8490c9a2f5cceea0fb52c5d0
eriohl <eriohl@gmail.com>
2013-02-21 14:37:39 +0100

    Moved initialize logging to before apps initialization setup.

--- src/Starcounter.Bootstrap/Control.cs
+++ src/Starcounter.Bootstrap/Control.cs
@@ -125,6 +125,9 @@ namespace StarcounterInternal.Bootstrap
             mem += 512;
             OnKernelMemoryConfigured();
 
+            ulong hlogs = ConfigureLogging(configuration, hmenv);
+            OnLoggingConfigured();
+
             // Initializing Apps internal HTTP request parser.
             HttpRequest.sc_init_http_parser();
 
@@ -140,9 +143,6 @@ namespace StarcounterInternal.Bootstrap
 
             // Initializing AppsBootstrapper.
             AppsBootstrapper.InitAppsBootstrapper(configuration.DefaultUserHttpPort);
-
-            ulong hlogs = ConfigureLogging(configuration, hmenv);
-            OnLoggingConfigured();
 
             ConfigureHost(configuration, hlogs);
             OnHostConfigured();
