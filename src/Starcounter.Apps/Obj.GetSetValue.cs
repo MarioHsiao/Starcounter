38124ec929d59147d40f2b5c60edbcdbd0358ec3
Joachim Wester <joachimwester@mac.com>
2013-02-26 13:22:35 +0100

    Removed dependency to Trigger from XSON base classes

--- src/Starcounter.Apps/Obj.GetSetValue.cs
+++ src/Starcounter.Apps/Obj.GetSetValue.cs
@@ -241,36 +241,6 @@ namespace Starcounter {
         /// Gets the value.
         /// </summary>
         /// <param name="property">The property.</param>
-        /// <returns>Action.</returns>
-        /// <exception cref="Starcounter.JockeNotImplementedException"></exception>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public Action GetValue(TTrigger property) {
-#if QUICKTUPLE
-            return _Values[property.Index];
-#else
-            throw new JockeNotImplementedException();
-#endif
-        }
-
-        /// <summary>
-        /// Sets the value.
-        /// </summary>
-        /// <param name="property">The property.</param>
-        /// <param name="value">The value.</param>
-        /// <exception cref="Starcounter.JockeNotImplementedException"></exception>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public void SetValue(TTrigger property, Action value) {
-#if QUICKTUPLE
-            _Values[property.Index] = value;
-#else
-            throw new JockeNotImplementedException();
-#endif
-        }
-
-        /// <summary>
-        /// Gets the value.
-        /// </summary>
-        /// <param name="property">The property.</param>
         /// <returns>Arr.</returns>
         /// <exception cref="Starcounter.JockeNotImplementedException"></exception>
         [EditorBrowsable(EditorBrowsableState.Never)]
