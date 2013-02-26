93a29bbc9ddea54a621215679457a3abb717424c
Joachim Wester <joachimwester@mac.com>
2013-02-26 16:11:28 +0100

    Allow the Db.SQL and Db.SlowSQL to use the specific result sets for backwards compatibility.

--- src/Starcounter.Internal/ISqlResult.cs
+++ src/Starcounter.Internal/ISqlResult.cs
@@ -10,6 +10,14 @@ namespace Starcounter {
     /// <remarks>
     /// We use an abstract class instead of an interface to allow implicit casts when setting
     /// array properties in Objs (Messages and Puppets) to SQL results.
+    /// 
+    /// This allows the programmer to do this:
+    /// <example>
+    /// Message myOrder = new OrderMsg();
+    /// myOrder.Items = SQL("SELECT I FROM OrderItem I"); // Implicit conversion
+    /// </example>
+    /// 
+    /// http://stackoverflow.com/questions/143485/implicit-operator-using-interfaces
     /// </remarks>
     public abstract class Rows : IEnumerable {
         public virtual dynamic First {
@@ -25,12 +33,21 @@ namespace Starcounter {
         }
     }
 
+
     /// <summary>
     /// 
     /// </summary>
     /// <remarks>
     /// We use an abstract class instead of an interface to allow implicit casts when setting
     /// array properties in Objs (Messages and Puppets) to SQL results.
+    /// 
+    /// This allows the programmer to do this:
+    /// <example>
+    /// Message myOrder = new OrderMsg();
+    /// myOrder.Items = SQL("SELECT I FROM OrderItem I"); // Implicit conversion
+    /// </example>
+    /// 
+    /// http://stackoverflow.com/questions/143485/implicit-operator-using-interfaces
     /// </remarks>
     public abstract class Rows<T> : Rows, IEnumerable<T> {
         public new T First {
@@ -42,7 +59,7 @@ namespace Starcounter {
 
         IEnumerator<T> IEnumerable<T>.GetEnumerator() {
             // The compiler does not allow us to declare this abstract although it is
-            return (IEnumerator<T>)base.GetEnumerator();
+            return (IEnumerator<T>)GetEnumerator();
         }
     }
 }
