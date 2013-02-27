3a9cf750a7eed36da43a53fff9136751d5f1f054
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-02-27 13:20:06 +0100

    SQL parser: added structure of calls to parsers and optimizers.

--- src/Starcounter/Query/QueryPreparation.cs
+++ src/Starcounter/Query/QueryPreparation.cs
@@ -6,6 +6,22 @@ namespace Starcounter.Query {
     internal static class QueryPreparation {
         internal static IExecutionEnumerator PrepareQuery(String query) {
             Scheduler vproc = Scheduler.GetInstance();
+            // Call to Bison parser and type checker
+#if !PROLOG_ONLY
+#endif
+            // Call to Prolog parser and type checker
+#if !BISON_ONLY
+#endif
+            // Check equality
+#if !BISON_ONLY && !PROLOG_ONLY
+#endif
+            // Call to Bison optimizer
+#if !PROLOG_ONLY
+#endif
+            // Call to Prolog optimizer
+#if !BISON_ONLY
+#endif
+            // Return Bison based execution plan if available
             return PrologManager.ProcessSqlQuery(vproc, query);
         }
     }
