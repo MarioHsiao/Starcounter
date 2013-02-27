66efe148c96cc91767d62c12fc1b221abc947b13
alemoi <alexey.moiseenko@starcounter.com>
2013-02-27 17:59:54 +0100

    Added DllExport assembly to weaver blacklist.

--- src/scweaver/CodeWeaver.cs
+++ src/scweaver/CodeWeaver.cs
@@ -690,7 +690,9 @@ namespace Weaver {
                 "PostSharp*.dll",
                 "Roslyn*.dll",
                 "FasterThanJson.dll",
-                "Starcounter.dll"}
+                "Starcounter.dll",
+                "RGiesecke.DllExport.Metadata.dll"    
+            }
                 ) {
                 AddExcludeExpression(exclude, weaverExcludes);
             }
