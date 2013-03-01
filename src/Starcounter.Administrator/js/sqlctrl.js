d95d8213a47352fbad2c65f9aa9ca2e83ea848db
Ander Wahlgren <anders.wahlgren@societyobjects.com>
2013-03-01 19:07:50 +0100

    Added the ability to do SQL queries (there will be bugs, dont except it to be 100% working)

---
+++ src/Starcounter.Administrator/js/sqlctrl.js
@@ -0,0 +1,26 @@
+﻿
+
+function sqlctrl($scope) {
+
+    $scope.$parent.Rows = [];
+    $scope.$parent.Columns = [];
+
+    $scope.$parent.$watch("Output", function (newValue) {
+
+        if (newValue == "") return;
+        var json = angular.fromJson(newValue);
+
+        $scope.hasErrorMessage = json.error.message != null;
+
+        if ($scope.hasErrorMessage) {
+            // Has error.
+            $scope.ErrorMessage = json.error.message;
+            return;
+        }
+
+        $scope.$parent.Columns = json.columns;
+        $scope.$parent.Rows = json.rows;
+
+    }, false);
+
+}
