b8237f5a4ae46a9d0f78e8b2ab1c59f0899f3713
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-03-06 15:33:47 +0100

    Query processing: added method to measure query preparation.

---
+++ src/Samples/QueryProcessingTest/QueryProcessingPerformance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryProcessingTest {
+    public static class QueryProcessingPerformance {
+        public static void MeasurePrepareQuery() {
+            int nrIterations = 10000;
+            String query = "select a from Account a, User u where a.Client = u and u.FirstName = ? fetch ? offset ?";
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < nrIterations; i++)
+                Starcounter.Query.QueryPreparation.PrepareQuery(query);
+            timer.Stop();
+            Console.WriteLine("It took " + timer.ElapsedMilliseconds / nrIterations + " ms to prepare the query.");
+        }
+    }
+}
