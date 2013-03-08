3b38c0219f93c6c603264de040d979798ddc404f
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-03-08 13:18:48 +0100

    Query processing: fixed nightly test to not use console.

--- src/Samples/QueryProcessingTest/HelpMethods.cs
+++ src/Samples/QueryProcessingTest/HelpMethods.cs
@@ -5,15 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Starcounter;
+using Starcounter.TestFramework;
 
 namespace QueryProcessingTest {
     public static class HelpMethods {
+        internal static TestLogger logger = null;
+
         internal static void PrintQueryPlan(String query) {
             Console.WriteLine(((IEnumerator)Db.SQL(query, null).GetEnumerator()).ToString());
         }
 
         internal static void PrintSlowQueryPlan(String query) {
             Console.WriteLine(((IEnumerator)Db.SlowSQL(query, null).GetEnumerator()).ToString());
+        }
+
+        internal static void LogEvent(String eventString) {
+            logger.Log(eventString);
         }
     }
 }
