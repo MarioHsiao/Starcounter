32383e4ebb36770513f1f3054f870cddb1aa7721
Per Samuelsson <per@starcounter.com>
2013-03-10 21:20:08 +0100

    Added new class HttpResponseBuilder.Slow and implemented a first method FromStatusHeadersAndStringContent that allows user code to create responses based on a code, on given headers and a given string-based content. See code comments for all the details.

--- src/Starcounter.Rest/Http/HttpResponseBuilder.cs
+++ src/Starcounter.Rest/Http/HttpResponseBuilder.cs
@@ -7,6 +7,7 @@
 using Starcounter.Internal.REST;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 
@@ -17,7 +18,94 @@ namespace Starcounter.Internal.Web {
     /// </summary>
    public class HttpResponseBuilder {
 
+       public const string CRLF = "\r\n";
 
+       /// <summary>
+       /// Expose a set of factory methods that are slowish prototypes of
+       /// HTTP response creation functionality we should consider making
+       /// an effort to support as fast.
+       /// </summary>
+       public static class Slow {
+
+           /// <summary>
+           /// Build a HTTP/1.1 response using a given status code, a set
+           /// of headers and content given in the form of a <see cref="string"/>.
+           /// </summary>
+           /// <remarks>
+           /// Clients should not specify the entity header Content-Type
+           /// as part of the <paramref name="headers"/> set. This is set by
+           /// the method, and based on the <paramref name="contentEncoding"/>
+           /// and <paramref name="contentType"/> parameters respectively.
+           /// Specifying a Content-Type as part of the headers collection
+           /// yields in an undefined behaviour.
+           /// </remarks>
+           /// <param name="code">The status code.</param>
+           /// <param name="headers">General-, response and/or entity headers.</param>
+           /// <param name="content">The content to include in the response, forming
+           /// it's body. If <see langword="null"/> is specified, the response will
+           /// be created with no entity/content.</param>
+           /// <param name="contentEncoding">Type of encoding to use when turning
+           /// the given content to an array of bytes. Ignored if <paramref name="content"/>
+           /// is <see langword="null"/>.</param>
+           /// <param name="contentType">The content type to indicating the media
+           /// type of the given content. If not given, this method assumes
+           /// "application/json" as the default.</param>
+           /// <returns>A byte array that is the HTTP response message before any
+           /// transfer encoding is applied (i.e. it's uncompressed).</returns>
+           public static byte[] FromStatusHeadersAndStringContent(
+               int code,
+               NameValueCollection headers, 
+               string content,
+               Encoding contentEncoding = null,
+               string contentType = "application/json"
+               ) {
+               string reason;
+               string msgHeader;
+               string header;
+               bool hasContent;
+               byte[] message;
+               byte[] entityBody;
+               int bodyLength;
+
+               hasContent = !string.IsNullOrEmpty(content);
+               bodyLength = 0;
+               entityBody = null;
+               if (!HttpStatusCodeAndReason.TryGetRecommendedHttp11ReasonPhrase(code, out reason)) {
+                   reason = HttpStatusCodeAndReason.ReasonNotAvailable;
+               }
+               msgHeader = HttpStatusCodeAndReason.ToStatusLineFormatNoValidate(code, reason);
+               msgHeader += CRLF;
+
+               if (headers != null) {
+                   foreach (var key in headers.AllKeys) {
+                       var value = headers[key];
+                       header = string.Concat(key, ":", value, CRLF);
+                       msgHeader += header;
+                   }
+               }
+               
+               if (hasContent) {
+                   contentEncoding = contentEncoding ?? Encoding.UTF8;
+                   entityBody = contentEncoding.GetBytes(content);
+                   bodyLength = entityBody.Length;
+                   header = string.Concat("Content-Type:", contentType, ";", contentEncoding.WebName, CRLF);
+                   msgHeader += header;
+                   header = string.Concat("Content-Length:", bodyLength, CRLF);
+                   msgHeader += header;   
+               }
+
+               msgHeader += CRLF;
+               var msgHeaderBytes = Encoding.UTF8.GetBytes(msgHeader);
+
+               message = new byte[msgHeaderBytes.Length + bodyLength];
+               msgHeaderBytes.CopyTo(message, 0);
+               if (hasContent) {
+                   entityBody.CopyTo(message, msgHeaderBytes.Length);
+               }
+
+               return message;
+           }
+       }
 
        /// <summary>
        /// The created201
