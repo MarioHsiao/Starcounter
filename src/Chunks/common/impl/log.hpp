c3af35807854f5aa86cd8eff458e4c553c0445cb
andthy <andreas.thyrhaug@starcounter.com>
2013-03-13 15:17:16 +0100

    IPC: IPC monitor do some Starcounter logging.

--- src/Chunks/common/impl/log.hpp
+++ src/Chunks/common/impl/log.hpp
@@ -40,13 +40,13 @@ try {
 		std::size_t size = 0;
 		log_exception::error_code_type err = 0;
 		
-		if ((err = ::make_sc_process_uri(server_name, process_name, 0, &size) != 0)) {
+		if ((err = process_uri(server_name, process_name, 0, &size) != 0)) {
 			throw log_exception(err);
 		}
 		
 		std::vector<wchar_t> host_name(size);
 		
-		if ((err = ::make_sc_process_uri(server_name, process_name, &host_name[0], &size)) != 0) {
+		if ((err = process_uri(server_name, process_name, &host_name[0], &size)) != 0) {
 			throw log_exception(err);
 		}
 		
@@ -54,7 +54,7 @@ try {
 			throw log_exception(err);
 		}
 		
-		if ((err = ::sccorelog_connect_to_logs(&host_name[0], 0, &handle_)) != 0) { // &host_name[0] is host_name.begin()
+		if ((err = ::sccorelog_connect_to_logs(&host_name[0], 0, &handle_)) != 0) {
 			throw log_exception(err);
 		}
 		
@@ -76,37 +76,145 @@ inline void log::close() {
 	}
 }
 
-inline void log::critical(const wchar_t* message) {
+inline void log::debug(uint32_t error_code) {
 	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
-	SC_ENTRY_CRITICAL, 0, message);
+	entry_debug, error_code, 0);
 	_SC_ASSERT(err == 0);
 	err = ::sccorelog_flush_to_logs(handle_);
 	_SC_ASSERT(err == 0);
 }
 
-inline void log::error(const wchar_t* message) {
+inline void log::debug(const wchar_t* message, uint32_t error_code) {
 	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
-	SC_ENTRY_ERROR, 0, message);
+	entry_debug, error_code, message);
 	_SC_ASSERT(err == 0);
 	err = ::sccorelog_flush_to_logs(handle_);
 	_SC_ASSERT(err == 0);
 }
 
-inline void log::debug(const wchar_t* message) {
+inline void log::success_audit(uint32_t error_code) {
 	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
-	SC_ENTRY_DEBUG, 0, message);
+	entry_success_audit, error_code, 0);
 	_SC_ASSERT(err == 0);
 	err = ::sccorelog_flush_to_logs(handle_);
 	_SC_ASSERT(err == 0);
 }
 
-inline void log::verbose(const wchar_t* message) {
-	::wprintf(L"<DEBUG> %s\n", message);
+inline void log::success_audit(const wchar_t* message, uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_success_audit, error_code, message);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::failure_audit(uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_failure_audit, error_code, 0);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::failure_audit(const wchar_t* message, uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_failure_audit, error_code, message);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::notice(uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_notice, error_code, 0);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::notice(const wchar_t* message, uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_notice, error_code, message);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::warning(uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_warning, error_code, 0);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::warning(const wchar_t* message, uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_warning, error_code, message);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::error(uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_error, error_code, 0);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::error(const wchar_t* message, uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_error, error_code, message);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::critical(uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_critical, error_code, 0);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline void log::critical(const wchar_t* message, uint32_t error_code) {
+	log_exception::error_code_type err = ::sccorelog_kernel_write_to_logs(handle_,
+	entry_critical, error_code, message);
+	_SC_ASSERT(err == 0);
+	err = ::sccorelog_flush_to_logs(handle_);
+	_SC_ASSERT(err == 0);
+}
+
+inline int32_t log::process_uri(const char* server_name, const char* process_name,
+wchar_t* buffer, std::size_t* size) {
+	char computer_name[MAX_COMPUTERNAME_LENGTH +1];
+	uint32_t computer_name_size = MAX_COMPUTERNAME_LENGTH +1;
 	
-	if (handle_ != 0) {
-		// Logging to log file.
-		debug(message);
+	if (::GetComputerNameA(computer_name, (DWORD*) &computer_name_size) == 0) {
+		return ::GetLastError();
 	}
+
+	std::size_t buffer_size_needed =
+	+5 // 'sc://'
+	+computer_name_size
+	+1 // '/'
+	+strlen(server_name)
+	+1 // '/'
+	+strlen(process_name)
+	+1; // '\0'
+	
+	std::size_t buffer_size = *size;
+	*size = buffer_size_needed;
+
+	if (buffer_size_needed <= buffer_size) {
+		swprintf(buffer, L"sc://%S/%S/%S", computer_name, server_name, process_name);
+		_wcslwr(buffer);
+		return 0;
+	}
+	return 0;
 }
 
 } // namespace starcounter
