b41a52836d7e1de6f63e98d1ffe0168b131c2e3c
Joachim Wester <joachimwester@mac.com>
2013-03-13 16:02:53 +0100

    Introducing the Handle class.

---
+++ src/Starcounter.Internal/Handle.cs
@@ -0,0 +1,372 @@
+﻿
+
+using Starcounter.Advanced;
+using System;
+namespace Starcounter {
+
+    
+    /// <summary>
+    /// Allows you to register communication endpoints such as REST style handlers
+    /// (GET/POST/PUT/DELETE/PATCH etc.)
+    /// </summary>
+    /// <remarks>
+    /// Allows endpoints to be registred that corresponds to methods and URIs (paths
+    /// with optinal parameters). Even though REST handlers are typically associated with http, they 
+    /// can also be implmented in protocols such as HTTP1.1, SPDY, HTTP2.0 (DRAFT),
+    /// WebSockets and other means of communication. As long as their endpoints are defined as methods
+    /// (verbs such as GET) and URI templates (i.e. /news/sports/{?}).
+    /// </remarks>
+    public class Handle {
+
+
+        /// <summary>
+        /// Inject REST handler function provider here
+        /// </summary>
+        public static IREST _REST;
+
+        /// <summary>
+        /// Register the specified uri with a GET verb.
+        /// </summary>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void GET(string uri, Func<object> handler) {
+            _REST.RegisterHandler("GET " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with one variable parameter, with a GET verb
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void GET<T>(string uri, Func<T, object> handler) {
+            _REST.RegisterHandler<T>("GET " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with two variable parameters, with a GET verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void GET<T1, T2>(string uri, Func<T1, T2, object> handler) {
+            _REST.RegisterHandler<T1, T2>("GET " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with three variable parameters, with a GET verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <param name="uri">The uri to register</param>
+        /// <param name="handler">The handler.</param>
+        public static void GET<T1, T2, T3>(string uri, Func<T1, T2, T3, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3>("GET " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with four variable parameters, with a GET verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void GET<T1, T2, T3, T4>(string uri, Func<T1, T2, T3, T4, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4>("GET " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with five variable parameters, with a GET verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void GET<T1, T2, T3, T4, T5>(string uri, Func<T1, T2, T3, T4, T5, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4, T5>("GET " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri with a PUT verb.
+        /// </summary>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PUT(string uri, Func<object> handler) {
+            _REST.RegisterHandler("PUT " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with one variable parameter, with a PUT verb
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PUT<T>(string uri, Func<T, object> handler) {
+            _REST.RegisterHandler<T>("PUT " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with two variable parameters, with a PUT verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PUT<T1, T2>(string uri, Func<T1, T2, object> handler) {
+            _REST.RegisterHandler<T1, T2>("PUT " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with three variable parameters, with a PUT verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <param name="uri">The uri to register</param>
+        /// <param name="handler">The handler.</param>
+        public static void PUT<T1, T2, T3>(string uri, Func<T1, T2, T3, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3>("PUT " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with four variable parameters, with a PUT verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PUT<T1, T2, T3, T4>(string uri, Func<T1, T2, T3, T4, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4>("PUT " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with five variable parameters, with a PUT verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PUT<T1, T2, T3, T4, T5>(string uri, Func<T1, T2, T3, T4, T5, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4, T5>("PUT " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri with a GET verb.
+        /// </summary>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void POST(string uri, Func<object> handler) {
+            _REST.RegisterHandler("POST " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with one variable parameter, with a POST verb
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void POST<T>(string uri, Func<T, object> handler) {
+            _REST.RegisterHandler<T>("POST " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with two variable parameters, with a POST verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void POST<T1, T2>(string uri, Func<T1, T2, object> handler) {
+            _REST.RegisterHandler<T1, T2>("POST " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with three variable parameters, with a POST verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <param name="uri">The uri to register</param>
+        /// <param name="handler">The handler.</param>
+        public static void POST<T1, T2, T3>(string uri, Func<T1, T2, T3, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3>("POST " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with four variable parameters, with a POST verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void POST<T1, T2, T3, T4>(string uri, Func<T1, T2, T3, T4, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4>("POST " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with five variable parameters, with a POST verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void POST<T1, T2, T3, T4, T5>(string uri, Func<T1, T2, T3, T4, T5, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4, T5>("POST " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri with a DELETE verb.
+        /// </summary>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void DELETE(string uri, Func<object> handler) {
+            _REST.RegisterHandler("DELETE " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with one variable parameter, with a DELETE verb
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void DELETE<T>(string uri, Func<T, object> handler) {
+            _REST.RegisterHandler<T>("DELETE " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with two variable parameters, with a DELETE verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void DELETE<T1, T2>(string uri, Func<T1, T2, object> handler) {
+            _REST.RegisterHandler<T1, T2>("DELETE " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with three variable parameters, with a DELETE verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <param name="uri">The uri to register</param>
+        /// <param name="handler">The handler.</param>
+        public static void DELETE<T1, T2, T3>(string uri, Func<T1, T2, T3, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3>("DELETE " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with four variable parameters, with a DELETE verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void DELETE<T1, T2, T3, T4>(string uri, Func<T1, T2, T3, T4, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4>("DELETE " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with five variable parameters, with a DELETE verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void DELETE<T1, T2, T3, T4, T5>(string uri, Func<T1, T2, T3, T4, T5, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4, T5>("DELETE " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri with a PATCH verb.
+        /// </summary>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PATCH(string uri, Func<object> handler) {
+            _REST.RegisterHandler("PATCH " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with one variable parameter, with a PATCH verb
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PATCH<T>(string uri, Func<T, object> handler) {
+            _REST.RegisterHandler<T>("PATCH " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with two variable parameters, with a PATCH verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PATCH<T1, T2>(string uri, Func<T1, T2, object> handler) {
+            _REST.RegisterHandler<T1, T2>("PATCH " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with three variable parameters, with a PATCH verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <param name="uri">The uri to register</param>
+        /// <param name="handler">The handler.</param>
+        public static void PATCH<T1, T2, T3>(string uri, Func<T1, T2, T3, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3>("PATCH " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with four variable parameters, with a PATCH verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PATCH<T1, T2, T3, T4>(string uri, Func<T1, T2, T3, T4, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4>("PATCH " + uri, handler);
+        }
+
+        /// <summary>
+        /// Register the specified uri, with five variable parameters, with a PATCH verb
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter.</typeparam>
+        /// <param name="uri">The uri to register.</param>
+        /// <param name="handler">The handler.</param>
+        public static void PATCH<T1, T2, T3, T4, T5>(string uri, Func<T1, T2, T3, T4, T5, object> handler) {
+            _REST.RegisterHandler<T1, T2, T3, T4, T5>("PATCH " + uri, handler);
+        }
+    }
+}
