3a80efbaa162e4555e4ad01de47862836b8e9df4
Per Samuelsson <per@starcounter.com>
2013-03-15 15:02:01 +0100

    Added the TypeSpecification backing infrastructure class, providing an API to consume type specifications from assemblies.

---
+++ src/Starcounter/Hosting/TypeSpecification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Hosting {
+    /// <summary>
+    /// Represents the interface to a database type specification, as
+    /// defined <a href="http://www.starcounter.com/internal/wiki/W3">
+    /// here</a>.
+    /// </summary>
+    public sealed class TypeSpecification {
+        /// <summary>
+        /// Provides the type specification class name.
+        /// </summary>
+        public const string Name = "__starcounterTypeSpecification";
+
+        internal TypeSpecification(Type typeSpecType) {
+
+        }
+    }
+}
