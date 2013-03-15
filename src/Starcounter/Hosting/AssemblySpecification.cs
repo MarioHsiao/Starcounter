4640d81ef6f3ba20f3de32a1a6fca10213d4ae5d
Per Samuelsson <per@starcounter.com>
2013-03-15 08:25:01 +0100

    Added the AssemblySpecification class, encapsulating the internals of the assembly-level assembly specification, produced by the weaver and consumed by the host.

---
+++ src/Starcounter/Hosting/AssemblySpecification.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Hosting {
+    /// <summary>
+    /// Represents the interface to a assembly specification, as it is
+    /// defined <a href="http://www.starcounter.com/internal/wiki/W3">
+    /// here</a>.
+    /// </summary>
+    public sealed class AssemblySpecification {
+        /// <summary>
+        /// Allow instantiation only from factory method.
+        /// </summary>
+        private AssemblySpecification() {
+
+        }
+    }
+}
