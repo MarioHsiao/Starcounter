350c01361c1f8b91c4f8dfe092cd5eb1b367b913
Per Samuelsson <per@starcounter.com>
2013-03-15 09:05:33 +0100

    Extended the AssemblySpecification class with a factory method that creates one (not implemented) and a constant holding it's name.

--- src/Starcounter/Hosting/AssemblySpecification.cs
+++ src/Starcounter/Hosting/AssemblySpecification.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Starcounter.Hosting {
     /// <summary>
@@ -16,7 +17,28 @@ namespace Starcounter.Hosting {
         /// Allow instantiation only from factory method.
         /// </summary>
         private AssemblySpecification() {
+        }
 
+        /// <summary>
+        /// Provides the assembly specification class name.
+        /// </summary>
+        public const string Name = "__starcounterAssemblySpecification";
+        
+        /// <summary>
+        /// Loads the assembly specification from a given assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly from which to load the
+        /// assembly specification.</param>
+        /// <returns>An instance representing the assembly specification
+        /// found in the given assembly.</returns>
+        /// <exception cref="BackingException">
+        /// A backing exception indicating an error occured when this
+        /// method consumed the backing infrastructure, for example that
+        /// an assembly specification was not found. The error code of
+        /// the exception, along with any inner exceptions, will describe
+        /// more precisely the problem.</exception>
+        public static AssemblySpecification LoadFrom(Assembly assembly) {
+            throw new NotImplementedException();
         }
     }
 }
