1be4cec4214f52a8eaa83f5cbd83416afded4a72
Per Samuelsson <per@starcounter.com>
2013-03-16 13:04:33 +0100

    Changed long name of database from "db" to "database", according to directives.

--- src/Server/sccli/Program.cs
+++ src/Server/sccli/Program.cs
@@ -30,7 +30,7 @@ namespace star {
             public const string Serverport = "serverport";
             public const string Server = "server";
             public const string ServerHost = "serverhost";
-            public const string Db = "db";
+            public const string Db = "database";
             public const string LogSteps = "logsteps";
             public const string NoDb = "nodb";
             public const string NoAutoCreateDb = "noautocreate";
