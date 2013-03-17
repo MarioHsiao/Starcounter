1a63c7426dfa07a35ba9c9316ddef966aa8b2d90
Joachim Wester <joachimwester@mac.com>
2013-03-17 13:29:29 +0100

    Fixed name conflict in codeine

--- src/Starcounter.Apps.CodeGeneration/SimpleDomGenerator/DomGenerator.cs
+++ src/Starcounter.Apps.CodeGeneration/SimpleDomGenerator/DomGenerator.cs
@@ -226,7 +226,7 @@ namespace Starcounter.Internal.Application.CodeGeneration
                 Parent = acn,
                 IsStatic = true,
                 _Inherits = null,
-                _ClassName = "Json"
+                _ClassName = "json"
             };
             GenerateJsonAttributes(acn, json);
 
@@ -376,7 +376,7 @@ namespace Starcounter.Internal.Application.CodeGeneration
             appTemplate = rootTemplate;
             mapParts = jsonMapName.Split('.');
 
-            // We skip the two first parts since the first one will always be "Json" 
+            // We skip the two first parts since the first one will always be "json" 
             // and the second the rootTemplate.
             for (Int32 i = 1; i < mapParts.Length; i++)
             {
