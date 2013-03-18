b69c5631c86f4de87d6dc6b31782dffbec52df5a
Per Samuelsson <per@starcounter.com>
2013-03-18 16:56:16 +0100

    Added new backing infrastructure emission class AssemblySpecificationEmit to the weaver.

---
+++ src/scweaver/Starcounter.Internal.Weaver/BackingInfrastructure/AssemblySpecificationEmit.cs
@@ -0,0 +1,55 @@
+﻿
+using PostSharp.Sdk.CodeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter.Hosting;
+using System.Reflection;
+
+namespace Starcounter.Internal.Weaver.BackingInfrastructure {
+    /// <summary>
+    /// Encapsulates the emission of the assembly specification according
+    /// to <see cref="http://www.starcounter.com/internal/wiki/W3#Assembly_specification"/>.
+    /// </summary>
+    /// <remarks>
+    /// Code hosts that want to safely consume the emitted infrastructure should
+    /// use the corresponding <see cref="AssemblySpecification"/> type.
+    /// </remarks>
+    internal sealed class AssemblySpecificationEmit {
+        ModuleDeclaration module;
+        TypeDefDeclaration assemblyTypeDefinition;
+        TypeDefDeclaration databaseClassIndexTypeDefinition;
+
+        public AssemblySpecificationEmit(ModuleDeclaration module) {
+            this.module = module;
+            EmitSpecification();
+        }
+
+        void EmitSpecification() {
+            assemblyTypeDefinition = new TypeDefDeclaration {
+                Name = AssemblySpecification.Name,
+                Attributes = TypeAttributes.Class | TypeAttributes.NestedAssembly | TypeAttributes.Sealed
+            };
+            module.Types.Add(assemblyTypeDefinition);
+
+            databaseClassIndexTypeDefinition = new TypeDefDeclaration {
+                Name = AssemblySpecification.DatabaseClassIndexName,
+                Attributes = TypeAttributes.Class | TypeAttributes.NestedAssembly | TypeAttributes.Sealed
+            };
+            assemblyTypeDefinition.Types.Add(databaseClassIndexTypeDefinition);
+        }
+
+        internal void IncludeDatabaseClass(TypeDefDeclaration databaseClassTypeDef) {
+            var name = AssemblySpecification.TypeNameToClassIndexName(databaseClassTypeDef.Name);
+            var typeReference = new FieldDefDeclaration {
+                Name = name,
+                Attributes = (FieldAttributes.Assembly | FieldAttributes.Static),
+                FieldType = module.Cache.GetType(typeof(Type))
+            };
+
+            databaseClassIndexTypeDefinition.Fields.Add(typeReference);
+        }
+    }
+}
