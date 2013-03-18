fbb17861f0ea1f3ca75055e2d60b4bdcfd394c24
Per Samuelsson <per@starcounter.com>
2013-03-18 21:38:31 +0100

    Added the TypeSpecificationEmit class, part of the weaver. This class provides the interface to the transformation engine, used to emit backing infrastructure type specifications.

---
+++ src/scweaver/Starcounter.Internal.Weaver/BackingInfrastructure/TypeSpecificationEmit.cs
@@ -0,0 +1,78 @@
+﻿
+using PostSharp.Sdk.CodeModel;
+using Starcounter.Binding;
+using Starcounter.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Internal.Weaver.BackingInfrastructure {
+    /// <summary>
+    /// Encapsulates the emission of type specifications according
+    /// to <see cref="http://www.starcounter.com/internal/wiki/W3#Type_specification"/>.
+    /// </summary>
+    /// <remarks>
+    /// Code hosts that want to safely consume the emitted infrastructure should
+    /// use the corresponding <see cref="TypeSpecification"/> type.
+    /// </remarks>
+    internal sealed class TypeSpecificationEmit {
+        ModuleDeclaration module;
+        ITypeSignature typeBindingType;
+        ITypeSignature ushortType;
+        ITypeSignature objectRefType;
+        Dictionary<TypeDefDeclaration, TypeDefDeclaration> typeToSpec;
+
+        public TypeSpecificationEmit(ModuleDeclaration module) {
+            this.module = module;
+            typeBindingType = module.Cache.GetType(typeof(TypeBinding));
+            objectRefType = module.Cache.GetType(typeof(ObjectRef));
+            ushortType = module.Cache.GetIntrinsic(IntrinsicType.UInt16);
+            typeToSpec = new Dictionary<TypeDefDeclaration, TypeDefDeclaration>();
+        }
+
+        public void EmitForType(TypeDefDeclaration typeDef) {
+            var typeSpec = new TypeDefDeclaration {
+                Name = TypeSpecification.Name,
+                Attributes = TypeAttributes.Class | TypeAttributes.NestedAssembly | TypeAttributes.Sealed
+            };
+            typeDef.Types.Add(typeSpec);
+            typeToSpec.Add(typeDef, typeSpec);
+
+            var tableHandle = new FieldDefDeclaration {
+                Name = TypeSpecification.TableHandleName,
+                Attributes = (FieldAttributes.FamORAssem | FieldAttributes.Static),
+                FieldType = ushortType
+            };
+            typeSpec.Fields.Add(tableHandle);
+
+            var typeBindingReference = new FieldDefDeclaration {
+                Name = TypeSpecification.TypeBindingName,
+                Attributes = (FieldAttributes.FamORAssem | FieldAttributes.Static),
+                FieldType = typeBindingType
+            };
+            typeSpec.Fields.Add(typeBindingReference);
+
+            var thisHandle = new FieldDefDeclaration {
+                Name = TypeSpecification.ThisHandleName,
+                Attributes = FieldAttributes.Private,
+                FieldType = objectRefType
+            };
+            typeDef.Fields.Add(thisHandle);
+
+            var thisBinding = new FieldDefDeclaration {
+                Name = TypeSpecification.ThisBindingName,
+                Attributes = FieldAttributes.Private,
+                FieldType = typeBindingType
+            };
+            typeDef.Fields.Add(thisBinding);
+        }
+
+        public void IncludeField(TypeDefDeclaration typeDef) {
+            var specType = typeToSpec[typeDef];
+            throw new NotImplementedException();
+        }
+    }
+}
