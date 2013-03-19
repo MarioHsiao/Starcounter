1b60c3c44795e59ec67a7b456149cfee4c7534b8
Per Samuelsson <per@starcounter.com>
2013-03-19 10:39:57 +0100

    Added the IObjectProxy interface, extending the IObjectView with a small API to allow underlying managed objects to be mutable (where the IObjectView is designed to provide read-only access).

---
+++ src/Starcounter/Binding/IObjectProxy.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Binding {
+    
+    /// <summary>
+    /// Expose a low-level state related interface to managed database
+    /// objects. A proxy is a view with a small additional API to allow
+    /// modification of the underlying state.
+    /// </summary>
+    public interface IObjectProxy : IObjectView {
+        /// <summary>
+        /// Gets or sets the underlying "this handle".
+        /// </summary>
+        ObjectRef ThisHandle { get; set; }
+
+        /// <summary>
+        /// Binds the underlying object to the given address, object id
+        /// and type binding.
+        /// </summary>
+        /// <param name="addr">The objects (opaque) address.</param>
+        /// <param name="oid">The objects unique identity.</param>
+        /// <param name="typeBinding">The <see cref="TypeBinding"/> to
+        /// assign the underlying object.</param>
+        void Bind(ulong addr, ulong oid, TypeBinding typeBinding);
+    }
+}
