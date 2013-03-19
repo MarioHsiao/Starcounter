9bc3b2b03e087cefc06856a474959657122f19c6
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-03-19 11:21:36 +0100

    Renamed Message to TypedJson

--- src/Development/Starcounter.VS.CSMessageItemTemplate/Properties/AssemblyInfo.cs
+++ src/Development/Starcounter.VS.CSMessageItemTemplate/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Starcounter.VS.CSMessageItemTemplate")]
+[assembly: AssemblyTitle("Starcounter.VS.CSTypedJsonItemTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Starcounter.VS.CSMessageItemTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Starcounter.VS.CSTypedJsonItemTemplate")]
+[assembly: AssemblyCopyright("Copyright © Starcounter 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
