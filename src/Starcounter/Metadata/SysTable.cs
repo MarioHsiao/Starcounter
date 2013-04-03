7482b43b0a6f4f6055c295d0b1a2bbeb60563aa3
Per Samuelsson <per@starcounter.com>
2013-04-03 14:39:56 +0200

    Renamed the, internally used, Entity2 class to Entity, now that the public Entity class is gone.

--- src/Starcounter/Metadata/SysTable.cs
+++ src/Starcounter/Metadata/SysTable.cs
@@ -14,7 +14,7 @@ namespace Starcounter.Metadata {
     /// Class SysTable
     /// </summary>
     [Database]
-    public sealed class SysTable : Entity2 {
+    public sealed class SysTable : Entity {
         #region Infrastructure, reflecting what is emitted by the weaver.
 #pragma warning disable 0649, 0169
         internal sealed class __starcounterTypeSpecification {
