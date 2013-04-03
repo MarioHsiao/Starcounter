5cffee8852e7941088661827993c92d5ef950ce3
Per Samuelsson <per@starcounter.com>
2013-04-03 14:35:43 +0200

    Moved the NotPersistent custom attribute to its own file.

---
+++ src/Starcounter/NotPersistentAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Starcounter {
+    /// <summary>
+    /// Custom attribute that, when applied to a field in a database
+    /// class, makes the field "volatile", meaning it will not be stored
+    /// in the database.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class NotPersistentAttribute : Attribute {
+    }
+}
