1f64fbd97ad00d73144f37f1734fc9d85763f3dc
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-04-16 14:28:07 +0200

    Object identity: adding test of ObjectID.

--- src/Samples/QueryProcessingTest/ObjectIdentityTest.cs
+++ src/Samples/QueryProcessingTest/ObjectIdentityTest.cs
@@ -6,6 +6,12 @@ namespace QueryProcessingTest {
     public static class ObjectIdentityTest {
         public static void TestObjectIdentityInSQL() {
             HelpMethods.LogEvent("Test object identities in SQL");
+            var r = Db.SQL<string>("select objectid from account");
+            var e = r.GetEnumerator();
+            Trace.Assert(e.MoveNext());
+            string s = e.Current;
+            Trace.Assert(s != null && s != "");
+            Console.WriteLine(s);
             ulong n = Db.SQL<ulong>("select objectno from account").First;
             Trace.Assert(n > 0);
             HelpMethods.LogEvent("Finished testing object identities in SQL");
