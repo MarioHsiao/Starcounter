d037e9eefbe15ac4d1262c62854f3541f597cf00
Per Samuelsson <per@starcounter.com>
2013-04-17 12:33:30 +0200

    Added a first message type (ExecRequest) to the Starcounter.Server.Client project and set up project references correspondingly.

---
+++ src/Server/Starcounter.Server.Client/MessageTypes/ExecRequest.json.cs
@@ -0,0 +1,17 @@
+﻿
+using Starcounter;
+
+namespace Starcounter.Server.Client.MessageTypes {
+    /// <summary>
+    /// Represents a request to execute user code in a Starcounter
+    /// host code process.
+    /// </summary>
+    /// <remarks>
+    /// By design, the request to execute holds no representation
+    /// of what host/database it targets. Such information is assumed
+    /// to be given in addition to the actual request (for example,
+    /// in the form of a database URI when making a REST call).
+    /// </remarks>
+    partial class ExecRequest : Json {
+    }
+}
