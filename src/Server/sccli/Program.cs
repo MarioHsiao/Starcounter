48d9782f264656e75bb37532cc4f7bb0dbabc423
Per Samuelsson <per@starcounter.com>
2013-04-18 13:14:25 +0200

    Moved StarOption class to it's own file.

--- src/Server/sccli/Program.cs
+++ src/Server/sccli/Program.cs
@@ -18,25 +18,6 @@ using System.Threading.Tasks;
 namespace star {
 
     class Program {
-
-        static class StarOption {
-            public const string Help = "help";
-            public const string HelpEx = "helpextended";
-            public const string Version = "version";
-            public const string Info = "info";
-            public const string Serverport = "serverport";
-            public const string Server = SharedCLI.Option.Server;
-            public const string ServerHost = SharedCLI.Option.ServerHost;
-            public const string Db = SharedCLI.Option.Db;
-            public const string LogSteps = SharedCLI.Option.LogSteps;
-            public const string NoDb = SharedCLI.Option.NoDb;
-            public const string NoAutoCreateDb = SharedCLI.Option.NoAutoCreateDb;
-            public const string Verbosity = "verbosity";
-            public const string Syntax = "syntax";
-            public const string NoColor = "nocolor";
-            public const string ShowHttp = "http";
-            public const string AttatchCodeHostDebugger = "debug";
-        }
        
         static void Main(string[] args) {
             ApplicationArguments appArgs;
@@ -50,9 +31,6 @@ namespace star {
             }
 
             var syntax = DefineCommandLineSyntax();
-
-            // Parse and evaluate the given input
-
             var parser = new Parser(args);
             try {
                 appArgs = parser.Parse(syntax);
---
+++ src/Server/sccli/StarOptions.cs
@@ -0,0 +1,24 @@
+﻿
+using Starcounter.Server;
+
+namespace star {
+
+    static class StarOption {
+        public const string Help = "help";
+        public const string HelpEx = "helpextended";
+        public const string Version = "version";
+        public const string Info = "info";
+        public const string Serverport = "serverport";
+        public const string Server = SharedCLI.Option.Server;
+        public const string ServerHost = SharedCLI.Option.ServerHost;
+        public const string Db = SharedCLI.Option.Db;
+        public const string LogSteps = SharedCLI.Option.LogSteps;
+        public const string NoDb = SharedCLI.Option.NoDb;
+        public const string NoAutoCreateDb = SharedCLI.Option.NoAutoCreateDb;
+        public const string Verbosity = "verbosity";
+        public const string Syntax = "syntax";
+        public const string NoColor = "nocolor";
+        public const string ShowHttp = "http";
+        public const string AttatchCodeHostDebugger = "debug";
+    }
+}
