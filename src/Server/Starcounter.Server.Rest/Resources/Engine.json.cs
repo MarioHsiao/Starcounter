0daf4b5cf2073c4e238b7cf89fb4b6e613e9624c
Per Samuelsson <per@starcounter.com>
2013-04-23 11:05:19 +0200

    Added two new server resources to be exposed from the admin server REST API: the "Engine" resource (representing/abstracting a running engine, i.e. a set of processes together comprising a logical running database) and the "Executable" resource that abstracts an executable known by the server.

---
+++ src/Server/Starcounter.Server.Rest/Resources/Engine.json.cs
@@ -0,0 +1,10 @@
+﻿
+using Starcounter;
+
+namespace Starcounter.Server.Rest.Resources {
+    /// <summary>
+    /// Represents an engine resource.
+    /// </summary>
+    partial class Engine : Json {
+    }
+}
