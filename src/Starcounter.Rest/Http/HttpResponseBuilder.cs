fe1febe67850d50511d3849720ce7919dcd4bb32
Per Samuelsson <per@starcounter.com>
2013-04-24 16:32:38 +0200

    Changed HttpResponseBuilder.FromStatusHeadersAndStringContent to use a generic string dictionary instead of a name value collection for headers; it's faster, doesn't allow duplicates and is more 2013.

--- src/Starcounter.Rest/Http/HttpResponseBuilder.cs
+++ src/Starcounter.Rest/Http/HttpResponseBuilder.cs
@@ -36,7 +36,6 @@ namespace Starcounter.Internal.Web
     /// </summary>
     public static class Slow
     {
-
         /// <summary>
         /// Build a HTTP/1.1 response using a given status code, a set
         /// of headers and content given in the form of a <see cref="string"/>.
@@ -64,12 +63,11 @@ namespace Starcounter.Internal.Web
         /// transfer encoding is applied (i.e. it's uncompressed).</returns>
         public static byte[] FromStatusHeadersAndStringContent(
             int code,
-            NameValueCollection headers, 
+            Dictionary<string, string> headers,
             string content,
             Encoding contentEncoding = null,
             string contentType = "application/json"
-            )
-        {
+            ) {
             string reason;
             string msgHeader;
             string header;
@@ -88,9 +86,8 @@ namespace Starcounter.Internal.Web
             msgHeader += CRLF;
 
             if (headers != null) {
-                foreach (var key in headers.AllKeys) {
-                    var value = headers[key];
-                    header = string.Concat(key, ":", value, CRLF);
+                foreach (var pair in headers) {
+                    header = string.Concat(pair.Key, ":", pair.Value, CRLF);
                     msgHeader += header;
                 }
             }
