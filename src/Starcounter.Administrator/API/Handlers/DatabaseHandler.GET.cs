43fd5c8784ae3cf8101dbadb97a64cd5f73b4a97
Per Samuelsson <per@starcounter.com>
2013-04-24 16:36:32 +0200

    Added DatabaseHandler for the API subsystem, responsible for implementing the REST methods targeting a singleton database resource.

---
+++ src/Starcounter.Administrator/API/Handlers/DatabaseHandler.GET.cs
@@ -0,0 +1,41 @@
+﻿using Starcounter.Administrator.API.Utilities;
+using Starcounter.Advanced;
+using Starcounter.Server.PublicModel;
+using Starcounter.Server.Rest.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    internal static partial class DatabaseHandler {
+        
+        /// <summary>
+        /// Handles a GET for this resource.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the database to return a representation of.</param>
+        /// <param name="request">
+        /// The REST request.</param>
+        /// <returns>The response to be sent back to the client.</returns>
+        static object OnGET(string name, Request request) {
+            var server = RootHandler.Host.Runtime;
+            var applicationDatabase = server.GetDatabaseByName(name);
+            if (applicationDatabase == null)
+                return 404;
+
+            var body = ToJSONDatabase(applicationDatabase).ToJson();
+            return RESTUtility.CreateJSONResponse(body);
+        }
+
+        static Database ToJSONDatabase(DatabaseInfo applicationDatabase) {
+            var admin = RootHandler.API;
+            var db = new Database();
+            db.DatabaseName = applicationDatabase.Name;
+            db.Engine.HRef = admin.FormatUri(admin.Uris.Engine, applicationDatabase.Name);
+            return db;
+        }
+    }
+}
---
+++ src/Starcounter.Administrator/API/Handlers/DatabaseHandler.cs
@@ -0,0 +1,23 @@
+﻿using Starcounter.Advanced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Administrator.API.Handlers {
+    /// <summary>
+    /// Provides the handlers for the admin server database resource.
+    /// </summary>
+    internal static partial class DatabaseHandler {
+        /// <summary>
+        /// Install handlers for the resource represented by this class and
+        /// performs custom setup.
+        internal static void Setup() {
+            var uri = RootHandler.API.Uris.Database;
+
+            Handle.GET<string, Request>(uri, OnGET);
+            RootHandler.Register405OnAllUnsupported(uri, new string[] { "GET" });
+        }
+    }
+}
