e58c4d0a4245d3213dc4ac341473963ced31f59e
Per Samuelsson <per@starcounter.com>
2013-04-24 10:34:57 +0200

    Added API RootHandler class to the collection of API resource handler set in the admin server.

---
+++ src/Starcounter.Administrator/API/Handlers/RootHandler.cs
@@ -0,0 +1,41 @@
+﻿
+using Starcounter.Server;
+using Starcounter.Server.PublicModel;
+using Starcounter.Server.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    /// <summary>
+    /// Provides handlers for REST calls targeting the admin server API
+    /// root resource (i.e. the principal API REST entrypoint).
+    /// </summary>
+    internal static partial class RootHandler {
+        /// <summary>
+        /// Provides a set of references to the currently running
+        /// admin server host. Used and shared by handlers when fullfilling
+        /// REST requests.
+        /// </summary>
+        public static class Host {
+            public static ServerEngine Engine { get; private set; }
+            public static IServerRuntime Runtime { get; private set; }
+            public static string ServerHost { get; private set; }
+            public static int ServerPort { get; private set; }
+
+            public static void Setup(
+                string serverHost,
+                int serverPort,
+                ServerEngine engine,
+                IServerRuntime runtime) {
+                Engine = engine;
+                Runtime = runtime;
+                ServerHost = serverHost;
+                ServerPort = serverPort;
+            }
+        }
+    }
+}
