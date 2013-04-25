beed3ffb4565b94f02332b3290016d8b25567e01
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-04-25 11:21:04 +0200

    Tests: added custom listener for Trace.Assert. Testing it in query processing test.

---
+++ src/Starcounter.Internal/ErrorHandling/TestTraceListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Starcounter.Internal.ErrorHandling {
+    public class TestTraceListener : TraceListener {
+        List<string> messages = new List<string>();
+        public TestTraceListener() : base() {
+        }
+
+        public TestTraceListener(String name)
+            : base(name) {
+        }
+
+        public override void Fail(string message) {
+            throw new Exception("Test assertion failure. " + message);
+        }
+
+        public override void Fail(string message, string detailMessage) {
+            Fail(message + " " + detailMessage);
+        }
+
+        public override void Write(string message) {
+            messages.Add(message);
+        }
+
+        public override void WriteLine(string message) {
+            messages.Add(message);
+        }
+    }
+}
