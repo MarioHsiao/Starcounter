3a68eb9b3bfe430489c05661ff2defae35491327
Per Samuelsson <per@starcounter.com>
2013-04-25 13:23:28 +0200

    Implemented DatabaseCollectionHandler.GET+OnGET.

--- src/Starcounter.Administrator/API/Handlers/DatabaseCollectionHandler.GET.cs
+++ src/Starcounter.Administrator/API/Handlers/DatabaseCollectionHandler.GET.cs
@@ -1,4 +1,6 @@
-﻿using Starcounter.Advanced;
+﻿using Starcounter.Administrator.API.Utilities;
+using Starcounter.Advanced;
+using Starcounter.Server.Rest.Resources;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,19 @@ namespace Starcounter.Administrator.API.Handlers {
         /// The REST request.</param>
         /// <returns>The response to be sent back to the client.</returns>
         static object OnGET(Request request) {
-            throw new NotImplementedException();
+            var server = RootHandler.Host.Runtime;
+            var applicationDatabases = server.GetDatabases();
+            var admin = RootHandler.API;
+
+            var result = new DatabaseCollection();
+            foreach (var applicationDatabase in applicationDatabases) {
+                var db = result.Databases.Add();
+                var uri = admin.FormatUri(admin.Uris.Database, applicationDatabase.Name);
+                db.DatabaseName = applicationDatabase.Name;
+                db.Uri = RootHandler.MakeAbsoluteUri(uri);
+            }
+
+            return RESTUtility.CreateJSONResponse(result.ToJson());
         }
     }
 }
