20e57caf3b98a40675235bf295e3081ff40980e6
Per Samuelsson <per@starcounter.com>
2013-04-25 13:48:44 +0200

    Implemented DatabaseCollectionHandler.POST+OnPOST in the simplest possible manner, and excluding proper error handling.

--- src/Starcounter.Administrator/API/Handlers/DatabaseCollectionHandler.POST.cs
+++ src/Starcounter.Administrator/API/Handlers/DatabaseCollectionHandler.POST.cs
@@ -1,4 +1,7 @@
-﻿using Starcounter.Advanced;
+﻿using Starcounter.Administrator.API.Utilities;
+using Starcounter.Advanced;
+using Starcounter.Server.PublicModel.Commands;
+using Starcounter.Server.Rest.Resources;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +18,38 @@ namespace Starcounter.Administrator.API.Handlers {
         /// The REST request.</param>
         /// <returns>The response to be sent back to the client.</returns>
         static object OnPOST(Request request) {
-            throw new NotImplementedException();
+            var engine = RootHandler.Host.Engine;
+            var runtime = RootHandler.Host.Runtime;
+            var admin = RootHandler.API;
+
+            var db = new Database();
+            db.PopulateFromJson(request.GetBodyStringUtf8_Slow());
+
+            var name = db.DatabaseName;
+            var command = new CreateDatabaseCommand(engine, name) {
+                EnableWaiting = true
+            };
+
+            var info = runtime.Execute(command);
+            if (command.EnableWaiting) {
+                info = runtime.Wait(info);
+            }
+
+            if (info.HasError) {
+                // Check for already existing database - return 200 with a reference.
+                // And map other errors.
+                // TODO:
+                throw new NotImplementedException();
+            }
+
+            // Assure we only return the neccessary bits (in accordance with with our
+            // REST API documentation) by recreating the database object and set them.
+            
+            db = new Database();
+            db.DatabaseName = name;
+            db.Uri = RootHandler.MakeAbsoluteUri(admin.Uris.Database, name);
+
+            return RESTUtility.CreateJSONResponse(db.ToJson(), 201);
         }
     }
 }
