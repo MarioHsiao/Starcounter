55732cb437c3bd9727a1786f792941c296ad4648
Per Samuelsson <per@starcounter.com>
2013-04-26 08:10:07 +0200

    Added the class EngineCollectionHandler providing the admin server functionality acting on the engine collection resource. POST still not properly implemented.

---
+++ src/Starcounter.Administrator/API/Handlers/EngineCollectionHandler.POST.cs
@@ -0,0 +1,22 @@
+﻿
+using Starcounter.Advanced;
+using System;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    internal static partial class EngineCollectionHandler {
+
+        /// <summary>
+        /// Handles a POST to this resource.
+        /// </summary>
+        /// <param name="request">
+        /// The REST request.</param>
+        /// <returns>The response to be sent back to the client.</returns>
+        static object OnPOST(Request request) {
+            var engine = RootHandler.Host.Engine;
+            var runtime = RootHandler.Host.Runtime;
+            throw new NotImplementedException();
+
+        }
+    }
+}
