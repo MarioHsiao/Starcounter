6291ccc37a9827474107063677de2c4d405b0451
Per Samuelsson <per@starcounter.com>
2013-04-26 08:10:07 +0200

    Added the class EngineCollectionHandler providing the admin server functionality acting on the engine collection resource. POST still not properly implemented.

---
+++ src/Starcounter.Administrator/API/Handlers/EngineCollectionHandler.GET.cs
@@ -0,0 +1,32 @@
+﻿
+using Starcounter.Administrator.API.Utilities;
+using Starcounter.Advanced;
+using Starcounter.Server.Rest.Resources;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    internal static partial class EngineCollectionHandler {
+        /// <summary>
+        /// Handles a GET for this resource.
+        /// </summary>
+        /// <param name="request">
+        /// The REST request.</param>
+        /// <returns>The response to be sent back to the client.</returns>
+        static object OnGET(Request request) {
+            var server = RootHandler.Host.Runtime;
+            var applicationDatabases = server.GetDatabases();
+            var admin = RootHandler.API;
+
+            var result = new EngineCollection();
+            foreach (var db in applicationDatabases) {
+                if (db.HostProcessId != 0) {
+                    var engine = result.Engines.Add();
+                    engine.Name = db.Name;
+                    engine.Uri = RootHandler.MakeAbsoluteUri(admin.Uris.Engine, db.Name);
+                }
+            }
+
+            return RESTUtility.CreateJSONResponse(result.ToJson());
+        }
+    }
+}
---
+++ src/Starcounter.Administrator/API/Handlers/EngineCollectionHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Starcounter.Advanced;
+using Starcounter.Server.Rest;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    /// <summary>
+    /// Excapsulates the admin server functionality acting on the
+    /// engine collection resource. 
+    /// </summary>
+    internal static partial class EngineCollectionHandler {
+        /// <summary>
+        /// Install handlers for the resource represented by this class and
+        /// performs custom setup.
+        internal static void Setup() {
+            var uri = RootHandler.API.Uris.Engines;
+            
+            Handle.POST<Request>(uri, OnPOST);
+            Handle.POST<Request>(uri, OnGET);
+            RootHandler.Register405OnAllUnsupported(uri, new string[] { "GET", "POST" });
+        }
+    }
+}
