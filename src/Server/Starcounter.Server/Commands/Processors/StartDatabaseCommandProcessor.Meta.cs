063bad796df22120c2f8951e3b16431203a2cddd
Per Samuelsson <per@starcounter.com>
2013-04-27 20:59:09 +0200

    Added the task- and progress backing constructs for the StartDatabaseCommand and it's default processor.

---
+++ src/Server/Starcounter.Server/Commands/Processors/StartDatabaseCommandProcessor.Meta.cs
@@ -0,0 +1,47 @@
+﻿
+using Starcounter.Server.PublicModel;
+using Starcounter.Server.PublicModel.Commands;
+
+namespace Starcounter.Server.Commands.Processors {
+
+    internal sealed partial class StartDatabaseCommandProcessor : CommandProcessor {
+
+        public static int ProcessorToken = CreateToken(typeof(StartDatabaseCommandProcessor));
+
+        public static CommandDescriptor MakeDescriptor() {
+            return new CommandDescriptor() {
+                ProcessorToken = ProcessorToken,
+                CommandDescription = "Starts a database engine.",
+                Tasks = new TaskInfo[] { 
+                    Task.StartDatabaseProcess.ToPublicModel(), 
+                    Task.StartCodeHostProcess.ToPublicModel(),
+                    Task.AwaitCodeHostOnline.ToPublicModel(),
+                }
+            };
+        }
+
+        private static class Task {
+
+            public static readonly CommandTask StartDatabaseProcess = new CommandTask(
+                StartDatabaseCommand.DefaultProcessor.Tasks.StartDatabaseProcess,
+                "Starting database process",
+                TaskDuration.ShortIndeterminate,
+                "Starts the database process. The task is marked as cancelled if the process is found already running."
+                );
+
+            internal static readonly CommandTask StartCodeHostProcess = new CommandTask(
+                StartDatabaseCommand.DefaultProcessor.Tasks.StartCodeHostProcess,
+                "Starting code host process",
+                TaskDuration.ShortIndeterminate,
+                "Starts the code host process. The task is marked as cancelled if the process is found already running."
+                );
+
+            internal static readonly CommandTask AwaitCodeHostOnline = new CommandTask(
+                StartDatabaseCommand.DefaultProcessor.Tasks.AwaitCodeHostOnline,
+                "Assuring code host is ready for service",
+                TaskDuration.NormalIndeterminate,
+                "Awaits the code host boot sequence to get to the point where it accepts requests."
+                );
+        }
+    }
+}
