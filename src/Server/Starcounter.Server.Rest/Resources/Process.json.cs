d3b2b4b56f30adeae18f741ee2baa40c9657480e
Per Samuelsson <per@starcounter.com>
2013-04-28 15:22:42 +0200

    Added a JSON representation of process resource to the admin server REST API. By defining a generic process resource, we can allow for general processing manipulation throughout the API. This can be very useful, for example to support the starting/restarting/stopping of only a single process part of a larger abstraction, such as the Engine.

---
+++ src/Server/Starcounter.Server.Rest/Resources/Process.json.cs
@@ -0,0 +1,11 @@
+﻿
+using Starcounter;
+
+namespace Starcounter.Server.Rest.Resources {
+    /// <summary>
+    /// Provides the JSON representation of a process
+    /// resource.
+    /// </summary>
+    partial class Process : Json {
+    }
+}
