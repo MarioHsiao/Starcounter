d26d3c109430071d7025077895364fbd0a4ce712
Per Samuelsson <per@starcounter.com>
2013-04-29 11:56:23 +0200

    Added the EngineHandler class to admin server API and it's collection of resource handler classes. This class governs the functionality supporting all interactions with Engine resources.

---
+++ src/Starcounter.Administrator/API/Handlers/EngineHandler.DbProc.GET.cs
@@ -0,0 +1,38 @@
+﻿
+using Starcounter.Administrator.API.Utilities;
+using Starcounter.Advanced;
+using Starcounter.Server.Rest.Resources;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    internal static partial class EngineHandler {
+        /// <summary>
+        /// Handles a GET for the referenced database process resource.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the database whose engine the client is asking
+        /// to GET the database process for.</param>
+        /// <param name="request">
+        /// The REST request.</param>
+        /// <returns>The response to be sent back to the client.</returns>
+        static object OnDatabaseProcessGET(string name, Request request) {
+            var server = RootHandler.Host.Runtime;
+            var applicationDatabase = server.GetDatabaseByName(name);
+            if (applicationDatabase == null) {
+                var errDetail = RESTUtility.JSON.CreateError(Error.SCERRDATABASENOTFOUND);
+                return RESTUtility.CreateJSONResponse(errDetail.ToJson(), 404);
+            }
+
+            if (applicationDatabase.DatabaseProcessRunning) {
+                var host = new Engine.DatabaseProcessApp();
+                host.Uri = RootHandler.MakeAbsoluteUri(uriTemplateDbProcess, name);
+                host.Running = true;
+                return RESTUtility.CreateJSONResponse(host.ToJson());
+            }
+
+            // We return an empty 404 to indicate we didn't find the process,
+            // semantically equivalent to it is not running.
+            return 404;
+        }
+    }
+}
---
+++ src/Starcounter.Administrator/API/Handlers/EngineHandler.cs
@@ -0,0 +1,62 @@
+﻿using Starcounter.Advanced;
+using Starcounter.Server.PublicModel;
+using Starcounter.Server.Rest.Resources;
+
+namespace Starcounter.Administrator.API.Handlers {
+    /// <summary>
+    /// Provides the handlers for the admin server database
+    /// engine resource.
+    /// </summary>
+    internal static partial class EngineHandler {
+        static string uriTemplate;
+        static string uriTemplateHostProcess;
+        static string uriTemplateDbProcess;
+
+        internal static class JSON {
+            /// <summary>
+            /// Creates a JSON representation of the given application level
+            /// database state object.
+            /// </summary>
+            /// <param name="state">The database whose semantics the JSON
+            /// representation represents.</param>
+            /// <returns>A strongly types JSON <see cref="Engine"/> instance.
+            /// </returns>
+            internal static Engine CreateRepresentation(DatabaseInfo state) {
+                var engine = new Engine();
+                var name = state.Name;
+
+                engine.Uri = RootHandler.MakeAbsoluteUri(uriTemplate, name);
+                engine.Database.Name = name;
+                engine.Database.Uri = RootHandler.MakeAbsoluteUri(RootHandler.API.Uris.Database, name);
+                engine.CodeHostProcess.Uri = RootHandler.MakeAbsoluteUri(uriTemplateHostProcess, name);
+                engine.CodeHostProcess.PID = state.HostProcessId;
+                engine.DatabaseProcess.Uri = RootHandler.MakeAbsoluteUri(uriTemplateDbProcess, name);
+                engine.DatabaseProcess.Running = state.DatabaseProcessRunning;
+
+                return engine;
+            }
+        }
+
+        /// <summary>
+        /// Install handlers for the resource represented by this class and
+        /// performs custom setup.
+        /// </summary>
+        internal static void Setup() {
+            uriTemplate = RootHandler.API.Uris.Engine;
+            uriTemplateHostProcess = uriTemplate + "/host";
+            uriTemplateDbProcess = uriTemplate + "/db";
+
+            // Handlers for the larger engine resource/abstraction
+            Handle.GET<string, Request>(uriTemplate, OnGET);
+            Handle.DELETE<string, Request>(uriTemplate, OnDELETE);
+            RootHandler.Register405OnAllUnsupported(uriTemplate, new string[] { "GET", "DELETE" });
+
+            // Handlers for referenced primary processes
+            Handle.GET<string, Request>(uriTemplateHostProcess, OnHostGET);
+            Handle.DELETE<string, Request>(uriTemplateHostProcess, OnHostDELETE);
+            RootHandler.Register405OnAllUnsupported(uriTemplateHostProcess, new string[] { "GET", "DELETE" });
+            Handle.GET<string, Request>(uriTemplateDbProcess, OnDatabaseProcessGET);
+            RootHandler.Register405OnAllUnsupported(uriTemplateDbProcess, new string[] { "GET" });
+        }
+    }
+}
