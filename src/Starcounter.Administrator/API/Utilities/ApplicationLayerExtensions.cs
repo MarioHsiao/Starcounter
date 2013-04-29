c32bd6cab7d45743bee756d5740420eb60eec79e
Per Samuelsson <per@starcounter.com>
2013-04-29 15:01:12 +0200

    Added class ApplicationLayerExtensions to the admin server implementation, that provides utility methods to work with application layer constructs (i.e. the server library model).

---
+++ src/Starcounter.Administrator/API/Utilities/ApplicationLayerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Starcounter.Server.PublicModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Administrator.API.Utilities {
+    /// <summary>
+    /// Provides a set of utility methods that extends the application
+    /// layer, promoting code sharing inside the REST API.
+    /// </summary>
+    internal static class ApplicationLayerExtensions {
+
+        public static bool HasNoDbSwitch(this DatabaseInfo db) {
+            var args = db.CodeHostArguments;
+            return string.IsNullOrEmpty(args) ? false : ContainsFlag(args, "NoDb");
+        }
+
+        public static bool HasLogStepsSwitch(this DatabaseInfo db) {
+            var args = db.CodeHostArguments;
+            return string.IsNullOrEmpty(args) ? false : ContainsFlag(args, "LogSteps");
+        }
+
+        static bool ContainsFlag(string arguments, string flag) {
+            var compare = StringComparison.InvariantCultureIgnoreCase;
+            var candidates = new string[] { "--FLAG:" + flag, "--" + flag };
+            foreach (var candidate in candidates) {
+                if (arguments.IndexOf(candidate, compare) != -1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
