3df552a2873030dc6682337626c8d89e2516c27a
Per Samuelsson <per@starcounter.com>
2013-04-30 13:38:45 +0200

    Added support for server library processors to assign an exit code upon completion. This allows for an model that allows logical errors that is not solely based on exceptions.

--- src/Server/Starcounter.Server/PublicModel/CommandInfo.cs
+++ src/Server/Starcounter.Server/PublicModel/CommandInfo.cs
@@ -130,6 +130,15 @@ namespace Starcounter.Server.PublicModel {
         }
 
         /// <summary>
+        /// Gets the exit code of the command, or null if no
+        /// exit code was provided by the command.
+        /// </summary>
+        public int? ExitCode {
+            get;
+            internal set;
+        }
+
+        /// <summary>
         /// Gets a possible outcome from the processor. There
         /// should never be a result unless the command is considered
         /// completed (successfully or erred).
