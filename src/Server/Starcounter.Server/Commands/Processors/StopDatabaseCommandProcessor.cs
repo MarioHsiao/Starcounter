f75bf5d630da29dc7e4a9d39dd2ad3de2c816d37
Per Samuelsson <per@starcounter.com>
2013-04-30 14:10:11 +0200

    Extended StopDatabase command processor of the server library to support the returning of results (snapshots from a thread-safe context) and allowing fingerprints to make conditional commands possible.

--- src/Server/Starcounter.Server/Commands/Processors/StopDatabaseCommandProcessor.cs
+++ src/Server/Starcounter.Server/Commands/Processors/StopDatabaseCommandProcessor.cs
@@ -6,6 +6,7 @@
 
 using System;
 using Starcounter.Server.PublicModel.Commands;
+using Starcounter.Server.PublicModel;
 
 namespace Starcounter.Server.Commands.Processors {
     
@@ -23,13 +24,30 @@ namespace Starcounter.Server.Commands.Processors {
 
         /// <inheritdoc />
         protected override void Execute() {
+            DatabaseInfo result;
             StopDatabaseCommand command = (StopDatabaseCommand)this.Command;
             Database database;
             bool stopped;
-
+            
             if (!this.Engine.Databases.TryGetValue(command.Name, out database)) {
                 throw ErrorCode.ToException(Error.SCERRDATABASENOTFOUND, string.Format("Database: '{0}'.", command.DatabaseUri));
             }
+
+            // Implement conditional execution by using fingerprinting. We map
+            // fingerprints to a running engine, meaning that the presence of a
+            // given fingerprint on the command has the implicit semantics of
+            // a running engine with that identity. The match performed is a
+            // equality match.
+            if (!string.IsNullOrWhiteSpace(command.Fingerprint)) {
+                result = Engine.CurrentPublicModel.GetDatabase(command.DatabaseUri);
+                if (result.Engine == null || !result.Engine.Fingerprint.Equals(command.Fingerprint)) {
+                    this.SetResult(result, (int)Error.SCERRCOMMANDFINGERPRINTCOMPARE);
+                    return;
+                }
+            }
+
+            // Verify any fingerprint, and if it's a not match, don't do anything,
+            // including not touching the model.
 
             WithinTask(Task.StopCodeHostProcess, (task) => {
                 stopped = Engine.DatabaseEngine.StopCodeHostProcess(database);
@@ -43,7 +61,8 @@ namespace Starcounter.Server.Commands.Processors {
                 });
             }
             
-            Engine.CurrentPublicModel.UpdateDatabase(database);
+            result = Engine.CurrentPublicModel.UpdateDatabase(database);
+            SetResult(result);
         }
     }
 }
