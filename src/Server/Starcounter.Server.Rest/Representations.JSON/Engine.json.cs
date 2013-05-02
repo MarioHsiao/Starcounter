7f0de30a3c05236c42984f76f36320aae3c77e7b
Per Samuelsson <per@starcounter.com>
2013-05-02 11:52:26 +0200

    Created method Engine.GetExecutable to allow retreival of hosted executables by their path.

--- src/Server/Starcounter.Server.Rest/Representations.JSON/Engine.json.cs
+++ src/Server/Starcounter.Server.Rest/Representations.JSON/Engine.json.cs
@@ -2,9 +2,21 @@
 using Starcounter;
 
 namespace Starcounter.Server.Rest.Representations.JSON {
+    using ExecutableReference = Engine.ExecutablesApp.ExecutingApp;
+
     /// <summary>
     /// Represents an engine resource.
     /// </summary>
     partial class Engine : Json {
+
+        public ExecutableReference GetExecutable(string exePath) {
+            for (int i = 0; i < this.Executables.Executing.Count; i++) {
+                var exe = this.Executables.Executing[i];
+                if (exe.Path.Equals(exePath)) {
+                    return exe;
+                }
+            }
+            return null;
+        }
     }
 }
