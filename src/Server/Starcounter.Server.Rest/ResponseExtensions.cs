5c689019174799ef1e8155a069b5d165db221c96
Per Samuelsson <per@starcounter.com>
2013-05-02 10:18:56 +0200

    Added a set of utility methods / extension for the Node- and the Response class.

---
+++ src/Server/Starcounter.Server.Rest/ResponseExtensions.cs
@@ -0,0 +1,35 @@
+﻿
+using Starcounter.Advanced;
+
+namespace Starcounter.Server.Rest {
+    /// <summary>
+    /// Provides a set of extension methods for the <see cref="Response"/>
+    /// class.
+    /// </summary>
+    /// <remarks>
+    /// These are not server specific; consider moving them to a lower level
+    /// in the hierarchy.
+    /// </remarks>
+    public static class ResponseExtensions {
+        public static bool IsSuccessOr(this Response response, params int[] codes) {
+            if (response.IsSuccessStatusCode)
+                return true;
+
+            foreach (var code in codes) {
+                if (code == response.StatusCode)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsAnyOf(this Response response, params int[] codes) {
+            foreach (var code in codes) {
+                if (code == response.StatusCode)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
