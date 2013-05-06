2865cebf37b8af73119e69c7f34ca3de05650dd8
Per Samuelsson <per@starcounter.com>
2013-05-06 16:44:22 +0200

    Implemented the actual service provided by the DatabaseEngineMonitor, now properly reacting to exiting code host processes and updating the server state accordingly.

--- src/Server/Starcounter.Server/CoreServices/DatabaseEngineMonitor.cs
+++ src/Server/Starcounter.Server/CoreServices/DatabaseEngineMonitor.cs
@@ -1,7 +1,10 @@
 ﻿
+using Starcounter.Internal;
 using Starcounter.Logging;
 using Starcounter.Server.Commands;
+using Starcounter.Server.PublicModel;
 using Starcounter.Server.PublicModel.Commands;
+using System;
 using System.Diagnostics;
 
 namespace Starcounter.Server {
@@ -10,8 +13,8 @@ namespace Starcounter.Server {
     /// that exit.
     /// </summary>
     internal sealed class DatabaseEngineMonitor {
-        LogSource log = ServerLogSources.Default;
-
+        readonly LogSource log = ServerLogSources.Default;
+        
         /// <summary>
         /// Gets the server that has instantiated this monitor.
         /// </summary>
@@ -40,26 +43,39 @@ namespace Starcounter.Server {
         /// <param name="engineProcess">The process to begin monitoring.</param>
         internal void BeginMonitoring(Database database, Process engineProcess) {
             var description = string.Format("Synchronizing server state for exiting engine \"{0}\"", database.Name);
-            log.Debug("Begin monitoring engine process {0}, PID {1}, running database {2}", engineProcess.ProcessName, engineProcess.Id, database.Name);
 
-            engineProcess.EnableRaisingEvents = true;
-            engineProcess.Exited += (sender, args) => {
-                log.Debug("Detected exiting of engine process {0}, PID {1}, running database {2}", engineProcess.ProcessName, engineProcess.Id, database.Name);
-                var x = new ActionCommand<Process, Database>(this.Server, ReactToProcessExit, engineProcess, database, description);
-                this.Server.CurrentPublicModel.Execute(x);
-            };
+            if (engineProcess.ProcessName.Equals(
+                StarcounterConstants.ProgramNames.ScCode, StringComparison.InvariantCultureIgnoreCase)) {
+                log.Debug("Begin monitoring code host process {0}, PID {1}, running database {2}", engineProcess.ProcessName, engineProcess.Id, database.Name);
+                engineProcess.EnableRaisingEvents = true;
+                engineProcess.Exited += (sender, args) => {
+                    log.Debug("Detected exiting of code host {0}, PID {1}, running database {2}", engineProcess.ProcessName, engineProcess.Id, database.Name);
+                    var x = new ActionCommand<Process, Database>(this.Server, ReactToCodeHostExit, engineProcess, database, description);
+                    this.Server.CurrentPublicModel.Execute(x);
+                };
+            }
         }
 
-        void ReactToProcessExit(ICommandProcessor processor, Process processExited, Database database) {
-            log.Debug(
-                "Updating state due to the exiting of engine process {0}, PID {1}, running database {2}", processExited.ProcessName, processExited.Id, database.Name);
-            
-            // Implement the monitoring behaviour
-            // TODO:
+        void ReactToCodeHostExit(ICommandProcessor processor, Process processExited, Database database) {
+            log.Debug("Updating state due to the exiting of code host process {0}, PID {1}, running database {2}", 
+                processExited.ProcessName, processExited.Id, database.Name);
 
-            // Finally, update the database public state
-            // TODO:
-            // Server.CurrentPublicModel.UpdateDatabase(database);
+            if (!database.SupposedToBeStarted || (database.CodeHostProcess != null && database.CodeHostProcess.Id != processExited.Id)) {
+                log.Debug("Ignoring state update for code host with PID {0}; state already up-to-date.", processExited.Id);
+                return;
+            }
+
+            // Update the state to reflect the unexpected exit of the
+            // process. It's a bit unclear how we should treat the flag
+            // SupposedToBeStarted - it could be advocated both that it
+            // should be cleared and should be kept intact.
+
+            database.Apps.Clear();
+            database.CodeHostProcess = null;
+            database.SupposedToBeStarted = false;
+            processExited.Close();
+
+            Server.CurrentPublicModel.UpdateDatabase(database);
         }
     }
 }
