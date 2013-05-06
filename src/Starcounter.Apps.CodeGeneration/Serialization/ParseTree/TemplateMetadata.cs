984baa031af45781792f599f8a80ee1f8dae2662
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-05-06 10:48:11 +0200

    Partial commit: Refactoring in serialization generation code.

---
+++ src/Starcounter.Apps.CodeGeneration/Serialization/ParseTree/TemplateMetadata.cs
@@ -0,0 +1,37 @@
+﻿// ***********************************************************************
+// <copyright file="RequestProcessorMetaData.cs" company="Starcounter AB">
+//     Copyright (c) Starcounter AB.  All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using System.Collections.Generic;
+using System;
+using Starcounter.Templates;
+
+namespace Starcounter.CodeGeneration.Serialization {
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class TemplateMetadata  {
+        /// <summary>
+        /// The template index.
+        /// </summary>
+        internal int TemplateIndex { get { return Template.TemplateIndex } }
+
+        /// <summary>
+        /// The name of the template.
+        /// </summary>
+        internal string TemplateName { get { return Template.PropertyName; } }
+
+       /// <summary>
+        /// The Template.
+        /// </summary>
+        internal Template Template;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal Int32 HandlerId;
+    }
+
+}
