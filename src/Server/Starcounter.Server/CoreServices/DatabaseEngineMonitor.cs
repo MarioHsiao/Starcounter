9d48c14119864f05a2cc2eda39898afc48d993b9
Per Samuelsson <per@starcounter.com>
2013-05-06 15:35:11 +0200

    Added some debug logging to the engine monitor to be able to closely follow its execution.

--- src/Server/Starcounter.Server/CoreServices/DatabaseEngineMonitor.cs
+++ src/Server/Starcounter.Server/CoreServices/DatabaseEngineMonitor.cs
@@ -1,4 +1,5 @@
 ﻿
+using Starcounter.Logging;
 using Starcounter.Server.Commands;
 using Starcounter.Server.PublicModel.Commands;
 using System.Diagnostics;
@@ -9,6 +10,8 @@ namespace Starcounter.Server {
     /// that exit.
     /// </summary>
     internal sealed class DatabaseEngineMonitor {
+        LogSource log = ServerLogSources.Default;
+
         /// <summary>
         /// Gets the server that has instantiated this monitor.
         /// </summary>
@@ -37,17 +40,26 @@ namespace Starcounter.Server {
         /// <param name="engineProcess">The process to begin monitoring.</param>
         internal void BeginMonitoring(Database database, Process engineProcess) {
             var description = string.Format("Synchronizing server state for exiting engine \"{0}\"", database.Name);
-            
+            log.Debug("Begin monitoring engine process {0}, PID {1}, running database {2}", engineProcess.ProcessName, engineProcess.Id, database.Name);
+
             engineProcess.EnableRaisingEvents = true;
             engineProcess.Exited += (sender, args) => {
+                log.Debug("Detected exiting of engine process {0}, PID {1}, running database {2}", engineProcess.ProcessName, engineProcess.Id, database.Name);
                 var x = new ActionCommand<Process, Database>(this.Server, ReactToProcessExit, engineProcess, database, description);
                 this.Server.CurrentPublicModel.Execute(x);
             };
         }
 
         void ReactToProcessExit(ICommandProcessor processor, Process processExited, Database database) {
+            log.Debug(
+                "Updating state due to the exiting of engine process {0}, PID {1}, running database {2}", processExited.ProcessName, processExited.Id, database.Name);
+            
             // Implement the monitoring behaviour
             // TODO:
+
+            // Finally, update the database public state
+            // TODO:
+            // Server.CurrentPublicModel.UpdateDatabase(database);
         }
     }
 }
