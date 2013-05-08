accb8a2ebe1a999cf2a6410620d555bcdf03e13a
Per Samuelsson <per@starcounter.com>
2013-05-08 09:43:29 +0200

    Added ExecutablesHandler class, implementing the code host functionality behind the code host "Executables"  management resource.

---
+++ src/Starcounter.Bootstrap/Management/ExecutablesHandler.cs
@@ -0,0 +1,30 @@
+﻿
+using Starcounter.Advanced;
+
+namespace Starcounter.Bootstrap.Management {
+    /// <summary>
+    /// Implements the code host functionality behind the code host "Executables"
+    /// management resource.
+    /// </summary>
+    internal static class ExecutablesHandler {
+
+        /// <summary>
+        /// Performs setup of the <see cref="ExecutablesHandler"/>.
+        /// </summary>
+        internal static void Setup() {
+            var uri = CodeHostAPI.Uris.Host;
+            var port = ManagementService.Port;
+
+            Handle.POST<Request>(port, uri, ExecutablesHandler.OnPOST);
+        }
+
+        static object OnPOST(Request request) {
+            if (ManagementService.Unavailable) {
+                return 503;
+            }
+
+            // Not implemented currently.
+            return 501;
+        }
+    }
+}
