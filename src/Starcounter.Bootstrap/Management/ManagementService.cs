aa78fa8ba64c7f7a425fac459d3204dbe75a630c
Per Samuelsson <per@starcounter.com>
2013-05-08 08:31:39 +0200

    Implemented the principal behaviour of the management service, except for registering handlers.

--- src/Starcounter.Bootstrap/Management/ManagementService.cs
+++ src/Starcounter.Bootstrap/Management/ManagementService.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Threading;
 
 namespace Starcounter.Bootstrap.Management {
     /// <summary>
@@ -12,15 +13,55 @@ namespace Starcounter.Bootstrap.Management {
     /// engines and code host processes.
     /// </remarks>
     static internal class ManagementService {
+        static ManualResetEvent shutdownEvent;
+
+        /// <summary>
+        /// Governs the availability of the management service.
+        /// </summary>
+        /// <remarks>
+        /// When the service is unavailable, all HTTP requests to any of
+        /// its  resources will return a 503. Normally, the service will
+        /// be unavailable during the bootstrap sequence of the code host
+        /// and after it has been shut down (and before the process exits
+        /// while handlers are still registered).
+        /// </remarks>
+        public static bool Unavailable { get; private set; }
+
+        /// <summary>
+        /// Gets a value that indicates if the current service is running
+        /// in the administrator host.
+        /// </summary>
+        public static bool IsAdministrator { get; private set; }
+        
         /// <summary>
         /// Initializes the management service.
         /// </summary>
-        /// <param name="port"></param>
-        /// <param name="databaseName"></param>
-        internal static void Setup(ushort port, string databaseName) {
-            // Setup all handlers we need.
-            // TODO:
-            throw new NotImplementedException();
+        /// <param name="port">The port all handlers should register under.</param>
+        /// <param name="hostIdentity">The identity of the host. Used when
+        /// constructing and registering management URIs.</param>
+        public static void Setup(ushort port, string hostIdentity) {
+            shutdownEvent = new ManualResetEvent(false);
+            Unavailable = true;
+            IsAdministrator = NewConfig.IsAdministratorApp;
+        }
+
+        /// <summary>
+        /// Instructs the service to shut down.
+        /// </summary>
+        public static void Shutdown() {
+            shutdownEvent.Set();
+        }
+
+        /// <summary>
+        /// Makes the management service functionality available to clients
+        /// and blocks the calling thread until a request indicates the service
+        /// should shut down (semantically eqivivalent to stopping the code
+        /// host).
+        /// </summary>
+        public static void RunUntilShutdown() {
+            Unavailable = false;
+            shutdownEvent.WaitOne();
+            Unavailable = true;
         }
     }
 }
