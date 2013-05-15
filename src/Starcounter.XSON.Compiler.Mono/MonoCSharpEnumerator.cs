31677fbd84f174152854ce7b1f36823b25d4d457
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-05-15 19:03:18 +0200

    Implementation of codebehind analyser using Mono.CSharp.

---
+++ src/Starcounter.XSON.Compiler.Mono/MonoCSharpEnumerator.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Mono.CSharp;
+
+namespace Starcounter.XSON.Compiler.Mono {
+    internal class MonoCSharpEnumerator {
+        private string code;
+        private Tokenizer tokenizer;
+        private CSharpToken currentToken;
+        private Stack<string> nsStack;
+        private Stack<string> classStack;
+        private Stack<CSharpToken> tokenStack;
+
+        internal MonoCSharpEnumerator(string filePath) {
+            code = File.ReadAllText(filePath);
+            currentToken = CSharpToken.UNDEFINED;
+            nsStack = new Stack<string>();
+            classStack = new Stack<string>();
+            tokenStack = new Stack<CSharpToken>();
+
+            CreateMonoTokenizer();
+        }
+
+        internal bool MoveNext() {
+            currentToken = (CSharpToken)tokenizer.token();
+            if (currentToken == CSharpToken.EOF) // EOF
+                currentToken = CSharpToken.UNDEFINED;
+            return (currentToken != CSharpToken.UNDEFINED);
+        }
+
+        internal string Value {
+            get {
+                return ((Tokenizer.LocatedToken)tokenizer.Value).Value;
+            }
+        }
+
+        internal CSharpToken Token {
+            get {
+                return (CSharpToken)currentToken;
+            }
+        }
+
+        internal CSharpToken Peek() {
+            return (CSharpToken)tokenizer.peek_token();
+        }
+
+        internal string LastFoundJsonAttribute { get; set; }
+
+        internal string CurrentNamespace {
+            get {
+                string ns = null;
+                foreach (string partialNs in nsStack) {
+                    if (ns == null)
+                        ns = partialNs;
+                    else
+                        ns += '.' + partialNs;
+                }
+                return ns;
+            }
+        }
+
+        internal string CurrentClass {
+            get {
+                return classStack.Peek();
+            }
+        }
+
+        internal string FullClassName {
+            get {
+                string fullClass = CurrentNamespace;
+                foreach (string partialClass in classStack) {
+                    if (fullClass == null)
+                        fullClass = partialClass;
+                    else
+                        fullClass += '.' + partialClass;
+                }
+                return fullClass;
+            }
+        }
+
+        internal List<string> ClassList {
+            get {
+                return classStack.ToList<string>();
+            }
+        }
+
+        internal void PushClass(string cn) {
+            tokenStack.Push(CSharpToken.CLASS);
+            classStack.Push(cn);
+        }
+
+        internal void PushNamespace(string ns) {
+            tokenStack.Push(CSharpToken.NAMESPACE);
+            nsStack.Push(ns);
+        }
+
+        internal void PushBlock() {
+            tokenStack.Push(CSharpToken.UNDEFINED);
+        }
+
+        internal void PopBlock() {
+            CSharpToken token = tokenStack.Pop();
+            if (token == CSharpToken.NAMESPACE)
+                nsStack.Pop();
+            else if (token == CSharpToken.CLASS)
+                classStack.Pop();
+        }
+
+        private void CreateMonoTokenizer() {
+            CompilerSettings settings = new CompilerSettings();
+            CompilerContext ctx = new CompilerContext(settings, new ConsoleReportPrinter());
+            ModuleContainer module = new ModuleContainer(ctx);
+            CompilationSourceFile file = new CompilationSourceFile(module, null);
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(code));
+            SeekableStreamReader seekable = new SeekableStreamReader(stream, Encoding.UTF8, null);
+            tokenizer = new Tokenizer(seekable, file, new ParserSession());
+            tokenizer.PropertyParsing = false;
+        }
+    }
+}
--- src/Starcounter.XSON/CodeBehindMetaData/CodeBehindMetadata.cs
+++ src/Starcounter.XSON/CodeBehindMetaData/CodeBehindMetadata.cs
@@ -19,59 +19,38 @@ namespace Starcounter.XSON.Metadata
         /// codebehind file.
         /// </summary>
         public static readonly CodeBehindMetadata Empty
-            = new CodeBehindMetadata("", null, false, new List<JsonMapInfo>(), new List<InputBindingInfo>());
+            = new CodeBehindMetadata();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeBehindMetadata" /> class.
+        /// </summary>
+        public CodeBehindMetadata() { }
 
         /// <summary>
         /// The root namespace of the main app.
         /// </summary>
-        public readonly String RootNamespace;
+        public String RootNamespace;
 
         /// <summary>
         /// Boolean telling if this app inherits from a generic App and the properties
         /// in the app should be automatically bound to the dataobject.
         /// </summary>
-        public readonly bool AutoBindToDataObject;
+        public bool AutoBindToDataObject;
 
         /// <summary>
         /// Contains the generic argument (if any) for the class.
         /// </summary>
-        public readonly string GenericArgument;
+        public string GenericArgument;
 
         /// <summary>
         /// A list of classes from the code-behind file that should be connected
         /// to the correct app in the generated code.
         /// </summary>
-        public readonly List<JsonMapInfo> JsonPropertyMapList;
+        public List<JsonMapInfo> JsonPropertyMapList = new List<JsonMapInfo>();
 
         /// <summary>
         /// A list of inputbindings which should be registered in the generated code.
         /// </summary>
-        public readonly List<InputBindingInfo> InputBindingList;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CodeBehindMetadata" /> class.
-        /// </summary>
-        /// <param name="ns">The root namespace</param>
-        /// <param name="genericArgument">
-        /// the generic argument if any of the class
-        /// </param>
-        /// <param name="autoBindToDataObject">
-        /// If true all properties in the json file should be bound 
-        /// to the underlying dataobject
-        /// </param>
-        /// <param name="mapList">The list of mappings</param>
-        /// <param name="inputList">The list of inputbindings.</param>
-        public CodeBehindMetadata(string ns,
-                                    string genericArgument,
-                                    bool autoBindToDataObject,
-                                    List<JsonMapInfo> mapList, 
-                                    List<InputBindingInfo> inputList)
-        {
-            RootNamespace = ns;
-            GenericArgument = genericArgument;
-            AutoBindToDataObject = autoBindToDataObject;
-            JsonPropertyMapList = mapList;
-            InputBindingList = inputList;
-        }
+        public List<InputBindingInfo> InputBindingList = new List<InputBindingInfo>();
     }
 }
