a12b8f189b4cf9934ae5ad98aad6a80710d553aa
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-05-16 12:30:56 +0200

    document file and (empty) comments.

--- src/Starcounter.Tests/TestDecimal.cs
+++ src/Starcounter.Tests/TestDecimal.cs
@@ -2,8 +2,14 @@
 using NUnit.Framework;
 
 namespace Starcounter.Tests {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class TestDecimal {
 
+        /// <summary>
+        /// 
+        /// </summary>
         [Test]
         public static void DecimalTest() {
             Assert.True(false);
