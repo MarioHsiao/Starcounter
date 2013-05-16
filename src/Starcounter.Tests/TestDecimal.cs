13abe284429523fa615e24e9c92431b8a369816f
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-05-16 12:23:48 +0200

    Added Starcounter.Tests test project.

---
+++ src/Starcounter.Tests/TestDecimal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Starcounter.Tests {
+    public static class TestDecimal {
+
+        [Test]
+        public static void DecimalTest() {
+            Assert.True(false);
+        }
+    }
+}
