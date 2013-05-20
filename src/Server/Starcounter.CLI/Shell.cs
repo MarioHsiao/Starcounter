9033d6ad018f81544d25b3e1cf1f56a0fb9e2cf1
Per Samuelsson <per@starcounter.com>
2013-05-20 11:06:51 +0200

    Added the Shell class to Starcounter.CLI, provides the CLI specifics used when an executable is being ran from the OS shell (and is to be hosted in Starcounter).

---
+++ src/Server/Starcounter.CLI/Shell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Starcounter.CLI {
+    /// <summary>
+    /// Provides the CLI specifics used when an executable is
+    /// being ran from the OS shell (e.g. by double-clicking the
+    /// executable).
+    /// </summary>
+    public static class Shell {
+        /// <summary>
+        /// Boots the executable that caused the current process to
+        /// launch in the Starcounter code host.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// If this method is executed inside the code host process,
+        /// it will silently return.
+        /// </para>
+        /// <para>
+        /// This is the call that will be weaved, during compile-time,
+        /// into executables that want to support shell bootstrapping.
+        /// </para>
+        /// </remarks>
+        [DebuggerNonUserCode]
+        public static void BootInHost() {
+            throw new NotImplementedException();
+        }
+    }
+}
