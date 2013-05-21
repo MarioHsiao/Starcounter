b9cf82e3f4ddc8e83586953d453ca5e2b3fd2baf
alemoi <alexey.moiseenko@starcounter.com>
2013-05-21 19:16:05 +0200

    Removed buillding Level1 BuildServerTest configuration from batch script.

--- src/scnetworkgateway/scripts/gw_test_bs_run_local.bat
+++ src/scnetworkgateway/scripts/gw_test_bs_run_local.bat
@@ -1,7 +1,6 @@
 :: Setting same environment variables as on the build server.
 SET SC_RUNNING_ON_BUILD_SERVER=True
 SET SC_RUN_GATEWAY_TESTS=True
-SET SC_RUN_TESTS_LOCALLY=True
 
 :: Starting main test script.
 gw_test_bs_run_all.bat
