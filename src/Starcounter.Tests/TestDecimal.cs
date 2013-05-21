2467e6b41e612a6d9f54506e664ab7235f4ccc34
andthy <andreas.thyrhaug@starcounter.com>
2013-05-21 16:05:01 +0200

    clr_decimal_conversion: uni test DllImport won't work.

--- src/Starcounter.Tests/TestDecimal.cs
+++ src/Starcounter.Tests/TestDecimal.cs
@@ -2,12 +2,9 @@
 using NUnit.Framework;
 
 namespace Starcounter.Tests {
- 	/// <summary>
-	/// Function used by DecimalTest().
-	/// </summary>
-	[DllImport("decimal_conversion.dll", CallingConvention = CallingConvention.StdCall)]
-	public unsafe extern static UInt32 clr_decimal_to_encoded_x6_decimal
-	(Int32* decimal_part_ptr, ref Int64 encoded_x6_decimal_ptr);
+	//[DllImport("decimal_conversion.dll", CallingConvention = CallingConvention.StdCall)]
+	//public unsafe extern static UInt32 clr_decimal_to_encoded_x6_decimal
+	//(Int32* decimal_part_ptr, ref Int64 encoded_x6_decimal_ptr);
 
 	/// <summary>
 	/// 
