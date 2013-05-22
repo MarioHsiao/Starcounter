213d05036bbeb6ccf18c42ecaa0da04295e2d0f6
andthy <andreas.thyrhaug@starcounter.com>
2013-05-22 09:53:07 +0200

    clr_decimal_conversion: fixed linking to decimal_conversion.dll in Starcounter.Tests/TestDecimal.cs

--- src/Starcounter.Tests/TestDecimal.cs
+++ src/Starcounter.Tests/TestDecimal.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 
 namespace Starcounter.Tests {
-	//[DllImport("decimal_conversion.dll", CallingConvention = CallingConvention.StdCall)]
-	//public unsafe extern static UInt32 clr_decimal_to_encoded_x6_decimal
-	//(Int32* decimal_part_ptr, ref Int64 encoded_x6_decimal_ptr);
-
 	/// <summary>
 	/// 
 	/// </summary>
 	public static class TestDecimal {
+		/// <summary>
+		/// Function used by TestDecimal().
+		/// </summary>
+		[DllImport("decimal_conversion.dll", CallingConvention = CallingConvention.StdCall)]
+		public unsafe extern static UInt32 clr_decimal_to_encoded_x6_decimal
+		(Int32* decimal_part_ptr, ref Int64 encoded_x6_decimal_ptr);
+		
 		/// <summary>
 		/// 
 		/// </summary>
