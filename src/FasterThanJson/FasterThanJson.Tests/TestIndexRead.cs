93572277377571f1338c77fec200a026c3f3e69b
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-05-30 10:41:30 +0200

    FasterThanJson: added index read of string values and simple test.

--- src/FasterThanJson/FasterThanJson.Tests/TestIndexRead.cs
+++ src/FasterThanJson/FasterThanJson.Tests/TestIndexRead.cs
@@ -34,5 +34,27 @@ namespace FasterThanJson.Tests {
                 Assert.AreEqual(13, readArray.ReadUInt(8));
             }
         }
+
+        [Test]
+        public unsafe void StringSimpleTest() {
+            fixed (byte* start = new byte[1024]) {
+                TupleWriter writeArray = new TupleWriter(start, 5, 2);
+                writeArray.Write("a");
+                writeArray.Write("I've verified that this has been fixed in the next branch. I will keep this issue open until we merged next into develop.");
+                writeArray.Write("AAAAAA");
+                writeArray.Write("");
+                writeArray.Write("AAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBcccccccccccccccccccccccccccccEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEdddddddddddddddddddddddZZZZZZZZZZZZZZZZZZZZZZ");
+                writeArray.SealTuple();
+
+                TupleReader readArray = new TupleReader(start, 5);
+                Assert.AreEqual("a", readArray.ReadString(0));
+                Assert.AreEqual("I've verified that this has been fixed in the next branch. I will keep this issue open until we merged next into develop.", readArray.ReadString(1));
+                Assert.AreEqual("AAAAAA", readArray.ReadString(2));
+                Assert.AreEqual("", readArray.ReadString(3));
+                Assert.AreEqual("AAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBcccccccccccccccccccccccccccccEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEdddddddddddddddddddddddZZZZZZZZZZZZZZZZZZZZZZ",
+                    readArray.ReadString(4));
+            }
+
+        }
     }
 }
