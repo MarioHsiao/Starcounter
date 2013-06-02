ed9bd786ff51bcc1a9fe31b9fa159f14a33e111c
alemoi <alexey.moiseenko@starcounter.com>
2013-06-02 17:13:47 +0200

    Adopted build system for creating installer wrapper.

--- src/Starcounter.Installer/Starcounter.InstallerWrapper/Starcounter.InstallerWrapper.cs
+++ src/Starcounter.Installer/Starcounter.InstallerWrapper/Starcounter.InstallerWrapper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -84,15 +85,10 @@ namespace Starcounter.InstallerWrapper
             }
             catch
             {
-                // Returning error.
-                return -1;
+                return 2;
             }
 
-            // Checking for the child error code explicitly.
-            if (exitCode != 0)
-                return exitCode;
-
-            return 0;
+            return exitCode;
         }
 
         /// <summary>
@@ -118,19 +114,55 @@ namespace Starcounter.InstallerWrapper
             return true;
         }
 
-        const String net45ResName = "dotnetfx45_full_x86_x64.exe";
-        const String scSetupResName = "Starcounter-Setup.exe";
+        /// <summary>
+        /// Check if another instance of setup is running.
+        /// </summary>
+        static Boolean AnotherSetupRunning()
+        {
+            // Trying to find through all processes.
+            Process[] processeslist = Process.GetProcesses();
+            foreach (Process process in processeslist)
+            {
+                if (process.ProcessName.StartsWith("Starcounter", StringComparison.CurrentCultureIgnoreCase) &&
+                    process.ProcessName.EndsWith("Setup", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    // Checking process IDs.
+                    if (Process.GetCurrentProcess().Id != process.Id)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        const String Net45ResName = "dotnetfx45_full_x86_x64.exe";
+        const String ScSetupVersion = "2.0.0.0";
+        const String ScSetupResName = "Starcounter-Setup.exe";
+        const String ScSetupExtractName = ScSetupResName; //"Starcounter-" + ScSetupVersion + "-Setup.exe";
 
         static void Main(string[] args)
         {
+            // Simply exiting if another setup is running.
+            if (AnotherSetupRunning())
+            {
+                MessageBox.Show("Another Starcounter setup instance is already running.",
+                    "Starcounter setup...", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                return;
+            }
+
             // Extracting everything to temp directory.
             String userTempDir = Environment.GetEnvironmentVariable("TEMP");
 
             // Checking if .NET 4.5 is installed.
             if (!IsNet45Installed())
             {
-                String net45ExePath = Path.Combine(userTempDir, net45ResName);
-                if (ExtractResourceToFile(net45ResName, net45ExePath))
+                MessageBox.Show("Microsoft .NET Framework 4.5 is not detected on your computer. It will be installed now.",
+                    "Starcounter setup...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                String net45ExePath = Path.Combine(userTempDir, Net45ResName);
+                if (ExtractResourceToFile(Net45ResName, net45ExePath))
                 {
                     if (0 == RunAndWaitForProgram(net45ExePath, ""))
                     {
@@ -145,19 +177,20 @@ namespace Starcounter.InstallerWrapper
 
 RUN_SC_INSTALLER:
 
-            String scSetupExePath = Path.Combine(userTempDir, scSetupResName);
+            String scSetupExePath = Path.Combine(userTempDir, ScSetupExtractName);
 
-            if (ExtractResourceToFile(scSetupResName, scSetupExePath))
+            // Extracting installer and starting it.
+            if (ExtractResourceToFile(ScSetupResName, scSetupExePath))
             {
+                // Skipping waiting for installer, just quiting.
                 if (0 == RunAndWaitForProgram(scSetupExePath, "", false))
-                {
                     return;
-                }
             }
 
 ERROR_OCCURIED:
 
-            MessageBox.Show("Sorry, Starcounter can't be installed, your environment is crap..");
+            MessageBox.Show("Starcounter was not successfully installed.",
+                "Starcounter setup...", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
