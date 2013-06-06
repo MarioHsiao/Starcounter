e05747f9dd8d0dcce2a9bce793cc221277385cd6
alemoi <alexey.moiseenko@starcounter.com>
2013-06-06 18:55:55 +0200

    Added flag to pack and upload artifacts.

--- src/BuildSystem/PackArtifactsAndUpload/PackArtifactsAndUpload.cs
+++ src/BuildSystem/PackArtifactsAndUpload/PackArtifactsAndUpload.cs
@@ -19,9 +19,11 @@ namespace PackArtifactsAndUpload
                 BuildSystem.PrintToolWelcome("Pack artifacts and upload them to FTP");
 
                 // Checking if its a nightly or a special build.
-                if ((!BuildSystem.IsNightlyBuild()) || (Environment.GetEnvironmentVariable(BuildSystem.UploadToUsFtp) != "True"))
+                if ((Environment.GetEnvironmentVariable(BuildSystem.PackAndUploadArtifacts) != "True") ||
+                    (Environment.GetEnvironmentVariable(BuildSystem.UploadToUsFtp) != "True") ||
+                    (!BuildSystem.IsNightlyBuild()))
                 {
-                    Console.WriteLine("Skipping generation of packed artifacts since its a standard or personal build...");
+                    Console.WriteLine("Skipping generation of packed artifacts...");
                     return 0;
                 }
 
