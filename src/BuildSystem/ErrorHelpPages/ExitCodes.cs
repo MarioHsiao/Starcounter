1a76140c24f1ea64b95544df98a105469167a51d
Per Samuelsson <per@starcounter.com>
2013-06-11 10:16:56 +0200

    Added ExitCodes class to ErrorHelpPages project, exposing  constants for all exit codes used by ErrorHelpPages.exe.

---
+++ src/BuildSystem/ErrorHelpPages/ExitCodes.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ErrorHelpPages {
+    /// <summary>
+    /// Defines the exit codes the program use.
+    /// </summary>
+    internal static class ExitCodes {
+        public const int WrongArguments = 1;
+    }
+}
