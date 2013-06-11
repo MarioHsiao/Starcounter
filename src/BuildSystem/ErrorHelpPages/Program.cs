f0569a6d9b9c4b77be77b390bb0852ed39621cb2
Per Samuelsson <per@starcounter.com>
2013-06-11 09:27:47 +0200

    Added the ErrorHelpPages project to the BuildSystemCode solution, and configured it propertly.

---
+++ src/BuildSystem/ErrorHelpPages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHelpPages {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
