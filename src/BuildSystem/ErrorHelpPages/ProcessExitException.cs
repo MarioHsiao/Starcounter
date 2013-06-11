abfcec6092e48be0c5e9aa21892d44202969cdbe
Per Samuelsson <per@starcounter.com>
2013-06-11 12:39:22 +0200

    Added ProcessExitException class, representing (here) a failing git command-line invocation.

---
+++ src/BuildSystem/ErrorHelpPages/ProcessExitException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHelpPages {
+
+    public class ProcessExitException : Exception {
+        public readonly int ExitCode;
+        public string ExeFileName { get; set; }
+        public string Arguments { get; set; }
+
+        public ProcessExitException(int exitCode) {
+            this.ExitCode = exitCode;
+        }
+
+        public override string ToString() {
+            return string.Format("{0} \"{1}\" exited with code {2}", Path.GetFileName(ExeFileName), Arguments, ExitCode);
+        }
+    }
+}
