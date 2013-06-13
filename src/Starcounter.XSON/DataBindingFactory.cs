61a55fc3b2d2f2907de999ab5cde1f92396197d0
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-06-13 13:25:10 +0200

    Codedocumentation on classes for databinding in typed json.

--- src/Starcounter.XSON/DataBindingFactory.cs
+++ src/Starcounter.XSON/DataBindingFactory.cs
@@ -10,12 +10,24 @@ using Starcounter.Logging;
 using Starcounter.Templates;
 
 namespace Starcounter.XSON {
+    /// <summary>
+    /// Internal class responsible for creating and verifying code that reads and writes values to and from
+    /// dataobjects. Used in typed json to create binding between json and data.
+    /// </summary>
     internal class DataBindingFactory {
         internal static bool ThrowExceptionOnBindindRecreation = false;
         private static LogSource logSource = new LogSource("json");
         private static string warning = "The existing databinding for '{0}' was created for another type of dataobject. Binding needs to be recreated.";
         private static string propNotFound = "Property '{2}' was not found in type '{3}' (or baseclass). Json property: '{0}.{1}'.";
 
+        /// <summary>
+        /// Verifies that the current cached binding can be used with the current dataobject. 
+        /// If the dataobject is of another type than the cached binding specifies it will be recreated
+        /// </summary>
+        /// <param name="template">The template.</param>
+        /// <param name="dataType">The type of the dataobject.</param>
+        /// <param name="bindingName">The name of the property in the dataobject to create (if needed) the binding for.</param>
+        /// <returns>An cached or created instance of <see cref="DataValueBinding{IEnumerable}"/></returns>
         internal static DataValueBinding<IEnumerable> VerifyOrCreateBinding(TObjArr template, Type dataType, string bindingName) {
             var binding = template.dataBinding;
             if (VerifyBinding(binding, dataType, template))
@@ -27,6 +39,14 @@ namespace Starcounter.XSON {
             return binding;
         }
 
+        /// <summary>
+        /// Verifies that the current cached binding can be used with the current dataobject. 
+        /// If the dataobject is of another type than the cached binding specifies it will be recreated
+        /// </summary>
+        /// <param name="template">The template.</param>
+        /// <param name="dataType">The type of the dataobject.</param>
+        /// <param name="bindingName">The name of the property in the dataobject to create (if needed) the binding for.</param>
+        /// <returns>An cached or created instance of <see cref="DataValueBinding{IBindable}"/></returns>
         internal static DataValueBinding<IBindable> VerifyOrCreateBinding(TObj template, Type dataType, string bindingName) {
             var binding = template.dataBinding;
             if (VerifyBinding(binding, dataType, template))
@@ -38,6 +58,15 @@ namespace Starcounter.XSON {
             return binding;
         }
 
+        /// <summary>
+        /// Verifies that the current cached binding can be used with the current dataobject. 
+        /// If the dataobject is of another type than the cached binding specifies it will be recreated
+        /// </summary>
+        /// <typeparam name="TVal">The primitive instance type of the template.</typeparam>
+        /// <param name="template">The template.</param>
+        /// <param name="dataType">The type of the dataobject.</param>
+        /// <param name="bindingName">The name of the property in the dataobject to create (if needed) the binding for.</param>
+        /// <returns>An cached or created instance of <see cref="DataValueBinding{TVal}"/></returns></returns>
         internal static DataValueBinding<TVal> VerifyOrCreateBinding<TVal>(TValue<TVal> template, Type dataType, string bindingName) {
             var binding = template.dataBinding;
             if (VerifyBinding(binding, dataType, template))
@@ -49,6 +78,14 @@ namespace Starcounter.XSON {
             return binding;
         }
 
+        /// <summary>
+        /// Returns the property with the specified name from the data type. If not found an exception 
+        /// is thrown.
+        /// </summary>
+        /// <param name="dataType"></param>
+        /// <param name="bindingName"></param>
+        /// <param name="template"></param>
+        /// <returns></returns>
         private static PropertyInfo GetPropertyForBinding(Type dataType, string bindingName, Template template) {
             var pInfo = dataType.GetProperty(bindingName, BindingFlags.Instance | BindingFlags.Public);
             if (pInfo == null) {
@@ -63,6 +100,13 @@ namespace Starcounter.XSON {
             return pInfo;
         }
 
+        /// <summary>
+        /// Verifies that the existing binding can be used for the current datatype and template.
+        /// </summary>
+        /// <param name="binding"></param>
+        /// <param name="dataType"></param>
+        /// <param name="template"></param>
+        /// <returns>True if the binding can be used, false if not or binding is null</returns>
         private static bool VerifyBinding(DataValueBinding binding, Type dataType, Template template) {
             if (binding != null) {
                 if (dataType.Equals(binding.DataType) || dataType.IsSubclassOf(binding.DataType)) {
@@ -77,6 +121,11 @@ namespace Starcounter.XSON {
             return false;
         }
 
+        /// <summary>
+        /// Used when exception is raised to get the correct classname for the template.
+        /// </summary>
+        /// <param name="template"></param>
+        /// <returns></returns>
         internal static string GetParentClassName(Template template) {
             string className = null;
             if (template.Parent is TObj)
@@ -91,6 +140,9 @@ namespace Starcounter.XSON {
         }
     }
 
+    /// <summary>
+    /// Baseclass for bindings.
+    /// </summary>
     internal abstract class DataValueBinding {
         protected Type dataType;
         private Template template;
@@ -106,6 +158,11 @@ namespace Starcounter.XSON {
         internal PropertyInfo Property { get { return property; } }
     }
 
+    /// <summary>
+    /// Generic class for bindings to a specific type. Creates code that gets and sets the
+    /// values to and from dataobject.
+    /// </summary>
+    /// <typeparam name="TVal"></typeparam>
     internal class DataValueBinding<TVal> : DataValueBinding {
         private static MethodInfo dateTimeToStringInfo = typeof(DateTime).GetMethod("ToString", new Type[] { typeof(string) });
         private static MethodInfo dateTimeParseInfo = typeof(DateTime).GetMethod("Parse", new Type[] { typeof(string) });
