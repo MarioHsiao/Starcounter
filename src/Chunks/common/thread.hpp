d0499d3eed7dbf0cae54ad6006349af9b6bdfc7a
andthy <andreas.thyrhaug@starcounter.com>
2013-06-24 09:56:25 +0200

    andreas_506: Added thread files again and updated solution file.

---
+++ src/Chunks/common/thread.hpp
@@ -0,0 +1,17 @@
+//
+// thread.hpp
+//
+// Copyright © 2006-2013 Starcounter AB. All rights reserved.
+// Starcounter® is a registered trademark of Starcounter AB.
+//
+
+#ifndef STARCOUNTER_CORE_THREAD_HPP
+#define STARCOUNTER_CORE_THREAD_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "thread/thread.hpp"
+
+#endif // STARCOUNTER_CORE_THREAD_HPP
