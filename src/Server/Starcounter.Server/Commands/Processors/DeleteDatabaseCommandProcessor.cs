c8c633d84fdce292a2cc29bdf8fad7ffdf1b0ca4
Per Samuelsson <per@starcounter.com>
2013-07-04 10:53:38 +0200

    Added the server processor "DeleteDatabaseCommandProcessor", the default to handle dispatched DeleteDatabaseCommand instances.

---
+++ src/Server/Starcounter.Server/Commands/Processors/DeleteDatabaseCommandProcessor.cs
@@ -0,0 +1,24 @@
+﻿
+using Starcounter.Server.PublicModel.Commands;
+
+namespace Starcounter.Server.Commands {
+
+    [CommandProcessor(typeof(DeleteDatabaseCommand))]
+    internal sealed class DeleteDatabaseCommandProcessor : CommandProcessor {
+        /// <summary>
+        /// Initializes a new <see cref="DeleteDatabaseCommandProcessor"/>.
+        /// </summary>
+        /// <param name="server">The server in which the processor executes.</param>
+        /// <param name="command">The <see cref="DeletedDatabaseCommand"/> the
+        /// processor should exeucte.</param>
+        public DeleteDatabaseCommandProcessor(ServerEngine server, ServerCommand command)
+            : base(server, command) {
+        }
+
+        /// <inheritdoc />
+        protected override void Execute() {
+            var command = (DeleteDatabaseCommand)this.Command;
+            Log.Debug("Asked to deleted database {0}. This is currently not implemented.", command.DatabaseName);
+        }
+    }
+}
