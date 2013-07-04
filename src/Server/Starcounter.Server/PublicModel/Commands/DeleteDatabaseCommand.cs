e31702a6cdee063dfb75ca82aeef5d8d94ebe12c
Per Samuelsson <per@starcounter.com>
2013-07-04 14:28:02 +0200

    Changed DeleteDatabaseCommand to inherit DatabaseCommand rather than ServerCommand.

--- src/Server/Starcounter.Server/PublicModel/Commands/DeleteDatabaseCommand.cs
+++ src/Server/Starcounter.Server/PublicModel/Commands/DeleteDatabaseCommand.cs
@@ -6,24 +6,26 @@ namespace Starcounter.Server.PublicModel.Commands {
     /// Defines the command to use when a host want to issue a delete
     /// of a database.
     /// </summary>
-    public sealed class DeleteDatabaseCommand : ServerCommand {
-        /// <summary>
-        /// Gets the name of the database to delete.
-        /// </summary>
-        public readonly string DatabaseName;
-
+    public sealed class DeleteDatabaseCommand : DatabaseCommand {
         /// <summary>
         /// Gets a value indicating if the data files (i.e image-
         /// and transaction log files) are to be deleted too.
         /// </summary>
         public readonly bool DeleteDataFiles;
 
-        public DeleteDatabaseCommand(ServerEngine engine, string databaseName, bool deleteDataFiles = false) : base(engine, "Deleting database {0}", databaseName) {
+        /// <summary>
+        /// Initialize a new <see cref="DeleteDatabaseCommand"/>.
+        /// </summary>
+        /// <param name="engine">The engine to which the command belong.</param>
+        /// <param name="databaseName">The name of the database to delete.</param>
+        /// <param name="deleteDataFiles">A value indicating if data files should
+        /// be deleted too.</param>
+        public DeleteDatabaseCommand(ServerEngine engine, string databaseName, bool deleteDataFiles = false) : 
+            base(engine, CreateDatabaseUri(engine, databaseName), "Deleting database {0}{1}", databaseName, deleteDataFiles ? " (including data files)" : string.Empty) {
             if (string.IsNullOrEmpty(databaseName)) {
                 throw new ArgumentNullException("databaseName");
             }
 
-            this.DatabaseName = databaseName;
             this.DeleteDataFiles = deleteDataFiles;
         }
     }
