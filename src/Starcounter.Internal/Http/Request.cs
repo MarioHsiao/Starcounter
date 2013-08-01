1003c8ef7935516541f22361fdfbea4f340deeed
Joachim Wester <joachimwester@mac.com>
2013-08-02 00:04:29 +0200

    Implemented a temporary PreferedMimeTypes implementation and added some documentation.

--- src/Starcounter.Internal/Http/Request.cs
+++ src/Starcounter.Internal/Http/Request.cs
@@ -73,10 +73,14 @@ namespace Starcounter.Advanced {
         }
 
 
-        /// <summary>
+        /// Returns the single most preferred mime type according to the Accept header of the request amongst a 
+        /// set of common mime types. If the mime type is not in the enum of known common mime types, the
+        /// value MimeType.Other is returned. If there is no Accept header or if the Accept header is empty,
+        /// the value MimeType.Unspecified is returned.
+        /// <remarks>
         /// TODO! Implement proper fast method! Include all mime types in xml file and speed up using
         /// similar code generation as the URI matcher.
-        /// </summary>
+        /// </remarks>
         public MimeType PreferredMimeType {
             get {
                 var a = this["Accept"];
@@ -101,11 +105,17 @@ namespace Starcounter.Advanced {
         }
 
         /// <summary>
-        /// TODO! Implement!
+        /// Returns a list of requested mime types in preference order as discovered in the Accept header
+        /// of the request.
         /// </summary>
+        /// <remarks>
+        /// TODO! Implement! Does currently only return a single mime type and supports only a few mime types.
+        /// </remarks>
         public IEnumerator<MimeType> PreferredMimeTypes {
             get {
-                return (new List<MimeType>()).GetEnumerator();
+                var l = new List<MimeType>();
+                l.Add( PreferredMimeType );
+                return l.GetEnumerator();
             }
         }
 
