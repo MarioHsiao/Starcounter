8a018edaa7522bab8de1220a9564d8136060a3c0
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-08-05 15:07:23 +0200

    Query cache benchmark: more fine tests.

--- src/Samples/QueryProcessingTest/BenchmarkQueryCache.cs
+++ src/Samples/QueryProcessingTest/BenchmarkQueryCache.cs
@@ -12,43 +12,91 @@ namespace QueryProcessingTest {
         public static void BenchQueryCache() {
             HelpMethods.LogEvent("Start benchmark of query cache");
             int nrIterations = 1000000;
-            if (TestLogger.IsRunningOnBuildServer())
-                nrIterations = nrIterations * 10;
+            int sleepTimeout = 100;
+            if (TestLogger.IsRunningOnBuildServer()) {
+                nrIterations = nrIterations * 100;
+                sleepTimeout = sleepTimeout * 10;
+            }
             for (int schedulers = 1; schedulers <= Environment.ProcessorCount; schedulers++) {
                 Stopwatch timer = new Stopwatch();
                 nrFinishedWorkers = 0;
                 timer.Start();
                 for (byte schedulerId = 0; schedulerId < schedulers; schedulerId++) {
                     DbSession dbs = new DbSession();
-                    dbs.RunAsync(() => GetEnumerator(2), schedulerId);
+                    dbs.RunAsync(() => QueryEnumerator(2), schedulerId);
                 }
                 while (nrFinishedWorkers != schedulers)
-                    Thread.Sleep(100);
+                    Thread.Sleep(sleepTimeout);
                 timer.Stop();
-                HelpMethods.LogEvent("Worm up of " + schedulers + " schedulers took " + timer.ElapsedMilliseconds + " ms.");
-                nrFinishedWorkers = 0;
-                timer.Reset();
-                timer.Start();
-                for (byte schedulerId = 0; schedulerId < schedulers; schedulerId++) {
-                    DbSession dbs = new DbSession();
-                    dbs.RunAsync(() => GetEnumerator(nrIterations), schedulerId);
-                }
-                while (nrFinishedWorkers != schedulers)
-                    Thread.Sleep(1000);
-                timer.Stop();
-                HelpMethods.LogEvent("Accessing cache on " + schedulers + " schedulers took " + timer.ElapsedMilliseconds +
-                    " ms for " + nrIterations + " iterations on each scheduler, i.e., " + 
-                    (long)nrIterations*schedulers*1000 /timer.ElapsedMilliseconds+
-                    " cache accesses per second.");
+                HelpMethods.LogEvent("Warm up of " + schedulers + " schedulers took " + timer.ElapsedMilliseconds + " ms.");
+                BenchmarkAction(schedulers, nrIterations, () => QueryEnumerator(nrIterations), "Obtaining enumerator on ");
+                BenchmarkAction(schedulers, nrIterations, () => DbSQL(nrIterations), "Calling Db.SQL on ");
+                BenchmarkAction(schedulers, nrIterations, () => GetEnumerator(nrIterations), "Calling GetEnumerator on ");
+                BenchmarkAction(schedulers, nrIterations, () => GetSchedulerInstance(nrIterations), "Getting a scheduler instance on ");
+                BenchmarkAction(schedulers, nrIterations, () => GetCachedEnumerator(nrIterations), "Getting cached enumerator on ");
                 HelpMethods.LogEvent("Finished benchmark of query cache");
             }
         }
 
-        public static void GetEnumerator(int nrIterations) {
+        public static void BenchmarkAction(int nrSchedulers, int nrIterations, Action work, String prefix) {
+            Stopwatch timer = new Stopwatch();
+            nrFinishedWorkers = 0;
+            timer.Start();
+            for (byte schedulerId = 0; schedulerId < nrSchedulers; schedulerId++) {
+                DbSession dbs = new DbSession();
+                dbs.RunAsync(work, schedulerId);
+            }
+            while (nrFinishedWorkers != nrSchedulers)
+                Thread.Sleep(100);
+            timer.Stop();
+            HelpMethods.LogEvent(prefix + nrSchedulers + " schedulers took " + timer.ElapsedMilliseconds +
+                " ms for " + nrIterations + " iterations on each scheduler, i.e., " +
+                (long)nrIterations * nrSchedulers * 1000 / timer.ElapsedMilliseconds +
+                " accesses per second.");
+        }
+
+        public static void QueryEnumerator(int nrIterations) {
             for (int i = 0; i < nrIterations; i++) {
                 var results = Db.SQL(query, 10).GetEnumerator();
                 results.Dispose();
             }
+            lock (query) {
+                nrFinishedWorkers++;
+            }
+        }
+
+        public static void DbSQL(int nrIterations) {
+            for (int i=0; i<nrIterations;i++)
+                Db.SQL(query, 10);
+            lock (query) {
+                nrFinishedWorkers++;
+            }
+        }
+
+        public static void GetEnumerator(int nrIterations) {
+            var sqlResults = Db.SQL(query, 10);
+            for (int i = 0; i < nrIterations; i++) {
+                var results = sqlResults.GetEnumerator();
+                results.Dispose();
+            }
+            lock (query) {
+                nrFinishedWorkers++;
+            }
+        }
+
+        public static void GetSchedulerInstance(int nrIterations) {
+            for (int i = 0; i < nrIterations; i++)
+                Scheduler.GetInstance();
+            lock (query) {
+                nrFinishedWorkers++;
+            }
+        }
+
+        public static void GetCachedEnumerator(int nrIterations) {
+            var scheduler = Scheduler.GetInstance();
+            Type t = typeof(Object);
+            for (int i = 0; i < nrIterations; i++)
+                scheduler.SqlEnumCache.GetCachedEnumerator(query, t);
             lock (query) {
                 nrFinishedWorkers++;
             }
