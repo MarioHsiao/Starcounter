51430e5c4da7045686d4a8d34059589396d3636e
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-08-06 11:48:09 +0200

    Added projects in level 1 for errorreporting.

---
+++ src/Programs/Starcounter.ErrorReport/Model/StarcounterReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Sc.Tools.Logging;
+using Starcounter.Applications.UsageTrackerApp.Model;
+
+namespace Starcounter.Applications.ErrorReport.Model {
+	/// <summary>
+	/// 
+	/// </summary>
+	[Database]
+	public class StarcounterReport {
+		/// <summary>
+		/// 
+		/// </summary>
+		public Installation Installation;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public IEnumerable<LoggedItem> Items {
+			get {
+				return Db.SQL<LoggedItem>("SELECT l FROM LoggedItem l WHERE Report=?", this);
+			}
+		}
+	}
+}
