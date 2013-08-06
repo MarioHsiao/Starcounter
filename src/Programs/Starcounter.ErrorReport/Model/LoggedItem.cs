64af57ef2860dc79c15ef4aa1ce358ea74966631
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-08-06 11:48:09 +0200

    Added projects in level 1 for errorreporting.

---
+++ src/Programs/Starcounter.ErrorReport/Model/LoggedItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sc.Tools.Logging;
+
+namespace Starcounter.Applications.ErrorReport.Model {
+	/// <summary>
+	/// 
+	/// </summary>
+	[Database]
+	public class LoggedItem {
+		/// <summary>
+		/// The main report this item belongs to.
+		/// </summary>
+		public StarcounterReport Report;
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime Date;
+		/// <summary>
+		/// 
+		/// </summary>
+		public uint Errorcode;
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Hostname;
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Message;
+		/// <summary>
+		/// 
+		/// </summary>
+		public Severity Severity;
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Source;
+	}
+}
---
+++ src/Programs/Starcounter.ErrorReport/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Starcounter.Internal;
+using Starcounter.Applications.ErrorReport.Model;
+using Starcounter.Applications.UsageTrackerApp.Model;
+using Sc.Tools.Logging;
+
+namespace Starcounter.Applications.ErrorReport {
+    class Program {
+        static void Main(string[] args) {
+            AppsBootstrapper.Bootstrap(null, 8585);
+
+			Handle.PUT("/api/errorreport", (Report report) => {
+				Db.Transaction(() => {
+					var sr = new StarcounterReport();
+					sr.Installation = Db.SQL<Installation>("SELECT i FROM Installation i WHERE i.InstallationNo=?", report.InstallationNo).First;
+
+					foreach (Report.LoggedItemsObj item in report.LoggedItems) {
+						var dbItem = new LoggedItem();
+						dbItem.Report = sr;
+						dbItem.Date = DateTime.Parse(item.Date);
+						dbItem.Errorcode = (uint)item.Errorcode;
+						dbItem.Hostname = item.Hostname;
+						dbItem.Message = item.Message;
+						dbItem.Severity = (Severity)item.Severity;
+						dbItem.Source = item.Source;
+					}
+				});
+				return 200;
+			});
+        }
+    }
+}
