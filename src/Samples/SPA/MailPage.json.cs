4434b7019a9064bb4d680d89f5dd5885e4ecafc4
alemoi <alexey.moiseenko@starcounter.com>
2013-08-07 22:33:35 +0200

    Added clean up task for generated JSON source files.
    Added new transaction handling for view models.

--- src/Samples/SPA/MailPage.json.cs
+++ src/Samples/SPA/MailPage.json.cs
@@ -1,7 +1,13 @@
-﻿
-using Starcounter;
-
+﻿using Starcounter;
 
 partial class MailPage : Json<Email> {
+    void Handle(Input.Title input)
+    {
+        input.Cancel();
+    }
 
+    void Handle(Input.Content input)
+    {
+        input.Cancel();
+    }
 }
