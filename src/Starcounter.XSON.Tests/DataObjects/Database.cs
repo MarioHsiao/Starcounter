580141b8eda3694308a251170c260065eb963a4c
Joachim Wester <joachimwester@mac.com>
2013-08-12 22:34:42 +0200

    Added XML docs

--- src/Starcounter.XSON.Tests/DataObjects/Database.cs
+++ src/Starcounter.XSON.Tests/DataObjects/Database.cs
@@ -3,13 +3,26 @@ using Starcounter.Advanced;
 using System.Collections.Generic;
 namespace Starcounter.Internal.XSON.Tests {
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class Database : IBindableRetriever {
 
+        /// <summary>
+        /// 
+        /// </summary>
         public static Database The = new Database();
 
+        /// <summary>
+        /// 
+        /// </summary>
         public static List<Entity> Entities = new List<Entity>();
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="identifier"></param>
+        /// <returns></returns>
         IBindable IBindableRetriever.Retrieve(ulong identifier) {
             return Entities[(int)identifier];
         }
