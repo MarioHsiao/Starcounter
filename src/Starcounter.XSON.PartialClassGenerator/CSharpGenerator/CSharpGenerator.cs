b62472aca6e343aa789f32b6338b239c093d062b
Joachim Wester <joachimwester@mac.com>
2013-08-15 01:45:45 +0200

    Updated Bound to enum

--- src/Starcounter.XSON.PartialClassGenerator/CSharpGenerator/CSharpGenerator.cs
+++ src/Starcounter.XSON.PartialClassGenerator/CSharpGenerator/CSharpGenerator.cs
@@ -433,7 +433,7 @@ namespace Starcounter.Internal.Application.CodeGeneration  {
                     sb.Append('"');
                     
                     TValue tv = mn.Template as TValue;
-                    if (tv != null && tv.Bound && tv.PropertyName != tv.Bind) {
+                    if (tv != null && ( tv.Bound == Bound.Yes ) && tv.PropertyName != tv.Bind) {
                         if (tv.Bind == null) {
                             sb.Append(", bind:null");
                         } else {
