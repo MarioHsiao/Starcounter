38a2d82f56e7391c0aca8ca23fafa1d6a21ceea1
Joachim Wester <joachimwester@mac.com>
2013-08-17 18:46:37 +0200

    First PMail test

---
+++ src/Starcounter.XSON.CodeGeneration.Tests/PMail/PMailTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Starcounter;
+using Starcounter.Templates;
+using System;
+using System.IO;
+
+
+namespace Starcounter.XSON.PartialClassGeneration.Tests {
+
+    [TestFixture]
+    static class PMailTests {
+
+        internal static TJson ReadTemplate(string path) {
+            var str = File.ReadAllText(path);
+            return TJson.CreateFromJson(str);
+        }
+
+        [Test]
+        public static void GenerateCsDOM() {
+            var tj = ReadTemplate("PMail\\ContactApp.json");
+            Assert.NotNull(tj);
+
+//            Starcounter.Internal.XSON.PartialClassGenerator.
+        }
+
+    }
+}
