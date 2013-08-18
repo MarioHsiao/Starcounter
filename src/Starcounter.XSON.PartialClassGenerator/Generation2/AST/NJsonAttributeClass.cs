5507f8f7202d1972f90ef1763c350bd9d355b5c4
Joachim Wester <joachimwester@mac.com>
2013-08-18 11:41:09 +0200

    Starting generation 2 codegen as a clone of generation 1 codegen

---
+++ src/Starcounter.XSON.PartialClassGenerator/Generation2/AST/NJsonAttributeClass.cs
@@ -0,0 +1,23 @@
+﻿// ***********************************************************************
+// <copyright file="NJsonAttributeClass.cs" company="Starcounter AB">
+//     Copyright (c) Starcounter AB.  All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+namespace Starcounter.Internal.MsBuild.Codegen {
+
+
+    /// <summary>
+    /// See TemplateAttribute
+    /// </summary>
+    public class NJsonAttributeClass : NOtherClass {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gen"></param>
+        public NJsonAttributeClass(DomGenerator gen)
+            : base(gen) {
+        }
+
+    }
+}
