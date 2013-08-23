ebcc59e9f97476b1ae2e3e698ede9351fd57adf5
Per Samuelsson <per@starcounter.com>
2013-08-23 14:11:44 +0200

    Redecorated the TransientAttribute to allow it to be applied to .NET properties.

--- src/Starcounter/TransientAttribute.cs
+++ src/Starcounter/TransientAttribute.cs
@@ -3,11 +3,12 @@ using System;
 
 namespace Starcounter {
     /// <summary>
-    /// Custom attribute that, when applied to a field in a database
-    /// class, makes the field transient, meaning it will not be stored
-    /// in the database.
+    /// Custom attribute that, when applied to a field or auto-implemented
+    /// property in a database class, makes the field/property transient, 
+    /// meaning it will not be stored in the database nor accessible using
+    /// SQL.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Field)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public sealed class TransientAttribute : Attribute {
     }
 }
