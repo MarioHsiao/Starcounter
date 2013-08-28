f25fc9e93a30d005f43f307c70887659b70c5763
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-08-28 14:27:33 +0200

    Added content-type to for content on 500 responses including exception string.

--- src/Starcounter.Apps.JsonPatch/AppRestServer.cs
+++ src/Starcounter.Apps.JsonPatch/AppRestServer.cs
@@ -76,9 +76,9 @@ namespace Starcounter.Internal.Web {
             catch (Exception ex) {
 				// Logging the exception to server log.
 				LogSources.Hosting.LogException(ex);
-
-                byte[] error = Encoding.UTF8.GetBytes(this.GetExceptionString(ex));
-                return new Response() { Uncompressed = HttpResponseBuilder.Create500WithContent(error) };
+				return new Response() {
+					Uncompressed = HttpResponseBuilder.Slow.FromStatusHeadersAndStringContent(500, null, GetExceptionString(ex), null, "text/plain")
+				};
             }
         }
 
@@ -172,9 +172,9 @@ namespace Starcounter.Internal.Web {
 
                         // Logging the exception to server log.
                         LogSources.Hosting.LogException(exc);
-
-                        byte[] error = Encoding.UTF8.GetBytes(this.GetExceptionString(exc));
-                        return new Response() { Uncompressed = HttpResponseBuilder.Create500WithContent(error) };
+						return new Response() {
+							Uncompressed = HttpResponseBuilder.Slow.FromStatusHeadersAndStringContent(500, null, GetExceptionString(exc), null, "text/plain")
+						};
                     }
                     finally
                     {
