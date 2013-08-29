add160af47a7f5531032b46547ce887e24741a34
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-08-29 16:15:06 +0200

    Added attribute for specifying BindChildren oin codebehind.

--- src/Starcounter.XSON/Templates/Foundation/Bound.cs
+++ src/Starcounter.XSON/Templates/Foundation/Bound.cs
@@ -8,12 +8,12 @@ namespace Starcounter.Templates {
         Auto
     }
 
-	//[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-	//public class BindChildrenAttribute : Attribute {
-	//	public BindChildrenAttribute(Bound bound) {
-	//		Bound = bound;
-	//	}
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class BindChildrenAttribute : Attribute {
+		public BindChildrenAttribute(Bound bound) {
+			Bound = bound;
+		}
 
-	//	public Bound Bound { get; set; }
-	//}
+		public Bound Bound { get; set; }
+	}
 }
