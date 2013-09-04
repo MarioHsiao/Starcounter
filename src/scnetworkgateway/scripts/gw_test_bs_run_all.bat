d7cdebd860b4beb40c30cf0beaab8cd6db7eccf0
alemoi <alexey.moiseenko@starcounter.com>
2013-09-04 11:07:21 +0200

    Adopted some testing scripts with GatewayWorkersNumber option.

--- src/scnetworkgateway/scripts/gw_test_bs_run_all.bat
+++ src/scnetworkgateway/scripts/gw_test_bs_run_all.bat
@@ -1,6 +1,3 @@
-:: gw_test_bs.bat 1_NUM_WORKERS 2_MODE 3_NUM_CONNS 4_NUM_ECHOES 5_MAX_TIME_SEC 6_STATS_NAME 7_SCHED_NUM 8_APPS_MODE 9_APPS_PORT_NUM 10_NUM_CHUNKS 11_DB_OPTIONS
-:: gw_test_bs.bat "1 MODE_GATEWAY_SMC_RAW 1000 10000000 100 BSStatsBlaBla" 1 MODE_GATEWAY_SMC_RAW 81 131072
-
 IF "%SC_RUN_GATEWAY_TESTS%"=="False" GOTO :EOF
 
 :: Some predefined constants.
@@ -20,90 +17,90 @@ sccreatedb.exe -ip %DB_DIR% %DB_NAME%
 
 :: HTTP Tests
 
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_HTTP 1000 10000000 100 gateway_simple_http_rps_1worker_1000conns" 1 MODE_GATEWAY_HTTP 81 8192"
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_HTTP 1000 10000000 100 gateway_simple_http_rps_1worker_1000conns" 1 MODE_GATEWAY_HTTP 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_HTTP 10000 2000000 100 gateway_simple_http_rps_1worker_10000conns" 1 MODE_GATEWAY_HTTP 81 16384"
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_HTTP 10000 2000000 100 gateway_simple_http_rps_1worker_10000conns" 1 MODE_GATEWAY_HTTP 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_HTTP 100000 2000000 100 gateway_simple_http_rps_1worker_100000conns" 1 MODE_GATEWAY_HTTP 81 262144"
-IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_HTTP 1000 10000000 100 gateway_simple_http_rps_2workers_1000conns" 2 MODE_GATEWAY_HTTP 81 8192"
-IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_HTTP 10000 10000000 100 gateway_simple_http_rps_2workers_10000conns" 2 MODE_GATEWAY_HTTP 81 16384"
-IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_HTTP 100000 2000000 100 gateway_simple_http_rps_2workers_100000conns" 2 MODE_GATEWAY_HTTP 81 262144"
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_HTTP 100000 2000000 100 gateway_simple_http_rps_1worker_100000conns" 1 MODE_GATEWAY_HTTP 81 262144"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_HTTP 1002 10000000 100 gateway_simple_http_rps_3workers_1000conns" 3 MODE_GATEWAY_HTTP 81 8192"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_HTTP 1000 10000000 100 gateway_simple_http_rps_2workers_1000conns" 2 MODE_GATEWAY_HTTP 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_HTTP 10002 10000000 100 gateway_simple_http_rps_3workers_10000conns" 3 MODE_GATEWAY_HTTP 81 16384"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_HTTP 10000 10000000 100 gateway_simple_http_rps_2workers_10000conns" 2 MODE_GATEWAY_HTTP 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_HTTP 100002 2000000 100 gateway_simple_http_rps_3workers_100000conns" 3 MODE_GATEWAY_HTTP 81 262144"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_HTTP 100000 2000000 100 gateway_simple_http_rps_2workers_100000conns" 2 MODE_GATEWAY_HTTP 81 262144"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_SMC_HTTP 1000 10000000 100 gateway_with_smc_simple_http_rps_1worker_1000conns" 1 MODE_GATEWAY_SMC_HTTP 81 8192"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_HTTP 1002 10000000 100 gateway_simple_http_rps_3workers_1000conns" 3 MODE_GATEWAY_HTTP 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_SMC_HTTP 10000 2000000 100 gateway_with_smc_simple_http_rps_1worker_10000conns" 1 MODE_GATEWAY_SMC_HTTP 81 16384"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_HTTP 10002 10000000 100 gateway_simple_http_rps_3workers_10000conns" 3 MODE_GATEWAY_HTTP 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-::CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_SMC_HTTP 100000 2000000 100 gateway_with_smc_simple_http_rps_1worker_100000conns" 1 MODE_GATEWAY_SMC_HTTP 81 262144"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_HTTP 100002 2000000 100 gateway_simple_http_rps_3workers_100000conns" 3 MODE_GATEWAY_HTTP 81 262144"
+IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
+
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_SMC_HTTP 1000 10000000 100 gateway_with_smc_simple_http_rps_1worker_1000conns" 1 MODE_GATEWAY_SMC_HTTP 81 8192"
+IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_SMC_HTTP 10000 2000000 100 gateway_with_smc_simple_http_rps_1worker_10000conns" 1 MODE_GATEWAY_SMC_HTTP 81 16384"
+IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
+::CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_SMC_HTTP 100000 2000000 100 gateway_with_smc_simple_http_rps_1worker_100000conns" 1 MODE_GATEWAY_SMC_HTTP 81 262144"
 ::IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_SMC_HTTP 1000 10000000 100 gateway_with_smc_simple_http_rps_2workers_1000conns" 2 MODE_GATEWAY_SMC_HTTP 81 8192"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_SMC_HTTP 1000 10000000 100 gateway_with_smc_simple_http_rps_2workers_1000conns" 2 MODE_GATEWAY_SMC_HTTP 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_SMC_HTTP 10000 10000000 100 gateway_with_smc_simple_http_rps_2workers_10000conns" 2 MODE_GATEWAY_SMC_HTTP 81 16384"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_SMC_HTTP 10000 10000000 100 gateway_with_smc_simple_http_rps_2workers_10000conns" 2 MODE_GATEWAY_SMC_HTTP 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-::CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_SMC_HTTP 100000 2000000 100 gateway_with_smc_simple_http_rps_2workers_100000conns" 2 MODE_GATEWAY_SMC_HTTP 81 262144"
+::CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_SMC_HTTP 100000 2000000 100 gateway_with_smc_simple_http_rps_2workers_100000conns" 2 MODE_GATEWAY_SMC_HTTP 81 262144"
 ::IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_SMC_HTTP 1002 10000000 100 gateway_with_smc_simple_http_rps_3workers_1000conns" 3 MODE_GATEWAY_SMC_HTTP 81 8192"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_SMC_HTTP 1002 10000000 100 gateway_with_smc_simple_http_rps_3workers_1000conns" 3 MODE_GATEWAY_SMC_HTTP 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_SMC_HTTP 10002 10000000 100 gateway_with_smc_simple_http_rps_3workers_10000conns" 3 MODE_GATEWAY_SMC_HTTP 81 16384"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_SMC_HTTP 10002 10000000 100 gateway_with_smc_simple_http_rps_3workers_10000conns" 3 MODE_GATEWAY_SMC_HTTP 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-::CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_SMC_HTTP 100002 2000000 100 gateway_with_smc_simple_http_rps_3workers_100000conns" 3 MODE_GATEWAY_SMC_HTTP 81 262144"
+::CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_SMC_HTTP 100002 2000000 100 gateway_with_smc_simple_http_rps_3workers_100000conns" 3 MODE_GATEWAY_SMC_HTTP 81 262144"
 ::IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
 :: Raw Port Tests
 
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_RAW 1000 10000000 100 gateway_simple_raw_eps_1worker_1000conns" 1 MODE_GATEWAY_RAW 81 8192"
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_RAW 1000 10000000 100 gateway_simple_raw_eps_1worker_1000conns" 1 MODE_GATEWAY_RAW 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_RAW 10000 2000000 100 gateway_simple_raw_eps_1worker_10000conns" 1 MODE_GATEWAY_RAW 81 16384"
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_RAW 10000 2000000 100 gateway_simple_raw_eps_1worker_10000conns" 1 MODE_GATEWAY_RAW 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_RAW 100000 2000000 100 gateway_simple_raw_eps_1worker_100000conns" 1 MODE_GATEWAY_RAW 81 262144"
-IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_RAW 1000 20000000 100 gateway_simple_raw_eps_2workers_1000conns" 2 MODE_GATEWAY_RAW 81 8192"
-IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_RAW 10000 2000000 100 gateway_simple_raw_eps_2workers_10000conns" 2 MODE_GATEWAY_RAW 81 16384"
-IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_RAW 100000 2000000 100 gateway_simple_raw_eps_2workers_100000conns" 2 MODE_GATEWAY_RAW 81 262144"
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_RAW 100000 2000000 100 gateway_simple_raw_eps_1worker_100000conns" 1 MODE_GATEWAY_RAW 81 262144"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_RAW 1002 30000000 100 gateway_simple_raw_eps_3workers_1000conns" 3 MODE_GATEWAY_RAW 81 8192"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_RAW 1000 20000000 100 gateway_simple_raw_eps_2workers_1000conns" 2 MODE_GATEWAY_RAW 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_RAW 10002 2000000 100 gateway_simple_raw_eps_3workers_10000conns" 3 MODE_GATEWAY_RAW 81 16384"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_RAW 10000 2000000 100 gateway_simple_raw_eps_2workers_10000conns" 2 MODE_GATEWAY_RAW 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_RAW 100002 2000000 100 gateway_simple_raw_eps_3workers_100000conns" 3 MODE_GATEWAY_RAW 81 262144"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_RAW 100000 2000000 100 gateway_simple_raw_eps_2workers_100000conns" 2 MODE_GATEWAY_RAW 81 262144"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_SMC_RAW 1000 10000000 100 gateway_with_smc_simple_raw_eps_1worker_1000conns" 1 MODE_GATEWAY_SMC_RAW 81 8192"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_RAW 1002 30000000 100 gateway_simple_raw_eps_3workers_1000conns" 3 MODE_GATEWAY_RAW 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_SMC_RAW 10000 2000000 100 gateway_with_smc_simple_raw_eps_1worker_10000conns" 1 MODE_GATEWAY_SMC_RAW 81 16384"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_RAW 10002 2000000 100 gateway_simple_raw_eps_3workers_10000conns" 3 MODE_GATEWAY_RAW 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-::CMD /C "gw_test_bs.bat "1 MODE_GATEWAY_SMC_RAW 100000 2000000 100 gateway_with_smc_simple_raw_eps_1worker_100000conns" 1 MODE_GATEWAY_SMC_RAW 81 262144"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_RAW 100002 2000000 100 gateway_simple_raw_eps_3workers_100000conns" 3 MODE_GATEWAY_RAW 81 262144"
+IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
+
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_SMC_RAW 1000 10000000 100 gateway_with_smc_simple_raw_eps_1worker_1000conns" 1 MODE_GATEWAY_SMC_RAW 81 8192"
+IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
+CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_SMC_RAW 10000 2000000 100 gateway_with_smc_simple_raw_eps_1worker_10000conns" 1 MODE_GATEWAY_SMC_RAW 81 16384"
+IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
+::CMD /C "gw_test_bs.bat 1 "MODE_GATEWAY_SMC_RAW 100000 2000000 100 gateway_with_smc_simple_raw_eps_1worker_100000conns" 1 MODE_GATEWAY_SMC_RAW 81 262144"
 ::IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_SMC_RAW 1000 20000000 100 gateway_with_smc_simple_raw_eps_2workers_1000conns" 2 MODE_GATEWAY_SMC_RAW 81 8192"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_SMC_RAW 1000 20000000 100 gateway_with_smc_simple_raw_eps_2workers_1000conns" 2 MODE_GATEWAY_SMC_RAW 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_SMC_RAW 10000 10000000 100 gateway_with_smc_simple_raw_eps_2workers_10000conns" 2 MODE_GATEWAY_SMC_RAW 81 16384"
+CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_SMC_RAW 10000 10000000 100 gateway_with_smc_simple_raw_eps_2workers_10000conns" 2 MODE_GATEWAY_SMC_RAW 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-::CMD /C "gw_test_bs.bat "2 MODE_GATEWAY_SMC_RAW 100000 2000000 100 gateway_with_smc_simple_raw_eps_2workers_100000conns" 2 MODE_GATEWAY_SMC_RAW 81 262144"
+::CMD /C "gw_test_bs.bat 2 "MODE_GATEWAY_SMC_RAW 100000 2000000 100 gateway_with_smc_simple_raw_eps_2workers_100000conns" 2 MODE_GATEWAY_SMC_RAW 81 262144"
 ::IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_SMC_RAW 1002 30000000 100 gateway_with_smc_simple_raw_eps_3workers_1000conns" 3 MODE_GATEWAY_SMC_RAW 81 8192"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_SMC_RAW 1002 30000000 100 gateway_with_smc_simple_raw_eps_3workers_1000conns" 3 MODE_GATEWAY_SMC_RAW 81 8192"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_SMC_RAW 10002 10000000 100 gateway_with_smc_simple_raw_eps_3workers_10000conns" 3 MODE_GATEWAY_SMC_RAW 81 16384"
+CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_SMC_RAW 10002 10000000 100 gateway_with_smc_simple_raw_eps_3workers_10000conns" 3 MODE_GATEWAY_SMC_RAW 81 16384"
 IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
-::CMD /C "gw_test_bs.bat "3 MODE_GATEWAY_SMC_RAW 100002 2000000 100 gateway_with_smc_simple_raw_eps_3workers_100000conns" 3 MODE_GATEWAY_SMC_RAW 81 262144"
+::CMD /C "gw_test_bs.bat 3 "MODE_GATEWAY_SMC_RAW 100002 2000000 100 gateway_with_smc_simple_raw_eps_3workers_100000conns" 3 MODE_GATEWAY_SMC_RAW 81 262144"
 ::IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 
 :: Success message.
