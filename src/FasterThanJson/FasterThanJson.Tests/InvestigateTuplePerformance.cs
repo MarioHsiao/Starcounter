afe841091c5a6ec23ad0720565bc8abecff036b0
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-09-09 14:25:47 +0200

    FasterThanJson: added perfomance test for byte array.

--- src/FasterThanJson/FasterThanJson.Tests/InvestigateTuplePerformance.cs
+++ src/FasterThanJson/FasterThanJson.Tests/InvestigateTuplePerformance.cs
@@ -236,6 +236,66 @@ namespace FasterThanJson.Tests {
             }
         }
 
+        //[Test]
+        public unsafe void BenchmarkTupleByte1Scale() {
+            byte[] value = new byte[1] { 12 };
+            uint[] valueCounts = new uint[] { 20, 10, 2, 1 };
+            Stopwatch timer = new Stopwatch();
+            fixed (byte* buffer = new byte[100]) {
+                foreach (uint valueCount in valueCounts) {
+                    timer.Start();
+                    for (int i = 0; i < nrIterations; i++) {
+                        TupleWriterBase64 tuple = new TupleWriterBase64(buffer, valueCount, 2);
+                        for (int j = 0; j < valueCount; j++)
+                            tuple.Write(value);
+                    }
+                    timer.Stop();
+                    Print(timer, "TupleWriter creates and " + valueCount + " 1-byte array writes", nrIterations);
+                    TupleReaderBase64 reader = new TupleReaderBase64(buffer, valueCount);
+                    for (int j = 0; j < valueCount; j++)
+                        Assert.AreEqual(value, reader.ReadByteArray());
+                    timer.Start();
+                    for (int i = 0; i < nrIterations; i++) {
+                        TupleReaderBase64 tuple = new TupleReaderBase64(buffer, valueCount);
+                        for (int j = 0; j < valueCount; j++)
+                            tuple.ReadByteArray();
+                    }
+                    timer.Stop();
+                    Print(timer, "TupleReader creates and " + valueCount + " 1-byte array reads", nrIterations);
+                }
+            }
+        }
+
+        //[Test]
+        public unsafe void BenchmarkTupleByte10Scale() {
+            byte[] value = new byte[10] { 12, 255, 0, 124, 4, 0, 32, 43, 255, 231 };
+            uint[] valueCounts = new uint[] { 20, 10, 2, 1 };
+            Stopwatch timer = new Stopwatch();
+            fixed (byte* buffer = new byte[321]) {
+                foreach (uint valueCount in valueCounts) {
+                    timer.Start();
+                    for (int i = 0; i < nrIterations; i++) {
+                        TupleWriterBase64 tuple = new TupleWriterBase64(buffer, valueCount, 2);
+                        for (int j = 0; j < valueCount; j++)
+                            tuple.Write(value);
+                    }
+                    timer.Stop();
+                    Print(timer, "TupleWriter creates and " + valueCount + " 10-bytes array writes", nrIterations);
+                    TupleReaderBase64 reader = new TupleReaderBase64(buffer, valueCount);
+                    for (int j = 0; j < valueCount; j++)
+                        Assert.AreEqual(value, reader.ReadByteArray());
+                    timer.Start();
+                    for (int i = 0; i < nrIterations; i++) {
+                        TupleReaderBase64 tuple = new TupleReaderBase64(buffer, valueCount);
+                        for (int j = 0; j < valueCount; j++)
+                            tuple.ReadByteArray();
+                    }
+                    timer.Stop();
+                    Print(timer, "TupleReader creates and " + valueCount + " 10-bytes array reads", nrIterations);
+                }
+            }
+        }
+
         [Test]
         [Category("LongRunning")]
         public unsafe void RunAllTests() {
