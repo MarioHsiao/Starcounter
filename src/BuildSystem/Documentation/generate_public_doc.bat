bca09f4ce9f6d9fc4c3b2ff162acc40e799d95ef
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-09-17 12:20:10 +0200

    Renamed files.

---
+++ src/BuildSystem/Documentation/generate_public_doc.bat
@@ -0,0 +1,5 @@
+:: Variables
+set OutputPath=..\..\..\docs
+set DocXPath=C:\Program Files (x86)\Innovasys\DocumentX2012\bin\DocumentXCommandLinex64.exe
+
+"%DocXPath%" ".\public\starcounter.dxp" [/buildconfiguration="Public"]
