bf48468dd53f27c1cd0b5761c78daec239933055
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-09-17 14:55:17 +0200

    Update of output-path for generated documentation.

--- src/BuildSystem/Documentation/generate_public_doc.bat
+++ src/BuildSystem/Documentation/generate_public_doc.bat
@@ -1,5 +1,3 @@
-:: Variables
-set OutputPath=..\..\..\docs
 set DocXPath=C:\Program Files (x86)\Innovasys\DocumentX2012\bin\DocumentXCommandLinex64.exe
 
 "%DocXPath%" ".\public\starcounter.dxp" [/buildconfiguration="Public"]
