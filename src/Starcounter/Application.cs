27fab4fd8f7362579f1c74e4243d94c51dd8187d
Per Samuelsson <per@starcounter.com>
2013-09-20 14:24:04 +0200

    Defined the primary API to be used by hosted code to access Application data inside the code host.

--- src/Starcounter/Application.cs
+++ src/Starcounter/Application.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Reflection;
 
 namespace Starcounter {
 
@@ -8,6 +9,60 @@ namespace Starcounter {
     /// to run in, a Starcounter code host.
     /// </summary>
     public sealed class Application {
+        [ThreadStatic]
+        internal static Application CurrentAssigned;
+
+        /// <summary>
+        /// Gets the current application, running in the current Starcounter
+        /// code host.
+        /// </summary>
+        /// <remarks>
+        /// The current application is determined by context where this property
+        /// is read. The code host has several ways to figure out the which is
+        /// the current application, based on the calling code.
+        /// </remarks>
+        /// <exception cref="ArgumentException">Thrown when the application can't
+        /// be resolved based on the given context.</exception>
+        public static Application Current {
+            get {
+                var current = CurrentAssigned;
+                current = current ?? GetApplication(Assembly.GetCallingAssembly());
+                return current;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Application"/> the given <paramref name="assembly"/>
+        /// represent or is part of.
+        /// </summary>
+        /// <param name="assembly">The assembly whose <see cref="Application"/> are
+        /// being requested.</param>
+        /// <returns>The application <paramref name="assembly"/> represent or runs
+        /// as a part of.</returns>
+        /// <exception cref="AgrumentNullException">Thrown when <paramref name="assembly"/>
+        /// is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when the application can't
+        /// be resolved based on the given assembly.</exception>
+        public static Application GetApplication(Assembly assembly) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Application"/> the given <paramref name="fileName"/>
+        /// represent. Both short names, such as "foo.exe", and full names, such as
+        /// "\path\to\foo.exe", will be considered.
+        /// </summary>
+        /// <param name="fileName">The file name whose <see cref="Application"/> are
+        /// being requested.</param>
+        /// <returns>The <see cref="Application"/> launched by the given file.
+        /// <exception cref="AgrumentNullException">Thrown when <paramref name="fileName"/>
+        /// is null or empty.</exception>
+        /// <exception cref="ArgumentException">Thrown when the application can't
+        /// be resolved based on the given file.</exception>
+        public static Application GetApplication(string fileName) {
+            throw new NotImplementedException();
+        }
+
         /// <summary>
         /// Gets the name, including the full path, of the applications
         /// primary file.
