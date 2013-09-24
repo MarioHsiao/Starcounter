6083935dd8ee4d8821ab7e564d287201bcc77e6e
Per Samuelsson <per@starcounter.com>
2013-09-24 13:16:56 +0200

    Added the ExecutableHandler.DELETE.cs file with the ExecutableHandler.OnDelete stub method to the Starcounter.Administrator project, and extended the ExecutableHandler class to register DELETE as a supported verb on an executable resource. Still just returns 501 Not Implemented.

---
+++ src/Starcounter.Administrator/API/Handlers/ExecutableHandler.DELETE.cs
@@ -0,0 +1,24 @@
+﻿
+using Starcounter.Advanced;
+
+namespace Starcounter.Administrator.API.Handlers {
+
+    internal static partial class ExecutableHandler {
+        /// <summary>
+        /// Handles a DELETE of this resource.
+        /// </summary>
+        /// <param name="database">
+        /// The name of the database hosting the executable collection
+        /// represented by this resource.</param>
+        /// <param name="executable">The identity of the executable to
+        /// delete from the collection of executables (i.e. semantically,
+        /// the executable to unload from the code host).
+        /// </param>
+        /// <param name="request">
+        /// The REST request.</param>
+        /// <returns>The response to be sent back to the client.</returns>
+        static Response OnDELETE(string database, string executable, Request request) {
+            return 501;
+        }
+    }
+}
