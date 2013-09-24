f62decb5c2ade5df79fe50b2e68d3b67f2131d61
Per Samuelsson <per@starcounter.com>
2013-09-24 14:12:07 +0200

    Added stub processor class ("StopExecutableCommandProcessor") for the StopExecutableCommand. This processor will hold the server side logic to stop a certain executable running inside a given database (host).

---
+++ src/Server/Starcounter.Server/Commands/Processors/StopExecutableCommandProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Starcounter.Server.PublicModel.Commands;
+
+namespace Starcounter.Server.Commands {
+
+    /// <summary>
+    /// Executes a queued and dispatched <see cref="StopExecutableCommand"/>.
+    /// </summary>
+    [CommandProcessor(typeof(StopExecutableCommand))]
+    internal sealed partial class StopExecutableCommandProcessor : CommandProcessor {
+        /// <summary>
+        /// Initializes a new <see cref="StopExecutableCommandProcessor"/>.
+        /// </summary>
+        /// <param name="server">The server in which the processor executes.</param>
+        /// <param name="command">The <see cref="StopExecutableCommand"/> the
+        /// processor should exeucte.</param>
+        public StopExecutableCommandProcessor(ServerEngine server, ServerCommand command)
+            : base(server, command) {
+        }
+
+        /// <inheritdoc />
+        protected override void Execute() {
+            var command = this.Command as StopExecutableCommand;
+            Log.Debug(command.Executable);
+            throw ErrorCode.ToException(Error.SCERRNOTIMPLEMENTED);
+        }
+    }
+}
