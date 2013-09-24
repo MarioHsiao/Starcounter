b16fc94e07176593f72ec46d440214b1ac9831be
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-09-24 12:00:32 +0200

    QueryProcessing: fixed exception in the case of syntax error in create index statement.

--- src/Samples/QueryProcessingTest/TestErrorMessages.cs
+++ src/Samples/QueryProcessingTest/TestErrorMessages.cs
@@ -19,23 +19,7 @@ namespace QueryProcessingTest {
             RunErrorQuery("select from fro fro");
             Trace.Assert(ignored == 1);
             Trace.Assert(sqlexceptions == 1);
-            try {
-                var res = Db.SQL("select u from user u where firstname = ?", 1).First;
-            } catch (ArgumentException ex) {
-                Trace.Assert((uint)ex.Data[ErrorCode.EC_TRANSPORT_KEY] == Error.SCERRBADARGUMENTS);
-            }
-            try {
-                Db.SQL("create indx asdf on account(client)");
-            } catch (SqlException ex) {
-                Trace.Assert((uint)ex.Data[ErrorCode.EC_TRANSPORT_KEY] == Error.SCERRSQLINCORRECTSYNTAX);
-            }
-            try {
-                Db.Transaction(delegate {
-                    Db.SQL("create index indx on account (accountid)");
-                });
-            } catch (DbException ex) {
-                Trace.Assert((uint)ex.Data[ErrorCode.EC_TRANSPORT_KEY] == Error.SCERRTRANSACTIONLOCKEDONTHREAD);
-            }
+            TestSomeExceptions();
             HelpMethods.LogEvent("Finished test of error messages");
         }
 
@@ -53,6 +37,43 @@ namespace QueryProcessingTest {
                     throw;
                 ignored++;
             }
+        }
+
+        internal static void TestSomeExceptions() {
+            Boolean wasException = false;
+            try {
+                var res = Db.SQL("select u from user u where firstname = ?", 1).First;
+            } catch (ArgumentException ex) {
+                Trace.Assert((uint)ex.Data[ErrorCode.EC_TRANSPORT_KEY] == Error.SCERRBADARGUMENTS);
+                wasException = true;
+            }
+            Trace.Assert(wasException == true);
+            wasException = false;
+            try {
+                Db.SQL("create indx asdf on account(client)");
+            } catch (SqlException ex) {
+                Trace.Assert((uint)ex.Data[ErrorCode.EC_TRANSPORT_KEY] == Error.SCERRSQLINCORRECTSYNTAX);
+                wasException = true;
+            }
+            Trace.Assert(wasException == true);
+            wasException = false;
+            try {
+                Db.Transaction(delegate {
+                    Db.SQL("create index indx on account (accountid)");
+                });
+            } catch (DbException ex) {
+                Trace.Assert((uint)ex.Data[ErrorCode.EC_TRANSPORT_KEY] == Error.SCERRTRANSACTIONLOCKEDONTHREAD);
+                wasException = true;
+            }
+            Trace.Assert(wasException == true);
+            wasException = false;
+            try {
+                Db.SQL("create unique index indx on account (accouintid)");
+            } catch (SqlException) {
+                wasException = true;
+            }
+            Trace.Assert(wasException == true);
+            wasException = false;
         }
     }
 }
