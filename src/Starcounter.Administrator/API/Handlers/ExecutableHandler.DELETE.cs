a50a8710ae757333fad14f6e29cc850395a687df
Per Samuelsson <per@starcounter.com>
2013-09-25 09:45:36 +0200

    Implemented the simplest possible OnDELETE handler on the executable resource, semantically use to stop a single running executable.

--- src/Starcounter.Administrator/API/Handlers/ExecutableHandler.DELETE.cs
+++ src/Starcounter.Administrator/API/Handlers/ExecutableHandler.DELETE.cs
@@ -1,5 +1,6 @@
 ﻿
 using Starcounter.Advanced;
+using Starcounter.Server.PublicModel.Commands;
 
 namespace Starcounter.Administrator.API.Handlers {
 
@@ -18,7 +19,18 @@ namespace Starcounter.Administrator.API.Handlers {
         /// The REST request.</param>
         /// <returns>The response to be sent back to the client.</returns>
         static Response OnDELETE(string database, string executable, Request request) {
-            return 501;
+            var serverEngine = RootHandler.Host.Engine;
+            var runtime = RootHandler.Host.Runtime;
+            
+            var stop = new StopExecutableCommand(serverEngine, database, executable);
+            stop.EnableWaiting = true;
+            
+            var commandInfo = runtime.Execute(stop);
+            if (stop.EnableWaiting) {
+                commandInfo = runtime.Wait(commandInfo);
+            }
+
+            return commandInfo.HasError ? 500 : 200;
         }
     }
 }
