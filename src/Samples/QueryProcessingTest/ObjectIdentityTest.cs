e2a63ebcb52bf89b4c92e95e1f7867d381d4228c
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-10-08 16:55:05 +0200

    QueryProcessing: fixed GetObjectID and ObjectIdLookup.EvaluateToObject to use faster than json. Fixed tests. Added tests. FasterThanJson: replaced 000 with 255 in the decode table for invalid values.

--- src/Samples/QueryProcessingTest/ObjectIdentityTest.cs
+++ src/Samples/QueryProcessingTest/ObjectIdentityTest.cs
@@ -10,7 +10,7 @@ namespace QueryProcessingTest {
             Account a = Db.SQL<Account>("select a from account a").First;
             Trace.Assert(a != null);
             Trace.Assert(a.GetObjectID() != null);
-            Trace.Assert(DbHelper.Base64ForUrlDecode(a.GetObjectID()) == a.GetObjectNo());
+            Trace.Assert(DbHelper.Base64DecodeObjectID(a.GetObjectID()) == a.GetObjectNo());
             Trace.Assert(DbHelper.GetObjectNo(a) == a.GetObjectNo());
             Trace.Assert(DbHelper.GetObjectID(a) == a.GetObjectID());
             var r = Db.SQL<string>("select objectid from account");
@@ -107,6 +107,8 @@ namespace QueryProcessingTest {
             a = Db.SQL<Account>("select a from account a where objectid = ?", "").First;
             Trace.Assert(a == null);
             a = Db.SQL<Account>("select a from account a where objectid = ?", "/asdfasdfa/asdfasdf").First;
+            Trace.Assert(a == null);
+            a = Db.SQL<Account>("select a from account a where objectid = ?", "/a+").First;
             Trace.Assert(a == null);
             HelpMethods.LogEvent("Finished testing object identities");
         }
---
+++ src/Starcounter.Tests/PerformanceTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Starcounter.Internal;
+
+namespace Starcounter.Tests {
+    [TestFixture]
+    public static class PerformanceTest {
+        static void Print(String prefix, int nrIter, Stopwatch timer) {
+            Console.WriteLine(prefix + " takes " +
+                timer.ElapsedMilliseconds + " ms for " + nrIter + " times, i.e., " +
+                timer.ElapsedMilliseconds * 100000 / nrIter + " ns per conversion or " +
+                nrIter * 1000 / timer.ElapsedMilliseconds + " tps");
+            timer.Reset();
+        }
+
+        [Test]
+        [Category("LongRunning")]
+        public static void MeasureUrlGetObjectID() {
+            int nrIter = 1000000;
+            UInt64 objNo = UInt32.MaxValue;
+            String objID = "";
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < nrIter; i++)
+                objID = DbHelper.Base64ForUrlEncode(objNo);
+            timer.Stop();
+            Print("Converting " + objNo + " to " + objID, nrIter, timer);
+            UInt64 decodeObjNo = 0;
+            timer.Start();
+            for (int i = 0; i < nrIter; i++)
+                decodeObjNo = DbHelper.Base64ForUrlDecode(objID);
+            timer.Stop();
+            Print("Converting " + objID + " to " + objNo, nrIter, timer);
+            Assert.AreEqual(objNo, decodeObjNo);
+        }
+
+        [Test]
+        [Category("LongRunning")]
+        public static void MeasureFtjGetObjectID() {
+            int nrIter = 1000000;
+            UInt64 objNo = UInt32.MaxValue;
+            String objID = "";
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < nrIter; i++)
+                objID = DbHelper.Base64EncodeObjectNo(objNo);
+            timer.Stop();
+            Print("Converting " + objNo + " to " + objID, nrIter, timer);
+            UInt64 decodeObjNo = 0;
+            timer.Start();
+            for (int i = 0; i < nrIter; i++)
+                decodeObjNo = DbHelper.Base64DecodeObjectID(objID);
+            timer.Stop();
+            Print("Converting " + objID + " to " + decodeObjNo, nrIter, timer);
+            Assert.AreEqual(objNo, decodeObjNo);
+        }
+    }
+}
