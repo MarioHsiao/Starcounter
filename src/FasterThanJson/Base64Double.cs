263ead0d065a1592528d55874dbbb88b5856fc4c
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-10-11 13:30:26 +0200

    FasterThanJson: added writing and reading Double. Added simple tests.

---
+++ src/FasterThanJson/Base64Double.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Starcounter.Internal {
+    /// <summary>
+    /// The input format of Double is
+    /// 0-51 bits - mantissa
+    /// 52-62 bits - exponent
+    /// 63 bit - sign
+    /// The serialization format is at least 3 bytes:
+    /// 2 bytes contains 12 bits:
+    ///   0 bit - sign
+    ///   1-11 bits - exponent
+    /// 1 or more bytes contains mantissa (up to 11 bytes until 9 byte representation of UInt64  is implemented)
+    /// 
+    /// Nullable value will be stored as a single byte.
+    /// </summary>
+    public static class Base64Double {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] // Available starting with .NET framework version 4.5
+        public static unsafe int Write(byte* buffer, Double value) {
+            ulong valueUInt = *(ulong*)&value;
+            ulong encValue = (valueUInt << 12) | (valueUInt >> 52);
+            int len = Base64Int.Write(buffer, encValue);
+            Debug.Assert(valueUInt == ((encValue >> 12) | (encValue << 52)));
+            return len;
+        }
+
+        public static unsafe int MeasureNeededSize(Double value) {
+            return Base64Int.MeasureNeededSize(*(ulong*)&value << 12);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] // Available starting with .NET framework version 4.5
+        public static unsafe Double Read(int size, byte* buffer) {
+            ulong value = Base64Int.Read(size, buffer);
+            value = (value >> 12) | (value << 52);
+            return *(Double*)&value;
+        }
+}
+
+    public static class Base64Single {
+    }
+}
---
+++ src/FasterThanJson/FasterThanJson.Tests/TestDouble.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+using Starcounter.Internal;
+
+namespace FasterThanJson.Tests {
+    [TestFixture]
+    public static class TestDouble {
+        [Test]
+        public unsafe static void TestSimpleDouble() {
+            fixed (byte* buffer = new byte[13]) {
+                Double value = 0;
+                int size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(1, size);
+                value = -1;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(2, size);
+                value = 1;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(2, size);
+                value = -0.02;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(11, size);
+                value = 1.02;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(11, size);
+                value = -0;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Double.MaxValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = value / 1000000000;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Double.MinValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = value / 1000000000;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Double.NaN;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Double.NegativeInfinity;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Double.PositiveInfinity;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = (Double)X6Decimal.MaxDecimalValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = (Double)X6Decimal.MinDecimalValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+            }
+        }
+    }
+}
