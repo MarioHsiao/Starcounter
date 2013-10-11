5017708744b986319b1800c2f6c970f37b5c2b8f
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-10-11 16:37:04 +0200

    FasterThanJson: fixed bug in measuring needed size for Double. Added fast tuple writer and reader for Single. Added test.

--- src/FasterThanJson/FasterThanJson.Tests/TestDouble.cs
+++ src/FasterThanJson/FasterThanJson.Tests/TestDouble.cs
@@ -120,5 +120,61 @@ namespace FasterThanJson.Tests {
                 Assert.AreEqual(1, size);
             }
         }
+
+        [Test]
+        public unsafe static void TestSimpleSingle() {
+            fixed (byte* buffer = new byte[11]) {
+                Single value = 0;
+                int size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(1, size);
+                value = -1;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(2, size);
+                value = 1;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(2, size);
+                value = -0.02f;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(11, size);
+                value = 1.02f;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                Assert.AreEqual(11, size);
+                value = -0;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Single.MaxValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = value / 1000000000;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Single.MinValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = value / 1000000000;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Single.NaN;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Single.NegativeInfinity;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = Single.PositiveInfinity;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = (Single)X6Decimal.MaxDecimalValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+                value = (Single)X6Decimal.MinDecimalValue;
+                size = Base64Double.Write(buffer, value);
+                Assert.AreEqual(value, Base64Double.Read(size, buffer));
+            }
+        }
     }
 }
