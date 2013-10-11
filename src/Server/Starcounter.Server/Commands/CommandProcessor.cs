7059de1deb130bb8e896aec2f977fd776ea0666f
Per Samuelsson <per@starcounter.com>
2013-10-11 09:58:12 +0200

    Extended the server library command processor base class to write traces to the timestamp file (if any) and to trace output for every started and ended task.

--- src/Server/Starcounter.Server/Commands/CommandProcessor.cs
+++ src/Server/Starcounter.Server/Commands/CommandProcessor.cs
@@ -411,6 +411,8 @@ namespace Starcounter.Server.Commands {
             message = string.Format(message, args);
             if (restartWatch) stopwatch.Restart();
             Diagnostics.WriteTrace("server", stopwatch.ElapsedTicks, message);
+
+            Diagnostics.WriteTimeStamp("SERVER", message);
         }
 
         #region Progress tracking
@@ -436,6 +438,8 @@ namespace Starcounter.Server.Commands {
 
             if (task.Duration.IsDeterminate())
                 throw new InvalidOperationException();
+
+            Trace("Begin task '{0}'", false, task.ShortText);
 
             progressInfo = new ProgressInfo(task.ID, 0, -1, null);
 
@@ -602,13 +606,7 @@ namespace Starcounter.Server.Commands {
         /// </summary>
         /// <param name="task">The <see cref="CommandTask"/> to cancel.
         protected void CancelTask(CommandTask task) {
-            ProgressInfo info;
-
-            info = this.progress[task.ID];
-            if (info.IsCompleted) throw new InvalidOperationException();
-            info.Cancel();
-
-            NotifyStatusChanged();
+            EndTask(task, true);
         }
 
         /// <summary>
@@ -619,6 +617,8 @@ namespace Starcounter.Server.Commands {
         /// be marked as cancelled or fulfilled.</param>
         protected void EndTask(CommandTask task, bool cancel = false) {
             ProgressInfo info;
+
+            Trace("End task (cancelled={0})", false, cancel);
 
             info = this.progress[task.ID];
             EndSingleProgress(info, cancel);
