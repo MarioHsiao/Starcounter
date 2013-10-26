f5ce25d0a40da9c1ecd9d30e01a4015d8671b491
alemoi <alexey.moiseenko@starcounter.com>
2013-10-26 19:11:08 +0200

    Added verifications for handlers registrations. Added unit tests.

--- src/Starcounter.Apps.JsonPatch/BuiltInRestHandlers/SqlRestHandler.cs
+++ src/Starcounter.Apps.JsonPatch/BuiltInRestHandlers/SqlRestHandler.cs
@@ -18,6 +18,7 @@ using Starcounter.Query.Execution;
 using jp = Starcounter.Internal.JsonPatch;
 using System.Runtime.CompilerServices;
 using Starcounter.Templates;
+using Starcounter.Internal.Uri;
 
 [assembly: InternalsVisibleTo("Starcounter.Bootstrap, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e758955f5e1537c52891c61cd689a8dd1643807340bd32cc12aee50d2add85eeeaac0b44a796cefb6055fac91836a8a72b5dbf3b44138f508bc2d92798a618ad5791ff0db51b8662d7c936c16b5720b075b2a966bb36844d375c1481c2c7dc4bb54f6d72dbe9d33712aacb6fa0ad84f04bfa6c951f7b9432fe820884c81d67db")]
 
@@ -109,7 +110,7 @@ namespace Starcounter.Internal {
             // for each new port that is used.
             registeredPorts.Add(port);
 
-            Handle.GET(port, ScSessionClass.DataLocationUriPrefix + "{?}", (Session session) => {
+            Handle.GET(port, ScSessionClass.DataLocationUriPrefix + Handle.UriParameterIndicator, (Session session) => {
 
                 Debug.Assert(null != session);
 
@@ -125,7 +126,7 @@ namespace Starcounter.Internal {
             });
 
 
-            Handle.PATCH(port, ScSessionClass.DataLocationUriPrefix + "{?}", (Session session, Request request) => {
+            Handle.PATCH(port, ScSessionClass.DataLocationUriPrefix + Handle.UriParameterIndicator, (Session session, Request request) => {
 
                 Debug.Assert(null != session);
 
