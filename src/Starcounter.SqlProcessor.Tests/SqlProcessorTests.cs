638cbe306225d8176dc8e59d5c5d26ca045ebdef
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-10-29 08:37:54 +0100

    SqlProcessor: calling native SqlProcessor. Testing.

--- src/Starcounter.SqlProcessor.Tests/SqlProcessorTests.cs
+++ src/Starcounter.SqlProcessor.Tests/SqlProcessorTests.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using NUnit.Framework;
+using Starcounter.SqlProcessor;
 
 namespace Starcounter.SqlProcessor.Tests {
     [TestFixture]
     public class SqlProcessorTests {
         [Test]
         public static void HelloProcessor() {
+            Assert.AreEqual(0, SqlProcessor.CallSqlProcessor(""));
         }
     }
 }
