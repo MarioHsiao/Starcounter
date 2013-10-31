85f290fc8e4f4c5957c62a32e9226d535d69f80c
Per Samuelsson <per@starcounter.com>
2013-10-31 08:34:07 +0100

    Extended Starcounter.Server library with a Starcounter.Server.Windows namespace, containing types relating to platform-specifics we depend on when turning to a deployment model where the server is installed as a service.

---
+++ src/Server/Starcounter.Server/Windows/WindowsExceptionHelper.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.ComponentModel;
+
+namespace Starcounter.Server.Windows {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class WindowsExceptionHelper {
+        public static bool IsOriginatingFromNativeError(Exception exception, int windowsErrorCode) {
+            int? result = TryGetNativeErrorCode(exception);
+            return result.HasValue && result.Value == windowsErrorCode;
+        }
+
+        static int? TryGetNativeErrorCode(Exception exception, bool recursive = true) {
+            if (exception == null) throw new ArgumentNullException("exception");
+
+            var windowsException = exception as Win32Exception;
+            for (; ; ) {
+                if (windowsException == null) {
+                    if (recursive == false)
+                        break;
+
+                    exception = exception.InnerException;
+                    if (exception == null)
+                        break;
+                }
+            }
+
+            if (windowsException == null) return null;
+            return windowsException.NativeErrorCode;
+        }
+    }
+}
