d164e5475ee12849442480766e31fd6421ec25e9
Per Samuelsson <per@starcounter.com>
2013-10-31 08:34:07 +0100

    Extended Starcounter.Server library with a Starcounter.Server.Windows namespace, containing types relating to platform-specifics we depend on when turning to a deployment model where the server is installed as a service.

---
+++ src/Server/Starcounter.Server/Windows/StartupType.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Starcounter.Server.Windows {
+
+    public enum StartupType: uint {
+        Automatic = Win32Service.SERVICE_START.SERVICE_AUTO_START,
+        Manual = Win32Service.SERVICE_START.SERVICE_DEMAND_START,
+        Disabled = Win32Service.SERVICE_START.SERVICE_DISABLED
+    }
+}
