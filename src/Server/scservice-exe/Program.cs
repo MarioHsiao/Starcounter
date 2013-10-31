66af8304f08a2de1c627b456424da15aa6dd5d71
Per Samuelsson <per@starcounter.com>
2013-10-31 11:38:43 +0100

    Made the new, managed scservice.exe invoke the principal entrypoint of the scservice.dll library.

--- src/Server/scservice-exe/Program.cs
+++ src/Server/scservice-exe/Program.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.Runtime.InteropServices;
 
 namespace scservice {
+
     class Program {
+        [DllImport("scservice.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern unsafe int wmain(int argumentCount, string[] args, string[] ignored);
+
         static void Main(string[] args) {
             Console.WriteLine("Booh!");
+            Environment.ExitCode = wmain(args.Length, args, null);
         }
     }
 }
