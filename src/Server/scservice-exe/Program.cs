c620a747e75d0599c50b2fd7f368ea1f8081de41
Per Samuelsson <per@starcounter.com>
2013-11-01 09:42:45 +0100

    Implemented the first full entrypoint of the "scservice-exe" project, now having argument parsing moved there and it properly launching the system server in the right manner.

--- src/Server/scservice-exe/Program.cs
+++ src/Server/scservice-exe/Program.cs
@@ -1,16 +1,63 @@
 ﻿
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.ServiceProcess;
 
 namespace scservice {
 
     class Program {
-        [DllImport("scservice.dll", CallingConvention = CallingConvention.StdCall)]
-        static extern unsafe int wmain(int argumentCount, string[] args, string[] ignored);
 
         static void Main(string[] args) {
-            Console.WriteLine("Booh!");
-            Environment.ExitCode = wmain(args.Length, args, null);
+            var startedAsService = IsStartedAsService(args);
+            string serverName = "Personal";
+            bool logSteps = false;
+
+            if (args.Length > 2) {
+                if (!startedAsService) {
+                    Usage();
+                }
+                return;
+            }
+
+            var ignoreCase = StringComparison.InvariantCultureIgnoreCase;
+            for (int i = 0; i < args.Length; i++) {
+                var arg = args[i];
+                if (arg.Equals("--logsteps", ignoreCase)) {
+                    logSteps = true;
+                } else if (arg.Equals("?") || arg.Equals("-h") || arg.Equals("--help")) {
+                    if (!startedAsService) {
+                        Usage();
+                    }
+                    return;
+                } else {
+                    serverName = arg;
+                }
+            }
+
+            var serviceProcess = new SystemServerProcess(serverName) {
+                LogSteps = logSteps
+            };
+            serviceProcess.Launch(startedAsService);
+        }
+
+        static bool IsStartedAsService(string[] args) {
+            return Process.GetCurrentProcess().SessionId == 0;
+        }
+
+        static void Usage() {
+            Console.WriteLine("scservice.exe [ServerName] [--logsteps]");
+            Console.WriteLine("Example: scservice.exe personal");
+            Console.WriteLine("When no ServerName argument is supplied 'personal' is used.");
+            Console.WriteLine();
+            Console.WriteLine("How it works:");
+            Console.WriteLine("scservice will load XML-file called [ServerName].xml");
+            Console.WriteLine("from the same directory as scservice.exe and");
+            Console.WriteLine("will fetch corresponding server directory from it.");
+            Console.WriteLine("From obtained directory it will load [ServerName].config.xml");
+            Console.WriteLine("to read server-related settings.");
+            Console.WriteLine("scservice will then start and monitor all required");
+            Console.WriteLine("Starcounter components, like scnetworkgateway, scipcmonitor, etc.");
         }
     }
 }
