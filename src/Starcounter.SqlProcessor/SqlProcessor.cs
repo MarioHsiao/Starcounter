0ca1ad8763db00b8139606c7e0e5b0daa3aa6862
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2013-11-04 09:39:35 +0100

    SqlProcessor: updated test on error codes to test interface functions directly.

--- src/Starcounter.SqlProcessor/SqlProcessor.cs
+++ src/Starcounter.SqlProcessor/SqlProcessor.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 namespace Starcounter.SqlProcessor {
     public class SqlProcessor {
         [DllImport("scsqlprocessor.dll")]
-        internal static unsafe extern uint scsql_process_query([MarshalAs(UnmanagedType.LPWStr)]string query);
+        public static unsafe extern uint scsql_process_query([MarshalAs(UnmanagedType.LPWStr)]string query);
             /*void* caller, void* executor, */
         [DllImport("scsqlprocessor.dll")]
-        internal static unsafe extern ScError* scsql_get_error();
+        public static unsafe extern ScError* scsql_get_error();
         [DllImport("scsqlprocessor.dll")]
-        internal static unsafe extern uint scsql_free_memory();
+        public static unsafe extern uint scsql_free_memory();
 
         public static unsafe uint CallSqlProcessor(String query) {
             uint err = scsql_process_query(query);
@@ -24,14 +24,14 @@ namespace Starcounter.SqlProcessor {
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    internal unsafe struct ScError {
-        internal uint scerrorcode;
-        internal sbyte* scerrmessage;
-        internal int scerrposition;
-        internal byte isKeyword;
+    public unsafe struct ScError {
+        public uint scerrorcode;
+        public sbyte* scerrmessage;
+        public int scerrposition;
+        public byte isKeyword;
         private IntPtr _token;
 
-        internal String token {
+        public String token {
             get {
                 return Marshal.PtrToStringAuto(_token);
             }
