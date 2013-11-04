fbfb4ee516268239fc9658a4960db002458608e0
Christian Holmstrand <christian.holmstrand@starcounter.com>
2013-11-04 17:03:25 +0100

    Readded this[int index] on generic array.

--- src/Starcounter.XSON/Class Json/Json.Array.Generic.cs
+++ src/Starcounter.XSON/Class Json/Json.Array.Generic.cs
@@ -112,29 +112,13 @@ namespace Starcounter {
         /// </summary>
         /// <param name="index"></param>
         /// <returns></returns>
-//		public new T this[int index] {
-//			get {
-//#if QUICKTUPLE
-//				return (T)list[index];
-//#else
-//			throw new NotImplementedException();
-//#endif
-//			}
-//			set {
-//				list[index] = value;
-//				var s = Session;
-//				if (Session != null) {
-//					if (ArrayAddsAndDeletes == null) {
-//						ArrayAddsAndDeletes = new List<Change>();
-//					}
-//					this._CallHasChanged((TObjArr)this.Template, index);
-////                    this._
-////                    Changes.Add(Change.Add((Obj)this.Parent, (TObjArr)this.Template, index));
-//				}
-//			}
-//		}
-
-
+		public new T this[int index] {
+			get {
+				return (T)base[index];
+			}
+			set {
+				base[index] = value;
+			}
+		}
     }
-
 }
