5532e32269c586401aa0315f56cfc5dabfbf184b
Per Samuelsson <per@starcounter.com>
2013-11-05 10:25:52 +0100

    Extended the mock service installer/uninstaller in staradmin.exe to properly adjust the environment before installing and after uninstalling the service.

--- src/Server/staradmin/SystemServiceInstall.cs
+++ src/Server/staradmin/SystemServiceInstall.cs
@@ -1,13 +1,16 @@
 ﻿
+using Starcounter.Internal;
 using Starcounter.Server.Service;
 using Starcounter.Server.Setup;
 using Starcounter.Server.Windows;
+using System;
 
 namespace staradmin {
 
     internal static class SystemServiceInstall {
 
         internal static void Install() {
+            PreInstall();
             var setup = new SystemServiceSetup();
             setup.StartupType = StartupType.Manual;
             setup.Execute();
@@ -15,6 +18,22 @@ namespace staradmin {
 
         internal static void Uninstall() {
             SystemServerService.Delete(SystemServerService.Name);
+            PostUnInstall();
+        }
+
+        static void PreInstall() {
+            var installDir = Environment.GetEnvironmentVariable(StarcounterEnvironment.VariableNames.InstallationDirectory);
+            if (string.IsNullOrEmpty(installDir)) {
+                throw new InvalidOperationException();
+            }
+            Environment.SetEnvironmentVariable(StarcounterEnvironment.VariableNames.InstallationDirectory, installDir, EnvironmentVariableTarget.Machine);
+        }
+
+        static void PostUnInstall() {
+            var x = Environment.GetEnvironmentVariable(StarcounterEnvironment.VariableNames.InstallationDirectory, EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(x)) {
+                Environment.SetEnvironmentVariable(StarcounterEnvironment.VariableNames.InstallationDirectory, null, EnvironmentVariableTarget.Machine);
+            }
         }
     }
 }
