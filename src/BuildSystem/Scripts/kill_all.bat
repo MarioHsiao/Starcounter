000765312e8f6a0c79a36ab720265ebb611bedd0
alemoi <alexey.moiseenko@starcounter.com>
2013-11-07 00:53:23 +0100

    Changed kill_all.bat to use staradmin. Undefined GW_IOCP_IMMEDIATE_COMPLETION.

--- src/BuildSystem/Scripts/kill_all.bat
+++ src/BuildSystem/Scripts/kill_all.bat
@@ -1,15 +1,2 @@
 :: Killing all Starcounter-related processes if any are running.
-TASKKILL /f /t /im sccode.exe
-TASKKILL /f /t /im scdata.exe
-TASKKILL /f /t /im scdblog.exe
-TASKKILL /f /t /im scnetworkgateway.exe
-TASKKILL /f /t /im scnetworkgatewayloopedtest.exe
-TASKKILL /f /t /im scipcmonitor.exe
-TASKKILL /f /t /im scadminserver.exe
-TASKKILL /f /t /im scweaver.exe
-TASKKILL /f /t /im scsqlparser.exe
-
-:: Sleeping some time using ping.
-ping -n 3 127.0.0.1 > nul
-
-::IF "%SC_RUNNING_ON_BUILD_SERVER%"=="True" EXIT 0
+staradmin.exe -killall
