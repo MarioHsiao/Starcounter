21cdf73ddf03051f4a9a5ca9d352b8a6c933a7e3
Per Samuelsson <per@starcounter.com>
2013-11-08 15:15:56 +0100

    Added class ServerHandler to Starcounter.Administrator, aimed to handle requests targeting the server itself (i.e. normally "/api/server").

---
+++ src/Starcounter.Administrator/API/Handlers/ServerHandler.cs
@@ -0,0 +1,17 @@
+﻿using Starcounter.Advanced;
+
+namespace Starcounter.Administrator.API.Handlers {
+    /// <summary>
+    /// Provides the handlers for the admin server database resource.
+    /// </summary>
+    internal static partial class ServerHandler {
+        /// <summary>
+        /// Install handlers for the resource represented by this class and
+        /// performs custom setup.
+        /// </summary>
+        internal static void Setup() {
+            var uri = RootHandler.API.Uris.Server;
+            RootHandler.Register405OnAllUnsupported(uri, new string[] {});
+        }
+    }
+}
