5c0da061b0ec15b13780b256178948fa3bc7dd5f
andwah <anders.wahlgren@societyobjects.com>
2013-11-11 16:27:49 +0100

    Added the use of a BackgroundWorker to poll (this is a "workaround" the get the events on the same thread as the GUI thread)

--- src/Programs/Starcounter.TrayIcon/Service.cs
+++ src/Programs/Starcounter.TrayIcon/Service.cs
@@ -2,6 +2,7 @@
 using Starcounter.Advanced;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -33,6 +34,14 @@ namespace Starcounter.Tools {
         /// </summary>
         public event ChangedEventHandler Changed;
 
+        private System.Windows.Forms.Timer Timer;
+
+        /// <summary>
+        /// Background worker is used to make sure the callback is on the same thread as the GUI
+        /// </summary>
+        private BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+
         /// <summary>
         /// 
         /// </summary>
@@ -40,10 +49,14 @@ namespace Starcounter.Tools {
 
             this.Port = port;
 
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 2000; // Poll intervall 
-            timer.Elapsed += timer_Elapsed;
-            timer.Start();
+            this.Timer = new System.Windows.Forms.Timer();
+            this.Timer.Interval = 2000; // Poll intervall 
+            this.Timer.Tick += timer_Tick;
+
+            this.backgroundWorker.DoWork += new DoWorkEventHandler(BackgroundWorkerWork);
+            this.backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorkerCompleted);
+            this.backgroundWorker.RunWorkerAsync();
+   
         }
 
 
@@ -52,39 +65,77 @@ namespace Starcounter.Tools {
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void timer_Elapsed(object sender, ElapsedEventArgs e) {
+        private void timer_Tick(object sender, EventArgs e) {
+            System.Windows.Forms.Timer timer = sender as System.Windows.Forms.Timer;
+            this.Timer.Stop();
+            backgroundWorker.RunWorkerAsync();
+        }
 
-            System.Timers.Timer timer = sender as System.Timers.Timer;
-            timer.Stop();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackgroundWorkerWork(object sender, DoWorkEventArgs e) {
+            BackgroundWorker worker = sender as BackgroundWorker;
 
             string url = string.Format("{0}:{1}{2}", "127.0.0.1", this.Port, "/api/server");
 
-            X.GET(url, null, null, (Response respAsync, Object userObject) => {
+            StatusEventArgs status;
 
-                timer.Enabled = true;
+            Response response; 
+            X.GET(url, null, out response, 10000);
 
-                if (respAsync.IsSuccessStatusCode) {
-                    bool isService = false;
+            if (response.IsSuccessStatusCode) {
+                bool isService = false;
 
-                    try {
-                        dynamic incomingJson = DynamicJson.Parse(respAsync.Body);
-                        bool bValid = incomingJson.IsDefined("Context");
-                        if (bValid) {
-                            string context = incomingJson.Context;
-                            isService = !context.Contains('@');
-                        }
+                try {
+                    dynamic incomingJson = DynamicJson.Parse(response.Body);
+                    bool bValid = incomingJson.IsDefined("Context");
+                    if (bValid) {
+                        string context = incomingJson.Context;
+                        isService = !context.Contains('@');
                     }
-                    catch (Exception) {
-                        // TODO: Log?
-                    }
-
-                    OnChanged(new StatusEventArgs() { Connected = true, IsService = isService });
                 }
-                else {
-                    OnChanged(new StatusEventArgs() { Connected = false });
+                catch (Exception) {
+                    // TODO: Log?
                 }
 
-            }, 10000); // 10 Sec timeout
+                status = new StatusEventArgs() { Connected = true, IsService = isService };
+
+            }
+            else {
+                status = new StatusEventArgs() { Connected = false };
+
+            }
+
+            e.Result = status;
+
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackgroundWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            this.Timer.Enabled = true;
+            OnChanged(e.Result as StatusEventArgs);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e) {
+            System.Timers.Timer timer = sender as System.Timers.Timer;
+            timer.Stop();
+            this.backgroundWorker.RunWorkerAsync();
         }
 
 
