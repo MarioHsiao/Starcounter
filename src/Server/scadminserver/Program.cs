dfa7ebca12c57042899586354888dbf0e4724949
Per Samuelsson <per@starcounter.com>
2013-11-11 20:08:30 +0100

    Workaround in scadminserver executable to make sure the admin server really exits when the entrypoint returns (currently, it doesn't because of a background thread with usage statistics that runs forever).

--- src/Server/scadminserver/Program.cs
+++ src/Server/scadminserver/Program.cs
@@ -1,12 +1,14 @@
 ﻿
 using Starcounter.Internal;
 using StarcounterInternal.Bootstrap;
+using System;
 
 namespace scadminserver {
     class Program {
         static void Main(string[] args) {
             Diagnostics.WriteTimeStamp("SCADMINSERVER", "Started scadminserver Main()");
             Control.Main(args);
+            Environment.Exit(Environment.ExitCode);
         }
     }
 }
