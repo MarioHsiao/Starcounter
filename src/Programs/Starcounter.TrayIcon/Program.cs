29b654eb3dbbf11c1da9dfe4ab87bf4568e31570
andwah <anders.wahlgren@societyobjects.com>
2013-11-11 15:24:52 +0100

    Added Server/Service running status

--- src/Programs/Starcounter.TrayIcon/Program.cs
+++ src/Programs/Starcounter.TrayIcon/Program.cs
@@ -1,7 +1,10 @@
 ﻿
+using Starcounter.Internal;
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Starcounter.Tools {
@@ -16,18 +19,23 @@ namespace Starcounter.Tools {
     public class TrayIconApp : ApplicationContext {
 
         #region Private Members
-        private System.ComponentModel.IContainer mComponents;
+        private System.ComponentModel.IContainer applicationContainer;
         private NotifyIcon mNotifyIcon;
         private ContextMenuStrip mContextMenu;
         private ToolStripMenuItem mDisplayForm;
         private ToolStripMenuItem mExitApplication;
+        private Icon Connected;
+        private Icon Disconnected;
+
+        private ushort Port;
+
         #endregion
 
         /// <summary>
         /// 
         /// </summary>
         [STAThread]
-        public static void Main() {
+        public static void Main(string[] args) {
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
@@ -38,16 +46,70 @@ namespace Starcounter.Tools {
         /// <summary>
         /// 
         /// </summary>
-        public TrayIconApp() {     
+        public TrayIconApp() {
+
+            Application.ThreadException += Application_ThreadException;
+            Application.ApplicationExit += Application_ApplicationExit;
+
 
             //Instantiate the component Module to hold everything    
-            mComponents = new System.ComponentModel.Container();
+            this.applicationContainer = new System.ComponentModel.Container();
+
+            this.SetupPort();
+
+            // Setup icons
+            this.SetupIcons();
+
+            // Create System tray 
+            this.CreateSystemTray();
+
+            // Setup and start polling service
+            Service service = new Service();
+            service.Changed += service_Changed;
+
+            service.Start(this.Port);
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetupPort() {
+
+            ushort port;
+            string errorMessage;
+            bool result = Utils.GetPort(out port, out errorMessage);
+            if (result == false) {
+                MessageBox.Show(errorMessage, "Starcounter scTrayIcon Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Port = Starcounter.Internal.StarcounterConstants.NetworkPorts.DefaultPersonalServerSystemHttpPort;
+            }
+            else {
+                this.Port = port;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void service_Changed(object sender, StatusEventArgs e) {
+            this.SetStatus(e);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CreateSystemTray() {
 
             //Instantiate the NotifyIcon attaching it to the components container and    
             //provide it an icon, note, you can imbed this resource   
-            mNotifyIcon = new NotifyIcon(this.mComponents);
-            mNotifyIcon.Icon = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Starcounter.Tools.sc.ico"));
-            mNotifyIcon.Text = "Starcounter";
+            mNotifyIcon = new NotifyIcon(this.applicationContainer);
+            //mNotifyIcon.Icon = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Starcounter.Tools.sc.ico"));
+            //mNotifyIcon.Icon = new Icon(this.GetType(), "sc.ico");
+            mNotifyIcon.Icon = this.Disconnected;
+            mNotifyIcon.Text = "Starcounter" + Environment.NewLine + "Connecting...";
             mNotifyIcon.Visible = true;
 
             //Instantiate the context menu and items   
@@ -59,7 +121,7 @@ namespace Starcounter.Tools {
             mNotifyIcon.ContextMenuStrip = mContextMenu;
 
             //Setup the items and add them to the menu strip, adding handlers to be created later   
-            mDisplayForm.Text = "Show Administrator";
+            mDisplayForm.Text = "Administrator";
             mDisplayForm.Click += new EventHandler(mDisplayForm_Click);
             mContextMenu.Items.Add(mDisplayForm);
             mContextMenu.Items.Add("-");
@@ -70,29 +132,122 @@ namespace Starcounter.Tools {
 
 
         /// <summary>
+        /// Setup icons
+        /// </summary>
+        private void SetupIcons() {
+
+            // http://stackoverflow.com/questions/2338518/distorted-system-tray-icons
+
+            // Setup icons
+            this.Connected = new Icon(this.GetType(), "sc_logo.ico");
+            //this.Connected = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Starcounter.Tools.sc_logo.ico"),16,16);
+
+            Image img = this.Connected.ToBitmap();
+            Image geyImage = Utils.MakeGrayscale(img);
+            Bitmap bmp = new Bitmap(geyImage);
+            this.Disconnected = System.Drawing.Icon.FromHandle(bmp.GetHicon());
+
+            System.Drawing.Image canvas = new Bitmap(this.Connected.Width, this.Connected.Height);
+            Graphics artist = Graphics.FromImage(canvas);
+            artist.DrawString("H", new Font("Arial", 4), System.Drawing.Brushes.Black, (float)(this.Connected.Width), (float)(this.Connected.Height));
+            artist.Save();
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="statusArgs"></param>
+        private void SetStatus(StatusEventArgs statusArgs) {
+
+            mNotifyIcon.Text = "Starcounter " + CurrentVersion.Version + Environment.NewLine;
+
+            if (statusArgs.Connected) {
+                mNotifyIcon.Icon = this.Connected;
+
+                if (statusArgs.IsService) {
+                    mNotifyIcon.Text += "Service running";
+                }
+                else {
+                    mNotifyIcon.Text += "Server running";
+                }
+            }
+            else {
+                mNotifyIcon.Icon = this.Disconnected;
+                mNotifyIcon.Text += "Not running";
+            }
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Application_ApplicationExit(object sender, EventArgs e) {
+            CleanUpResources();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e) {
+            CleanUpResources();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CleanUpResources() {
+            mNotifyIcon.Dispose();
+        }
+
+
+        /// <summary>
         /// User choosed the 'Exit' menu choice
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void mExitApplication_Click(object sender, EventArgs e) {
-
+        private void mExitApplication_Click(object sender, EventArgs e) {
             // Exit
             ExitThreadCore();
         }
 
-         
+
         /// <summary>
         /// User choosed the 'Show Administrator' menu choice
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void mDisplayForm_Click(object sender, EventArgs e) {
-            Process p = new Process();
-            p.StartInfo = new ProcessStartInfo("http://localhost:8181");
-            p.Start();
+        private void mDisplayForm_Click(object sender, EventArgs e) {
+
+            string parameter = string.Format("http://127.0.0.1:{0}", this.Port);
+
+            try {
+                Process.Start(new ProcessStartInfo(parameter));
+            }
+            catch (Win32Exception) {
+
+                try {
+                    Process.Start(new ProcessStartInfo("explorer.exe", parameter));
+                }
+                catch (Win32Exception ee) {
+                    string message = "Can not open external browser." + Environment.NewLine + ee.Message + Environment.NewLine + parameter;
+                    Console.WriteLine(message);
+                }
+
+            }
+
         }
 
     }
+
 
 
 }
---
+++ src/Programs/Starcounter.TrayIcon/Utils.cs
@@ -0,0 +1,187 @@
+﻿using Starcounter.Advanced;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Tools {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Utils {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="original"></param>
+        /// <returns></returns>
+        public static Image MakeGrayscale(Image original) {
+            Image newBitmap = new Bitmap(original.Width, original.Height);
+            Graphics g = Graphics.FromImage(newBitmap);
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][] 
+                {
+                    new float[] {0.299f, 0.299f, 0.299f, 0, 0},
+                    new float[] {0.587f, 0.587f, 0.587f, 0, 0},
+                    new float[] {.114f, .114f, .114f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+                });
+
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(colorMatrix);
+            g.DrawImage(
+                original,
+                new Rectangle(0, 0, original.Width, original.Height),
+                0, 0, original.Width, original.Height,
+                GraphicsUnit.Pixel, attributes);
+
+            g.Dispose();
+            return newBitmap;
+        }
+
+        //public static BitmapSource GenerateBitmapSource(Visual visual, double renderWidth, double renderHeight) {
+        //    var bmp = new RenderTargetBitmap((int)renderWidth, (int)renderHeight, 96, 96, PixelFormats.Pbgra32);
+        //    var dv = new DrawingVisual();
+        //    using (DrawingContext dc = dv.RenderOpen()) {
+        //        dc.DrawRectangle(new VisualBrush(visual), null, new Rect(0, 0, renderWidth, renderHeight));   
+        //    }
+        //    bmp.Render(dv);
+        //    return bmp;
+        //}
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static bool GetPort(out ushort port, out string error) {
+
+            port = 0;
+            error = null;
+
+            string file = "personal.xml";
+            string serverDir;
+
+            bool result = ReadConfiguration(file, out serverDir, out error);
+            if (result == false) {
+                return false;
+            }
+
+            string serverConfig = Path.Combine(serverDir, "Personal.server.config");
+
+            result = ReadServerConfiguration(serverConfig, out port, out error);
+            if (result == false) {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="serverDir"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        private static bool ReadConfiguration(string file, out string serverDir, out string error) {
+
+            string result;
+            serverDir = null;
+            error = null;
+
+            bool success = ReadConfigFile(file, "server-dir", out result, out error);
+            if (success) {
+
+                serverDir = result;
+                if (!Directory.Exists(serverDir)) {
+                    error = string.Format("Invalid server folder {0} ", serverDir);
+                    return false;
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="port"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        private static bool ReadServerConfiguration(string file, out ushort port, out string error) {
+
+            string result;
+            port = 0;
+            error = null;
+
+            bool success = ReadConfigFile(file, "SystemHttpPort", out result, out error);
+            if (success) {
+                if (ushort.TryParse(result, out port)) {
+                    if (port > IPEndPoint.MaxPort || port < IPEndPoint.MinPort) {
+                        error = string.Format("Invalid port number {0}.", port);
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="tag"></param>
+        /// <param name="result"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        private static bool ReadConfigFile(string file, string tag, out string result, out string error) {
+
+            result = null;
+            error = null;
+
+            if (!File.Exists(file)) {
+                error = string.Format("Missing configuration file {0}.", file);
+                return false;
+            }
+
+            string[] lines = File.ReadAllLines(file);
+            foreach (string line in lines) {
+
+                if (line.StartsWith("//")) continue;
+
+
+
+                int startIndex = line.IndexOf("<" + tag + ">", StringComparison.CurrentCultureIgnoreCase);
+                if (startIndex != -1) {
+
+                    int len = tag.Length + 2;
+                    int endIndex = line.IndexOf("</" + tag + ">", startIndex + len, StringComparison.CurrentCultureIgnoreCase);
+
+                    result = line.Substring(startIndex + len, endIndex - (startIndex + len));
+                    return true;
+                }
+            }
+
+            error = string.Format("Failed to find the <{0}> tag in {1}", tag, file);
+            return false;
+        }
+
+
+    }
+}
