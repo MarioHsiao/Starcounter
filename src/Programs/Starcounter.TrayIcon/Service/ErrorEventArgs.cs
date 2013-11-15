7fb7a5c840e2eec9cfc9f67ca2b3953d17cea095
andwah <anders.wahlgren@societyobjects.com>
2013-11-15 15:40:45 +0100

    Added Shutdown function to the trayicon
    Added doubleclick on trayicon to open Starcounter administrator

---
+++ src/Programs/Starcounter.TrayIcon/Service/ErrorEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Tools.Service {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ErrorEventArgs : EventArgs {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool HasError {
+            get {
+                return !string.IsNullOrEmpty(this.ErrorMessage);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+
+    }
+
+}
---
+++ src/Programs/Starcounter.TrayIcon/Service/StatusTask.cs
@@ -0,0 +1,43 @@
+﻿using Codeplex.Data;
+using Starcounter.Advanced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Tools.Service.Task {
+    internal class StatusTask {
+
+        public static void Execute(StarcounterService service, out StatusEventArgs status) {
+
+            string url = string.Format("{0}:{1}{2}", service.IPAddress, service.Port, "/api/server");
+
+            Response response;
+            X.GET(url, null, out response, 10000);
+
+            if (response.IsSuccessStatusCode) {
+                bool interactiveMode = false;
+
+                try {
+                    dynamic incomingJson = DynamicJson.Parse(response.Body);
+                    bool bValid = incomingJson.IsDefined("Context");
+                    if (bValid) {
+                        string context = incomingJson.Context;
+                        interactiveMode = context.Contains('@');
+                    }
+                }
+                catch (Exception e) {
+                    throw new Exception(e.ToString());
+                }
+
+                status = new StatusEventArgs() { Connected = true, InteractiveMode = interactiveMode };
+
+            }
+            else {
+                status = new StatusEventArgs() { Connected = false };
+            }
+        }
+
+    }
+}
