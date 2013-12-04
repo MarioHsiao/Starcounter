eb1c80bcefdcb5c0a6af48212744593b2b67f821
alemoi <alexey.moiseenko@starcounter.com>
2013-12-04 20:20:29 +0100

    Removed some predefined script lines in tests.

--- src/Samples/NodeTest/run_node_proxy_test.bat
+++ src/Samples/NodeTest/run_node_proxy_test.bat
@@ -1,21 +1,20 @@
 :: Checking if test should be run.
 IF "%SC_RUN_NODE_TEST_PROXY%"=="False" GOTO :EOF
 
+SET SERVER_DIR=.srv
+
 :: Killing all processes.
 staradmin -killall
 
 :: Creating repository if it does not exist.
-IF NOT EXIST ".srv" star.exe @@CreateRepo .srv
-COPY /Y scnetworkgateway_proxy_test.xml .srv\personal\scnetworkgateway.xml
-
-:: Setting StarcounterBin as current directory.
-SET StarcounterBin=%CD%
+IF NOT EXIST "%SERVER_DIR%" GOTO TESTFAILED
+COPY /Y scnetworkgateway_proxy_test.xml %SERVER_DIR%\personal\scnetworkgateway.xml
 
 :: Starting service in background.
 START CMD /C "scservice.exe"
 
 :: Waiting for test to initialize.
-ping -n 7 127.0.0.1 > nul
+ping -n 10 127.0.0.1 > nul
 
 :: Starting the client part of the test.
 NodeTest.exe %*
@@ -26,6 +25,9 @@ IF %ERRORLEVEL% NEQ 0 GOTO TESTFAILED
 :: Success message.
 ECHO Test finished successfully!
 
+:: Copying back the default gateway config.
+COPY /Y scnetworkgateway.xml %SERVER_DIR%\personal\scnetworkgateway.xml
+
 :: Killing all processes.
 staradmin -killall
 GOTO :EOF
@@ -34,4 +36,8 @@ GOTO :EOF
 :TESTFAILED
 ECHO Error occurred during the test! 1>&2
 staradmin -killall
+
+:: Copying back the default gateway config.
+COPY /Y scnetworkgateway.xml %SERVER_DIR%\personal\scnetworkgateway.xml
+
 EXIT 1
