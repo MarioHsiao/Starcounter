378a53544d90b51460089ec8a91631182d6a4019
alemoi <alexey.moiseenko@starcounter.com>
2013-12-11 14:38:50 +0100

    Added parameter for code docs generation.

--- src/BuildSystem/Documentation/generate_all.bat
+++ src/BuildSystem/Documentation/generate_all.bat
@@ -1,3 +1,8 @@
+@echo off
+
+:: Checking if documentation should be generated.
+IF "%SC_GENERATE_CODE_DOCS%"=="False" GOTO :EOF
+
 set DocXPath=C:\Program Files (x86)\Innovasys\DocumentX2012\bin\DocumentXCommandLinex64.exe
 
 call "%DocXPath%" ".\public\starcounter.dxp" [/buildconfiguration="Public"]
