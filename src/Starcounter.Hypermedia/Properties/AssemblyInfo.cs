e4722d3a9537a6601a784ffe446ab80995d6c3cb
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-01-21 13:24:27 +0100

    Updated unittests after refactoring of static webserver.

--- src/Starcounter.Hypermedia/Properties/AssemblyInfo.cs
+++ src/Starcounter.Hypermedia/Properties/AssemblyInfo.cs
@@ -22,6 +22,14 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("734663b1-5e88-439b-8cf5-f02d5968a366")]
 
+[assembly: InternalsVisibleTo("Starcounter.Web.Tests, PublicKey="
+                              + "002400000480000094000000060200000024000052534131000400"
+                              + "0001000100e758955f5e1537c52891c61cd689a8dd1643807340bd3"
+                              + "2cc12aee50d2add85eeeaac0b44a796cefb6055fac91836a8a72b5d"
+                              + "bf3b44138f508bc2d92798a618ad5791ff0db51b8662d7c936c16b5"
+                              + "720b075b2a966bb36844d375c1481c2c7dc4bb54f6d72dbe9d33712"
+                              + "aacb6fa0ad84f04bfa6c951f7b9432fe820884c81d67db")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
