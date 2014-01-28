feea56ca67e4b4df0944fb84dc9944ec3efddedb
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-01-28 13:42:20 +0100

    Removed TODO comment.

--- src/Starcounter.Apps.JsonPatch/AppRestServer.cs
+++ src/Starcounter.Apps.JsonPatch/AppRestServer.cs
@@ -158,12 +158,6 @@ namespace Starcounter.Internal.Web {
                                 // Starting session.
                                 Session.Start(s);
 
-								// TODO:
-								// This needs to be revisited. For the current solution we only use the cache 
-								// for internal requests (x.GET() from one handler to another) and not for 
-								// external requests. We need some better mechanism to determine if the cached 
-								// should be used or not.
-
                                 // Checking if we can reuse the cache.
                                 if (request.IsInternal && X.CheckLocalCache(request.Uri, null, null, out response)) {
 
