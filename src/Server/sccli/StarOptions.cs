cbd948537d8059f1b60eda4148ab569f48e4d1da
Per Samuelsson <per@starcounter.com>
2014-02-11 11:11:57 +0100

    Extended star.exe to support the custom application naming switch to be given and have it part of the usage message. Still no functionality behind it though.

--- src/Server/sccli/StarOptions.cs
+++ src/Server/sccli/StarOptions.cs
@@ -13,6 +13,7 @@ namespace star {
         public const string Server = SharedCLI.Option.Server;
         public const string ServerHost = SharedCLI.Option.ServerHost;
         public const string Db = SharedCLI.Option.Db;
+        public const string AppName = SharedCLI.Option.AppName;
         public const string ResourceDirectory = SharedCLI.Option.ResourceDirectory;
         public const string LogSteps = SharedCLI.Option.LogSteps;
         public const string NoDb = SharedCLI.Option.NoDb;
