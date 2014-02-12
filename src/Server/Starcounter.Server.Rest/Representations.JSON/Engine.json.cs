78646648b7db67f49003b934d5ff5e6d07979009
Per Samuelsson <per@starcounter.com>
2014-02-12 10:19:24 +0100

    Refactored and commented Engine.GetExecutable(string) method.

--- src/Server/Starcounter.Server.Rest/Representations.JSON/Engine.json.cs
+++ src/Server/Starcounter.Server.Rest/Representations.JSON/Engine.json.cs
@@ -8,10 +8,18 @@ namespace Starcounter.Server.Rest.Representations.JSON {
     /// Represents an engine resource.
     /// </summary>
     partial class Engine : Json {
-
-        public ExecutableReference GetExecutable(string exePath) {
-			foreach (ExecutableReference exe in this.Executables.Executing){
-                if (exe.Path.Equals(exePath, System.StringComparison.InvariantCultureIgnoreCase)) {
+        /// <summary>
+        /// Gets an executable that match the given application file, if
+        /// such is running inside the current database engine.
+        /// </summary>
+        /// <param name="applicationFile">The application file path of
+        /// the executable being queried for.</param>
+        /// <returns>An executable with the specified application file path,
+        /// if such is running in the current database engine; <c>null</c>
+        /// otherwise.</returns>
+        public ExecutableReference GetExecutable(string applicationFile) {
+            foreach (ExecutableReference exe in this.Executables.Executing) {
+                if (exe.Path.Equals(applicationFile, System.StringComparison.InvariantCultureIgnoreCase)) {
                     return exe;
                 }
             }
