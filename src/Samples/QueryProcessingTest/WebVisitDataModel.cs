ef3518faea02ee43fc473f94492aa289fdc99511
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2014-02-14 12:26:09 +0100

    Reproducing issue #1616 on query optimizer.

---
+++ src/Samples/QueryProcessingTest/WebVisitDataModel.cs
@@ -0,0 +1,39 @@
+﻿using Starcounter;
+using System;
+
+namespace QueryProcessingTest {
+    [Database]
+    public class WebPage {
+        public string Title;
+        public string URL;
+        public int PageValue;
+        public int PersonalPageValue;
+        public string TrackingCode;
+        public bool Located;
+        public bool Deleted;
+    }
+
+    [Database]
+    public class Impression {
+        public Visit Visit;
+        public DateTime Start;
+        public WebPage WebPage;
+        public String QueryString;
+    }
+
+    // A Visit consists of Webpage Impressions
+    [Database]
+    public class Visit {
+        public int Id;
+        public DateTime Start;
+        public DateTime End;
+        public int Spent;
+        public int PageViewCount;
+        public string Ip; //note: Henrik said they never show Ip in the interface, it is not even stored in PF3 database
+        public string Referer;
+        public string UserAgent;
+        public string TrackingCookie;
+        public string Protocol;
+        public string DomainName;
+    }
+}
