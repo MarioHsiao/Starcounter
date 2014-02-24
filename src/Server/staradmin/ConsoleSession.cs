f47f3330f224a7617821387e07668e76a8c7a14e
Per Samuelsson <per@starcounter.com>
2014-02-24 12:51:04 +0100

    Added new class ConsoleSession to staradmin, acting as a controller and container for a set of code host consoles in a given staradmin process (running a console session).

---
+++ src/Server/staradmin/ConsoleSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace staradmin {
+
+    internal sealed class ConsoleSession {
+        ManualResetEvent stopped;
+
+        private ConsoleSession() {
+            stopped = new ManualResetEvent(false);
+        }
+
+        public static ConsoleSession StartNew(params CodeHostConsole[] consoles) {
+            return new ConsoleSession();
+        }
+
+        public void Stop() {
+            stopped.Set();
+        }
+
+        public void Wait() {
+            stopped.WaitOne();
+        }
+    }
+}
