41022258e2c01ac3b33426b4389f804fd6020bae
Per Samuelsson <per@starcounter.com>
2014-02-24 12:36:38 +0100

    Added class CodeHostConsole to staradmin, defining the principal entrypoint to use when routing code host console output.

---
+++ src/Server/staradmin/CodeHostConsole.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace staradmin {
+
+    /// <summary>
+    /// Implements a console bound to a given code host.
+    /// </summary>
+    internal sealed class CodeHostConsole {
+        Action<CodeHostConsole> openedCallback;
+        Action<CodeHostConsole> closedCallback;
+        Action<CodeHostConsole, string> messageCallback;
+
+        /// <summary>
+        /// Gets the name of the database running in the code host
+        /// whose console the current instance represent.
+        /// </summary>
+        public readonly string DatabaseName;
+
+        public CodeHostConsole(string databaseName) {
+            if (string.IsNullOrEmpty(databaseName)) {
+                throw new ArgumentNullException("databaseName");
+            }
+            DatabaseName = databaseName;
+        }
+
+        public Action<CodeHostConsole> Opened {
+            set {
+                openedCallback = value;
+            }
+        }
+
+        public Action<CodeHostConsole> Closed {
+            set {
+                closedCallback = value;
+            }
+        }
+
+        public Action<CodeHostConsole, string> MessageWritten {
+            set {
+                messageCallback = value;
+            }
+        }
+
+        public void Open() {
+            throw new NotImplementedException();
+        }
+
+        public void Close() {
+            throw new NotImplementedException();
+        }
+    }
+}
