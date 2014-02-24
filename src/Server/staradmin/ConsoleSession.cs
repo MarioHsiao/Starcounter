828df4ef53b1200c890ff669b7c8abcd8b575154
Per Samuelsson <per@starcounter.com>
2014-02-24 13:02:43 +0100

    Added some basic tracing code to ConsoleSession (in staradmin).

--- src/Server/staradmin/ConsoleSession.cs
+++ src/Server/staradmin/ConsoleSession.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,14 +16,19 @@ namespace staradmin {
         }
 
         public static ConsoleSession StartNew(params CodeHostConsole[] consoles) {
+            Trace.WriteLine("Trace session starting");
+            Trace.Assert(consoles != null && consoles.Length > 0);
+
             return new ConsoleSession();
         }
 
         public void Stop() {
+            Trace.WriteLine("Trace session stopping");
             stopped.Set();
         }
 
         public void Wait() {
+            Trace.WriteLine("Waiting for trace session to stop");
             stopped.WaitOne();
         }
     }
