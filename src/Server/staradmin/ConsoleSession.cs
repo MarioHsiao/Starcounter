3429ec5ad63ede60ed6bd0ae85fed66496aeb0ba
Per Samuelsson <per@starcounter.com>
2014-02-24 14:34:27 +0100

    Made the ConsoleSession class reference its set of code host consoles and implemented OpenAll(), issued on the time the console session is started.

--- src/Server/staradmin/ConsoleSession.cs
+++ src/Server/staradmin/ConsoleSession.cs
@@ -10,16 +10,21 @@ namespace staradmin {
 
     internal sealed class ConsoleSession {
         ManualResetEvent stopped;
+        CodeHostConsole[] consoles;
 
-        private ConsoleSession() {
+        private ConsoleSession(CodeHostConsole[] cons) {
             stopped = new ManualResetEvent(false);
+            consoles = cons;
         }
 
         public static ConsoleSession StartNew(params CodeHostConsole[] consoles) {
             Trace.WriteLine("Trace session starting");
             Trace.Assert(consoles != null && consoles.Length > 0);
 
-            return new ConsoleSession();
+            var all = new CodeHostConsole[consoles.Length];
+            consoles.CopyTo(all, 0);
+
+            return new ConsoleSession(all).OpenAll();
         }
 
         public void Stop() {
@@ -30,6 +35,28 @@ namespace staradmin {
         public void Wait() {
             Trace.WriteLine("Waiting for trace session to stop");
             stopped.WaitOne();
+        }
+
+        ConsoleSession OpenAll() {
+            foreach (var console in consoles) {
+                console.Opened = OnConsoleOpened;
+                console.Closed = OnConsoleClosed;
+                console.MessageWritten = OnConsoleMessage;
+                console.Open();
+            }
+            return this;
+        }
+
+        void OnConsoleOpened(CodeHostConsole console) {
+            Trace.WriteLine(string.Format("Console \"{0}\" opened", console.DatabaseName));
+        }
+
+        void OnConsoleClosed(CodeHostConsole console) {
+            Trace.WriteLine(string.Format("Console \"{0}\" closed", console.DatabaseName));
+        }
+
+        void OnConsoleMessage(CodeHostConsole console, string message) {
+            Console.Write(message);
         }
     }
 }
