554a3718ca9252bc97798c19f44c7cd8a95e62b4
Per Samuelsson <per@starcounter.com>
2014-02-25 13:46:59 +0100

    Started an experimental implementation of CodeHostConsole in staradmin. Work in progress.

--- src/Server/staradmin/CodeHostConsole.cs
+++ src/Server/staradmin/CodeHostConsole.cs
@@ -1,5 +1,8 @@
 ﻿
+using SuperSocket.ClientEngine;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using WebSocket4Net;
 
 namespace staradmin {
@@ -11,7 +14,9 @@ namespace staradmin {
         Action<CodeHostConsole> openedCallback;
         Action<CodeHostConsole> closedCallback;
         Action<CodeHostConsole, string> messageCallback;
+        ManualResetEvent closed = new ManualResetEvent(false);
         WebSocket socket;
+        bool issuedClose = false;
 
         /// <summary>
         /// Gets the name of the database running in the code host
@@ -45,21 +50,68 @@ namespace staradmin {
         }
 
         public void Open() {
-            socket = new WebSocket(string.Format("ws://localhost:8181/__{0}/console/ws", DatabaseName.ToLowerInvariant()));
-            
-            var handshake = new byte[1] { 0 };
-            socket.Opened += (s, e) => {
-                socket.Send(handshake, 0, handshake.Length);
-                if (openedCallback != null) {
-                    openedCallback(this);
-                }
-            };
+            DoOpen();
+        }
 
-            socket.Open();
+        void DoOpen() {
+            Task.Run(() => {
+                var x = new AutoResetEvent(false);
+                var handshake = new byte[1] { 0 };
+
+                while (true) {
+                    socket = new WebSocket(string.Format("ws://localhost:8181/__{0}/console/ws", DatabaseName.ToLowerInvariant()));
+                    var state = WebSocketState.None;
+
+                    EventHandler opened = (s, e) => {
+                        socket.Send(handshake, 0, handshake.Length);
+                        state = WebSocketState.Open;
+                        x.Set();
+                    };
+                    EventHandler<ErrorEventArgs> errored = (s, e) => {
+                        Console.WriteLine("Error: {0}, State: {1}", e.Exception.Message, socket.State);
+                        x.Set();
+                    };
+                    
+                    socket.Opened += opened;
+                    socket.Error += errored;
+                    socket.MessageReceived += (s, e) => {
+                        if (messageCallback != null) {
+                            messageCallback(this, e.Message);
+                        }
+                    };
+                    socket.Closed += OnClosed;
+
+                    socket.Open();
+                    x.WaitOne();
+                    socket.Opened -= opened;
+                    socket.Error -= errored;
+
+                    if (state == WebSocketState.Open) {
+                        Console.WriteLine("Opened");
+                        if (openedCallback != null) {
+                            openedCallback(this);
+                        }
+                        break;
+                    }
+                }
+            });
         }
 
         public void Close() {
-            throw new NotImplementedException();
+            if (socket.State == WebSocketState.Open) {
+                issuedClose = true;
+                socket.Close();
+                closed.WaitOne();
+            }
+        }
+
+        void OnClosed(object sender, EventArgs args) {
+            Console.WriteLine("Event on closed");
+            closed.Set();
+            if (!issuedClose) {
+                closed.Reset();
+                DoOpen();
+            }
         }
     }
 }
