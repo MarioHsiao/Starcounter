fa89540817d755d32c43465970f52e5af666636e
Per Samuelsson <per@starcounter.com>
2014-02-27 09:40:45 +0100

    Made a more robust Close() implementation in CodeHostConsole, including correct closing of an underlying socket.

--- src/Server/staradmin/CodeHostConsole.cs
+++ src/Server/staradmin/CodeHostConsole.cs
@@ -13,12 +13,13 @@ namespace staradmin {
     /// Implements a console bound to a given code host.
     /// </summary>
     internal sealed class CodeHostConsole {
+        Task opening;
         Action<CodeHostConsole> openedCallback;
         Action<CodeHostConsole> closedCallback;
         Action<CodeHostConsole, string> messageCallback;
         ManualResetEvent closed = new ManualResetEvent(false);
         WebSocket socket;
-        bool issuedClose = false;
+        volatile bool closeIssued = false;
 
         /// <summary>
         /// Gets the name of the database running in the code host
@@ -56,11 +57,11 @@ namespace staradmin {
         }
 
         void DoOpen() {
-            Task.Run(() => {
+            opening = Task.Run(() => {
                 var x = new AutoResetEvent(false);
                 var handshake = new byte[1] { 0 };
 
-                while (true) {
+                while (!closeIssued) {
                     socket = new WebSocket(string.Format("ws://localhost:8181/__{0}/console/ws", DatabaseName.ToLowerInvariant()));
                     
                     EventHandler opened = (s, e) => {
@@ -68,7 +69,7 @@ namespace staradmin {
                         x.Set();
                     };
                     EventHandler<ErrorEventArgs> errored = (s, e) => {
-                        Trace.WriteLine("Error when opening web socket: {0}", e.Exception.Message);
+                        Trace.WriteLine(string.Format("Error when opening web socket: {0}", e.Exception.Message));
                         x.Set();
                     };
 
@@ -100,28 +101,28 @@ namespace staradmin {
         }
 
         public void Close() {
+            closeIssued = true;
+            opening.Wait();
+
             if (socket.State == WebSocketState.Open) {
-                issuedClose = true;
                 socket.Close();
-                closed.WaitOne();
             }
         }
 
         void OnError(object sender, ErrorEventArgs error) {
-            Trace.WriteLine("Error on web socket connection: {0}", error.Exception.Message);
-            closed.Set();
-            if (!issuedClose) {
-                closed.Reset();
+            Trace.WriteLine(string.Format("Error on web socket connection: {0}", error.Exception.Message));
+            if (!closeIssued) {
                 DoOpen();
             }
         }
 
         void OnClosed(object sender, EventArgs args) {
-            Trace.WriteLine("Web socket connection closed (Issued: {0})", issuedClose ? bool.TrueString : bool.FalseString);
-            closed.Set();
-            if (!issuedClose) {
-                closed.Reset();
+            Trace.WriteLine(
+                string.Format("Web socket connection closed (Issued: {0})", closeIssued ? bool.TrueString : bool.FalseString));
+            if (!closeIssued) {
                 DoOpen();
+            } else if (closedCallback != null) {
+                closedCallback(this);
             }
         }
 
