e7820428dceb802326f11ff4caa3525d17ea4dba
Per Samuelsson <per@starcounter.com>
2014-02-27 14:58:09 +0100

    Added embryo of the new StarConsole class to the Starcounter.CLI assembly, encapsulating the upcoming functionality to display CLI progress for ongoing jobs (like starting and stopping applications).

---
+++ src/Server/Starcounter.CLI/StarConsole.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.CLI {
+    /// <summary>
+    /// Manage the output of the star.exe command and it's
+    /// shell bootstrapper counterpart.
+    /// </summary>
+    public sealed class StarConsole {
+        readonly int cursorLeft;
+        readonly int cursorTop;
+        readonly int lines;
+        string currentJob;
+
+        /// <summary>
+        /// Gets or sets the console color used to display output
+        /// about a job/task in progress.
+        /// </summary>
+        public ConsoleColor ProgressColor {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the console color used to display output
+        /// about a job that has completed.
+        /// </summary>
+        public ConsoleColor CompletionColor {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the console color used to display output
+        /// routed from a starting/stopping application.
+        /// </summary>
+        public ConsoleColor ApplicationOutputColor {
+            get;
+            set;
+        }
+
+        private StarConsole(int lineCount, int left, int top) {
+            lines = lineCount;
+            cursorLeft = left;
+            cursorTop = top;
+            ProgressColor = ConsoleColor.DarkGray;
+            CompletionColor = ConsoleColor.DarkGreen;
+            ApplicationOutputColor = Console.ForegroundColor;
+        }
+
+        /// <summary>
+        /// Opens a new the console at the current position of the
+        /// underying system console.
+        /// </summary>
+        /// <param name="lines">The number of lines to reserve for the
+        /// console being opened; single (1) line if not given.</param>
+        /// <returns>A new console.</returns>
+        public static StarConsole Open(int lines = 1) {
+            if (lines < 1) throw new ArgumentOutOfRangeException();
+            return new StarConsole(lines, Console.CursorLeft, Console.CursorTop);
+        }
+
+        /// <summary>
+        /// Starts a job in the current console.
+        /// </summary>
+        /// <param name="job">The description of the job being started.</param>
+        /// <returns>Reference to self.</returns>
+        public StarConsole Start(string job) {
+            if (string.IsNullOrEmpty(job)) {
+                throw new ArgumentNullException("job");
+            }
+            currentJob = job;
+            return this;
+        }
+    }
+}
