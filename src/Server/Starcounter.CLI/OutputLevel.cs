7d2fb7ea10bb16a57b1a037e87e7c1a8cb978bd5
Per Samuelsson <per@starcounter.com>
2014-03-05 09:51:48 +0100

    Added new enum OutputLevel that defines the different levels of verbosity we support in the CLI.

---
+++ src/Server/Starcounter.CLI/OutputLevel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Starcounter.CLI {
+    /// <summary>
+    /// Defines possible output levels (i.e "verbosity") of a given CLI tool.
+    /// </summary>
+    public enum OutputLevel {
+        /// <summary>
+        /// Minimal output.
+        /// </summary>
+        Minimal,
+        /// <summary>
+        /// Normal output.
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// Verbose (detailed) output
+        /// </summary>
+        Verbose
+    }
+}
