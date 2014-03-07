2e3c039bec8efc1332c43ae08de2252125703399
Per Samuelsson <per@starcounter.com>
2014-03-07 11:09:24 +0100

    Introduced new class RedirectedStatusConsole. This class is instantiated when a new StarConsole is opened and console in/output is redirected. The effect is that no status bar output is emitted.

---
+++ src/Server/Starcounter.CLI/RedirectedStatusConsole.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Starcounter.CLI {
+    /// <summary>
+    /// Implements the StatusConsole features in an environment where console
+    /// output is redirected. In this environment, no status bar output should
+    /// be written.
+    /// </summary>
+    /// <seealso cref="StatusConsole.Open"/>
+    internal class RedirectedStatusConsole : StatusConsole {
+        internal RedirectedStatusConsole() : base() {
+        }
+
+        internal override StatusConsole OpenConsole() {
+            return this;
+        }
+
+        public override StatusConsole StartNewJob(string job) {
+            return this;
+        }
+
+        public override StatusConsole CompleteJob(string result = null) {
+            return this;
+        }
+
+        public override StatusConsole WriteTask(string task) {
+            return this;
+        }
+    }
+}
