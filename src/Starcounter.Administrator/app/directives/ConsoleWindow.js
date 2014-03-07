361a9ce5cfbfca672a3069735d1691045589c847
andwah <anders.wahlgren@societyobjects.com>
2014-03-07 08:45:49 +0100

    Default view is set 'icon' for Databases and Applications
    Changed the console window font to 'monospace'
    Fixed word wrapping in console window
    Changed the color of the 'start' button to Green  (Blue = Safe to click, Green = make changes)

--- src/Starcounter.Administrator/app/directives/ConsoleWindow.js
+++ src/Starcounter.Administrator/app/directives/ConsoleWindow.js
@@ -9,15 +9,14 @@ adminModule.directive("consolewindow", function () {
             ngModel: '=',
             height: '@heightAttr',
             width: '@widthAttr'
-    },
+        },
         restrict: "E",
-        template: "<div class='console' style='height:{{height}}; width:{{width}}; overflow: scroll; background-color: #000000' ng-bind-html=ngModel></div>",
+        template: "<div class='console' style='height:{{height}}; width:{{width}}; word-wrap: break-word;overflow-y: scroll; color: white;background-color: #000000' ng-bind-html=ngModel></div>",
         link: function (scope, elem, attrs) {
             scope.$watch('ngModel', function (newValue, oldValue) {
 
                 var element = elem.find('div');
                 element.scrollTop(element[0].scrollHeight);
-                //$("#consolewin").scrollTop($("#consolewin")[0].scrollHeight);
             });
         }
     }
