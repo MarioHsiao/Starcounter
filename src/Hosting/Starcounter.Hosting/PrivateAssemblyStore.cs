4a2c1b769af8e79cecfa0bb105cda7dfba034f77
Per Samuelsson <per@starcounter.com>
2014-03-11 15:45:56 +0100

    Added internal class PrivateAssemblyStore to Starcounter.Hosting.

---
+++ src/Hosting/Starcounter.Hosting/PrivateAssemblyStore.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Hosting {
+    /// <summary>
+    /// Represents the collection of private assemblies the code host is
+    /// aware of, based on loaded applications.
+    /// </summary>
+    internal sealed class PrivateAssemblyStore {
+        /// <summary>
+        /// The current store.
+        /// </summary>
+        public static readonly PrivateAssemblyStore Current = new PrivateAssemblyStore();
+
+        private PrivateAssemblyStore() {
+        }
+    }
+}
