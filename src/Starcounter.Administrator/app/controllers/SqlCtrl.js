c9c45dea150f0c8dcee96bff56aa5f6e05922dd6
andwah <anders.wahlgren@societyobjects.com>
2014-03-11 09:45:07 +0100

    Added history on sql queries
    Enhanced the console window directive
    Added a new filter 'Truncate'
    Changed the button order of the Execute and select database on the sql page

--- src/Starcounter.Administrator/app/controllers/SqlCtrl.js
+++ src/Starcounter.Administrator/app/controllers/SqlCtrl.js
@@ -3,7 +3,7 @@
  * Sql page Controller
  * ----------------------------------------------------------------------------
  */
-adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'SqlService', 'DatabaseService', 'UserMessageFactory', function ($scope, $log, $sce, NoticeFactory, SqlService, DatabaseService, UserMessageFactory) {
+adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', '$document', 'NoticeFactory', 'SqlService', 'DatabaseService', 'UserMessageFactory', function ($scope, $log, $sce, $document, NoticeFactory, SqlService, DatabaseService, UserMessageFactory) {
 
     // List of databases
     $scope.databases = DatabaseService.databases;
@@ -21,13 +21,83 @@ adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'S
         }
     }
 
-    //// TODO: Keep querystate
-    //$scope.queryState = {
-    //    selectedDatabaseName: null,
-    //    sqlQuery: "",
-    //    columns: [],
-    //    rows: []
-    //}
+    // Query history array
+    $scope.queryHistory = []; // { statement: "select s from materialized_table s", databaseName: "" }
+
+    /**
+     * Remember Query
+     * @param {object} query Query
+     */
+    $scope.rememberQuery = function (query) {
+
+        var maxItems = 10;
+
+        var trimmedQuery = query.statement.trim();
+
+        // Check if file is already 'rememberd'
+        for (var i = 0; i < $scope.queryHistory.length ; i++) {
+
+            // Query already rememberd
+            if (trimmedQuery == $scope.queryHistory[i].statement.trim()) {
+                return;
+            }
+
+        }
+
+        // Add new items to the beginning of an array:
+        $scope.queryHistory.unshift(query);
+
+        var toMany = $scope.queryHistory.length - maxItems;
+
+        if (toMany > 0) {
+            $scope.queryHistory.splice(maxItems, toMany);
+        }
+
+        var str = JSON.stringify($scope.queryHistory);
+        localStorage.setItem("queryHistory", str);
+    }
+
+
+    /**
+     * Can execute query
+     * @return {boolean} True if the conditions is correct.
+     */
+    $scope.canExecute = function () {
+        return $scope.queryState.selectedDatabaseName && $scope.isBusy == false && $scope.queryState.sqlQuery;
+    }
+
+
+    /**
+     * Refresh Query history
+     */
+    $scope.refreshQueryHistory = function () {
+
+        if (typeof (Storage) !== "undefined") {
+            var result = localStorage.getItem("queryHistory");
+            if (result) {
+                try {
+                    $scope.queryHistory = JSON.parse(result);
+                }
+                catch (err) {
+                    $log.error(err, "Removing invalid query history");
+                    localStorage.removeItem("queryHistory");
+                }
+            }
+        }
+        else {
+            // No web storage support..
+        }
+    }
+
+
+    /**
+     * Button click, Select One Query from history
+     * @param {string} query Query
+     */
+    $scope.btnSelectQuery = function (query) {
+        $scope.queryState.sqlQuery = query.statement;
+    }
+
 
     /**
      * Button click, Execut query
@@ -35,6 +105,16 @@ adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'S
      * @param {string} databaseName Database name
      */
     $scope.btnExecute = function (query, databaseName) {
+        $scope.execute(query, databaseName);
+    }
+
+
+    /**
+     * Button click, Execut query
+     * @param {string} query Query
+     * @param {string} databaseName Database name
+     */
+    $scope.execute = function (query, databaseName) {
 
         NoticeFactory.ClearAll();
 
@@ -51,6 +131,9 @@ adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'S
 
             $scope.isBusy = false;
 
+            $scope.rememberQuery({ statement: query, databaseName: databaseName });
+
+
             // Success
             $scope.queryState.columns = response.columns;
             $scope.queryState.rows = response.rows.rows;
@@ -64,7 +147,7 @@ adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'S
 
                 // Replace all occurrences of \r\n with the html tag <br>
                 // Replace all occurrences of \t with &emsp;
-                var plan = response.queryPlan.replace(/\r\n/g, "<br>").replace(/\t/g, "&emsp;");  
+                var plan = response.queryPlan.replace(/\r\n/g, "<br>").replace(/\t/g, "&emsp;");
 
                 $scope.queryState.queryPlan = $sce.trustAsHtml(plan);
             }
@@ -83,13 +166,32 @@ adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'S
                 }
 
             });
+
     }
 
+
+    // Controller destructor
+    $scope.$on('$destroy', function iVeBeenDismissed() {
+        // Unbind the keypress listener
+        $document.unbind('keypress', onKeyPress);
+    })
+    // bind the keypress listener
+    $document.bind('keypress', onKeyPress);
+
+
+    // Enabled CTRL+Enter to execute an query
+    function onKeyPress(event) {
+
+        if ($scope.canExecute() && event.ctrlKey && event.charCode == 10) {
+            $scope.execute($scope.queryState.sqlQuery, $scope.queryState.selectedDatabaseName);
+            event.preventDefault();
+        }
+    }
 
     // Init
     DatabaseService.refreshDatabases(function () {
 
-        if ($scope.databases.length > 0) {
+        if ($scope.databases.length > 0 && !$scope.queryState.selectedDatabaseName) {
             $scope.queryState.selectedDatabaseName = $scope.databases[0].name;
         }
 
@@ -98,7 +200,9 @@ adminModule.controller('SqlCtrl', ['$scope', '$log', '$sce', 'NoticeFactory', 'S
         UserMessageFactory.showErrorMessage(messageObject.header, messageObject.message, messageObject.helpLink, messageObject.stackTrace);
     });
 
+
     $scope.afterRender = scrollRefresh;
 
+    $scope.refreshQueryHistory();
 
 }]);
