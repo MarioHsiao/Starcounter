932ac31bc42e54b185ea709aaacc755ccad67000
Per Samuelsson <per@starcounter.com>
2014-03-12 13:22:29 +0100

    Added class AssemblyResolver to Starcounter.Hosting, as a step in outlining the new design of assembly loading and assembly reference resolving in the code host.

---
+++ src/Hosting/Starcounter.Hosting/AssemblyResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Hosting {
+
+    internal sealed class AssemblyResolver {
+
+        public readonly PrivateAssemblyStore PrivateAssemblies;
+
+        public AssemblyResolver(PrivateAssemblyStore store) {
+            PrivateAssemblies = store;
+        }
+
+        public Assembly ResolveApplication(string applicationHostFile) {
+            throw new NotImplementedException();
+        }
+
+        public Assembly ResolveApplicationReference(ResolveEventArgs args) {
+            throw new NotImplementedException();
+        }
+    }
+}
