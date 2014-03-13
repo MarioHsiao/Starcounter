7f2b4ab7e992c442316fe739527acf86b7e0f4d3
Per Samuelsson <per@starcounter.com>
2014-03-13 09:14:53 +0100

    Outlined the steps we need to take in AssemblyResolver.ResolveApplicationReference.

--- src/Hosting/Starcounter.Hosting/AssemblyResolver.cs
+++ src/Hosting/Starcounter.Hosting/AssemblyResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -32,6 +33,36 @@ namespace Starcounter.Hosting {
         }
 
         public Assembly ResolveApplicationReference(ResolveEventArgs args) {
+            // Always check first if we can find one loaded that has a signature
+            // we consider match. If we do, return that one.
+            // TODO:
+
+            // If we find none, or if we don't consider them a match, start
+            // looking for them in our private bin store.
+
+            var requesting = args.RequestingAssembly;
+            if (requesting == null) {
+                // We don't resolve references if we don't have a requesting
+                // assembly. There is a likelyhood this resolver is not what
+                // fits the needs, and we must tribute possible other resolvers.
+                // Debug log / trace this.
+                // TODO:
+                return null;
+            }
+            else if (!PrivateAssemblies.IsApplicationDirectory(Path.GetDirectoryName(requesting.Location))) {
+                // We only resolve references between assemblies stored in any
+                // of the application directories.
+                // Debug log / trace this.
+                // TODO:
+                return null;
+            }
+
+            // See if we can find an assembly with the given name. If we can't,
+            // we can't resolve. If we find one, load that - and log if we do it
+            // from another directory than the requestee. If we find several, we
+            // need to determine which to load.
+            // TODO:
+
             throw new NotImplementedException();
         }
 
