1460559b71022ae3087aa04449a3bb8f2097baae
Per Samuelsson <per@starcounter.com>
2014-03-19 14:20:26 +0100

    Added new class FileManager to the weaver that will govern the management of files to be processed by the weaver and that the source directory is always synchronized with the target directory after a successful weaving session.

---
+++ src/scweaver/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using Starcounter.Internal.Weaver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weaver {
+    /// <summary>
+    /// Governs the management of files to be processed by the weaver
+    /// and that the source directory is always synchronized with the
+    /// target directory after a successfull weaving session.
+    /// </summary>
+    internal class FileManager {
+        public readonly string SourceDirectory;
+        public readonly string TargetDirectory;
+        public readonly WeaverCache Cache;
+
+        public FileManager(string sourceDir, string targetDir, WeaverCache cache) {
+            SourceDirectory = sourceDir;
+            TargetDirectory = targetDir;
+            Cache = cache;
+        }
+    }
+}
