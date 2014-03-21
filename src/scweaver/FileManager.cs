0257c2592f0441dd4737f852cb1373d913580128
Per Samuelsson <per@starcounter.com>
2014-03-21 11:57:27 +0100

    Continuous work on the weaver FileManager class, now utilizing the new FileExclusionPolicy to evaluate what files are to be excluded from weaving.

--- src/scweaver/FileManager.cs
+++ src/scweaver/FileManager.cs
@@ -20,6 +20,7 @@ namespace Weaver {
         public readonly string SourceDirectory;
         public readonly string TargetDirectory;
         public readonly WeaverCache Cache;
+        public readonly FileExclusionPolicy exclusionPolicy;
 
         public Dictionary<string, ModuleLoadStrategy> OutdatedAssemblies {
             get { return outdatedAssemblies; }
@@ -29,6 +30,7 @@ namespace Weaver {
             SourceDirectory = sourceDir;
             TargetDirectory = targetDir;
             Cache = cache;
+            exclusionPolicy = new FileExclusionPolicy(sourceDir);
         }
 
         /// <summary>
@@ -43,6 +45,18 @@ namespace Weaver {
         /// <returns>An open file manager instance.</returns>
         public static FileManager Open(string sourceDir, string targetDir, WeaverCache cache) {
             return new FileManager(sourceDir, targetDir, cache).Open();
+        }
+
+        /// <summary>
+        /// Determines if the current file manager contains an assembly
+        /// with the specified file name.
+        /// </summary>
+        /// <param name="file">The assembly file name to query.</param>
+        /// <returns><c>true</c> if an assembly with the given name is
+        /// in the set of files that needs to be weaved; <c>false</c>
+        /// otherwise.</returns>
+        public bool Contains(string file) {
+            return outdatedAssemblies.ContainsKey(file);
         }
 
         /// <summary>
@@ -61,9 +75,62 @@ namespace Weaver {
             sourceFiles.AddRange(Directory.GetFiles(SourceDirectory, "*.dll"));
             sourceFiles.AddRange(Directory.GetFiles(SourceDirectory, "*.exe"));
 
+            foreach (var file in sourceFiles) {
+                // If it's not excluded, and if it's not in the cache,
+                // embrace it.
 
+                if (exclusionPolicy.IsExcluded(file)) {
+                    Exclude(file);
+                    continue;
+                }
+
+                var cached = Cache.Get(Path.GetFileNameWithoutExtension(file));
+                if (cached.Assembly != null) {
+                    Reuse(file, cached);
+                    continue;
+                }
+
+                WriteDebug("Unable to extract assembly \"{0}\" from the cache: {1}",
+                    Path.GetFileName(file),
+                    WeaverUtilities.GetExtractionFailureReason(cached)
+                    );
+                Include(file);
+            }
 
             return this;
+        }
+
+        void Exclude(string file) {
+            WriteInfo("Assembly {0} not processed, it's excluded by policy.", Path.GetFileName(file));
+
+            // All excluded files in the input directory will need
+            // to be mirrored in the target directory. Do that at
+            // the end.
+            // TODO:
+        }
+
+        void Reuse(string file, WeaverCache.CachedAssembly cachedAssembly) {
+            WriteDebug("Assembly {0} not processed, it's reused from the cache.", Path.GetFileName(file));
+
+            // It should be copied to the output directory if we are not
+            // just weaving to the cache.
+            // cachedAssembly.Files is what is to be copied.
+            // TODO:
+        }
+
+        void Include(string file) {
+            // Included files are those that need to be processed by
+            // the weaver.
+            var fullPath = Path.IsPathRooted(file) ? file : Path.GetFullPath(Path.Combine(SourceDirectory, Path.GetFileName(file)));
+            outdatedAssemblies.Add(fullPath, new CodeWeaverModuleLoadStrategy(fullPath));
+        }
+
+        void WriteInfo(string message, params object[] parameters) {
+            Program.WriteInformation(message, parameters);
+        }
+
+        void WriteDebug(string message, params object[] parameters) {
+            Program.WriteDebug(message, parameters);
         }
     }
 }
