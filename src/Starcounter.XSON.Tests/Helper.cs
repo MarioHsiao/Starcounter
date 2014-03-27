d1d0a424e812db34686547b062ad29a9d38af4c2
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-03-27 11:10:23 +0100

    Cleaned up tests and renamed testclass to the same name as the file.

--- src/Starcounter.XSON.Tests/Helper.cs
+++ src/Starcounter.XSON.Tests/Helper.cs
@@ -1,5 +1,7 @@
 ﻿
+using System;
 using System.IO;
+using NUnit.Framework;
 using Starcounter.Internal.JsonPatch;
 using Starcounter.Templates;
 
@@ -25,6 +27,14 @@ namespace Starcounter.Internal.XSON.Tests {
             return new AppAndTemplate(app, template);
         }
 
+        internal static TObject CreateJsonTemplateFromFile(string filePath) {
+            string json = File.ReadAllText(filePath);
+            string className = Path.GetFileNameWithoutExtension(filePath);
+            var tobj = TObject.CreateFromMarkup<Json, TObject>("json", json, className);
+            tobj.ClassName = className;
+            return tobj;
+        }
+
         internal static string Jsonify(string input) {
             return '"' + input + '"';
         }
@@ -36,5 +46,156 @@ namespace Starcounter.Internal.XSON.Tests {
                 Value = value
             }; 
         }
+
+        internal static void PrintTemplateDebugInfo<T>(Property<T> property) {
+            string str = property.TemplateName + " (index: " + property.TemplateIndex;
+            bool bound = (property.Bind != null);
+
+            if (bound) {
+                str += ", bind: " + property.Bind;
+            }
+
+            str += ", Type: " + property.GetType().Name;
+            str += ")";
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Property:");
+            Console.WriteLine(str);
+            Console.WriteLine();
+            Console.WriteLine("UnboundGetter:");
+            Console.WriteLine(property.DebugUnboundGetter);
+            Console.WriteLine();
+            Console.WriteLine("UnboundSetter:");
+            Console.WriteLine(property.DebugUnboundSetter);
+            Console.WriteLine();
+
+            if (bound) {
+                Console.WriteLine("BoundGetter:");
+                Console.WriteLine(property.DebugBoundGetter);
+                Console.WriteLine();
+                Console.WriteLine("BoundSetter:");
+                Console.WriteLine(property.DebugBoundSetter);
+                Console.WriteLine();
+            }
+        }
+
+        internal static void PrintTemplateDebugInfo(TObject property) {
+            string str = property.TemplateName + " (index: " + property.TemplateIndex;
+            bool bound = (property.Bind != null);
+
+            if (bound) {
+                str += ", bind: " + property.Bind;
+            }
+
+            str += ", Type: " + property.GetType().Name;
+            str += ")";
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Property:");
+            Console.WriteLine(str);
+            Console.WriteLine();
+            Console.WriteLine("UnboundGetter:");
+            Console.WriteLine(property.DebugUnboundGetter);
+            Console.WriteLine();
+            Console.WriteLine("UnboundSetter:");
+            Console.WriteLine(property.DebugUnboundSetter);
+            Console.WriteLine();
+
+            if (bound) {
+                Console.WriteLine("BoundGetter:");
+                Console.WriteLine(property.DebugBoundGetter);
+                Console.WriteLine();
+                Console.WriteLine("BoundSetter:");
+                Console.WriteLine(property.DebugBoundSetter);
+                Console.WriteLine();
+            }
+        }
+
+        internal static TObject CreateSimplePersonTemplateWithDataBinding() {
+            var personSchema = new TObject() { BindChildren = BindingStrategy.Bound };
+            personSchema.Add<TString>("FirstName$"); // Bound to FirstName
+            personSchema.Add<TString>("LastName", "Surname"); // Bound to Surname
+
+            var t = personSchema.Add<TLong>("Age"); // Will not be bound
+            t.BindingStrategy = BindingStrategy.Unbound;
+
+            personSchema.Add<TString>("Created");
+            personSchema.Add<TString>("Updated");
+            personSchema.Add<TString>("AbstractValue");
+            personSchema.Add<TString>("VirtualValue");
+
+            var misc = personSchema.Add<TString>("Misc");
+            misc.Bind = null; // Removing the binding for this specific template.
+
+            var phoneNumber = personSchema.Add<TObject>("PhoneNumber", "Number");
+            phoneNumber.BindChildren = BindingStrategy.Bound;
+            phoneNumber.Add<TString>("Number"); // Bound to Number
+
+            return personSchema;
+        }
+
+        internal static TObject CreateSimplePersonTemplate() {
+            var personSchema = new TObject();
+            personSchema.Add<TString>("FirstName$");
+            personSchema.Add<TString>("LastName");
+            personSchema.Add<TLong>("Age");
+
+            var phoneNumber = new TObject();
+            phoneNumber.Add<TString>("Number");
+            personSchema.Add<TArray<Json>>("PhoneNumbers", phoneNumber);
+
+            return personSchema;
+        }
+
+        internal static TObject CreateComplexPersonTemplate() {
+            var personSchema = new TObject();
+            personSchema.Add<TString>("FirstName$");
+            personSchema.Add<TString>("LastName");
+            personSchema.Add<TLong>("Age");
+            personSchema.Add<TDecimal>("Stats");
+
+            var field = new TObject();
+            field.Add<TString>("Type");
+            var info = field.Add<TObject>("Info");
+            info.Add<TString>("Text");
+            field.InstanceType = typeof(MyFieldMessage);
+            personSchema.Add<TArray<MyFieldMessage>>("Fields", field);
+
+            var extraInfo = personSchema.Add<TObject>("ExtraInfo");
+            extraInfo.Add<TString>("Text");
+
+            return personSchema;
+        }
+
+        internal static void AssertCorrectErrorCodeIsThrown(Action action, uint expectedErrorCode) {
+            uint ec;
+
+            try {
+                action();
+                Assert.Fail("An exception with error " + ErrorCode.ToFacilityCode(expectedErrorCode) + " should have been thrown");
+            } catch (Exception ex) {
+                if (ErrorCode.TryGetCode(ex, out ec)) {
+                    if (ec != expectedErrorCode)
+                        Assert.Fail("An exception with error " + ErrorCode.ToFacilityCode(expectedErrorCode) + " should have been thrown");
+                } else {
+                    Assert.Fail("An exception with error " + ErrorCode.ToFacilityCode(expectedErrorCode) + " should have been thrown");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Verifies the index path.
+        /// </summary>
+        /// <param name="expected">The expected.</param>
+        /// <param name="received">The received.</param>
+        internal static void VerifyIndexPath(Int32[] expected, Int32[] received) {
+            Assert.AreEqual(expected.Length, received.Length);
+            for (Int32 i = 0; i < expected.Length; i++) {
+                Assert.AreEqual(expected[i], received[i]);
+            }
+        }
+    }
+
+    internal class MyFieldMessage : Json {
     }
 }
