89ab995ceaa469da501d36c1dfa8df03b32f0f73
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2014-03-28 07:50:43 +0100

    InsertInto: fixed transaction scope around insert into in test.

--- src/Samples/QueryProcessingTest/InsertIntoTests.cs
+++ src/Samples/QueryProcessingTest/InsertIntoTests.cs
@@ -5,9 +5,11 @@ namespace QueryProcessingTest {
     public static class InsertIntoTests {
         public static void TestValuesInsertIntoWebVisits() {
             HelpMethods.LogEvent("Test insert into statements with values on web visit data model");
-            String query = "INSERT INTO WebPage (Title, uRL, PageValue, PersonalPageValue, TrackingCode, Located, deleted)" +
-                "Values ('MyCompany, AboutUs', '168.12.147.2/AboutUs', 100, 90, '', false, false)";
-            Db.SQL(query);
+            Db.Transaction(delegate {
+                String query = "INSERT INTO WebPage (Title, uRL, PageValue, PersonalPageValue, TrackingCode, Located, deleted)" +
+                    "Values ('MyCompany, AboutUs', '168.12.147.2/AboutUs', 100, 90, '', false, false)";
+                Db.SQL(query);
+            });
             HelpMethods.LogEvent("Finished testing insert into statements with values on web visit data model");
         }
     }
