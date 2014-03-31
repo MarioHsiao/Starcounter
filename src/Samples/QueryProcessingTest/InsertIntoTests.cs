6503d35d50bcd6ae8d04b5a882ba0056727e53cf
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2014-03-31 15:23:21 +0200

    InsertInto: completed insert into test with testing created object and set values.

--- src/Samples/QueryProcessingTest/InsertIntoTests.cs
+++ src/Samples/QueryProcessingTest/InsertIntoTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Starcounter;
+using System.Diagnostics;
 
 namespace QueryProcessingTest {
     public static class InsertIntoTests {
@@ -10,6 +11,15 @@ namespace QueryProcessingTest {
                     "Values ('MyCompany, AboutUs', '168.12.147.2/AboutUs', 100, 90, '', false, false)";
                 Db.SQL(query);
             });
+            WebPage w = Db.SQL<WebPage>("select w from Webpage w where title = ?", "MyCompany, AboutUs").First;
+            Trace.Assert(w != null);
+            Trace.Assert(w.Title == "MyCompany, AboutUs");
+            Trace.Assert(w.URL == "168.12.147.2/AboutUs");
+            Trace.Assert(w.PageValue == 100);
+            Trace.Assert(w.PersonalPageValue == 90);
+            Trace.Assert(w.TrackingCode == "");
+            Trace.Assert(w.Located == false);
+            Trace.Assert(w.Deleted == false);
             HelpMethods.LogEvent("Finished testing insert into statements with values on web visit data model");
         }
     }
