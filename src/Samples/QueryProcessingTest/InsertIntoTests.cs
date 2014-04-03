4c54813575ea4473b4c5d7f77c37840d190cd4d9
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2014-04-03 09:12:56 +0200

    InsertInto: added test of inserting with id, not working yet.

--- src/Samples/QueryProcessingTest/InsertIntoTests.cs
+++ src/Samples/QueryProcessingTest/InsertIntoTests.cs
@@ -10,8 +10,11 @@ namespace QueryProcessingTest {
                 if (Db.SQL("select c from company c").First != null) {
                     WebPage w1 = Db.SQL<WebPage>("select w from webpage w where title = ?", "MyCompany, AboutUs").First;
                     w1.Delete();
-                    foreach (Company c1 in Db.SQL<Company>("select c from company c"))
+                    foreach (Company c1 in Db.SQL<Company>("select c from company c")) {
+                        foreach (Visit v1 in Db.SQL<Visit>("select v from visit v where company = ?", c1))
+                            v1.Delete();
                         c1.Delete();
+                    }
                     foreach (Country c1 in Db.SQL<Country>("select c from country c"))
                         c1.Delete();
                 }
@@ -50,6 +53,39 @@ namespace QueryProcessingTest {
             Trace.Assert(co.Name == "Canal+");
             Trace.Assert(co.Country.Equals(c));
             Trace.Assert(!compEnum.MoveNext());
+#if false // Access violation
+            Db.Transaction(delegate {
+                Db.SQL("insert into starcounter.raw.QueryProcessingTest.visit (id, company, start, end, UserAgent) values (1, object "+
+                    co.GetObjectNo() + ",timestapm '2006-11-01 00:08:40',timestapm '2006-11-01 00:08:59','Opera')");
+            });
+#endif
+            Db.Transaction(delegate {
+                Db.SQL("insert into starcounter.raw.QueryProcessingTest.visit (id, company, UserAgent) values (1, object " +
+                    co.GetObjectNo() + ",'Opera')");
+            });
+            var visits = Db.SQL<Visit>("select v from visit v where company = ?", co).GetEnumerator();
+            Trace.Assert(visits.MoveNext());
+            Visit v = visits.Current;
+            Trace.Assert(v.Id == 1);
+            Trace.Assert(v.Company.Equals(co));
+            Trace.Assert(v.UserAgent == "Opera");
+            Trace.Assert(!visits.MoveNext());
+            // Test insert __id value
+            var vId = v.GetObjectNo();
+            Db.Transaction(delegate { v.Delete(); });
+            Db.SystemTransaction(delegate {
+                Db.SQL("insert into starcounter.raw.QueryProcessingTest.visit (__id, id, company, UserAgent) values (object " +
+                    vId + ",1, object " +
+                    co.GetObjectNo() + ",'Opera')");
+            });
+            visits = Db.SQL<Visit>("select v from visit v where company = ?", co).GetEnumerator();
+            Trace.Assert(visits.MoveNext());
+            v = visits.Current;
+            Trace.Assert(v.Id == 1);
+            Trace.Assert(v.Company.Equals(co));
+            Trace.Assert(v.UserAgent == "Opera");
+            Trace.Assert(v.GetObjectNo() == vId);
+            Trace.Assert(!visits.MoveNext());
             HelpMethods.LogEvent("Finished testing insert into statements with values on web visit data model");
         }
     }
