3fd9a8a8410d0352bd2affc08e959009cae428a5
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-05-05 16:05:19 +0200

    Rewrote handling of implicit transaction. The implicit transaction is no longer using the public Transaction class but an internal class and is not accessible by users. It will also be cleaned up in the end of each task.

--- src/Starcounter/Internal/TaskHelper.cs
+++ src/Starcounter/Internal/TaskHelper.cs
@@ -4,6 +4,14 @@ namespace Starcounter.Internal {
     /// <summary>
     /// </summary>
     public static class TaskHelper { // Internal
+        internal static void CreateOrSetImplicitTransaction() {
+            var state = ThreadData.Current;
+
+            if (state._implicitTransaction == null)
+                state._implicitTransaction = new ImplicitTransaction();
+
+            state._implicitTransaction.SetCurrent();
+        }
 
         /// <summary>
         /// Called when exiting managed task entry point to cleanup managed
@@ -14,6 +22,12 @@ namespace Starcounter.Internal {
         }
 
         private static void ResetCurrentTransaction() {
+            var threadData = ThreadData.Current;
+            if (threadData._implicitTransaction != null) {
+                threadData._implicitTransaction.Cleanup();
+                threadData._implicitTransaction = null;
+            }
+
             if (Transaction._current != null) {
                 uint r = sccoredb.sccoredb_set_current_transaction(0, 0, 0);
                 if (r == 0) {
