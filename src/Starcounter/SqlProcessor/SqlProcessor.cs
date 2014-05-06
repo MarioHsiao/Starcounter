e06c2de0b9865bf55554c9a9f8c287f4d847d4a0
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2014-05-06 16:38:46 +0200

    Added query into exception message.

--- src/Starcounter/SqlProcessor/SqlProcessor.cs
+++ src/Starcounter/SqlProcessor/SqlProcessor.cs
@@ -89,8 +89,8 @@ namespace Starcounter.SqlProcessor {
                     message += ", near or at token: " + token;
                     if (scerror->isKeyword > 0)
                         message += ". Note that the token is a keyword.";
-                } else
-                    message += ".";
+                }
+                message += ". In query: " + query;
                 return ErrorCode.ToException(errorCode, message, (m, e) => new SqlException(errorCode, m, message, position, token, query));
             }
         }
