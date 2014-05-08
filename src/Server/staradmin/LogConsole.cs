16e80f4a80950f94a4023de500293283bdc15e05
Per Samuelsson <per@starcounter.com>
2014-05-08 15:07:23 +0200

    Added LogConsole class to staradmin, governing the output of logs to be printed to the console.

---
+++ src/Server/staradmin/LogConsole.cs
@@ -0,0 +1,46 @@
+﻿using Sc.Tools.Logging;
+using Starcounter.CLI;
+using System;
+
+namespace staradmin {
+    /// <summary>
+    /// Governs the output of logs to be printed to the console.
+    /// </summary>
+    internal static class LogConsole {
+
+        public static void OutputLog(LogEntry log) {
+            var time = GetTimeString(log);
+            var color = GetColor(log);
+
+            ConsoleUtil.ToConsoleWithColor(log.Message, color);
+            ConsoleUtil.ToConsoleWithColor(
+                string.Format("[{0}, {1} @ {2}]", time, log.Source, log.HostName), 
+                ConsoleColor.DarkGray);
+            Console.WriteLine();
+        }
+
+        static string GetTimeString(LogEntry log) {
+            var x = log.DateTime;
+            if (x.Date == DateTime.Today) {
+                return x.TimeOfDay.ToString();
+            }
+            return x.ToString();
+        }
+
+        static ConsoleColor GetColor(LogEntry log) {
+            switch (log.Severity) {
+                case Severity.Debug:
+                    return ConsoleColor.Gray;
+                case Severity.Notice:
+                    return ConsoleColor.Yellow;
+                case Severity.Warning:
+                    return ConsoleColor.Magenta;
+                case Severity.Error:
+                case Severity.Critical:
+                    return ConsoleColor.Red;
+                default:
+                    return Console.ForegroundColor;
+            }
+        }
+    }
+}
