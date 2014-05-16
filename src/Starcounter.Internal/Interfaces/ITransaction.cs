384528c07036c49011012f8dfd2a22a5bc9b4495
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-05-16 14:18:15 +0200

    Added more methods on ITransaction to allow delegates with more parameters and return value. The methods are implemented explicitly on transaction so they are only visible for ITransaction.

--- src/Starcounter.Internal/Interfaces/ITransaction.cs
+++ src/Starcounter.Internal/Interfaces/ITransaction.cs
@@ -6,10 +6,13 @@ namespace Starcounter.Advanced {
         void Commit();
         void Rollback();
         void Add(Action action);
-        TResult Add<TResult>(Func<TResult> func);
-        TResult Add<T, TResult>(Func<T, TResult> func, T arg);
-        TResult Add<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 arg1, T2 arg2);
-        TResult Add<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func, T1 arg1, T2 arg2, T3 arg3);
+        void Add<T>(Action<T> action, T arg);
+        void Add<T1, T2>(Action<T1, T2> action, T1 arg1, T2 arg2);
+        void Add<T1, T2, T3>(Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3);
+        TResult AddAndReturn<TResult>(Func<TResult> func);
+        TResult AddAndReturn<T, TResult>(Func<T, TResult> func, T arg);
+        TResult AddAndReturn<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 arg1, T2 arg2);
+        TResult AddAndReturn<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func, T1 arg1, T2 arg2, T3 arg3);
         Boolean IsDirty { get; }
     }
 }
