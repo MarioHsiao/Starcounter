613f94737d711210ffda18ed78b7439fa9141548
Per Samuelsson <per@starcounter.com>
2014-05-22 19:58:02 +0200

    Added class ImageFile to Starcounter.Server. This class will allow clients to access metadata from Starcounter database image files.

---
+++ src/Server/Starcounter.Server/ImageFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Server {
+    /// <summary>
+    /// Represents a logical database image file.
+    /// </summary>
+    /// <remarks>
+    /// Starcounter secure data in image files, normally
+    /// two files per database. One contains the latest secured
+    /// checkpoint, the other one the upcoming checkpoint.
+    /// </remarks>
+    public sealed class ImageFile {
+        private ImageFile() {
+        }
+
+        public static ImageFile Read(string directory, string databaseName) {
+            throw new NotImplementedException();
+        }
+    }
+}
