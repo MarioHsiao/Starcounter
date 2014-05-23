6b2d90fbb06e10bd72374e1f539f6d4c78c86356
Per Samuelsson <per@starcounter.com>
2014-05-23 15:05:38 +0200

    Adapted the ServerEngine class to rely on the ServerConfigurations ability to resolve the configured database- and temp paths.

--- src/Server/Starcounter.Server/ServerEngine.cs
+++ src/Server/Starcounter.Server/ServerEngine.cs
@@ -162,21 +162,12 @@ namespace Starcounter.Server {
         /// Executes setup of the current engine.
         /// </summary>
         public void Setup() {
-            string serverRepositoryDirectory;
-            string tempDirectory;
-            string databaseDirectory;
-
             ServerHost.Configure(this.Configuration);
 
             // Validate the database directory exist. We refuse to start if
             // we can not properly resolve it to an existing directory.
 
-            serverRepositoryDirectory = Path.GetDirectoryName(Path.GetFullPath(this.Configuration.ConfigurationFilePath));
-            databaseDirectory = Environment.ExpandEnvironmentVariables(this.Configuration.DatabaseDirectory);
-            if (!Path.IsPathRooted(databaseDirectory)) {
-                databaseDirectory = Path.Combine(serverRepositoryDirectory, databaseDirectory);
-            }
-            databaseDirectory = Path.GetFullPath(databaseDirectory);
+            var databaseDirectory = this.Configuration.GetResolvedDatabaseDirectory();
             if (!Directory.Exists(databaseDirectory)) {
                 throw ErrorCode.ToException(Error.SCERRUNSPECIFIED, string.Format("Database directory doesn't exist. Path: {0}", databaseDirectory));
             }
@@ -186,11 +177,7 @@ namespace Starcounter.Server {
             // it's wrongly given and temporary files end up somewhere
             // not intended.
 
-            tempDirectory = Environment.ExpandEnvironmentVariables(this.Configuration.TempDirectory);
-            if (!Path.IsPathRooted(tempDirectory)) {
-                tempDirectory = Path.Combine(serverRepositoryDirectory, tempDirectory);
-            }
-            tempDirectory = Path.GetFullPath(tempDirectory);
+            var tempDirectory = this.Configuration.GetResolvedTempDirectory();
             if (!Directory.Exists(tempDirectory)) {
                 Directory.CreateDirectory(tempDirectory);
             }
