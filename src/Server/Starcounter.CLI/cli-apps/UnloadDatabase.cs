7454e923112bb0fee3ff263825a6bcde8ea89c0f
Per Samuelsson <per@starcounter.com>
2014-06-05 10:50:22 +0200

    Added first version of the UnloadDatabase application that will eventually be ran when doing a CLI database unload.

---
+++ src/Server/Starcounter.CLI/cli-apps/UnloadDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Starcounter;
+ 
+namespace UnloadDatabase {
+    class Program {
+        static void Main(string[] args) {
+            string filePath = @"C:\Users\Public\Documents\ReloadData.sql";
+            if (args.Length == 1)
+                filePath = args[0];
+            int unloaded = Db.Unload(filePath);
+            Console.WriteLine("Unloaded: {0} objects", unloaded);
+        }
+    }
+}
