864ede325bdd898d7d6c070efc90a3662f8c4131
Per Samuelsson <per@starcounter.com>
2014-06-06 10:09:52 +0200

    Implemented first, but somewhat trivial, version of the CLI unload.

--- src/Server/staradmin/Unload.cs
+++ src/Server/staradmin/Unload.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿
+using Starcounter.CLI;
+using Starcounter.CommandLine;
+using Starcounter.Internal;
+using System.IO;
 
 namespace staradmin {
 
     internal sealed class Unload {
-        
+        const string UnloadFileName = "UnloadDatabase.cs";
+
         public void Run() {
-            throw new NotImplementedException();
+            string exeFile;
+
+            var appFile = Path.Combine(
+                StarcounterEnvironment.InstallationDirectory,
+                SharedCLI.CLIAppFolderName,
+                UnloadFileName
+                );
+            SourceCodeCompiler.CompileSingleFileToExecutable(appFile, out exeFile);
+
+            var unload = ApplicationCLICommand.Create(appFile, exeFile, ApplicationArguments.Empty) as StartApplicationCommand;
+            unload.JobDescription = string.Format("Unloading {0}", unload.DatabaseName);
+            unload.JobCompletionDescription = "done";
+            unload.ApplicationStartingDescription = "unloading";
+
+            unload.Execute();
         }
     }
 }
