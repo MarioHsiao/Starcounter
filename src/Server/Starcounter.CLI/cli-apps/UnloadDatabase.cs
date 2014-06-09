e809f1881bf6bd990248d398c8cb4941a242a9c2
Per Samuelsson <per@starcounter.com>
2014-06-09 09:49:52 +0200

    Changed the unload application to display the time it started unloading, and the time it finished. Also commented the file somewhat.

--- src/Server/Starcounter.CLI/cli-apps/UnloadDatabase.cs
+++ src/Server/Starcounter.CLI/cli-apps/UnloadDatabase.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Starcounter;
+
+// NOTE:
+// This file is part of the Starcounter installation. It is
+// used when running 'staradmin unload' to support unloading of
+// databases. Do not modify it unless you are sure about what
+// you do. You risk breaking the 'staradmin unload' functionality!
  
 namespace UnloadDatabase {
+    /// <summary>
+    /// Implements a utility application supporting unloading a
+    /// database.
+    /// </summary>
     class Program {
         static void Main(string[] args) {
-            string filePath = @"C:\Users\Public\Documents\ReloadData.sql";
+            var filePath = @"C:\Users\Public\Documents\ReloadData.sql";
             if (args.Length == 1)
                 filePath = args[0];
+
+            Console.WriteLine("Unload started at {0}", DateTime.Now.TimeOfDay);
             int unloaded = Db.Unload(filePath);
-            Console.WriteLine("Unloaded: {0} objects", unloaded);
+            Console.WriteLine("{0}: Unloaded: {1} objects", DateTime.Now.TimeOfDay, unloaded);
         }
     }
 }
