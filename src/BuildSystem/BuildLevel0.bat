c59456d5e40f807693a612a9c5afc5f79cf8b8d7
alemoi <alexey.moiseenko@starcounter.com>
2014-06-10 16:08:20 +0200

    Added system internal port configuration to XMLs.

---
+++ src/BuildSystem/BuildLevel0.bat
@@ -0,0 +1,3 @@
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "..\..\..\Level0\msbuild\SqlProcessor\SqlProcessor.sln" /property:Configuration=Release;Platform=x64;GenerateFullPaths=true /consoleloggerparameters:Summary /verbosity:minimal /maxcpucount
+
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "..\..\..\Level0\msbuild\Blue.sln" /property:Configuration=Release;Platform=x64;GenerateFullPaths=true /consoleloggerparameters:Summary /verbosity:minimal /maxcpucount
