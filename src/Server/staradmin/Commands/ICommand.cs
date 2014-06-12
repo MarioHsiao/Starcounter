826762d35d6cfe85a34086e04582618d55f70dac
Per Samuelsson <per@starcounter.com>
2014-06-12 11:17:19 +0200

    Added ICommand interface to staradmin, abstracting something that is possible for the staradmin CLI application to execute.

---
+++ src/Server/staradmin/Commands/ICommand.cs
@@ -0,0 +1,10 @@
+﻿
+namespace staradmin.Commands {
+    /// <summary>
+    /// Defines the interface of a command that is possible to
+    /// execute within the scope of the staradmin CLI.
+    /// </summary>
+    internal interface ICommand {
+        void Execute();
+    }
+}
