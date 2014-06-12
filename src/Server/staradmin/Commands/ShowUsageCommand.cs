1ebb6e806215f10d504173c82165530f5a15b95a
Per Samuelsson <per@starcounter.com>
2014-06-12 11:33:30 +0200

    Added new ShowUsageCommand to staradmin. This command writes the usage message to the console.

---
+++ src/Server/staradmin/Commands/ShowUsageCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace staradmin.Commands {
+
+    internal class ShowUsageCommand : ICommand {
+
+        public void Execute() {
+            CommandLine.WriteUsage(Console.Out);
+        }
+    }
+}
