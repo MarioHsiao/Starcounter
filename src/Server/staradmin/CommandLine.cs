8bbee515703381757991ff65527e0ab2268fc121
Per Samuelsson <per@starcounter.com>
2014-06-12 09:28:11 +0200

    Added outline for new CommandLine class in staradmin. This class will define and parse the command-line.

---
+++ src/Server/staradmin/CommandLine.cs
@@ -0,0 +1,30 @@
+﻿
+using Starcounter.CommandLine;
+using Starcounter.CommandLine.Syntax;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staradmin {
+
+    internal static class CommandLine {
+        public static ApplicationArguments Parse(string[] args) {
+            if (args.Length == 0) {
+                return ApplicationArguments.Empty;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public static void WriteUsage(TextWriter writer) {
+            throw new NotImplementedException();
+        }
+
+        static IApplicationSyntax Define() {
+            throw new NotImplementedException();
+        }
+    }
+}
