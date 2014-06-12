83ff8a654f69dc2fc96999b5bc4d3e6e90715a08
Per Samuelsson <per@starcounter.com>
2014-06-12 13:41:37 +0200

    Made it possible to customize the ShowHelpCommand to suppress the header when showing help on help, and made sure it was utilized when displaying help on bad input.

--- src/Server/staradmin/Commands/ShowHelpCommand.cs
+++ src/Server/staradmin/Commands/ShowHelpCommand.cs
@@ -7,6 +7,8 @@ namespace staradmin.Commands {
     /// Govern the dispalying of help for a given topic.
     /// </summary>
     internal class ShowHelpCommand : ICommand {
+        bool HelpOnHelpSupressHeader = false;
+
         /// <summary>
         /// The topic to show help for, usually a staradmin
         /// command.
@@ -26,17 +28,19 @@ namespace staradmin.Commands {
         }
 
         void ReportUnrecognizedTopic() {
-            var helpOnHelp = new ShowHelpCommand(CommandLine.Commands.Help.Name);
+            var helpOnHelp = new ShowHelpCommand(CommandLine.Commands.Help.Name) { HelpOnHelpSupressHeader = true };
             var badInput = new ReportBadInputCommand(string.Format("Help for topic '{0}' not found.", Topic), helpOnHelp);
             badInput.Execute();
         }
 
         void ShowHelpOnHelp(TextWriter writer) {
-            writer.WriteLine("Displays help on a specified topic, usually a command.");
-            writer.WriteLine();
+            if (!HelpOnHelpSupressHeader) {
+                writer.WriteLine("Displays help on a specified topic, usually a command.");
+                writer.WriteLine();
+            }
             writer.WriteLine("Topics:");
             var formatting = "  {0,-22}{1,25}";
-            writer.WriteLine(formatting, string.Format("{0}", CommandLine.Commands.Help.Name), "Displays this help");
+            writer.WriteLine(formatting, string.Format("{0}", CommandLine.Commands.Help.Name), "Display help on the help command");
             writer.WriteLine();
             writer.WriteLine("To view the overall help, use staradmin --{0}", CommandLine.Options.Help.Name);
         }
