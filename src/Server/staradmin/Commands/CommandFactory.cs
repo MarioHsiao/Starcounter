6e40e31e1cf1c867a08f138aca7e386469339b5a
Per Samuelsson <per@starcounter.com>
2014-06-12 11:23:47 +0200

    Added new class CommandFactory to staradmin. This class will create real commands based on given input.

---
+++ src/Server/staradmin/Commands/CommandFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using Starcounter.CommandLine;
+using System;
+
+namespace staradmin.Commands {
+    /// <summary>
+    /// Govers the creation of executable <see cref="ICommand"/> instances
+    /// based on given input.
+    /// </summary>
+    internal class CommandFactory {
+        
+        /// <summary>
+        /// Creates a command based on given arguments.
+        /// </summary>
+        /// <param name="args">The arguments to use to decied what command
+        /// to create.</param>
+        /// <returns>A <see cref="ICommand"/> instance.</returns>
+        public ICommand CreateCommand(ApplicationArguments args) {
+            throw new NotImplementedException();
+        }
+    }
+}
