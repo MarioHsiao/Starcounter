3a1bf5bf21f682f4e1366e17b9aec647b845824a
Per Samuelsson <per@starcounter.com>
2014-06-16 13:40:13 +0200

    Added new staradmin interface IUserCommand, as part of a rewrite on how user-level commands are maintained, discovered and processed (by the help system and the command factory).

---
+++ src/Server/staradmin/Commands/IUserCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using Starcounter.CommandLine;
+using Starcounter.CommandLine.Syntax;
+
+namespace staradmin.Commands {
+
+    internal interface IUserCommand {
+        CommandSyntaxDefinition Define(ApplicationSyntaxDefinition appSyntax);
+        CommandLine.Command Info { get; }
+        ICommand CreateCommand(ApplicationArguments args);
+    }
+}
