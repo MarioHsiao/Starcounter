2acbecc136226822eef576e0944d62429d3e1a15
Per Samuelsson <per@starcounter.com>
2014-06-16 09:07:54 +0200

    Introduced new staradmin Context class. The context will provide information about the current staradmin session to commands.

---
+++ src/Server/staradmin/Context.cs
@@ -0,0 +1,13 @@
+﻿
+namespace staradmin {
+    /// <summary>
+    /// Provides information about the context of the current
+    /// staradmin sesssion.
+    /// </summary>
+    internal class Context {
+        /// <summary>
+        /// Gets or sets the name of the database.
+        /// </summary>
+        public string Database { get; set; }
+    }
+}
