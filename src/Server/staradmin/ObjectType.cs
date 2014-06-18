a24e309fab1d687c90c9a9146025c52eea22b3c0
Per Samuelsson <per@starcounter.com>
2014-06-18 09:37:02 +0200

    Introduced new ObjectType enum in staradmin. Values represent various type of objects recognized by staradmin for commands/operations to be acted upon.

---
+++ src/Server/staradmin/ObjectType.cs
@@ -0,0 +1,13 @@
+﻿
+namespace staradmin {
+    /// <summary>
+    /// Defines well-known object types, supported by
+    /// staradmin for various operations.
+    /// </summary>
+    internal enum ObjectType {
+        Unknown,
+        Database,
+        Application,
+        ServerLog
+    }
+}
