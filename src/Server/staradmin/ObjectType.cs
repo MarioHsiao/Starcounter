98d4695d46508aa63384db5cf1b04c88678e2236
Per Samuelsson <per@starcounter.com>
2014-06-18 09:44:25 +0200

    Added class ObjectTypeExtensions to staradmin, with methods supporting converting strings to and from ObjectType instances.

--- src/Server/staradmin/ObjectType.cs
+++ src/Server/staradmin/ObjectType.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 namespace staradmin {
     /// <summary>
     /// Defines well-known object types, supported by
@@ -9,5 +10,32 @@ namespace staradmin {
         Database,
         Application,
         ServerLog
+    }
+
+    internal static class ObjectTypeExtensions {
+        internal static ObjectType ToObjectType(this string value) {
+            switch (value.ToLowerInvariant()) {
+                case "app":
+                case "apps":
+                case "application":
+                case "applications":
+                    return ObjectType.Application;
+                case "db":
+                case "dbs":
+                case "database":
+                case "databases":
+                    return ObjectType.Database;
+                case "log":
+                case "logs":
+                case "serverlog":
+                    return ObjectType.ServerLog;
+                default:
+                    return ObjectType.Unknown;
+            }
+        }
+
+        internal static string ToString(this ObjectType type) {
+            return Enum.GetName(typeof(ObjectType), type);
+        }
     }
 }
