7e6e891dc161013527a6dd2022489be40b68451d
Per Samuelsson <per@starcounter.com>
2014-06-19 11:09:06 +0200

    Introduced new staradmin class StopAppCommand, outlining what will be needed to stop an application by its name.

---
+++ src/Server/staradmin/Commands/StopAppCommand.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace staradmin.Commands {
+
+    internal class StopAppCommand : StopCommand {
+        readonly string appReference;
+
+        internal StopAppCommand(string app) {
+            appReference = app;
+        }
+
+        protected override void Stop() {
+            if (string.IsNullOrWhiteSpace(appReference)) {
+                var helpOnStop = ShowHelpCommand.CreateAsInternalHelp(FactoryCommand.Info.Name);
+                var bad = new ReportBadInputCommand("Specify the application to stop", helpOnStop);
+                bad.Execute();
+                return;
+            }
+
+            Console.WriteLine("Stopping application {0}", appReference);
+        }
+    }
+}
