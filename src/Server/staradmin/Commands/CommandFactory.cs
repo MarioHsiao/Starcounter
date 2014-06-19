d3979594b3583e026a523026fe64780c02175d3b
Per Samuelsson <per@starcounter.com>
2014-06-19 10:39:08 +0200

    Added the new StopCommand to the set of well-known staradmin user commands, thereby publishing it to the help system.

--- src/Server/staradmin/Commands/CommandFactory.cs
+++ src/Server/staradmin/Commands/CommandFactory.cs
@@ -20,6 +20,7 @@ namespace staradmin.Commands {
             UserCommands.Add(new ListCommand.UserCommand());
             UserCommands.Add(new ConsoleCommand.UserCommand());
             UserCommands.Add(new NewCommand.UserCommand());
+            UserCommands.Add(new StopCommand.UserCommand());
         }
 
         /// <summary>
