ed70496a991a54a1b8b519c19f110e5cc9a8648c
Per Samuelsson <per@starcounter.com>
2014-06-26 15:08:33 +0200

    Added new stub-class StopCodeHostCommand in staradmin. This class will eventually support stopping of code hosts.

---
+++ src/Server/staradmin/Commands/StopCodeHostCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staradmin.Commands {
+
+    internal class StopCodeHostCommand : StopCommand {
+        protected override void Stop() {
+            throw new NotImplementedException();
+        }
+    }
+}
