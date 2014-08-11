52e83cc101d7923003008b1f0252821477ea2b12
Per Samuelsson <per@starcounter.com>
2014-08-11 11:12:49 +0200

    Added new class DatabaseTypePolicy to the weaver. This class govern what types are to be considered database types.

---
+++ src/scweaver/Starcounter.Internal.Weaver/DatabaseTypePolicy.cs
@@ -0,0 +1,31 @@
+﻿using PostSharp.Sdk.CodeModel;
+
+namespace Starcounter.Internal.Weaver {
+    /// <summary>
+    /// Represents the authority that decide what defined types are to be
+    /// considered database types.
+    /// </summary>
+    internal class DatabaseTypePolicy {
+        IType databaseAttributeType;
+
+        public DatabaseTypePolicy(string applicationDirectory, IType databaseAttribute) {
+            databaseAttributeType = databaseAttribute;
+        }
+
+        public bool IsDatabaseType(TypeDefDeclaration typeDef) {
+            return IsTaggedWithDatabaseAttribute(typeDef);
+        }
+
+        bool IsTaggedWithDatabaseAttribute(TypeDefDeclaration typeDef) {
+            var cursor = typeDef;
+            while (!cursor.CustomAttributes.Contains(databaseAttributeType)) {
+                if (cursor.BaseType != null) {
+                    cursor = cursor.BaseType.GetTypeDefinition();
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
