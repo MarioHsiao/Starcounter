70a3947ff082842815f9e39337462a73b9ba2e18
Per Samuelsson <per@starcounter.com>
2014-08-15 13:45:51 +0200

    Added class KnownClientContexts to Starcounter.Server. To replace StarcounterCLI.ClientContext.KnownContexts.

---
+++ src/Server/Starcounter.Server/KnownClientContexts.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Starcounter.Server {
+    /// <summary>
+    /// Enum-like class with constants of known management
+    /// client contexts.
+    /// </summary>
+    public static class KnownClientContexts {
+        /// <summary>
+        /// Visual Studio context.
+        /// </summary>
+        public const string VisualStudio = "VS";
+        /// <summary>
+        /// The star.exe context.
+        /// </summary>
+        public const string Star = "star.exe";
+        /// <summary>
+        /// The staradmin.exe context.
+        /// </summary>
+        public const string StarAdmin = "staradmin.exe";
+        /// <summary>
+        /// Context of the administration server.
+        /// </summary>
+        public const string Admin = "Admin";
+
+        /// Used when the context is unknown.
+        /// </summary>
+        public const string UnknownContext = "Unknown";
+    }
+}
