140f5e3cf41c3fcaaab5984cfb351000a0ebc31a
Per Samuelsson <per@starcounter.com>
2014-09-01 15:23:03 +0200

    Implemented first version of new StopDatabaseCommand in Starcounter.CLI. We currently support stopping the code host only.

--- src/Server/Starcounter.CLI/StopDatabaseCommand.cs
+++ src/Server/Starcounter.CLI/StopDatabaseCommand.cs
@@ -1,5 +1,11 @@
 ﻿
+using Starcounter.CommandLine;
+using Starcounter.Internal;
+using Starcounter.Server;
+using Starcounter.Server.Rest;
 using System;
+using Starcounter.Rest.ExtensionMethods;
+using Starcounter.Server.Rest.Representations.JSON;
 
 namespace Starcounter.CLI {
     /// <summary>
@@ -12,9 +18,84 @@ namespace Starcounter.CLI {
         /// </summary>
         public bool StopCodeHostOnly { get; set; }
 
+        internal StopDatabaseCommand()
+            : base() {
+        }
+
+        /// <summary>
+        /// Factory method used to create new commands.
+        /// </summary>
+        /// <param name="args">Optional well-known CLI-level arguments.</param>
+        /// <returns>A new command</returns>
+        public static StopDatabaseCommand Create(ApplicationArguments args = null) {
+            args = args ?? ApplicationArguments.Empty;
+            var cmd = new StopDatabaseCommand() {
+                AdminAPI = new AdminAPI()
+            };
+            cmd.ResolveServer(args);
+            return cmd;
+        }
+
         /// <inheritdoc/>
         protected override void Run() {
-            throw new NotImplementedException();
+            try {
+                Status.StartNewJob(string.Format("Stopping {0}", DatabaseName.ToLowerInvariant()));
+                
+                if (StarcounterEnvironment.ServerNames.PersonalServer.Equals(ServerName, StringComparison.CurrentCultureIgnoreCase)) {
+                    ShowStatus("retrieving server status", true);
+                    if (!ServerServiceProcess.IsOnline()) {
+                        SharedCLI.ShowErrorAndSetExitCode(ErrorCode.ToMessage(Error.SCERRSERVERNOTAVAILABLE), true);
+                    }
+                }
+
+                var node = Node;
+                var admin = AdminAPI;
+                var uris = admin.Uris;
+                var databaseName = DatabaseName.ToLowerInvariant();
+                int statusCode;
+
+                var response = node.GET(admin.FormatUri(uris.Engine, databaseName), null);
+                statusCode = response.FailIfNotSuccessOr(404);
+                if (statusCode == 404) {
+                    var errorDetail = new ErrorDetail();
+                    errorDetail.PopulateFromJson(response.Body);
+                    switch (errorDetail.ServerCode) {
+                        case Error.SCERRDATABASENOTFOUND:
+                        case Error.SCERRDATABASEENGINENOTRUNNING:
+                            var notAccessible = ErrorCode.ToMessage((uint)errorDetail.ServerCode, string.Format("Database: \"{0}\".", databaseName));
+                            SharedCLI.ShowErrorAndSetExitCode(notAccessible, true);
+                            break;
+                        default:
+                            var other404 = ErrorCode.ToMessage((uint)errorDetail.ServerCode, string.Format("Text from server: \"{0}\".", errorDetail.Text));
+                            SharedCLI.ShowErrorAndSetExitCode(other404, true);
+                            break;
+                    }
+                }
+
+                var engine = new Engine();
+                engine.PopulateFromJson(response.Body);
+
+                if (StopCodeHostOnly) {
+                    if (engine.CodeHostProcess.PID == 0) {
+                        var notRunning = ErrorCode.ToMessage(Error.SCERRDATABASEENGINENOTRUNNING, string.Format("Database: \"{0}\".", databaseName));
+                        SharedCLI.ShowErrorAndSetExitCode(notRunning, true);
+                    }
+
+                    response = node.DELETE(node.ToLocal(engine.CodeHostProcess.Uri), (string)null, null);
+                    response.FailIfNotSuccess();
+
+                } else {
+                    throw new NotImplementedException("Stopping databases are yet not supported");
+                }
+
+                Status.CompleteJob("stopped");
+
+            } catch (Exception e) {
+                SharedCLI.ShowErrorAndSetExitCode(e, true, false);
+                return;
+            }
         }
+
+
     }
 }
