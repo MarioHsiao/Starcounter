28794263f5a3bd71427e148db843d7762ff41009
Per Samuelsson <per@starcounter.com>
2014-09-01 13:40:33 +0200

    Introduce new StopDatabaseCommand class in Starcounter.CLI. This class will support stopping of databases and code hosts.

---
+++ src/Server/Starcounter.CLI/StopDatabaseCommand.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Starcounter.CLI {
+    /// <summary>
+    /// Provides functionality for a client to stop hosts and databases.
+    /// </summary>
+    public class StopDatabaseCommand : CLIClientCommand {
+        /// <summary>
+        /// Indicates stopping of the code host only. Other processes
+        /// running as part of the target database will remain.
+        /// </summary>
+        public bool StopCodeHostOnly { get; set; }
+
+        /// <inheritdoc/>
+        protected override void Run() {
+            throw new NotImplementedException();
+        }
+    }
+}
