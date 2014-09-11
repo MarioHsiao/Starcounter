c223fde4d37ca13a2b23c99e9823a93bacf769fa
Per Samuelsson <per@starcounter.com>
2014-09-11 14:24:30 +0200

    Simple refactoring of weaver DynamicTypesHelper class.

--- src/scweaver/Starcounter.Internal.Weaver/DynamicTypesHelper.cs
+++ src/scweaver/Starcounter.Internal.Weaver/DynamicTypesHelper.cs
@@ -20,47 +20,51 @@ namespace Starcounter.Internal.Weaver {
         /// <param name="attribute"></param>
         public static void ValidateDatabaseAttribute(DatabaseAttribute attribute) {
             if (attribute.IsTypeReference) {
-                var referencedType = attribute.AttributeType as DatabaseClass;
-                if (referencedType == null) {
-                    ScMessageSource.WriteError(
-                        MessageLocation.Unknown,
-                        Error.SCERRINVALIDTYPEREFERENCE,
-                        string.Format("Attribute {0}.{1} is not a reference to a database class",
-                        attribute.DeclaringClass.Name,
-                        attribute.Name
-                        ));
-                }
-
-                if (attribute.IsTransient) {
-                    ScMessageSource.WriteError(
-                        MessageLocation.Unknown,
-                        Error.SCERRINVALIDTYPEREFERENCE,
-                        string.Format("Attribute {0}.{1} is marked transient.",
-                        attribute.DeclaringClass.Name,
-                        attribute.Name
-                        ));
-                }
-
-                var other = attribute.DeclaringClass.FindAttributeInAncestors((candidate) => {
-                    return candidate != attribute && candidate.IsTypeReference;
-                });
-
-                if (other != null) {
-                    ScMessageSource.WriteError(
-                        MessageLocation.Unknown,
-                        Error.SCERRINVALIDTYPEREFERENCE,
-                        string.Format("Attribute {0}.{1} is marked a type; {2}.{3} is too.",
-                        attribute.DeclaringClass.Name,
-                        attribute.Name,
-                        other.DeclaringClass.Name,
-                        other.Name
-                        ));
-                }
-
-                // Check if it's incompatible with other type decorations, like
-                // if its a TypeName or Inherits
-                // TODO:
+                ValidateTypeReference(attribute);
             }
+        }
+
+        static void ValidateTypeReference(DatabaseAttribute attribute) {
+            var referencedType = attribute.AttributeType as DatabaseClass;
+            if (referencedType == null) {
+                ScMessageSource.WriteError(
+                    MessageLocation.Unknown,
+                    Error.SCERRINVALIDTYPEREFERENCE,
+                    string.Format("Attribute {0}.{1} is not a reference to a database class",
+                    attribute.DeclaringClass.Name,
+                    attribute.Name
+                    ));
+            }
+
+            if (attribute.IsTransient) {
+                ScMessageSource.WriteError(
+                    MessageLocation.Unknown,
+                    Error.SCERRINVALIDTYPEREFERENCE,
+                    string.Format("Attribute {0}.{1} is marked transient.",
+                    attribute.DeclaringClass.Name,
+                    attribute.Name
+                    ));
+            }
+
+            var other = attribute.DeclaringClass.FindAttributeInAncestors((candidate) => {
+                return candidate != attribute && candidate.IsTypeReference;
+            });
+
+            if (other != null) {
+                ScMessageSource.WriteError(
+                    MessageLocation.Unknown,
+                    Error.SCERRINVALIDTYPEREFERENCE,
+                    string.Format("Attribute {0}.{1} is marked a type; {2}.{3} is too.",
+                    attribute.DeclaringClass.Name,
+                    attribute.Name,
+                    other.DeclaringClass.Name,
+                    other.Name
+                    ));
+            }
+
+            // Check if it's incompatible with other type decorations, like
+            // if its a TypeName or Inherits
+            // TODO:
         }
     }
 }
