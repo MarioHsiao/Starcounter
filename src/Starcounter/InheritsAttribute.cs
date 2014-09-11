d647beebb335a20e6c1c6f59f90aca6a4ec1bee3
Per Samuelsson <per@starcounter.com>
2014-09-11 14:10:58 +0200

    Introduced new InheritsAttribute custom attribute class in the Starcounter assembly.

---
+++ src/Starcounter/InheritsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Starcounter {
+    /// <summary>
+    /// Custom attribute that, when applied to a non-transient field or a property
+    /// in a database class, instructs Starcounter to treat the tagged target as a
+    /// reference to a base type (forming a hierarchy).
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class InheritsAttribute : Attribute {
+    }
+}
