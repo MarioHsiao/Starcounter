51acbb343663a43b282550a33c42ab36a620533a
Per Samuelsson <per@starcounter.com>
2014-09-11 12:07:05 +0200

    Introduced new weaver helper class, DynamicTypesHelper, to support the new dynamic types
    weaving and analysis.

---
+++ src/scweaver/Starcounter.Internal.Weaver/DynamicTypesHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sc.Server.Weaver.Schema;
+
+namespace Starcounter.Internal.Weaver {
+    using DatabaseAttribute = Sc.Server.Weaver.Schema.DatabaseAttribute;
+
+    /// <summary>
+    /// Provides weaver helper methods for dynamic types.
+    /// </summary>
+    internal static class DynamicTypesHelper {
+        /// <summary>
+        /// Validates the given attribute to see if custom attributes are
+        /// correctly used.
+        /// </summary>
+        /// <param name="attribute"></param>
+        public static void ValidateDatabaseAttribute(DatabaseAttribute attribute) {
+            var referencedType = attribute.AttributeType as DatabaseClass;
+            if (referencedType == null) {
+                // Only applies to databae classes
+                // TODO:
+            }
+
+            if (attribute.IsTransient) {
+                // Nope.
+                // TODO:
+            }
+
+            // Check if it's incompatible with other type decorations, like
+            // if its a TypeName or Inherits
+            // TODO:
+        }
+    }
+}
