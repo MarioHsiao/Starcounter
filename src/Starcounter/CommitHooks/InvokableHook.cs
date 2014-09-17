2c4125f26a3bd7d4411239a5b0f016b7b6b61941
Per Samuelsson <per@starcounter.com>
2014-09-17 15:49:56 +0200

    Added helper methods InvokableHook.InvokeInsert, InvokeUpdate and InvokeDelete to support the Transaction class when it should invoke installed hooks.

--- src/Starcounter/CommitHooks/InvokableHook.cs
+++ src/Starcounter/CommitHooks/InvokableHook.cs
@@ -28,6 +28,40 @@ namespace Starcounter {
         internal static Dictionary<string, List<InvokableHook>> HooksPerTrigger = new Dictionary<string, List<InvokableHook>>();
 
         /// <summary>
+        /// Invokes all hooks installed to watch when instances of 
+        /// <paramref name="typeName"/> are inserted.
+        /// </summary>
+        /// <param name="typeName">The type name to look for.</param>
+        /// <param name="proxy">The carry to pass to the delegate.</param>
+        internal static void InvokeInsert(string typeName, IObjectView proxy) {
+            var key = typeName + Insert;
+            InvokeAllWithKey(key, proxy);
+        }
+
+        /// <summary>
+        /// Invokes all hooks installed to watch when instances of 
+        /// <paramref name="typeName"/> are updated.
+        /// </summary>
+        /// <param name="typeName">The type name to look for.</param>
+        /// <param name="proxy">The carry to pass to the delegate.</param>
+        internal static void InvokeUpdate(string typeName, IObjectView proxy) {
+            var key = typeName + Update;
+            InvokeAllWithKey(key, proxy);
+        }
+
+        /// <summary>
+        /// Invokes all hooks installed to watch when instances of 
+        /// <paramref name="typeName"/> are deleted.
+        /// </summary>
+        /// <param name="typeName">The type name to look for.</param>
+        /// <param name="proxy">The carry in the form of an object ID
+        /// to pass to the delegate.</param>
+        internal static void InvokeDelete(string typeName, ulong objectID) {
+            var key = typeName + Delete;
+            InvokeAllWithKey(key, objectID);
+        }
+
+        /// <summary>
         /// Invokes every hook installed for a given key.
         /// </summary>
         /// <param name="key">The key whose hooks are to be invoked.</param>
