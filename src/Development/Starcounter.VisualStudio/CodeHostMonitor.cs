19bcacf55a0a67ccc632caac1eef031761f01211
Per Samuelsson <per@starcounter.com>
2014-09-24 11:15:24 +0200

    Made the VisualStudio CodeHostManager class thread-safe (its shared by multiple projects/threads).

--- src/Development/Starcounter.VisualStudio/CodeHostMonitor.cs
+++ src/Development/Starcounter.VisualStudio/CodeHostMonitor.cs
@@ -14,6 +14,7 @@ namespace Starcounter.VisualStudio {
     internal class CodeHostMonitor {
         const string codeHostName = Starcounter.Internal.StarcounterConstants.ProgramNames.ScCode + ".exe";
         Dictionary<int, Process> hosts = new Dictionary<int, Process>();
+        object sync = new object();
 
         public static CodeHostMonitor Current = new CodeHostMonitor();
 
@@ -21,28 +22,33 @@ namespace Starcounter.VisualStudio {
         }
 
         public void AssureMonitored(int processId) {
-            if (!hosts.ContainsKey(processId)) {
-                var p = Process.GetProcessById(processId);
-                if (p == null || !IsCodeHost(p.ProcessName)) {
-                    throw new Exception(string.Format("Can not monitor process with ID {0}", processId));
+            lock (sync) {
+                if (!hosts.ContainsKey(processId)) {
+                    var p = Process.GetProcessById(processId);
+                    if (p == null || !IsCodeHost(p.ProcessName)) {
+                        throw new Exception(string.Format("Can not monitor process with ID {0}", processId));
+                    }
+                    hosts[processId] = p;
                 }
-                hosts[processId] = p;
             }
         }
 
         public void ProcessDetatched(int processId, string processName, VsPackage package) {
-            if (IsCodeHost(processName)) {
-                Process process;
+            Process process;
+
+            if (!IsCodeHost(processName)) {
+                return;
+            }
+
+            lock (sync) {
                 if (!hosts.TryGetValue(processId, out process)) {
-                    // Do what? Warn?
-                    // TODO:
                     return;
                 }
 
                 process.Refresh();
                 if (process.HasExited) {
                     // We can't get the error code; we must check if the log contains
-                    // any errors
+                    // any errors.
 
                     var log = new FilterableLogReader() {
                         Count = 10,
