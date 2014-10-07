1bccbebf00e5c24f27565c15c3426bc198eda12d
Per Samuelsson <per@starcounter.com>
2014-10-07 16:33:21 +0200

    Added new class CodeHostProcessMonitor to Starcounter.Server library. Every instance of this class monitor a specific code host process on behalf of the server; instances can be cancelled within the server via the DatabaseEngineMonitor service.

---
+++ src/Server/Starcounter.Server/CoreServices/CodeHostProcessMonitor.cs
@@ -0,0 +1,73 @@
+﻿using Starcounter.Logging;
+using Starcounter.Server.Commands;
+using Starcounter.Server.PublicModel.Commands;
+using System;
+using System.Diagnostics;
+
+namespace Starcounter.Server {
+
+    internal sealed class CodeHostProcessMonitor {
+        readonly LogSource log;
+
+        public DatabaseEngineMonitor Monitor { get; set; }
+
+        public string DatabaseName { get; set; }
+        public int PID { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public string WorkDescription {
+            get {
+                return string.Format("Synchronizing server state for exiting code host \"{0}\", PID={1}, StartTime={2}", DatabaseName, PID, StartTime);
+            }
+        }
+
+        public bool Cancelled { get; private set; }
+
+        public CodeHostProcessMonitor(DatabaseEngineMonitor monitor) {
+            Monitor = monitor;
+            log = monitor.Log;
+        }
+
+        public bool IsMonitorigDatabase(Database database) {
+            return DatabaseName.Equals(database.Name, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public bool Cancel() {
+            var wasCancelled = Cancelled == false;
+            Cancelled = true;
+            return wasCancelled;
+        }
+
+        public void CodeHostExited(object sender, EventArgs args) {
+            var server = this.Monitor.Server;
+            var cmd = new ActionCommand<Process>(this.Monitor.Server, Process, sender as Process, WorkDescription);
+            server.CurrentPublicModel.Execute(cmd);
+        }
+
+        public void Process(ICommandProcessor processor, Process process) {
+            var server = this.Monitor.Server;
+
+            if (Cancelled) {
+                log.Debug("Ignoring synchronization of server state for {0}; monitoring was cancelled.");
+                return;
+            }
+
+            Database database;
+            var databaseExist = server.Databases.TryGetValue(DatabaseName, out database);
+            if (!databaseExist) {
+                // Might have been deleted.
+                // Take no action.
+                return;
+            }
+
+            var boundProcess = Monitor.GetCodeHostProcess(database);
+            if (boundProcess != null) {
+                // The database is bound to some other process. We should
+                // let it be.
+                return;
+            }
+
+            Monitor.ResetInternalAndPublicState(server.DatabaseEngine, database, process);
+        }
+    }
+}
