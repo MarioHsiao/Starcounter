42441caa4c2288fdb1277fe2833bca0261809cf4
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-10-09 13:56:23 +0200

    Added unittest project containing json that will be compiled and used to test generated json as part of #1944

---
+++ src/Starcounter.XSON.Tests.CodegenJson/FakeX.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Starcounter;
+
+//namespace Starcounter.Internal.XSON.Tests.CodegenJson {
+    public static class X {
+        internal static void GET(string uri, out Response r) {
+            r = null;
+        }
+    }
+//}
