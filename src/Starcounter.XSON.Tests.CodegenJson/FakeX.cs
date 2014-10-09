d296011ddd3c39a794ada1a8d2fc8fe46df4b58a
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-10-09 14:25:36 +0200

    Enabled building new testproject in release.

--- src/Starcounter.XSON.Tests.CodegenJson/FakeX.cs
+++ src/Starcounter.XSON.Tests.CodegenJson/FakeX.cs
@@ -2,6 +2,9 @@
 using Starcounter;
 
 //namespace Starcounter.Internal.XSON.Tests.CodegenJson {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class X {
         internal static void GET(string uri, out Response r) {
             r = null;
