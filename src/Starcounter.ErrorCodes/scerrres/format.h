57f5bb9711dabbb29d87b46e355b66dd936556bf
Johan Lindh <johan@linkdata.se>
2014-10-13 13:02:49 +0200

    add extern C

--- src/Starcounter.ErrorCodes/scerrres/format.h
+++ src/Starcounter.ErrorCodes/scerrres/format.h
@@ -1,19 +1,28 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef WIN32
+# include <windows.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #ifdef WIN32
-#include <windows.h>
 VOID __stdcall FormatStarcounterErrorMessage(
-	DWORD errorCode,
-	LPWSTR outputBuffer,
-	DWORD outputBufferLength
+    DWORD errorCode,
+    LPWSTR outputBuffer,
+    DWORD outputBufferLength
 );
 #else
 void FormatStarcounterErrorMessage(
-	unsigned errorCode,
-	char* outputBuffer,
-	size_t outputBufferLength
+    unsigned errorCode,
+    char* outputBuffer,
+    size_t outputBufferLength
 );
 #endif
 
+#ifdef __cplusplus
+}
+#endif
