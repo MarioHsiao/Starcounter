e13d77102e136eac4bc4d418156df298cae97c03
Per Samuelsson <per@starcounter.com>
2014-10-17 08:53:11 +0200

    Added new internal class HookKey to Starcounter assembly. Will be used as the key when mapping from type+operation to installed hooks.

---
+++ src/Starcounter/CommitHooks/HookKey.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Starcounter {
+    /// <summary>
+    /// A key identifying a triggering hook type, including its
+    /// identity and the operation being hooked (e.g. Insert);
+    /// </summary>
+    internal sealed class HookKey {
+        /// <summary>
+        /// Gets the type id of this hook, normally a table id.
+        /// </summary>
+        public readonly uint TypeId;
+        /// <summary>
+        /// Gets the operation
+        /// </summary>
+        public readonly uint Operation;
+
+        /// <summary>
+        /// Initializes
+        /// </summary>
+        /// <param name="typeId"></param>
+        /// <param name="operation"></param>
+        private HookKey(uint typeId, uint operation) {
+            TypeId = typeId;
+            Operation = operation;
+        }
+
+        /// <summary>
+        /// Creates a key based on a table id.
+        /// </summary>
+        /// <param name="tableId">The table id that identifies the type
+        /// of the key.</param>
+        /// <param name="operation">The operation the key identifies.</param>
+        /// <returns>A new <see cref="HookKey"/> based on the given values.
+        /// </returns>
+        public static HookKey FromTable(ushort tableId, uint operation) {
+            // TODO: Change to INVALID_TABLE_ID constant.
+            if (tableId == ushort.MaxValue) throw new ArgumentOutOfRangeException();
+            return new HookKey(tableId, operation);
+        }
+    }
+}
