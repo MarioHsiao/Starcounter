ae7ce5b34acb3e265d03144c7b55454569d1d141
Johan Lindh <johan@linkdata.se>
2014-10-20 18:00:50 +0200

    revert scerrres format.h changes

--- src/Starcounter.ErrorCodes/scerrres/format.h
+++ src/Starcounter.ErrorCodes/scerrres/format.h
@@ -1,25 +1,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
-#ifdef WIN32
 #include <windows.h>
-VOID __stdcall FormatStarcounterErrorMessage(
-	DWORD errorCode,
-	LPWSTR outputBuffer,
-	DWORD outputBufferLength
-);
-#else
-#ifdef __cplusplus
-extern "C" {
-#endif
-void FormatStarcounterErrorMessage(
-	unsigned errorCode,
-	char* outputBuffer,
-	size_t outputBufferLength
-);
-#ifdef __cplusplus
-}
-#endif
-#endif
 
+VOID __stdcall FormatStarcounterErrorMessage(
+    DWORD errorCode,
+    LPWSTR outputBuffer,
+    DWORD outputBufferLength
+);
