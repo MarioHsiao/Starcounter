27c61b573573e2e3164343e6ec2adc2a66dcc470
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2014-10-24 12:35:13 +0200

    Fixed code indentation in Reload.

--- src/Starcounter/Mock/Reload.cs
+++ src/Starcounter/Mock/Reload.cs
@@ -76,44 +76,44 @@ namespace Starcounter {
                     selectObjs.Append(QuotePath(tbl.FullName));
                     selectObjs.Append(" __o");
                     using (SqlEnumerator<IObjectView> selectEnum = (SqlEnumerator<IObjectView>)Db.SQL<IObjectView>(selectObjs.ToString()).GetEnumerator()) {
-                    Debug.Assert(selectEnum.TypeBinding != null);
-                    while (selectEnum.MoveNext()) {
-                        IObjectView val = selectEnum.Current;
-                        string valTypeName = null;
-                        if (selectEnum.PropertyBinding == null) {
-                            Debug.Assert(selectEnum.TypeBinding.GetPropertyBinding(0).TypeCode == DbTypeCode.Object);
-                            Debug.Assert(selectEnum.TypeBinding.PropertyCount > 0);
-                            valTypeName = val.GetObject(0).GetType().ToString();
-                        }
-                        else
-                            valTypeName = val.GetType().ToString();
-                        Debug.Assert(valTypeName != null);
-                        if (valTypeName == tbl.FullName) {
-                            if (tblNrObj == 0)
-                                inStmt.Append("(");
-                            else
-                                inStmt.Append(",(");
-                            if (selectEnum.PropertyBinding == null)
-                                inStmt.Append("object " + (val.GetObject(0).GetObjectNo() + shiftId).ToString()); // Value __id
-                            else
-                                inStmt.Append("object " + (val.GetObjectNo() + shiftId).ToString()); // Value __id
-                            for (int i = 1; i < selectEnum.TypeBinding.PropertyCount; i++) {
-                                inStmt.Append(",");
-                                inStmt.Append(GetString(val, i, shiftId));
+                        Debug.Assert(selectEnum.TypeBinding != null);
+                        while (selectEnum.MoveNext()) {
+                            IObjectView val = selectEnum.Current;
+                            string valTypeName = null;
+                            if (selectEnum.PropertyBinding == null) {
+                                Debug.Assert(selectEnum.TypeBinding.GetPropertyBinding(0).TypeCode == DbTypeCode.Object);
+                                Debug.Assert(selectEnum.TypeBinding.PropertyCount > 0);
+                                valTypeName = val.GetObject(0).GetType().ToString();
                             }
-                            inStmt.Append(")");
-                            tblNrObj++;
-                            if (tblNrObj == 1000) {
-                                using (StreamWriter file = new StreamWriter(fileName, true)) {
-                                    file.WriteLine(inStmt.ToString());
+                            else
+                                valTypeName = val.GetType().ToString();
+                            Debug.Assert(valTypeName != null);
+                            if (valTypeName == tbl.FullName) {
+                                if (tblNrObj == 0)
+                                    inStmt.Append("(");
+                                else
+                                    inStmt.Append(",(");
+                                if (selectEnum.PropertyBinding == null)
+                                    inStmt.Append("object " + (val.GetObject(0).GetObjectNo() + shiftId).ToString()); // Value __id
+                                else
+                                    inStmt.Append("object " + (val.GetObjectNo() + shiftId).ToString()); // Value __id
+                                for (int i = 1; i < selectEnum.TypeBinding.PropertyCount; i++) {
+                                    inStmt.Append(",");
+                                    inStmt.Append(GetString(val, i, shiftId));
                                 }
-                                totalNrObj += tblNrObj;
-                                tblNrObj = 0;
-                                inStmt = new StringBuilder();
-                                inStmt.Append(insertHeader);
+                                inStmt.Append(")");
+                                tblNrObj++;
+                                if (tblNrObj == 1000) {
+                                    using (StreamWriter file = new StreamWriter(fileName, true)) {
+                                        file.WriteLine(inStmt.ToString());
+                                    }
+                                    totalNrObj += tblNrObj;
+                                    tblNrObj = 0;
+                                    inStmt = new StringBuilder();
+                                    inStmt.Append(insertHeader);
+                                }
                             }
                         }
-                    }
                     }
                     if (tblNrObj > 0)
                         using (StreamWriter file = new StreamWriter(fileName, true)) {
