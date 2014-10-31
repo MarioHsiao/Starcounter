9df66b2c5bc06a967039644b71d62438783cd834
Ruslan Fomkin <Ruslan.Fomkin@starcounter.com>
2014-10-31 12:43:02 +0100

    Fixed Starcounter's error code for assertion failures on the build server. Added assertion without message. Added unit tests for the assert methods. Needed for #2347.

--- src/Starcounter.Logging/ScAssertion.cs
+++ src/Starcounter.Logging/ScAssertion.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Starcounter.Internal;
+using System;
 using System.Diagnostics;
 
 namespace Starcounter {
     public class ScAssertion {
         public static void Assert(bool checkTrue, string message) {
             if (Environment.GetEnvironmentVariable("SC_RUNNING_ON_BUILD_SERVER") == "True")
-                throw new Exception(message);
+                throw ErrorCode.ToException(Error.SCERRTESTASSERTIONFAILURE, message);
             else
                 Trace.Assert(checkTrue, message);
+        }
+
+        public static void Assert(bool checkTrue) {
+            if (Environment.GetEnvironmentVariable("SC_RUNNING_ON_BUILD_SERVER") == "True")
+                throw ErrorCode.ToException(Error.SCERRTESTASSERTIONFAILURE);
+            else
+                Trace.Assert(checkTrue);
         }
     }
 }
