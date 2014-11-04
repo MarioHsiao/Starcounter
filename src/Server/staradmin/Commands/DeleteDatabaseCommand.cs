36bec72fb9afc46d4553a531aae9db79dd134caa
Per Samuelsson <per@starcounter.com>
2014-11-04 12:06:53 +0100

    Added stub class DeleteDatabaseCommand to staradmin, specializing the staradmin DeleteCommand to allow deleting databases.

---
+++ src/Server/staradmin/Commands/DeleteDatabaseCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staradmin.Commands {
+
+    internal class DeleteDatabaseCommand : DeleteCommand {
+        
+        protected override void Delete() {
+            // Get the name of the database we are about to
+            // delete.
+            // If not FORCE, ask the user to confirm by writing
+            // the name of the database.
+            // Carry out the delete.
+            // TODO:
+
+            throw new NotImplementedException();
+        }
+    }
+}
