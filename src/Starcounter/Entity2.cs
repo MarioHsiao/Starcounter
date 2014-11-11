f0256de886478d555fdab37e0f0958499daba0b5
Per Samuelsson <per@starcounter.com>
2014-11-11 11:07:05 +0100

    Added class Entity2 to Starcounter project as a starting point for reintroducing the Entity class.

---
+++ src/Starcounter/Entity2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter {
+
+    [Database]
+    public abstract class Entity2 : IEntity {
+
+        public abstract void OnDelete();
+    }
+}
