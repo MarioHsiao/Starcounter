b3746cfc2342c91b75579c442defbe4ed73a0b11
Per Samuelsson <per@starcounter.com>
2014-11-20 13:13:48 +0100

    Added new interface ISetValueCallback to the Starcounter assembly, in the Starcounter.Advanced namespace. This interface allows applications to subscribe to the updating of database attributes.

---
+++ src/Starcounter/Advanced/ISetValueCallback.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Starcounter.Advanced {
+
+    /// <summary>
+    /// Causes Starcounter to notify the implementing database
+    /// class every time a database attribute is updated.
+    /// </summary>
+    public interface ISetValueCallback {
+        /// <summary>
+        /// Invoked by Starcounter when a database attribute is
+        /// being assigned.
+        /// </summary>
+        /// <param name="attributeName">The name of the attriubyte
+        /// that was assigned.</param>
+        /// <param name="value">The value assigned to the given
+        /// database attribute.</param>
+        void OnValueSet(string attributeName, object value);
+    }
+}
