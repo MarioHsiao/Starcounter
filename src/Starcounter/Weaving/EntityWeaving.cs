e317914e715de3fb6adaa62192473fab1f8e8963
Per Samuelsson <per@starcounter.com>
2014-11-25 11:07:36 +0100

    Introduced new class EntityWeaving in Starcounter assembly to keep the tweaked treating of Entity-related schemata in a single place.

---
+++ src/Starcounter/Weaving/EntityWeaving.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Weaving {
+    /// <summary>
+    /// Utility class used by the weaver and (implicitly) the
+    /// code host loader to correctly weave the <see cref="Entity"/>
+    /// class.
+    /// </summary>
+    public static class EntityWeaving {
+
+    }
+}
