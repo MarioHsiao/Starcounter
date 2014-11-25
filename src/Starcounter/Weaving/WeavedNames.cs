1e148aa2f99f4a32ad72c2f1c681abbf97d37469
Per Samuelsson <per@starcounter.com>
2014-11-25 14:28:31 +0100

    Defined a set of names of implicit tokens, used by the weaver, in new utility class WeaverNames.

---
+++ src/Starcounter/Weaving/WeavedNames.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Sc.Server.Weaver {
+    /// <summary>
+    /// Defines a set of names used by the weaver for internal
+    /// and/or implicit constructs. These names are normally
+    /// reserved, and will be guarded against collision against
+    /// during weaving.
+    /// </summary>
+    public static class WeavedNames {
+        /// <summary>
+        /// Gets the full name of the implicit entity class.
+        /// </summary>
+        public const string ImplicitEntityClass = "Starcounter.ImplicitEntity";
+
+        /// <summary>
+        /// Gets the name of the implicit type column.
+        /// </summary>
+        public const string TypeColumn = "__sc__type__";
+
+        /// <summary>
+        /// Gets the name of the implicit type name column
+        /// </summary>
+        public const string TypeNameColumn = "__sc__type_name__";
+
+        /// <summary>
+        /// Gets the name of the implicit inherits column.
+        /// </summary>
+        public const string InheritsColumn = "__sc__inherits__";
+
+        /// <summary>
+        /// Gets the name of the implicit column that indicates if
+        /// an entity is a type.
+        /// </summary>
+        public const string IsTypeColumn = "__sc__is_type__";
+    }
+}
