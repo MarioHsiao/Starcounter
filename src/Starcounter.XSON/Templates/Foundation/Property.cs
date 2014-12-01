24a2902b5c5f45a8677f162576e0b6578a4e42a9
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-12-01 09:46:57 +0100

    Suppressing generating change to client for values in inputhandlers when value is not changed (#1345)

--- src/Starcounter.XSON/Templates/Foundation/Property.cs
+++ src/Starcounter.XSON/Templates/Foundation/Property.cs
@@ -227,8 +227,10 @@ namespace Starcounter.Templates {
         public void ProcessInput(Json parent, T value) {
             Input<T> input = null;
 
-            if (_inputEventCreator != null)
+            if (_inputEventCreator != null) {
                 input = _inputEventCreator.Invoke(parent, this, value);
+                input.ValueChanged = false;
+            }
 
             if (input != null && _inputHandler != null) {
                 input.OldValue = Getter(parent);
@@ -237,6 +239,9 @@ namespace Starcounter.Templates {
                 if (!input.Cancelled) {
                     Debug.WriteLine("Setting value after custom handler: " + input.Value);
                     Setter(parent, input.Value);
+
+                    if (!input.ValueChanged) // Incoming value have not been changed. Remove the dirtyflag.
+                        this.Checkpoint(parent);
                 } else {
                     Debug.WriteLine("Handler cancelled: " + value);
                 }
@@ -244,6 +249,7 @@ namespace Starcounter.Templates {
                 if (BasedOn == null) {
                     Debug.WriteLine("Setting value after no handler: " + value);
                     Setter(parent, value);
+                    this.Checkpoint(parent); // Incoming value have not been changed. Remove the dirtyflag.
                 } else {
                     // This is an inherited template with no inputhandler, lets 
                     // see if the base-template has a registered handler.
@@ -264,6 +270,7 @@ namespace Starcounter.Templates {
 
                 if (!input.Cancelled) {
                     existingInput.Value = input.Value;
+                    existingInput.ValueChanged = input.ValueChanged;
                 } else {
                     existingInput.Cancel();
                 }
