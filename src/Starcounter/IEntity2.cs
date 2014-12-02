fdc12a722c322ab4dd06140921e5b738c8256186
Per Samuelsson <per@starcounter.com>
2014-12-02 10:26:24 +0100

    Introduced new IEntity2 interface (will be renamed). With this interface, any database class can expose itself as an entity.

---
+++ src/Starcounter/IEntity2.cs
@@ -0,0 +1,64 @@
+﻿using Starcounter.Binding;
+
+namespace Starcounter {
+    
+    /// <summary>
+    /// Defines the interface of any entity. All database classes that
+    /// are defined not using <see cref="Entity"/> class as their base
+    /// class can be represented by the <see cref="IEntity2"/> interface
+    /// using runtime factory/cast method <see cref="Entity.From(obj)"/>.
+    /// </summary>
+    /// <remarks>
+    /// This interface will be renamed to IEntity as soon as we have
+    /// figured out how to support the functionality of the old interface
+    /// with the same name.
+    /// <p>
+    /// Possibly, we'll merge this interface with IObjectView/IObjectProxy
+    /// later on.
+    /// </p>
+    /// </remarks>
+    public interface IEntity2 {
+        /// <summary>
+        /// Gets or sets the type of the current entity.
+        /// </summary>
+        IEntity2 Type {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the type the current entity is
+        /// a subtype of.
+        /// </summary>
+        IEntity2 Inherits {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating if the current entity
+        /// is a type.
+        /// </summary>
+        bool IsType {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the current entity, normally
+        /// used when naming a type.
+        /// </summary>
+        string TypeName {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets a reference to a <see cref="IObjectProxy"/> that
+        /// can be used to access data of the current entity.
+        /// </summary>
+        IObjectProxy Proxy {
+            get;
+        }
+    }
+}
