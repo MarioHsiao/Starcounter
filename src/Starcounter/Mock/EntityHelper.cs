3950e8eb6756abf9afb7647e4cc8ddc72e060d4e
Per Samuelsson <per@starcounter.com>
2014-12-02 10:32:05 +0100

    Added new helper class EntityHelper, allowing users to access entity-level properties on any database class using a set of static helper methods.

---
+++ src/Starcounter/Mock/EntityHelper.cs
@@ -0,0 +1,59 @@
+﻿
+using Sc.Server.Weaver;
+using Starcounter.Binding;
+using Starcounter.Internal;
+using System;
+
+namespace Starcounter {
+
+    /// <summary>
+    /// Helper class to access dynamic types properties and fields
+    /// for database classes defined only with the [Database] attribute,
+    /// and not inheriting <see cref="Entity"/>.
+    /// </summary>
+    public abstract class EntityHelper {
+
+        static IEntity2 EntityFromObject(object obj) {
+            return Entity.From(obj);
+        }
+
+        public static IEntity2 GetType(object obj) {
+            return Entity.From(obj).Type;
+        }
+
+        public static void SetType(object obj, object type) {
+            var wrapper = EntityFromObject(obj);
+            wrapper.Type = (IEntity2)type;
+        }
+
+        public static string GetTypeName(object obj) {
+            var wrapper = EntityFromObject(obj);
+            return wrapper.TypeName;
+        }
+
+        public static void SetTypeName(object obj, string name) {
+            var wrapper = EntityFromObject(obj);
+            wrapper.TypeName = name;
+        }
+
+        public static IEntity2 GetInherits(object obj) {
+            var wrapper = EntityFromObject(obj);
+            return wrapper.Inherits;
+        }
+
+        public static void SetInherits(object obj, object type) {
+            var wrapper = EntityFromObject(obj);
+            wrapper.Inherits = (IEntity2)type;
+        }
+
+        public static bool IsType(object obj) {
+            var wrapper = EntityFromObject(obj);
+            return wrapper.IsType;
+        }
+
+        public static void SetIsType(object obj, bool value) {
+            var wrapper = EntityFromObject(obj);
+            wrapper.IsType = value;
+        }
+    }
+}
