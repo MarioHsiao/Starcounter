82f8e1bba88b2e622cda612e185c064c8da7768e
Christian Holmstrand <christian.holmstrand@starcounter.com>
2015-01-19 09:32:54 +0100

    Removed the file Json.Transaction.cs and move the members to Json.Object.Data.cs

--- src/Starcounter.XSON/Class Json/Json.Object.Data.cs
+++ src/Starcounter.XSON/Class Json/Json.Object.Data.cs
@@ -143,5 +143,35 @@ namespace Starcounter {
         /// </summary>
         protected virtual void OnData() {
         }
+
+        /// <summary>
+        /// Gets the nearest transaction.
+        /// </summary>
+        public ITransaction Transaction {
+            get {
+                // Returning first available transaction climbing up the tree starting from this node.
+                if (_transaction != null)
+                    return _transaction;
+
+                if (_parent != null)
+                    return _parent.Transaction;
+
+                return null;
+            }
+        }
+
+        public void AttachCurrentTransaction() {
+            if (_DB != null && _DB.CurrentTransaction != null)
+                _transaction = _DB.CurrentTransaction;
+        }
+
+        /// <summary>
+        /// Returns the transaction that is set on this app. Does NOT
+        /// look in parents.
+        /// </summary>
+        internal ITransaction ThisTransaction {
+            get { return _transaction; }
+            set { _transaction = value; }
+        }
     }
 }
