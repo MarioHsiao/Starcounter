a567cd10953ac0680e06e1aff2d8f05dc8a63953
Per Samuelsson <per@starcounter.com>
2015-01-28 09:19:32 +0100

    Extended BasicDynamicTypesModelling regression test to include instantiating of Foo1 and testing the auto-assigning of the type.

--- src/Samples/TestApps/BasicDynamicTypesModelling/BasicDynamicTypesModelling.cs
+++ src/Samples/TestApps/BasicDynamicTypesModelling/BasicDynamicTypesModelling.cs
@@ -6,6 +6,11 @@ class Program {
 	static void Main() {
 		var f1 = Db.SQL<Entity>("SELECT e FROM Entity e WHERE e.IsType = ? AND e.Name = ?", true, typeof(Foo1).Name).First;
 		ScAssertion.Assert(f1 != null, "The Foo1 type should be found");
-		ScAssertion.Assert(f1.Equals(Db.TypeOf<Foo1>()), "The Foo1 type should be equal to Db.TypeOf<Foo>");
+		ScAssertion.Assert(f1.Equals(Db.TypeOf<Foo1>()), "The Foo1 type should be equal to Db.TypeOf<Foo1>");
+		Db.Transaction(() => {
+			f1 = new Foo1();
+			ScAssertion.Assert(f1.Type != null, "Foo1.Type should not be NULL");
+			ScAssertion.Assert(f1.Type.Equals(Db.TypeOf<Foo1>()), "Foo1.Typeshould be equal to Db.TypeOf<Foo1>");
+		});
 	}
 }
