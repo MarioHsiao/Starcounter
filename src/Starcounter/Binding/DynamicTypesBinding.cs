88baaac22cb14828088d7e0a832108a5c97a5ebe
Per Samuelsson <per@starcounter.com>
2015-01-29 10:10:36 +0100

    Added new method IsUserDefinedProperty to DynamicsTypesBinding class. We'll use this method to determine if a user class defines a type reference excplicitly, or if it's derived from the Starcounter runtime classes.

--- src/Starcounter/Binding/DynamicTypesBinding.cs
+++ src/Starcounter/Binding/DynamicTypesBinding.cs
@@ -108,5 +108,28 @@ namespace Starcounter.Binding {
 
             return result;
         }
+
+        /// <summary>
+        /// Return <c>true</c> if <paramref name="property"/>, resolved
+        /// from <paramref name="resolvedFrom"/> is defined by the user, or
+        /// inherited from a Starcounter base type.
+        /// </summary>
+        static bool IsUserDefinedProperty(int property, TypeDef resolvedFrom) {
+            if (resolvedFrom.IsStarcounterType) {
+                return false;
+            }
+
+            if (resolvedFrom.BaseName == null) {
+                return true;
+            }
+
+            var prop = resolvedFrom.PropertyDefs[property];
+            var baseDef = Bindings.GetTypeDef(resolvedFrom.BaseName);
+            if (property < baseDef.PropertyDefs.Length) {
+                return IsUserDefinedProperty(property, baseDef);
+            }
+
+            return true;
+        }
     }
 }
