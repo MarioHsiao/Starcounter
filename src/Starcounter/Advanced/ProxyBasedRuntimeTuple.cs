2207be23abf5ae76b7ef1c07b16d890ada4a6ac5
Per Samuelsson <per@starcounter.com>
2015-01-29 15:48:01 +0100

    Implemented IDbTuple.Create on ProxyBasedRuntimeTuple.

--- src/Starcounter/Advanced/ProxyBasedRuntimeTuple.cs
+++ src/Starcounter/Advanced/ProxyBasedRuntimeTuple.cs
@@ -1,6 +1,7 @@
 ﻿using Sc.Server.Weaver;
 using Starcounter.Binding;
 using Starcounter.Internal;
+using System;
 
 namespace Starcounter.Advanced {
     /// <summary>
@@ -75,7 +76,23 @@ namespace Starcounter.Advanced {
         }
 
         IDbTuple IDbTuple.Create() {
-            throw new System.NotImplementedException();
+            // Proper error messages including new error codes.
+            // Delayed until final implementation though (see
+            // #2500 for more info).
+            // TODO:
+            var self = (IDbTuple)this;
+            if (!self.IsType) throw new InvalidOperationException("This object is not a type.");
+            if (string.IsNullOrEmpty(self.Name)) throw new InvalidOperationException("The type name is not specified.");
+
+            var tb = Bindings.GetTypeBinding(self.Name);
+            ulong oid = 0, addr = 0;
+            DbState.Insert(tb.TableId, ref oid, ref addr);
+            var proxy = tb.NewInstance(addr, oid);
+
+            var tuple = TupleHelper.ToTuple(proxy);
+            tuple.Type = self;
+
+            return tuple;
         }
     }
 }
