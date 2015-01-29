3c61247f937a1e6984e10a069b690bda13608682
Per Samuelsson <per@starcounter.com>
2015-01-29 12:02:01 +0100

    Adapted the BasicDynamicTypesModelling tests to the new implementation of auto-creation and -assigning of dynamic types.

--- src/Samples/TestApps/BasicDynamicTypesModelling/BasicDynamicTypesModelling.cs
+++ src/Samples/TestApps/BasicDynamicTypesModelling/BasicDynamicTypesModelling.cs
@@ -20,14 +20,15 @@ public class SomethingKind : Something {}
 
 class Program {
 	static void Main() {
-		var f1 = Db.SQL<Entity>("SELECT e FROM Entity e WHERE e.IsType = ? AND e.Name = ?", true, typeof(Foo1).Name).First;
-		ScAssertion.Assert(f1 != null, "The Foo1 type should be found");
-		ScAssertion.Assert(f1.Equals(Db.TypeOf<Foo1>()), "The Foo1 type should be equal to Db.TypeOf<Foo1>");
+		var f1Type = Db.TypeOf<Foo1>();
+		ScAssertion.Assert(f1Type != null, "The Foo1 type should exist");
+		
 		Db.Transaction(() => {
-			f1 = new Foo1();
+			var f1 = new Foo1();
 			ScAssertion.Assert(f1.Type != null, "Foo1.Type should not be NULL");
 			ScAssertion.Assert(f1.Type.Equals(Db.TypeOf<Foo1>()), "Foo1.Type should be equal to Db.TypeOf<Foo1>");
 		});
+		
 		Db.Transaction(() => {
 			var f2 = new Foo2();
 			ScAssertion.Assert(f2.Type != null, "Foo2.Type should not be NULL");
