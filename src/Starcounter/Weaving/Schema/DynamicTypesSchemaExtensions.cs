3abf7354ecc5b8b241282c741a9187023a8a05ed
Per Samuelsson <per@starcounter.com>
2015-02-24 15:01:40 +0100

    Added new class DynamicTypesSchemaExtensions that introduce helper methods for dynamic types on top of an underlying database schema produced by the weaver.
    (cherry picked from commit d46fac6f30527a4f0e987839170992370222f9bb)

---
+++ src/Starcounter/Weaving/Schema/DynamicTypesSchemaExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sc.Server.Weaver.Schema {
+
+    public static class DynamicTypesSchemaExtensions {
+
+        public static Dictionary<DatabaseAttribute, DatabaseEntityClass> GetCompileTimeTypeReferences(
+            this DatabaseSchema schema) {
+
+            var result = new Dictionary<DatabaseAttribute, DatabaseEntityClass>();
+            foreach (var dbc in schema.IndexedDatabaseClasses) {
+                var typeRef = dbc.Attributes.FirstOrDefault(a => a.IsTypeReference);
+                if (typeRef != null) {
+                    result.Add(typeRef, typeRef.AttributeType as DatabaseEntityClass);
+                }
+            }
+            return result;
+        }
+
+        public static List<DatabaseEntityClass> GetCompileTimeTypes(this DatabaseSchema schema) {
+            var result = new List<DatabaseEntityClass>();
+            var refs = GetCompileTimeTypeReferences(schema);
+            result.AddRange(refs.Values.Distinct());
+            return result;
+        }
+    }
+}
