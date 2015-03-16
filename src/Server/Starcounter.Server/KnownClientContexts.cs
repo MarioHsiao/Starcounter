56669948f4d30a0231f375539fd220089c8af678
Per Samuelsson <per@starcounter.com>
2015-03-16 09:24:12 +0100

    Moved class ClientContext from Starcounter.CLI to Starcounter.Server, to prepare for a redesign.

--- src/Server/Starcounter.Server/KnownClientContexts.cs
+++ src/Server/Starcounter.Server/KnownClientContexts.cs
@@ -1,5 +1,48 @@
 ﻿
+using System;
+using System.Diagnostics;
+
 namespace Starcounter.Server {
+
+    /// <summary>
+    /// Provides information about the calling client context.
+    /// </summary>
+    public static class ClientContext {
+        /// <summary>
+        /// The current context. Set by client applications.
+        /// </summary>
+        public static string Current = KnownClientContexts.UnknownContext;
+
+        /// <summary>
+        /// Creates a string containing the current client context
+        /// information, including information about the current client
+        /// and the user.
+        /// </summary>
+        /// <returns>A string representing the current context.</returns>
+        public static string GetCurrentContextInfo() {
+            return Make(Current);
+        }
+
+        static string Make(string context) {
+            // Note:
+            // Don't change this format unless also changing the parser
+            // method KnownClientContext.FromContextInfo() in Starcounter.Server.
+            // Example contexts:
+            //  * "star.exe, per@per-asus (via star.exe)"
+            //  * "VS, per@per-asus (via devenv.exe)"
+            var program = Process.GetCurrentProcess().MainModule.ModuleName;
+            try {
+                return string.Format("{0}, {1}@{2} (via {3})",
+                    context,
+                    Environment.UserName.ToLowerInvariant(),
+                    Environment.MachineName.ToLowerInvariant(), program
+                    );
+            } catch {
+                return string.Format("{0}, (via {1})", context, program);
+            }
+        }
+    }
+
     /// <summary>
     /// Enum-like class with constants of known management
     /// client contexts.
