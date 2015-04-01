ef5fd8f52f255df3c49a585bfc936dd926c6231a
Per Samuelsson <per@starcounter.com>
2015-04-01 12:41:25 +0200

    Added new helper class DynamicTypesHelper to the Starcounter assembly, and implemented shared method RuntimeNew for dynamic type instantiation.

---
+++ src/Starcounter/Internal/DynamicTypesHelper.cs
@@ -0,0 +1,20 @@
+﻿using Starcounter.Binding;
+using System;
+
+namespace Starcounter.Internal {
+
+    internal static class DynamicTypesHelper {
+
+        public static IObjectProxy RuntimeNew(int tableId) {
+            // Proper error messages including new error codes.
+            // Delayed until final implementation though (see
+            // #2500 for more info).
+            // TODO:
+            if (tableId == sccoredb.STAR_INVALID_TABLE_ID) throw new InvalidOperationException("This object is not a type.");
+            var tb = Bindings.GetTypeBinding(tableId);
+            ulong oid = 0, addr = 0;
+            DbState.Insert(tb.TableId, ref oid, ref addr);
+            return tb.NewInstance(addr, oid);
+        }
+    }
+}
