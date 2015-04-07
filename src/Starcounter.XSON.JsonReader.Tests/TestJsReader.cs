a9804803757bf8b32e45c0cfb5b600d7f14b05d2
Christian Holmstrand <christian.holmstrand@starcounter.com>
2015-04-07 16:04:09 +0200

    Added unittest for parsing json as primitive value and primitive values in array

--- src/Starcounter.XSON.JsonReader.Tests/TestJsReader.cs
+++ src/Starcounter.XSON.JsonReader.Tests/TestJsReader.cs
@@ -12,7 +12,7 @@ using TJson = Starcounter.Templates.TObject;
 
 namespace Starcounter.Internal.JsonTemplate.Tests {
     /// <summary>
-    /// Class TestJsReader
+    ///
     /// </summary>
     public class TestJsReader {
         /// <summary>
@@ -20,13 +20,7 @@ namespace Starcounter.Internal.JsonTemplate.Tests {
         /// </summary>
         [Test]
         public static void DefaultValueTest() {
-            string script = @"{
-                                StrVal: ""default"",
-                                BoolVal: true,
-                                DblVal: 2e4,
-                                DecVal: 23.4,
-                                LongVal: 99
-                              }";
+            string script = @"{StrVal: ""default"",BoolVal: true,DblVal: 2e4,DecVal: 23.4,LongVal: 99}";
 
             TObject schema = TObject.CreateFromJson(script);
 
@@ -42,8 +36,7 @@ namespace Starcounter.Internal.JsonTemplate.Tests {
         /// </summary>
         [Test]
         public static void CreateSimpleTemplateFromString() {
-            const string script2 = @"
-                                    {
+            const string script2 = @"{
                                       FirstName:'Joachim', 
                                       LastName:'Wester', 
                                       Selected:true,
@@ -73,7 +66,7 @@ namespace Starcounter.Internal.JsonTemplate.Tests {
         public static void CreateSimpleAdornedTemplateFromJs() {
             const string script2 = @"
                                     {
-                                      FirstName:'Joachim'               .Editable(), 
+                                      FirstName:'Joachim'.Editable(), 
                                     }.Class('TestApp')";
 
             var actual = TJson.CreateFromJson(script2);
@@ -182,6 +175,118 @@ namespace Starcounter.Internal.JsonTemplate.Tests {
 }.Class('CrmApp')";
             var actual = TJson.CreateFromMarkup<Json, TJson>("json", script, null);
             Assert.IsInstanceOf(typeof(TJson), actual);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public static void TestCreateTemplateFromPrimitiveJson() {
+            // string
+            string json = @"""value""";
+            TValue schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TString>(schema);
+            Assert.AreEqual("value", ((TString)schema).DefaultValue);
+
+            // integer
+            json = @"13";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TLong>(schema);
+            Assert.AreEqual(13, ((TLong)schema).DefaultValue);
+
+            // boolean
+            json = @"true";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TBool>(schema);
+            Assert.AreEqual(true, ((TBool)schema).DefaultValue);
+
+            // double
+            json = @"1e3";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TDouble>(schema);
+            Assert.AreEqual(1000.0d, ((TDouble)schema).DefaultValue);
+
+            // decimal
+            json = @"1.2354";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TDecimal>(schema);
+            Assert.AreEqual(1.2354m, ((TDecimal)schema).DefaultValue);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+//        [Test]
+        public static void TestCreateTemplateFromNamedPrimitive() {
+            // TODO:
+            // Test disabled. Not supported in parser. Needs to be fixed there.
+
+            string json = @"""namedmember"":""value""";
+            TValue schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+
+            Assert.IsInstanceOf<TString>(schema);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public static void TestCreateTemplateFromPrimitiveArrayJson() {
+            TValue elementType;
+
+            // string
+            string json = @"[""value""]";
+            TValue schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TObjArr>(schema);
+            elementType = ((TObjArr)schema).ElementType;
+            Assert.IsInstanceOf<TString>(elementType);
+            Assert.AreEqual("value", ((TString)elementType).DefaultValue);
+
+            // integer
+            json = @"[13]";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TObjArr>(schema);
+            elementType = ((TObjArr)schema).ElementType;
+            Assert.IsInstanceOf<TLong>(elementType);
+            Assert.AreEqual(13, ((TLong)elementType).DefaultValue);
+
+            // boolean
+            json = @"[true]";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TObjArr>(schema);
+            elementType = ((TObjArr)schema).ElementType;
+            Assert.IsInstanceOf<TBool>(elementType);
+            Assert.AreEqual(true, ((TBool)elementType).DefaultValue);
+
+            // double
+            json = @"[1e3]";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TObjArr>(schema);
+            elementType = ((TObjArr)schema).ElementType;
+            Assert.IsInstanceOf<TDouble>(elementType);
+            Assert.AreEqual(1000.0d, ((TDouble)elementType).DefaultValue);
+
+            // decimal
+            json = @"[1.2354]";
+            schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TObjArr>(schema);
+            elementType = ((TObjArr)schema).ElementType;
+            Assert.IsInstanceOf<TDecimal>(elementType);
+            Assert.AreEqual(1.2354m, ((TDecimal)elementType).DefaultValue);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public static void TestCreateTemplateWithPrimitiveArray() {
+            string json = @"{""Items"":[1]}";
+            TValue schema = TObject.CreateFromMarkup<Json, TValue>("JSON", json, null);
+            Assert.IsInstanceOf<TObject>(schema);
+
+            var tarr = (TObjArr)((TObject)schema).Properties[0];
+
+            Assert.IsInstanceOf<TLong>(tarr.ElementType);
         }
     }
 }
