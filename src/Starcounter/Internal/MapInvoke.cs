89c17084533a5cb0f26f3dd65b15a3ad9f60b1f7
Per Samuelsson <per@starcounter.com>
2015-04-21 13:37:15 +0200

    Added prototypish facade class MapInvoke, with static utility methods that are to be invoked by weaved database classes to support mapping.

---
+++ src/Starcounter/Internal/MapInvoke.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Starcounter.Internal {
+
+    public static class MapInvoke {
+
+        public static void POST(string name, ulong key) {
+            var target = MakeURI(name, key);
+            Console.WriteLine(target);
+        }
+
+        public static void PUT(string name, ulong key) {
+            var target = MakeURI(name, key);
+            Console.WriteLine(target);
+        }
+
+        public static void DELETE(string name, ulong key) {
+            var target = MakeURI(name, key);
+            Console.WriteLine(target);
+        }
+
+        static string MakeURI(string name, ulong key) {
+            var s = name.Replace('.', '/').ToLowerInvariant();
+            return string.Format("{0}/{1}", s, key.ToString());
+        }
+    }
+}
