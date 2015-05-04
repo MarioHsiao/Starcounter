cc17e9f6e34a2805c10b6486c77a2398c7cc534a
andwah <anders.wahlgren@societyobjects.com>
2015-05-04 15:19:49 +0200

    Fixed App image

--- src/Starcounter.Administrator/app/directives/VersionCheck.js
+++ src/Starcounter.Administrator/app/directives/VersionCheck.js
@@ -128,16 +128,3 @@ adminModule.directive("versionCheck", ['$http', '$log', 'VersionCheckService', '
     }
 
 }]);
-
-
-adminModule.directive('errSrc', function () {
-    return {
-        link: function (scope, element, attrs) {
-            element.bind('error', function () {
-                if (attrs.src != attrs.errSrc) {
-                    attrs.$set('src', attrs.errSrc);
-                }
-            });
-        }
-    }
-});
