d61fd3eb51f9797c55037882e69e9092999b9098
Christian Holmstrand <christian.holmstrand@starcounter.com>
2015-05-27 09:46:56 +0200

    Added unittest for testing conversion of enums in dataobject to and from string in typedjson (#2706)

---
+++ src/Starcounter.XSON.Tests/DataObjects/ObjWithEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Internal.XSON.Tests {
+    public class ObjWithEnum : Entity {
+        public TestEnum TestEnum { get; set; }
+    }
+
+    public enum TestEnum {
+        First,
+        Second,
+        Third
+    }
+}
