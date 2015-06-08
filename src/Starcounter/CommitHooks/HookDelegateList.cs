41fca254715b9ddd464a466b123268601c0cc90f
Per Samuelsson <per@starcounter.com>
2015-06-08 15:45:52 +0200

    Added bulk of the managed commit hook layer, migrated and somewhat redesigned from the NEXT branch, to instead work on the DEVELOP branch, using the recently added P/Invoke API's.

---
+++ src/Starcounter/CommitHooks/HookDelegateList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Starcounter {
+    /// <summary>
+    /// List of delegates with the same signature type.
+    /// </summary>
+    /// <typeparam name="T">Type of target.</typeparam>
+    internal class HookDelegateList<T> {
+        List<Action<T>> targets = new List<Action<T>>();
+
+        /// <summary>
+        /// Adds a new callback with the given target.
+        /// </summary>
+        /// <param name="callback">Delegate method.</param>
+        /// <returns>The index of the added callback.</returns>
+        public int Add(Action<T> callback) {
+            var index = targets.IndexOf(callback);
+            if (index == -1) {
+                targets.Add(callback);
+                index = targets.Count - 1;
+            }
+            return index;
+        }
+
+        /// <summary>
+        /// Gets a target callback by index.
+        /// </summary>
+        /// <param name="index">The index of the callback to
+        /// retreive</param>
+        /// <returns>The callback with the given index.</returns>
+        public Action<T> Get(int index) {
+            return targets[index];
+        }
+    }
+}
