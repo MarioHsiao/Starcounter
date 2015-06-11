d96fa8353e9b4d162cdaa49935d5f4a909e510aa
Per Samuelsson <per@starcounter.com>
2015-06-11 11:47:00 +0200

    Added new, basic regression test for commit hooks, as part of the TestApps suite.

---
+++ src/Samples/TestApps/CommitHooksTest/CommitHooksTest.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+REM Checking if test should be run.
+IF "%SC_RUN_COMMITHOOKSTEST%"=="False" GOTO :EOF
+
+REM Some predefined constants.
+SET DB_NAME=CommitHooksTest
+SET TEST_NAME=CommitHooksTest
+
+ECHO Running %TEST_NAME% regression test.
+
+if "%SC_RUNNING_ON_BUILD_SERVER%"=="True" GOTO skipdbdrop
+ECHO Delete database after server is started
+star --database=%DB_NAME% ..\EmptyScApp.cs
+staradmin --database=%DB_NAME% stop db
+staradmin --database=%DB_NAME% delete --force db
+
+:skipdbdrop
+
+star --database=%DB_NAME% CommitHooksTest.cs
+IF ERRORLEVEL 1 GOTO err
+
+
+ECHO %TEST_NAME% regression test succeeded.
+EXIT /b 0
+
+
+:err
+ECHO Error: %TEST_NAME% failed!
+EXIT /b 1
