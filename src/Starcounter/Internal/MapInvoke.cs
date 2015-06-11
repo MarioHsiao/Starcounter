23aaebadbdccbc44d37e7ed3307ef8d3eaef658f
Per Samuelsson <per@starcounter.com>
2015-06-11 15:51:32 +0200

    Bound MapInvoke methods to the new Map facade class.

--- src/Starcounter/Internal/MapInvoke.cs
+++ src/Starcounter/Internal/MapInvoke.cs
@@ -2,22 +2,26 @@
 using System;
 
 namespace Starcounter.Internal {
-
+    /// <summary>
+    /// Implement an intermediate call level layer, in between
+    /// the weaver/host and the actual mapper logic. Will be replaced
+    /// in the final, optimized solution.
+    /// </summary>
     public static class MapInvoke {
 
         public static void POST(string name, ulong key) {
             var target = MakeURI(name, key);
-            Console.WriteLine(target);
+            Map.POST(target);
         }
 
         public static void PUT(string name, ulong key) {
             var target = MakeURI(name, key);
-            Console.WriteLine(target);
+            Map.PUT(target);
         }
 
         public static void DELETE(string name, ulong key) {
             var target = MakeURI(name, key);
-            Console.WriteLine(target);
+            Map.DELETE(target);
         }
 
         static string MakeURI(string name, ulong key) {
