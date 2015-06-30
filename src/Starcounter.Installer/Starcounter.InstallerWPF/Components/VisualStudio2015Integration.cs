c7152824cb85978fc583f761fd3149a80fdf8f50
Per Samuelsson <per@starcounter.com>
2015-06-30 14:30:07 +0200

    Added neccessary VS 2015 installer components to the outer WPF project.

---
+++ src/Starcounter.Installer/Starcounter.InstallerWPF/Components/VisualStudio2015Integration.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Collections.ObjectModel;
+using Starcounter.InstallerEngine;
+using System;
+
+namespace Starcounter.InstallerWPF.Components
+{
+    public class VisualStudio2015Integration : BaseComponent
+    {
+        public const string Identifier = "VisualStudio2015Integration";
+
+        public override string ComponentIdentifier
+        {
+            get
+            {
+                return VisualStudio2015Integration.Identifier;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "VisualStudio 2015 Integration";
+            }
+        }
+
+        //public override bool IsExecuteCommandEnabled {
+        //    get {
+        //        return false;
+        //    }
+        //}
+
+        public override bool CanBeInstalled {
+            get {
+                return true;
+            }
+        }
+        //public override bool IsExecuteCommandEnabled {
+        //    get {
+        //        return false;
+        //    }
+        //}   
+
+
+        private readonly string[] _Dependencys = new string[] { VisualStudio2015.Identifier, PersonalServer.Identifier };
+        public override string[] Dependencys
+        {
+            get
+            {
+                return this._Dependencys;
+            }
+        }
+
+
+        protected override void SetDefaultValues()
+        {
+            base.SetDefaultValues();
+
+#if !SIMULATE_CLEAN_INSTALLATION
+            this.IsInstalled = MainWindow.InstalledComponents[(int)ComponentsCheck.Components.VS2015Integration];
+#endif
+            switch (this.Command)
+            {
+                case ComponentCommand.Install:
+                    this.ExecuteCommand = (!this.IsInstalled) && (DependenciesCheck.VStudio2015Installed());
+                    break;
+                case ComponentCommand.None:
+                    this.ExecuteCommand = false;
+                    break;
+                case ComponentCommand.Uninstall:
+                    this.ExecuteCommand = false;
+                    break;
+                case ComponentCommand.Update:
+                    this.ExecuteCommand = false;
+                    break;
+            }
+        }
+        public VisualStudio2015Integration(ObservableCollection<BaseComponent> components)
+            : base(components)
+        {
+        }
+
+        public override bool ValidateSettings() {
+            return true;
+        }
+
+    }
+}
