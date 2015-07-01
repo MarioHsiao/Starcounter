d3f24c1118afe23ce205d2dfebb948740eedf0db
Pär Eriksson <parherman@gmail.com>
2015-07-01 16:15:05 +0200

    Check if document generator exists instead of checking hostname

--- src/BuildSystem/Documentation/generate_all.bat
+++ src/BuildSystem/Documentation/generate_all.bat
@@ -1,12 +1,12 @@
 @echo off
 
-:: Checking if its SCBUILDSERVER.
-IF NOT "%COMPUTERNAME%"=="SCBUILDSERVER" GOTO :EOF
-
 :: Checking if documentation should be generated.
 IF "%SC_GENERATE_CODE_DOCS%"=="False" GOTO :EOF
 
-set DocXPath=C:\Program Files (x86)\Innovasys\DocumentX2013\bin\DocumentXCommandLinex64.exe
+:: Checking if DocumentX exist.
+set DocXPath=C:\Program Files (x86)\Innovasys\DocumentX2015\bin\DocumentXCommandLinex64.exe
+IF NOT EXIST "%DocXPath%" set DocXPath=C:\Program Files (x86)\Innovasys\DocumentX2013\bin\DocumentXCommandLinex64.exe
+IF NOT EXIST "%DocXPath%" GOTO :EOF
 
 call "%DocXPath%" ".\public\starcounter.dxp" [/buildconfiguration="Public"]
 call "%DocXPath%" ".\internal\starcounter.dxp" [/buildconfiguration="Internal"]
