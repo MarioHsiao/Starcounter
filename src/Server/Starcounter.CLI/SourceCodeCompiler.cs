4d1fdbbf2663313b423b565d03633350bbfbc3d8
Per Samuelsson <per@starcounter.com>
2015-08-04 13:46:31 +0200

    Removed Starcounter.Extensions from the set of built in references known to the star.exe source code compiler.

--- src/Server/Starcounter.CLI/SourceCodeCompiler.cs
+++ src/Server/Starcounter.CLI/SourceCodeCompiler.cs
@@ -34,7 +34,6 @@ namespace Starcounter.CLI {
             "Starcounter.Apps.JsonPatch",
             "Starcounter.HyperMedia",
             "Starcounter.Internal",
-            "Starcounter.Extensions",
             "Starcounter.Logging",
             "Starcounter.Node",
             "Starcounter.XSON",
