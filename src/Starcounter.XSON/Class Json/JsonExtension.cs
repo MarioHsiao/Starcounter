10f3f1fc7012c5083385abcbd98c60543bfd532b
alemoi <alexey.moiseenko@starcounter.com>
2015-08-11 01:51:28 +0200

    Added set for AppName inside Json scope calls.

--- src/Starcounter.XSON/Class Json/JsonExtension.cs
+++ src/Starcounter.XSON/Class Json/JsonExtension.cs
@@ -14,11 +14,22 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="action">The delegate to execute</param>
         public static void Scope(this Json json, Action action) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                StarcounterBase.TransactionManager.Scope(handle, action);
-            else 
-                action();
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    StarcounterBase.TransactionManager.Scope(handle, action);
+                else
+                    action();
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -27,11 +38,22 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="action">The delegate to execute</param>
         public static void Scope<T>(this Json json, Action<T> action, T arg) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                StarcounterBase.TransactionManager.Scope<T>(handle, action, arg);
-            else
-                action(arg);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    StarcounterBase.TransactionManager.Scope<T>(handle, action, arg);
+                else
+                    action(arg);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -40,11 +62,22 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="action">The delegate to execute</param>
         public static void Scope<T1, T2>(this Json json, Action<T1, T2> action, T1 arg1, T2 arg2) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                StarcounterBase.TransactionManager.Scope<T1, T2>(handle, action, arg1, arg2);
-            else
-                action(arg1, arg2);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    StarcounterBase.TransactionManager.Scope<T1, T2>(handle, action, arg1, arg2);
+                else
+                    action(arg1, arg2);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -53,11 +86,22 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="action">The delegate to execute</param>
         public static void Scope<T1, T2, T3>(this Json json, Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                StarcounterBase.TransactionManager.Scope<T1, T2, T3>(handle, action, arg1, arg2, arg3);
-            else
-                action(arg1, arg2, arg3);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    StarcounterBase.TransactionManager.Scope<T1, T2, T3>(handle, action, arg1, arg2, arg3);
+                else
+                    action(arg1, arg2, arg3);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -66,10 +110,21 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="func">The delegate to execute</param>
         public static TResult Scope<TResult>(this Json json, Func<TResult> func) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                return StarcounterBase.TransactionManager.Scope<TResult>(handle, func);
-            return func();
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    return StarcounterBase.TransactionManager.Scope<TResult>(handle, func);
+                return func();
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -78,10 +133,21 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="func">The delegate to execute</param>
         public static TResult Scope<T, TResult>(this Json json, Func<T, TResult> func, T arg) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                return StarcounterBase.TransactionManager.Scope<T, TResult>(handle, func, arg);
-            return func(arg);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    return StarcounterBase.TransactionManager.Scope<T, TResult>(handle, func, arg);
+                return func(arg);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -90,10 +156,21 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="func">The delegate to execute</param>
         public static TResult Scope<T1, T2, TResult>(this Json json, Func<T1, T2, TResult> func, T1 arg1, T2 arg2) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                return StarcounterBase.TransactionManager.Scope<T1, T2, TResult>(handle, func, arg1, arg2);
-            return func(arg1, arg2);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    return StarcounterBase.TransactionManager.Scope<T1, T2, TResult>(handle, func, arg1, arg2);
+                return func(arg1, arg2);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -102,10 +179,21 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="func">The delegate to execute</param>
         public static TResult Scope<T1, T2, T3, TResult>(this Json json, Func<T1, T2, T3, TResult> func, T1 arg1, T2 arg2, T3 arg3) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                return StarcounterBase.TransactionManager.Scope<T1, T2, T3, TResult>(handle, func, arg1, arg2, arg3);
-            return func(arg1, arg2, arg3);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    return StarcounterBase.TransactionManager.Scope<T1, T2, T3, TResult>(handle, func, arg1, arg2, arg3);
+                return func(arg1, arg2, arg3);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -114,10 +202,21 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="func">The delegate to execute</param>
         public static TResult Scope<T1, T2, T3, T4, TResult>(this Json json, Func<T1, T2, T3, T4, TResult> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                return StarcounterBase.TransactionManager.Scope<T1, T2, T3, T4, TResult>(handle, func, arg1, arg2, arg3, arg4);
-            return func(arg1, arg2, arg3, arg4);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    return StarcounterBase.TransactionManager.Scope<T1, T2, T3, T4, TResult>(handle, func, arg1, arg2, arg3, arg4);
+                return func(arg1, arg2, arg3, arg4);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
 
         /// <summary>
@@ -126,10 +225,21 @@ namespace Starcounter.Advanced.XSON {
         /// </summary>
         /// <param name="func">The delegate to execute</param>
         public static TResult Scope<T1, T2, T3, T4, T5, TResult>(this Json json, Func<T1, T2, T3, T4, T5, TResult> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) {
-            var handle = json.GetTransactionHandle(true);
-            if (handle != TransactionHandle.Invalid)
-                return StarcounterBase.TransactionManager.Scope<T1, T2, T3, T4, T5, TResult>(handle, func, arg1, arg2, arg3, arg4, arg5);
-            return func(arg1, arg2, arg3, arg4, arg5);
+
+            String origAppName = StarcounterEnvironment.AppName;
+
+            try {
+
+                StarcounterEnvironment.AppName = json._appName;
+
+                var handle = json.GetTransactionHandle(true);
+                if (handle != TransactionHandle.Invalid)
+                    return StarcounterBase.TransactionManager.Scope<T1, T2, T3, T4, T5, TResult>(handle, func, arg1, arg2, arg3, arg4, arg5);
+                return func(arg1, arg2, arg3, arg4, arg5);
+
+            } finally {
+                StarcounterEnvironment.AppName = origAppName;
+            }
         }
     }
 }
