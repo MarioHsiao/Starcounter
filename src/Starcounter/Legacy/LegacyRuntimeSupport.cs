652945d8c346300da608de81ce4688675e98dfca
Per Samuelsson <per@starcounter.com>
2015-09-04 12:56:20 +0200

    Extended LegacyRuntimeSupport with an overloaded New(object), allowing us to create runtime instances based on another (database class) instance.

--- src/Starcounter/Legacy/LegacyRuntimeSupport.cs
+++ src/Starcounter/Legacy/LegacyRuntimeSupport.cs
@@ -6,14 +6,36 @@ using System;
 namespace Starcounter.Legacy {
 
     public static class LegacyRuntimeSupport {
-        public static IObjectView New(int runtimeClassHandle) {
-            if (runtimeClassHandle < 0 || runtimeClassHandle > ushort.MaxValue) {
-                throw new ArgumentOutOfRangeException();
-            }
 
+        /// <summary>
+        /// Watch out: this is legacy support, intended to be used by those
+        /// that know how. Focus is performance, no validations occur! Passing
+        /// a bad argument has undefined behaviour.
+        /// </summary>
+        /// <param name="runtimeClassHandle">Runtime handle retreived from legacy layer
+        /// </param>
+        /// <returns>New instance</returns>
+        public static IObjectView New(int runtimeClassHandle) {
             var binding = Bindings.GetTypeBinding(runtimeClassHandle);
+            return NewInstance(binding);
+        }
+
+        /// <summary>
+        /// Watch out: this is legacy support, intended to be used by those
+        /// that know how. Focus is performance, no validations occur. Passing
+        /// a bad argument has undefined behaviour.
+        /// </summary>
+        /// <param name="runtimeProxyTemplate">Template proxy</param>
+        /// <returns>New instance</returns>
+        public static IObjectView New(object runtimeProxyTemplate) {
+            var proxy = (IObjectView)runtimeProxyTemplate;
+            var binding = (TypeBinding)proxy.TypeBinding;
+            return NewInstance(binding);
+        }
+
+        static IObjectView NewInstance(TypeBinding binding) {
             ulong oid = 0, addr = 0;
-            DbState.Insert((ushort)runtimeClassHandle, ref oid, ref addr);
+            DbState.Insert((ushort)binding.TableId, ref oid, ref addr);
             return binding.NewInstance(addr, oid);
         }
     }
