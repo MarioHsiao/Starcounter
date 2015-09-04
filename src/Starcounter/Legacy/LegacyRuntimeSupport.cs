7062dddef4a48e11ae444d21e0524523bed94ecc
Per Samuelsson <per@starcounter.com>
2015-09-04 11:07:39 +0200

    Added new class LegacyRuntimeSupport, allowing bound classes to be instantiated at runtime with a New(int) method, accepting a runtime handle.

---
+++ src/Starcounter/Legacy/LegacyRuntimeSupport.cs
@@ -0,0 +1,20 @@
+﻿
+using Starcounter.Binding;
+using Starcounter.Internal;
+using System;
+
+namespace Starcounter.Legacy {
+
+    public static class LegacyRuntimeSupport {
+        public static IObjectView New(int runtimeClassHandle) {
+            if (runtimeClassHandle < 0 || runtimeClassHandle > ushort.MaxValue) {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var binding = Bindings.GetTypeBinding(runtimeClassHandle);
+            ulong oid = 0, addr = 0;
+            DbState.Insert((ushort)runtimeClassHandle, ref oid, ref addr);
+            return binding.NewInstance(addr, oid);
+        }
+    }
+}
