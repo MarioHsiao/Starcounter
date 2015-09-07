6b2f2e33b07414358c0bec57edd5ce9d63a9b6d4
Per Samuelsson <per@starcounter.com>
2015-09-07 09:36:18 +0200

    Introduced new AppHostTest regression test in TestApps.

---
+++ src/Samples/TestApps/TestIApplicationHost/AppHostTest.cs
@@ -0,0 +1,32 @@
+
+namespace TestIApplicationHost {
+    using System;
+    using System.Diagnostics;
+    using Starcounter;
+    using Starcounter.Hosting;
+    
+    public class Program : IApplicationHost {
+        static Application application = null;
+        
+        public void HostApplication(CodeHost codeHost, Application app) {
+            Trace.Assert(application == null);
+            Trace.Assert(app != null);
+            Trace.Assert(codeHost != null);
+            
+            Console.WriteLine("Application host invoked on application: {0}", app);
+            application = app;
+        }
+        
+        static void Main() {
+            var currentApp = Application.Current;
+            if (application == null) {
+              throw new Exception("Application host not properly invoked: application == null.");
+            }
+            if (application != currentApp) {
+              throw new Exception("Inconsistency in application host: application != current applicaion.");
+            }
+            Console.WriteLine("Application host successfully invoked");
+        }
+    }
+}
+
