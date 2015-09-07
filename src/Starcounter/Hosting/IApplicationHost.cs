f8ba614e3667448a18539369b632b7244be19f54
Per Samuelsson <per@starcounter.com>
2015-09-07 08:52:35 +0200

    Introduced the new IApplicationHost interface, the primary entrypoint to the new, small application hosting layer offered by the 2.x code host.

---
+++ src/Starcounter/Hosting/IApplicationHost.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Starcounter.Hosting {
+    /// <summary>
+    /// By implementing this interface on the same class that defines the
+    /// application entrypoint, applications are given an opportunity to access
+    /// code host services just prior to the invocation of the entrypoint.
+    /// The callback is invoked in a context where the code host assure there
+    /// is no other application are booting too (as opposed to the entrypoint,
+    /// which can run in parallel of applications are started in an async
+    /// fashion).
+    /// </summary>
+    public interface IApplicationHost {
+        /// <summary>
+        /// Invoked by the code host when the given application are hosted but
+        /// before it's entrypoint is invoked.
+        /// </summary>
+        /// <param name="codeHost">Reference to the code host</param>
+        /// <param name="application">The application that is being booted.
+        /// </param>
+        void HostApplication(CodeHost codeHost, Application application);
+    }
+}
