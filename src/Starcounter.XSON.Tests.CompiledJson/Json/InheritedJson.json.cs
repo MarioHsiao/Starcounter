6083e75501a61f4814ceff5b6d874d4a085057aa
Christian Holmstrand <christian.holmstrand@starcounter.com>
2015-10-15 13:33:16 +0200

    Added unittest to reproduce #2955

---
+++ src/Starcounter.XSON.Tests.CompiledJson/Json/InheritedJson.json.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Starcounter.Internal.XSON.Tests.CompiledJson {
+    /// <summary>
+    /// </summary>
+    [InheritedJson_json]
+    partial class InheritedJson : BaseJson {
+    }
+}
