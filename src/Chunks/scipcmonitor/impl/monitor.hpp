e0e96bdea464c67ed5b645268a15f5f7b00809aa
Christian Holmstrand <christian.holmstrand@starcounter.com>
2015-10-23 15:05:53 +0200

    Upgraded missed projects

--- src/Chunks/scipcmonitor/impl/monitor.hpp
+++ src/Chunks/scipcmonitor/impl/monitor.hpp
@@ -190,7 +190,7 @@ namespace starcounter {
             // Example: "Local\PERSONAL_active_databases_updated_event"
             if ((length = _snprintf_s(active_databases_updated_event_name, _countof
                (active_databases_updated_event_name), active_databases_updated_event_name_size
-               -1 /* null */, "Local\\%s_"ACTIVE_DATABASES_UPDATED_EVENT, server_name_.c_str()))
+               -1 /* null */, "Local\\%s_" ACTIVE_DATABASES_UPDATED_EVENT, server_name_.c_str()))
                < 0) {
                   log().error(SCERRIPCMFORMATACTIVEDBUPDATEDEV);
                   throw ipc_monitor_exception(SCERRIPCMFORMATACTIVEDBUPDATEDEV);
