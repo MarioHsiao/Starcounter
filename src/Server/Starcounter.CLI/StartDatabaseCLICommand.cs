c40664a60e53681a75ed5baf6718c38bd6fbaeaf
Per Samuelsson <per@starcounter.com>
2015-10-26 13:00:32 +0100

    Finalized proper implementation of StartDatabaseCLICommand, mainly making sure it allows being executed within the context of a bigger job.

--- src/Server/Starcounter.CLI/StartDatabaseCLICommand.cs
+++ src/Server/Starcounter.CLI/StartDatabaseCLICommand.cs
@@ -42,80 +42,16 @@ namespace Starcounter.CLI {
 
         /// <inheritdoc/>
         protected override void Run() {
+            if (this.Parent != null) {
+                RunWithinContext();
+                return;
+            }
+
+            ResponseExtensions.OnUnexpectedResponse = HandleUnexpectedResponse;
+
             try {
                 Status.StartNewJob(string.Format("Starting {0}", DatabaseName.ToLowerInvariant()));
-
-                if (StarcounterEnvironment.ServerNames.PersonalServer.Equals(ServerName, StringComparison.CurrentCultureIgnoreCase)) {
-                    ShowStatus("retrieving server status", true);
-                    if (!ServerServiceProcess.IsOnline()) {
-                        SharedCLI.ShowErrorAndSetExitCode(ErrorCode.ToMessage(Error.SCERRSERVERNOTAVAILABLE), true);
-                    }
-                }
-
-                ErrorDetail errorDetail;
-                EngineReference engineRef;
-                var node = Node;
-                var admin = AdminAPI;
-                var uris = admin.Uris;
-                var args = CLIArguments;
-                var databaseName = DatabaseName.ToLowerInvariant();
-                int statusCode;
-
-                ResponseExtensions.OnUnexpectedResponse = HandleUnexpectedResponse;
-
-                var response = node.GET(admin.FormatUri(uris.Engine, databaseName), null);
-                statusCode = response.FailIfNotSuccessOr(404);
-
-                if (statusCode == 404) {
-                    errorDetail = new ErrorDetail();
-                    try {
-                        errorDetail.PopulateFromJson(response.Body);
-                    } catch {
-                        // The content of the response is not ErrorDetail json. It might be some other 
-                        // 404 sent from a different source, that sets the content to the original 
-                        // exception message. Lets handle it as an unexpected response.
-                        HandleUnexpectedResponse(response);
-                    }
-
-                    // Consider changing this when its done in isolation. I think
-                    // the experience should be the opposite - you specify if you
-                    // want it implicitly created.
-                    if (errorDetail.ServerCode == Error.SCERRDATABASENOTFOUND) {
-                        var allowed = !args.ContainsFlag(Option.NoAutoCreateDb);
-                        if (!allowed) {
-                            var notAllowed =
-                                ErrorCode.ToMessage(Error.SCERRDATABASENOTFOUND,
-                                string.Format("Database: \"{0}\". Remove --{1} to create automatically.", databaseName, Option.NoAutoCreateDb));
-                            SharedCLI.ShowErrorAndSetExitCode(notAllowed, true);
-                        }
-
-                        ShowStatus("creating database");
-                        
-                        // TODO:
-                        // CreateDatabase(node, uris, databaseName);
-                    }
-
-                    ShowStatus("starting database");
-                    engineRef = new EngineReference();
-                    engineRef.Name = databaseName;
-                    engineRef.NoDb = args.ContainsFlag(Option.NoDb);
-                    engineRef.LogSteps = args.ContainsFlag(Option.LogSteps);
-
-                    string codeHostCommands;
-                    if (args.TryGetProperty(UnofficialOption.CodeHostCommandLineOptions, out codeHostCommands)) {
-                        engineRef.CodeHostCommandLineAdditions = codeHostCommands;
-                    }
-
-                    response = node.POST(admin.FormatUri(uris.Engines), engineRef.ToJson(), null);
-                    response.FailIfNotSuccess();
-
-                    response = node.GET(admin.FormatUri(uris.Engine, databaseName), null);
-                    response.FailIfNotSuccess();
-                }
-
-                var engine = new Engine();
-                engine.PopulateFromJson(response.Body);
-
+                var engine = RunWithinContext();
                 Status.CompleteJob(string.Format("started, code host PID: {0}", engine.CodeHostProcess.PID));
 
             } catch (Exception e) {
@@ -123,6 +59,70 @@ namespace Starcounter.CLI {
                 WriteErrorLogsToConsoleAfterRun = true;
                 return;
             }
+        }
+
+        private Engine RunWithinContext() {
+            ShowStatus("assuring server is running", true);
+            StartServerCLICommand.Create().ExecuteWithin(this);
+
+            ErrorDetail errorDetail;
+            EngineReference engineRef;
+            var node = Node;
+            var admin = AdminAPI;
+            var uris = admin.Uris;
+            var args = CLIArguments;
+            var databaseName = DatabaseName.ToLowerInvariant();
+            int statusCode;
+
+            ShowStatus("retrieving database status", true);
+            var response = node.GET(admin.FormatUri(uris.Engine, databaseName), null);
+            statusCode = response.FailIfNotSuccessOr(404);
+
+            if (statusCode == 404) {
+                errorDetail = new ErrorDetail();
+                try {
+                    errorDetail.PopulateFromJson(response.Body);
+                } catch {
+                    // The content of the response is not ErrorDetail json. It might be some other 
+                    // 404 sent from a different source, that sets the content to the original 
+                    // exception message. Lets handle it as an unexpected response.
+                    HandleUnexpectedResponse(response);
+                }
+
+                if (errorDetail.ServerCode == Error.SCERRDATABASENOTFOUND) {
+                    var allowed = !args.ContainsFlag(Option.NoAutoCreateDb);
+                    if (!allowed) {
+                        var notAllowed =
+                            ErrorCode.ToMessage(Error.SCERRDATABASENOTFOUND,
+                            string.Format("Database: \"{0}\". Remove --{1} to create automatically.", databaseName, Option.NoAutoCreateDb));
+                        SharedCLI.ShowErrorAndSetExitCode(notAllowed, true);
+                    }
+
+                    CreateDatabaseCLICommand.Create(this.DatabaseName).ExecuteWithin(this);
+                }
+
+                ShowStatus("starting database");
+                engineRef = new EngineReference();
+                engineRef.Name = databaseName;
+                engineRef.NoDb = args.ContainsFlag(Option.NoDb);
+                engineRef.LogSteps = args.ContainsFlag(Option.LogSteps);
+
+                string codeHostCommands;
+                if (args.TryGetProperty(UnofficialOption.CodeHostCommandLineOptions, out codeHostCommands)) {
+                    engineRef.CodeHostCommandLineAdditions = codeHostCommands;
+                }
+
+                response = node.POST(admin.FormatUri(uris.Engines), engineRef.ToJson(), null);
+                response.FailIfNotSuccess();
+
+                response = node.GET(admin.FormatUri(uris.Engine, databaseName), null);
+                response.FailIfNotSuccess();
+            }
+
+            var engine = new Engine();
+            engine.PopulateFromJson(response.Body);
+
+            return engine;
         }
     }
 }
