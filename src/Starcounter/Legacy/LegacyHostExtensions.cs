38e81440d52a5dc2527f856b7b28523da992bc20
Per Samuelsson <per@starcounter.com>
2015-11-06 09:28:53 +0100

    Added new LegacyHostExtensions in Starcounter.Legacy, extending Application with GetLegacyContext() method.

---
+++ src/Starcounter/Legacy/LegacyHostExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Starcounter.Legacy {
+    /// <summary>
+    /// Host extension methods for legacy applications that need to
+    /// access a legacy context.
+    /// </summary>
+    public static class LegacyHostExtensions {
+        /// <summary>
+        /// Gets the legacy context of the given application.
+        /// </summary>
+        /// <param name="application">The application.</param>
+        /// <returns>A <see cref="LegacyContext"/> for the given
+        /// application.</returns>
+        public static LegacyContext GetLegacyContext(this Application application) {
+            return LegacyContext.GetContext(application);
+        }
+    }
+}
