0da18be455779f097c64ddbcc1f7c1fad8fe0d27
Per Samuelsson <per@starcounter.com>
2015-11-06 15:32:56 +0100

    Made all formatting methods of ValueFormatter virtual to support custom formatting.

--- src/Starcounter/ObjectView/ValueFormatter.cs
+++ src/Starcounter/ObjectView/ValueFormatter.cs
@@ -5,14 +5,14 @@ namespace Starcounter.ObjectView {
 
     public class ValueFormatter {
         
-        public string GetBinary(Binary? b) {
+        public virtual string GetBinary(Binary? b) {
             if (!b.HasValue) {
                 return Db.NullString;
             }
             return Db.BinaryToHex(b.Value);
         }
 
-        public string GetBoolean(bool? b) {
+        public virtual string GetBoolean(bool? b) {
             if (!b.HasValue) {
                 return Db.NullString;
             }
@@ -20,98 +20,98 @@ namespace Starcounter.ObjectView {
             return b.Value ? bool.TrueString : bool.FalseString;
         }
 
-        public string GetByte(byte? b) {
+        public virtual string GetByte(byte? b) {
             if (!b.HasValue) {
                 return Db.NullString;
             }
             return b.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetDateTime(DateTime? d) {
+        public virtual string GetDateTime(DateTime? d) {
             if (!d.HasValue) {
                 return Db.NullString;
             }
             return d.Value.ToString();
         }
 
-        public string GetDecimal(decimal? d) {
+        public virtual string GetDecimal(decimal? d) {
             if (!d.HasValue) {
                 return Db.NullString;
             }
             return d.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetDouble(double? d) {
+        public virtual string GetDouble(double? d) {
             if (!d.HasValue) {
                 return Db.NullString;
             }
             return d.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetInt16(short? s) {
+        public virtual string GetInt16(short? s) {
             if (!s.HasValue) {
                 return Db.NullString;
             }
             return s.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetInt32(int? i) {
+        public virtual string GetInt32(int? i) {
             if (!i.HasValue) {
                 return Db.NullString;
             }
             return i.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetInt64(long? l) {
+        public virtual string GetInt64(long? l) {
             if (!l.HasValue) {
                 return Db.NullString;
             }
             return l.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetObject(IObjectView o) {
+        public virtual string GetObject(IObjectView o) {
             if (o == null) {
                 return Db.NullString;
             }
             return o.ToString();
         }
 
-        public string GetSByte(sbyte? b) {
+        public virtual string GetSByte(sbyte? b) {
             if (!b.HasValue) {
                 return Db.NullString;
             }
             return b.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetSingle(float? f) {
+        public virtual string GetSingle(float? f) {
             if (!f.HasValue) {
                 return Db.NullString;
             }
             return f.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetString(string s) {
+        public virtual string GetString(string s) {
             if (s == null) {
                 return Db.NullString;
             }
             return s;
         }
 
-        public string GetUInt16(ushort? u) {
+        public virtual string GetUInt16(ushort? u) {
             if (!u.HasValue) {
                 return Db.NullString;
             }
             return u.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetUInt32(uint? u) {
+        public virtual string GetUInt32(uint? u) {
             if (!u.HasValue) {
                 return Db.NullString;
             }
             return u.Value.ToString(System.Globalization.NumberFormatInfo.InvariantInfo);
         }
 
-        public string GetUInt64(ulong? u) {
+        public virtual string GetUInt64(ulong? u) {
             if (!u.HasValue) {
                 return Db.NullString;
             }
