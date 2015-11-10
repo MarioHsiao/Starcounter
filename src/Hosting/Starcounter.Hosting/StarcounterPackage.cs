86728f1d20139debcc2c402c1b692eba8f2bf7ff
Per Samuelsson <per@starcounter.com>
2015-11-10 10:42:35 +0100

    Introduced new class StarcounterPackage in Starcounter.Hosting. In this class, we'll factor the types/special stuff we do when booting/hosting the Starcounter related assemblies.

---
+++ src/Hosting/Starcounter.Hosting/StarcounterPackage.cs
@@ -0,0 +1,47 @@
+﻿using Starcounter.Binding;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.Hosting {
+
+    /// <summary>
+    /// The package provided by Starcounter, containing types that are to be
+    /// loaded and maintained in every host.
+    /// </summary>
+    public class StarcounterPackage : Package {
+        private StarcounterPackage(TypeDef[] types, Stopwatch watch) : base(types, watch) {
+
+        }
+
+        /// <summary>
+        /// Create a <see cref="StarcounterPackage"/>, governing the right set of
+        /// type definitions.
+        /// </summary>
+        /// <param name="watch">Stop watch for diagnostics</param>
+        /// <returns>The package, ready to be processed.</returns>
+        public static StarcounterPackage Create(Stopwatch watch) {
+            var defs = DefineTypes();
+            return new StarcounterPackage(defs, watch);
+        }
+
+        static TypeDef[] DefineTypes() {
+            /*             Package package = new Package(
+                new TypeDef[] { //sysTableTypeDef, sysColumnTypeDef, sysIndexTypeDef, sysIndexColumnTypeDef,
+                    Starcounter.Metadata.Type.CreateTypeDef(), Starcounter.Metadata.DbPrimitiveType.CreateTypeDef(), 
+                    Starcounter.Metadata.MapPrimitiveType.CreateTypeDef(), ClrPrimitiveType.CreateTypeDef(),
+                    Table.CreateTypeDef(), RawView.CreateTypeDef(),
+                    VMView.CreateTypeDef(), ClrClass.CreateTypeDef(), 
+                    Member.CreateTypeDef(), Column.CreateTypeDef(), 
+                    Property.CreateTypeDef(), CodeProperty.CreateTypeDef(), MappedProperty.CreateTypeDef(),
+                    Index.CreateTypeDef(), IndexedColumn.CreateTypeDef()
+                },
+                stopwatch_
+                );*/
+            return null;
+        }
+    }
+}
