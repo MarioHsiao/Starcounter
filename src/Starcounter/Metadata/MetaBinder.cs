b9dafae052afc7542171dbca8c4f83f8dc39882a
Per Samuelsson <per@starcounter.com>
2015-11-10 13:33:00 +0100

    Defined new class MetaBinder, needed to bind metadata classes and to provide a way to inject a concrete binder from generated code.

---
+++ src/Starcounter/Metadata/MetaBinder.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using Starcounter.Binding;
+
+namespace Starcounter.Internal.Metadata {
+
+    /// <summary>
+    /// Defines the semantics we need to bind metadata classes, and
+    /// provides a way to inject a concrete binder from generated
+    /// code.
+    /// </summary>
+    public abstract partial class MetaBinder {
+        public static MetaBinder Instance;
+
+        public abstract TypeDef[] GetDefinitions();
+        public abstract Type[] GetSpecifications();
+    }
+}
