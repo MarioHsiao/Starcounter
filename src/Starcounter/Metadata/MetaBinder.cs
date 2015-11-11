26e239bfde4c0a7dce71dd88c008f38d04574998
Per Samuelsson <per@starcounter.com>
2015-11-11 07:59:09 +0100

    Commented on MetaBinder.

--- src/Starcounter/Metadata/MetaBinder.cs
+++ src/Starcounter/Metadata/MetaBinder.cs
@@ -10,9 +10,24 @@ namespace Starcounter.Internal.Metadata {
     /// code.
     /// </summary>
     public abstract partial class MetaBinder {
+        /// <summary>
+        /// Gets the runtime instance of the binder. Assigned by
+        /// generated code.
+        /// </summary>
         public static MetaBinder Instance;
 
+        /// <summary>
+        /// Return the set of <see cref="TypeDef"/> instances representing
+        /// metadata host classes.
+        /// </summary>
+        /// <returns>Metadata host definitions.</returns>
         public abstract TypeDef[] GetDefinitions();
+
+        /// <summary>
+        /// Gets the type of every type specification belonging to a
+        /// metadata class.
+        /// </summary>
+        /// <returns>Types of specifications.</returns>
         public abstract Type[] GetSpecifications();
     }
 }
