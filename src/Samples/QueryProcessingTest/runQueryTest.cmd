da6b3653d4c6501cda6466202c0b0765e5ad740f
Per Samuelsson <per@starcounter.com>
2015-11-17 19:17:21 +0100

    Adapted QueryProceessingTest batch/cmd scripts to recent changes in sccode.exe command line (now including the instance ID).

--- src/Samples/QueryProcessingTest/runQueryTest.cmd
+++ src/Samples/QueryProcessingTest/runQueryTest.cmd
@@ -1,4 +1,4 @@
 pushd C:\GitRepositories\Starcounter\Level1\bin\Debug
 scweaver.exe s\QueryProcessingTest\QueryProcessingTest.exe
-sccode.exe QUERYPROCESSINGTEST --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --FLAG:UseConsole  --FLAG:NoNetworkGateway
+sccode.exe 1 QUERYPROCESSINGTEST --DatabaseDir=.db --OutputDir=.db.output --TempDir=.db.output --FLAG:UseConsole  --FLAG:NoNetworkGateway
 popd
