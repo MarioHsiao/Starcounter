50d846d23387c64ced25d471b0b992be6601b035
alemoi <alexey.moiseenko@starcounter.com>
2015-12-07 20:06:33 +0100

    Added simple independent tests.

---
+++ src/Tests/SimpleIndependentTests/SimpleIndependentTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Starcounter;
+
+namespace SimpleIndependentTests {
+    class SimpleIndependentTests {
+
+        static Int32 Main() {
+
+            Int32 errCode = TestAppName.Run();
+            if (0 != errCode) {
+                return errCode;
+            }
+
+            return 0;
+        }
+    }
+}
