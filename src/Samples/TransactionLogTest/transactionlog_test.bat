8ae63da87debc2a874fd7fd9f06582dbd2520cad
bigwad <vadim.senchukov@starcounter.com>
2015-12-14 12:21:49 +0100

    fix transactionlog_test.bat and place it back to the test system

--- src/Samples/TransactionLogTest/transactionlog_test.bat
+++ src/Samples/TransactionLogTest/transactionlog_test.bat
@@ -1,36 +1,36 @@
 :: Some predefined constants.
-SET DB_DIR=.db
-SET DB_OUT_DIR=.db.output
+SET DB_DIR=%~dp0\..\..\.db
+SET DB_OUT_DIR=%~dp0\..\..\.db.output
 SET DB_NAME=TRANSACTIONLOG
 SET TEST_NAME=TransactionLogTest
 
 :: Killing all SC processes.
-staradmin kill all
+%~dp0\..\..\staradmin kill all
 
 mkdir %DB_DIR%
 mkdir %DB_OUT_DIR%
 
 :: Creating image files.
-sccreatedb.exe -ip %DB_DIR% %DB_NAME%
+%~dp0\..\..\sccreatedb.exe -ip %DB_DIR% %DB_NAME%
 
 :: Weaving the test.
-scweaver.exe "s\%TEST_NAME%\%TEST_NAME%.exe" || exit /b 1
+%~dp0\..\..\scweaver.exe "%~dp0\%TEST_NAME%.exe" || exit /b 1
 
 :: Starting IPC monitor first.
-START CMD /C "scipcmonitor.exe PERSONAL %DB_OUT_DIR%"
+START CMD /C "%~dp0\..\..\scipcmonitor.exe PERSONAL %DB_OUT_DIR%"
 
 :: Path to signed assembly.
-SET TEST_WEAVED_ASSEMBLY=s\%TEST_NAME%\.starcounter\%TEST_NAME%.exe
+SET TEST_WEAVED_ASSEMBLY=%~dp0\.starcounter\%TEST_NAME%.exe
 
 :: Starting database memory management process.
-START CMD /C "scdata.exe 1 %DB_NAME% %DB_OUT_DIR% %DB_NAME% %DB_DIR% %DB_OUT_DIR%"
+START CMD /C "%~dp0\..\..\scdata.exe 1 %DB_NAME% %DB_OUT_DIR% %DB_NAME% %DB_DIR% %DB_OUT_DIR%"
 
 :: Starting Prolog process.
-START CMD /C "32bitComponents\scsqlparser.exe 8066"
+START CMD /C "%~dp0\..\..\32bitComponents\scsqlparser.exe 8066"
 
 :: Sleeping some time using ping.
 ping -n 5 127.0.0.1 > nul
 
 :: Starting database with some delay.
-sccode.exe 1 %DB_NAME% --DatabaseDir=%DB_DIR% --OutputDir=%DB_OUT_DIR% --TempDir=%DB_OUT_DIR% --AutoStartExePath="%TEST_WEAVED_ASSEMBLY%" --FLAG:NoNetworkGateway || exit /b 1
+%~dp0\..\..\sccode.exe 1 %DB_NAME% --DatabaseDir=%DB_DIR% --OutputDir=%DB_OUT_DIR% --TempDir=%DB_OUT_DIR% --AutoStartExePath="%TEST_WEAVED_ASSEMBLY%" --FLAG:NoNetworkGateway || exit /b 1
 
