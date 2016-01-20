ba6dc6fc7ff4cf9f2a0af8fcb7e2c69c3fb63e86
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2016-01-20 12:36:31 +0100

    Fixed query processing test to recent changes.

--- src/Samples/QueryProcessingTest/MetadataTest.cs
+++ src/Samples/QueryProcessingTest/MetadataTest.cs
@@ -454,8 +454,8 @@ namespace QueryProcessingTest {
                 nrIndexes++;
                 nrIndColumns += Db.SQL<Int64>("select count(c) from indexedcolumn c where \"index\" = ?", i).First;
             }
-            Trace.Assert(nrIndexes == 58);
-            Trace.Assert(nrIndColumns == 66);
+            Trace.Assert(nrIndexes == 59);
+            Trace.Assert(nrIndColumns == 67);
             Trace.Assert(nrIndColumns == Db.SlowSQL<Int64>(
                 "select count(*) from indexedcolumn").First);
         }
