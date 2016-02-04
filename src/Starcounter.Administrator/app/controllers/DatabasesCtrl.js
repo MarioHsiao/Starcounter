01e857339f44c0d1ab27f0c6644cdaef7d5bf5e3
andwah <anders.wahlgren@societyobjects.com>
2016-02-04 10:25:23 +0100

    Fixed some spelling errors

--- src/Starcounter.Administrator/app/controllers/DatabasesCtrl.js
+++ src/Starcounter.Administrator/app/controllers/DatabasesCtrl.js
@@ -30,7 +30,7 @@ adminModule.controller('DatabasesCtrl', ['$scope', '$log', 'HostModelService', '
 
         var title = "Delete database";
 
-        var message = $sce.trustAsHtml("This will delete the database <strong>'" + database.ID + "'</strong> <strong>permantely!</strong>.</br>All data will be completly deleted, with no ways to recover it.</br>This action is not possible to reverse.");
+        var message = $sce.trustAsHtml("This will delete the database <strong>'" + database.ID + "'</strong> <strong>permanently!</strong>.</br>All data will be completly deleted, with no ways to recover it.</br>This action is not possible to reverse.");
         var buttons = [{ result: 0, label: 'Delete Database', cssClass: 'btn-danger' }, { result: 1, label: 'Cancel', cssClass: 'btn' }];
         var model = { "title": title, "message": message, "buttons": buttons, enteredDatabaseName: "" };
         model.pattern = database.ID;
