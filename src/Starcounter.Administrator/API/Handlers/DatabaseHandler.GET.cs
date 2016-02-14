e01c6675e92b85e243de349be533c37d9cd5c99e
Per Samuelsson <per@starcounter.com>
2016-02-14 09:20:09 +0100

    Adapted resource retrieval of JSON Database resources to include configuration settings when applicable.

--- src/Starcounter.Administrator/API/Handlers/DatabaseHandler.GET.cs
+++ src/Starcounter.Administrator/API/Handlers/DatabaseHandler.GET.cs
@@ -29,12 +29,38 @@ namespace Starcounter.Administrator.API.Handlers {
             return RESTUtility.JSON.CreateResponse(body);
         }
 
-        static Database ToJSONDatabase(DatabaseInfo applicationDatabase) {
+        /// <summary>
+        /// Handles a GET for this resource's configuration.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the database whose config to return a representation of.</param>
+        /// <param name="request">
+        /// The REST request.</param>
+        /// <returns>The response to be sent back to the client.</returns>
+        static Response OnGETConfig(string name, Request request) {
+            var server = RootHandler.Host.Runtime;
+            var applicationDatabase = server.GetDatabaseByName(name);
+            if (applicationDatabase == null)
+                return 404;
+
+            var body = ToJSONDatabase(applicationDatabase, true).ToJson();
+            return RESTUtility.JSON.CreateResponse(body);
+        }
+
+        static Database ToJSONDatabase(DatabaseInfo applicationDatabase, bool includeConfiguration = false) {
             var admin = RootHandler.API;
-            
+
             var db = new Database();
             db.Name = applicationDatabase.Name;
+            db.Configuration.Uri = admin.Uris.DatabaseConfiguration.ToAbsoluteUri(applicationDatabase.Name);
             db.Engine.Uri = admin.Uris.Engine.ToAbsoluteUri(applicationDatabase.Name);
+
+            if (includeConfiguration) {
+                db.Configuration.DefaultUserHttpPort = applicationDatabase.Configuration.Runtime.DefaultUserHttpPort;
+                db.Configuration.DataDirectory = applicationDatabase.Configuration.Runtime.ImageDirectory;
+                db.Configuration.TempDirectory = applicationDatabase.Configuration.Runtime.TempDirectory;
+            }
+
             return db;
         }
     }
