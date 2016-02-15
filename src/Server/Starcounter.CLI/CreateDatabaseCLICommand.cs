810bf3bbed1a01347fad39185af388533804d0f1
Per Samuelsson <per@starcounter.com>
2016-02-15 15:22:58 +0100

    Redesigned the Starcounter.CLI CreateDatabaseCLICommand and added new method ParseAndApplyParameters, allowing any client context can pass custom parameters when creating databases.

--- src/Server/Starcounter.CLI/CreateDatabaseCLICommand.cs
+++ src/Server/Starcounter.CLI/CreateDatabaseCLICommand.cs
@@ -5,6 +5,8 @@ using Starcounter.Server;
 using Starcounter.Server.Rest;
 using Starcounter.Server.Rest.Representations.JSON;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Starcounter.CLI {
 
@@ -12,6 +14,8 @@ namespace Starcounter.CLI {
     /// Represents a CLI operation that creates a database.
     /// </summary>
     public sealed class CreateDatabaseCLICommand : CLIClientCommand {
+        Database databaseModel;
+
         internal CreateDatabaseCLICommand() : base() { }
 
         /// <summary>
@@ -34,7 +38,61 @@ namespace Starcounter.CLI {
             };
             
             cmd.ResolveServer(args);
+            cmd.databaseModel = new Database();
+            
             return cmd;
+        }
+
+        /// <summary>
+        /// Allow custom parmeters to be given when the database is to be created.
+        /// </summary>
+        /// <param name="parameters">Set of custom parameters</param>
+        public void ParseAndApplyParameters(List<string> parameters) {
+            var cfg = databaseModel.Configuration;
+
+            foreach (var keyValueString in parameters) {
+                var tokens = keyValueString.Split('=');
+                if (tokens.Length != 2) {
+                    throw ErrorCode.ToException(Error.SCERRBADARGUMENTS, string.Format("Unable to parse parameter: {0}", keyValueString));
+                }
+
+                var key = tokens[0];
+                var value = tokens[1];
+
+                var template = cfg.Template.Properties[key];
+                if (template == null) {
+                    throw ErrorCode.ToException(Error.SCERRBADARGUMENTS, string.Format("Parameter {0} is not accepted.", key));
+                }
+
+                var dataType = template.InstanceType;
+                var supportedDataType = true;
+
+                try {
+                    if (dataType == typeof(string)) {
+                        cfg[key] = value;
+                    } else if (dataType == typeof(bool)) {
+                        cfg[key] = bool.Parse(value);
+                    } else if (dataType == typeof(decimal)) {
+                        cfg[key] = decimal.Parse(value);
+                    } else if (dataType == typeof(long)) {
+                        cfg[key] = long.Parse(value);
+                    } else if (dataType == typeof(double)) {
+                        cfg[key] = double.Parse(value);
+                    }
+                    else {
+                        supportedDataType = false;
+                    }
+                }
+                catch (FormatException) {
+                    throw ErrorCode.ToException(
+                        Error.SCERRBADARGUMENTS, string.Format("Unable to parse value {0} of {1} as data type {2}.", value, key, template.JsonType));
+                }
+
+                if (!supportedDataType) {
+                    throw ErrorCode.ToException(
+                        Error.SCERRBADARGUMENTS, string.Format("Data type {0} of {1} is not supported as a parameter.", template.JsonType, key));
+                }
+            }
         }
 
         /// <inheritdoc/>
@@ -62,8 +120,9 @@ namespace Starcounter.CLI {
         }
 
         private void RunWithinContext() {
-            var db = new Database();
+            var db = this.databaseModel;
             db.Name = DatabaseName;
+
             ShowStatus("creating database");
             var response = Node.POST(AdminAPI.Uris.Databases, db.ToJson(), null);
             response.FailIfNotSuccess();
