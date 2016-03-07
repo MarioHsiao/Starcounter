a08f1d70cca3e10b8997fd8dff03d9fa26e6ac4e
bigwad <vadim.senchukov@starcounter.com>
2016-03-07 14:45:14 +0100

    client for push a load on tpcc test

--- src/Samples/TPCC/LoadEmulation.cs
+++ src/Samples/TPCC/LoadEmulation.cs
@@ -5,60 +5,97 @@ using System.Threading.Tasks;
 
 namespace tpcc
 {
-  public class TransactionsSet
-  {
-    public IEnumerable<Action> transactions { get; private set; }
-
-    //generate transactions set according to 5.2.4.2.2
-    public TransactionsSet(TpccValuesGenerator gen, int w_id, int d_id)
+    public struct TransactionDefinition
     {
-      transactions = Enumerable.Range(1, 10).Select((_) => CreateNewOrderTransaction(gen, w_id))
-             .Concat(Enumerable.Range(1, 10).Select((_) => CreatePaymentTransaction(gen, w_id)))
-             .Concat(Enumerable.Range(1, 1).Select((_) => CreateOrderStatusTransaction(gen, w_id)))
-             .Concat(Enumerable.Range(1, 1).Select((_) => CreateDeliveryTransaction(gen, w_id, d_id)))
-             .Concat(Enumerable.Range(1, 1).Select((_) => CreateStockLevelTransaction(gen, w_id, d_id)));
+        public Action self_execute;
+        public string uri;
+        public string body;
+        public bool serialization_required;
     }
 
-    private Action CreateNewOrderTransaction(TpccValuesGenerator gen, int w_id)
+    public static class LoadEmulation
     {
-      var input = tpcc.Transactions.NewOrderTransaction.InputData.Generate(gen, w_id);
-      return () => { tpcc.Transactions.NewOrderTransaction.Execute(input); };
+        //generate transactions set according to 5.2.4.2.2
+        public static IEnumerable<TransactionDefinition> CreateSet(TpccValuesGenerator gen, int w_id, int d_id)
+        {
+            return Enumerable.Range(1, 10).Select((_) => CreateNewOrderTransaction(gen, w_id))
+                   .Concat(Enumerable.Range(1, 10).Select((_) => CreatePaymentTransaction(gen, w_id)))
+                   .Concat(Enumerable.Range(1, 1).Select((_) => CreateOrderStatusTransaction(gen, w_id)))
+                   .Concat(Enumerable.Range(1, 1).Select((_) => CreateDeliveryTransaction(gen, w_id, d_id)))
+                   .Concat(Enumerable.Range(1, 1).Select((_) => CreateStockLevelTransaction(gen, w_id, d_id)));
+        }
+
+        public static IEnumerable<TransactionDefinition> CreateDeck(TpccValuesGenerator gen, int w_id, int d_id, int sets_number)
+        {
+            return gen.shuffle(
+                        Enumerable.Repeat(CreateSet(gen, w_id, d_id), sets_number)
+                                  .SelectMany(x => x)
+                   );
+        }
+
+        public static TransactionDefinition CreateNewOrderTransaction(TpccValuesGenerator gen, int w_id)
+        {
+            var input = tpcc.Transactions.NewOrderTransaction.InputData.Generate(gen, w_id);
+            return new TransactionDefinition
+            {
+                self_execute = () => { tpcc.Transactions.NewOrderTransaction.Execute(input); },
+                uri = "/do/neworder",
+                body = new NewOrder_Input { Data = input }.ToJson(),
+                serialization_required = true
+            };
+        }
+
+        public static TransactionDefinition CreatePaymentTransaction(TpccValuesGenerator gen, int w_id)
+        {
+            var input = tpcc.Transactions.PaymentTransaction.InputData.Generate(gen, w_id);
+            return new TransactionDefinition
+            {
+                self_execute = () => { tpcc.Transactions.PaymentTransaction.Execute(input); },
+                uri = "/do/payment",
+                body = new Payment_Input { Data = input }.ToJson(),
+                serialization_required = true
+            };
+        }
+
+        public static TransactionDefinition CreateOrderStatusTransaction(TpccValuesGenerator gen, int w_id)
+        {
+            var input = tpcc.Transactions.OrderStatusTransaction.InputData.Generate(gen, w_id);
+            return new TransactionDefinition
+            {
+                self_execute = () => { tpcc.Transactions.OrderStatusTransaction.Execute(input); },
+                uri = "/do/orderstatus",
+                body = new OrderStatus_Input { Data = input }.ToJson(),
+                serialization_required = true
+            };
+        }
+
+        public static TransactionDefinition CreateDeliveryTransaction(TpccValuesGenerator gen, int w_id, int d_id)
+        {
+            var input = tpcc.Transactions.DeliveryTransaction.InputData.Generate(gen, w_id, d_id);
+            return new TransactionDefinition
+            {
+                self_execute = () => { tpcc.Transactions.DeliveryTransaction.Execute(input); },
+                uri = "/do/delivery",
+                body = new Delivery_Input { Data = input }.ToJson(),
+                serialization_required = false
+            };
+        }
+
+        public static TransactionDefinition CreateStockLevelTransaction(TpccValuesGenerator gen, int w_id, int d_id)
+        {
+            var input = tpcc.Transactions.StockLevelTransaction.InputData.Generate(gen, w_id, d_id);
+            return new TransactionDefinition
+            {
+                self_execute = () => { tpcc.Transactions.StockLevelTransaction.Execute(input); },
+                uri = "/do/stocklevel",
+                body = new StockLevel_Input{ Data = input }.ToJson(),
+                serialization_required = true
+            };
+        }
     }
 
-    private Action CreatePaymentTransaction(TpccValuesGenerator gen, int w_id)
+    public class DeckPerTerminal
     {
-      var input = tpcc.Transactions.PaymentTransaction.InputData.Generate(gen, w_id);
-      return () => { tpcc.Transactions.PaymentTransaction.Execute(input); };
+        public IEnumerable<TransactionDefinition> trasactions { get; private set; }
     }
-
-    private Action CreateOrderStatusTransaction(TpccValuesGenerator gen, int w_id)
-    {
-      var input = tpcc.Transactions.OrderStatusTransaction.InputData.Generate(gen, w_id);
-      return () => { tpcc.Transactions.OrderStatusTransaction.Execute(input); };
-    }
-
-    private Action CreateDeliveryTransaction(TpccValuesGenerator gen, int w_id, int d_id)
-    {
-      var input = tpcc.Transactions.DeliveryTransaction.InputData.Generate(gen, w_id, d_id);
-      return () => { tpcc.Transactions.DeliveryTransaction.Execute(input); };
-    }
-
-    private Action CreateStockLevelTransaction(TpccValuesGenerator gen, int w_id, int d_id)
-    {
-      var input = tpcc.Transactions.StockLevelTransaction.InputData.Generate(gen, w_id, d_id);
-      return () => { tpcc.Transactions.StockLevelTransaction.Execute(input); };
-    }
-  }
-
-  public class DeckPerTerminal
-  {
-    public IEnumerable<Action> trasactions { get; private set; }
-    public DeckPerTerminal(TpccValuesGenerator gen, int w_id, int d_id, int sets_number)
-    {
-      trasactions = gen.shuffle(
-                          Enumerable.Repeat(new TransactionsSet(gen, w_id, d_id).transactions, sets_number)
-                                    .SelectMany(x => x)
-                    );
-    }
-  }
 }
