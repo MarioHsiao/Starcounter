7630258f95cbbcdacc4aae1814bd58478a09d9bd
alemoi <alexey.moiseenko@starcounter.com>
2016-03-17 18:11:09 +0100

    Fixed scripts return error codes.

--- src/Tests/SimpleAggregationTests/SimpleAggregation_test.bat
+++ src/Tests/SimpleAggregationTests/SimpleAggregation_test.bat
@@ -1,6 +1,6 @@
 CMD /C "star.exe AggregationTestServer.cs"
 
-IF %ERRORLEVEL% NEQ 0 GOTO :EOF
+IF %ERRORLEVEL% NEQ 0 EXIT /b 1
 
 :: Checking if Debug or Release.
 SET TEST_DIR=AggregationTestClient\bin\Release
@@ -8,4 +8,4 @@ IF NOT EXIST "%TEST_DIR%\AggregationTestClient.exe" SET TEST_DIR=AggregationTest
 
 CMD /C "%TEST_DIR%\AggregationTestClient.exe"
 
-IF %ERRORLEVEL% NEQ 0 GOTO :EOF
+IF %ERRORLEVEL% NEQ 0 EXIT /b 1
