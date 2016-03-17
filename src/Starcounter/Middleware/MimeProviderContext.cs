d87a9e2c26eecede236093f1935c6f10225f1375
Per Samuelsson <per@starcounter.com>
2016-03-17 16:17:23 +0100

    Added new class MimeProviderContext, encapsulating the context given to installed MIME providers.

---
+++ src/Starcounter/Middleware/MimeProviderContext.cs
@@ -0,0 +1,27 @@
+﻿namespace Starcounter {
+
+    /// <summary>
+    /// Context passed to installed MIME providers.
+    /// </summary>
+    public class MimeProviderContext {
+        /// <summary>
+        /// Gets the originating request.
+        /// </summary>
+        public readonly Request Request;
+
+        /// <summary>
+        /// Gets the resource returned from the handler.
+        /// </summary>
+        public readonly IResource Resource;
+
+        /// <summary>
+        /// Gets or sets the result of this or any previous provider.
+        /// </summary>
+        public byte[] Result { get; set; }
+
+        internal MimeProviderContext(Request request, IResource resource) {
+            Request = request;
+            Resource = resource;
+        }
+    }
+}
