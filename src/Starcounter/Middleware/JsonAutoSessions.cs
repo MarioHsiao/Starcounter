c67d348aabcb663e327dad7a708902d3a53a8ab9
Per Samuelsson <per@starcounter.com>
2016-03-21 15:28:10 +0100

    Added new JsonAutoSessions middleware, auto-attaching JSON part of HTML provision to a session.

---
+++ src/Starcounter/Middleware/JsonAutoSessions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Starcounter {
+    /// <summary>
+    /// Middleware that, when installed, make sure that when HTML is
+    /// requested and the handler respond with JSON, the JSON instance
+    /// returned is attached to a (patching) session if it's not
+    /// already.
+    /// </summary>
+    public class JsonAutoSessions : IMiddleware {
+        void IMiddleware.Register(Application application) {
+            application.Use(MimeProvider.Html(this.Invoke));
+        }
+
+        void Invoke(MimeProviderContext context, Action next) {
+            var json = context.Resource as Json;
+            if (json != null) {
+                if (Session.Current == null) {
+                    json.Session = new Session(SessionOptions.PatchVersioning);
+                }
+            }
+
+            next();
+        }
+    }
+}
