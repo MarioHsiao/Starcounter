5297c127d9d89dcb3d7d017d8a15f17dfdea9c71
alemoi <alexey.moiseenko@starcounter.com>
2016-04-06 00:44:52 +0200

    Cleaned the apps test not to build solutions.

--- src/BuildSystem/Tests/store_apps_test.bat
+++ src/BuildSystem/Tests/store_apps_test.bat
@@ -1,9 +1,6 @@
 PUSHD %SC_CHECKOUT_DIR%
 
 cd SignIn
-ECHO Building SignIn
-"%MsbuildExe%" SignIn.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/SignIn.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 REM call npm install > NUL
@@ -13,9 +10,6 @@ IF ERRORLEVEL 1 GOTO FAILED
 cd ..
 
 cd Launcher
-ECHO Building Launcher
-"%MsbuildExe%" Launcher.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/Launcher.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 call npm install > NUL
@@ -24,9 +18,6 @@ node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
 cd ..
 
 cd Products
-ECHO Building Products
-"%MsbuildExe%" Products.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/Products.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 call npm install > NUL
@@ -35,9 +26,6 @@ node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
 cd ..
 
 cd Barcodes
-ECHO Building Barcodes
-"%MsbuildExe%" Barcodes.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/Barcodes.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 call npm install > NUL
@@ -46,9 +34,6 @@ node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
 cd ..
 
 cd Procurement
-ECHO Building Procurement
-"%MsbuildExe%" Procurement.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/Procurement.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 call npm install > NUL
@@ -57,9 +42,6 @@ node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
 cd ..
 
 cd Skyper
-ECHO Building Skyper
-"%MsbuildExe%" Skyper.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/Skyper.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 call npm install > NUL
@@ -68,26 +50,12 @@ node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
 cd ..
 
 cd Images
-ECHO Building Images
-"%MsbuildExe%" Images.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-IF ERRORLEVEL 1 GOTO FAILED
 star --database=PolyjuiceTestsDb "bin/%Configuration%/Images.exe"
 IF ERRORLEVEL 1 GOTO FAILED
 REM call npm install > NUL
 REM call npm install mocha-teamcity-reporter > NUL
 REM node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
 cd ..
-
-REM cd Cartographer
-REM ECHO Building Cartographer
-REM "%MsbuildExe%" Cartographer.sln /p:ReferencePath="%StarcounterBin%;%StarcounterBin%/EditionLibraries;%StarcounterBin%/LibrariesWithDatabaseClasses" /p:Configuration=%Configuration% %MsBuildCommonParams%
-REM IF ERRORLEVEL 1 GOTO FAILED
-REM star --database=PolyjuiceTestsDb "bin/%Configuration%/Cartographer.exe"
-REM IF ERRORLEVEL 1 GOTO FAILED
-REM call npm install > NUL
-REM call npm install mocha-teamcity-reporter > NUL
-REM node node_modules\mocha\bin\mocha --reporter mocha-teamcity-reporter
-REM cd ..
 
 staradmin --database=PolyjuiceTestsDb stop db
 staradmin --database=PolyjuiceTestsDb delete --force db
