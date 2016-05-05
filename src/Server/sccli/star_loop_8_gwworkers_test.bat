805703a7b75a0791a5218b58979536ba6ce37edb
alemoi <alexey.moiseenko@starcounter.com>
2016-05-05 16:37:54 +0200

    Added logsteps to star_loop_8_gwworkers_test.

--- src/Server/sccli/star_loop_8_gwworkers_test.bat
+++ src/Server/sccli/star_loop_8_gwworkers_test.bat
@@ -2,6 +2,7 @@
 
 SET SC_GW_WORKERS_NUMBER=8
 ECHO Starting test with number of gateway workers: %SC_GW_WORKERS_NUMBER%
+SET SC_TIMESTAMPS_FILE_PATH=star_loop_8_gwworkers_test_log.txt
 
 for /l %%x in (1, 1, 30) do (
 
@@ -9,7 +10,7 @@ for /l %%x in (1, 1, 30) do (
    echo %%x
    
    :: Starting NetworkIOTest
-   star.exe "%StarcounterBin%\s\NetworkIoTest\NetworkIoTest.exe" DbNumber=1 PortNumber=8080 TestType=MODE_NODE_TESTS
+   star.exe --logsteps "%StarcounterBin%\s\NetworkIoTest\NetworkIoTest.exe" DbNumber=1 PortNumber=8080 TestType=MODE_NODE_TESTS
    
    :: Checking exit code.
    IF ERRORLEVEL 1 GOTO TESTFAILED
