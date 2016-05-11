114bf5684f986ea542060d0bf7a7167101dd621c
Per Samuelsson <per@starcounter.com>
2016-05-11 14:43:59 +0200

    Reintroduced the admin server GatewayService, now responsible initially for unregistration of code hosts when they exit.

--- src/Server/Starcounter.Server/CoreServices/GatewayService.cs
+++ src/Server/Starcounter.Server/CoreServices/GatewayService.cs
@@ -1,40 +1,16 @@
-﻿#if false // TODO: Remove!
-using System.Diagnostics;
-using System.IO;
+﻿
+using Starcounter.Internal;
+using Starcounter.Logging;
 
-namespace Starcounter.Server {
+namespace Starcounter.Server
+{
     /// <summary>
     /// Encapsulates the starting and stopping of the Starcounter
     /// gateway process.
     /// </summary>
     internal sealed class GatewayService {
+        readonly LogSource log = ServerLogSources.Default;
         readonly ServerEngine engine;
-        string gatewayExePath;
-        string gatewayXmlConfigPath;
-        string monitorOutputDirectory;
-
-        /// <summary>
-        /// A constant value holding the name of the gateway executable.
-        /// </summary>
-        internal const string ExeName = StarcounterConstants.ProgramNames.ScNetworkGateway + ".exe";
-
-        /// <summary>
-        /// Launches the Starcounter gateway process with the given arguments.
-        /// </summary>
-        /// <param name="exePath">The path to the gateway executable.</param>
-        /// <param name="serverTypeName">The name/type of the server to give to the gateway.</param>
-        /// <param name="xmlConfigPath">The path to the gateway configuration file.</param>
-        /// <param name="monitoringOutputPath">The path to the monitoring output directory.</param>
-        internal static void LaunchGatewayProcess(
-            string exePath,
-            string serverTypeName,
-            string xmlConfigPath,
-            string monitoringOutputPath) {
-            var process = new Process();
-            process.StartInfo.FileName = exePath;
-            process.StartInfo.Arguments = string.Format("{0} \"{1}\" \"{2}\"", serverTypeName, xmlConfigPath, monitoringOutputPath);
-            ToolInvocationHelper.StartTool(process);
-        }
         
         /// <summary>
         /// Initializes a new <see cref="GatewayService"/> running as part
@@ -44,6 +20,7 @@ namespace Starcounter.Server {
         /// the gateway service runs.</param>
         internal GatewayService(ServerEngine engine) {
             this.engine = engine;
+            this.log = ServerLogSources.Default;
         }
 
         /// <summary>
@@ -51,25 +28,26 @@ namespace Starcounter.Server {
         /// running as part of a server.
         /// </summary>
         internal void Setup() {
-            gatewayExePath = Path.Combine(engine.InstallationDirectory, GatewayService.ExeName);
-            if (!File.Exists(gatewayExePath)) {
-                throw ErrorCode.ToException(Error.SCERRUNSPECIFIED, string.Format("Unable to find the gateway executable: {0}", gatewayExePath));
-            }
-            gatewayXmlConfigPath = Path.Combine(engine.InstallationDirectory, "scnetworkgateway.xml");
-            if (!File.Exists(gatewayExePath)) {
-                throw ErrorCode.ToException(Error.SCERRUNSPECIFIED, string.Format("Unable to find the gateway configuration file: {0}", gatewayXmlConfigPath));
-            }
-            monitorOutputDirectory = engine.Configuration.LogDirectory;
         }
 
         /// <summary>
-        /// Starts the <see cref="GatewayService"/>, effectively starting the
-        /// Starcounter gateway process using arguments fetched from the server
-        /// engine under which the current service runs.
+        /// Unregisters existing codehost.
         /// </summary>
-        internal void Start() {
-            LaunchGatewayProcess(gatewayExePath, engine.Name, gatewayXmlConfigPath, monitorOutputDirectory);
+        internal void UnregisterCodehost(string databaseName) {
+            var body = databaseName + " " + MixedCodeConstants.EndOfRequest;
+
+            var r = Http.DELETE(
+                "http://localhost:" + StarcounterEnvironment.Default.SystemHttpPort + "/gw/codehost", body, null);
+
+            if (!r.IsSuccessStatusCode) {
+                var errCodeStr = r.Headers[MixedCodeConstants.ScErrorCodeHttpHeader];
+                var code = string.IsNullOrWhiteSpace(errCodeStr) ? Error.SCERRUNSPECIFIED : uint.Parse(errCodeStr);
+
+                log.LogError(
+                    "GatewayService: failed to unregister code host for database {0}. Gateway returned: {1}/{2}.", databaseName, r.StatusCode, code);
+
+                throw ErrorCode.ToException(code, r.Body);
+            }
         }
     }
 }
-#endif
