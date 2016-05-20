094c7835a3302eff5b49725e6b20d45976ff49c1
Per Samuelsson <per@starcounter.com>
2016-05-20 08:22:13 +0200

    Introduced specific InvalidCodeBehindException and corresponding InvalidCodeBehindError enum to provide accurate error feedback.

---
+++ src/Starcounter.XSON.PartialClassGenerator/CodeBehindParser/InvalidCodeBehindException.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starcounter.XSON.PartialClassGenerator {
+    public enum InvalidCodeBehindError {
+        NotSpecified = 0,
+        DefineInstanceConstructor,
+        MultipleMappingAttributes,
+        RootClassWithCustomMapping,
+        ClassNotPartial,
+        ClassGeneric,
+        MultipleRootClasses,
+        InputHandlerStatic,
+        InputHandlerBadParameterCount,
+        InputHandlerHasTypeParameters
+    }
+
+    public class InvalidCodeBehindException : Exception {
+        public readonly InvalidCodeBehindError Error = InvalidCodeBehindError.NotSpecified;
+        public readonly SyntaxNode Node;
+
+        public InvalidCodeBehindException(InvalidCodeBehindError error, SyntaxNode node = null) : 
+            base(Enum.GetName(typeof(InvalidCodeBehindError), error)) {
+            Error = error;
+            Node = node;
+        }
+    }
+}
