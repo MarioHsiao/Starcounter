57e521e499d7764cdf7beca8169f49202777b6cc
Per Samuelsson <per@starcounter.com>
2016-05-25 15:12:43 +0200

    Extended AdminCLI API to support stopping databases, and modified the algorithm that delete databases to trigger stopping if the database run.

--- src/Server/Starcounter.CLI/AdminCLI.cs
+++ src/Server/Starcounter.CLI/AdminCLI.cs
@@ -132,6 +132,53 @@ namespace Starcounter.CLI {
         }
 
         /// <summary>
+        /// Stops the database specified in <paramref name="database"/>.
+        /// </summary>
+        /// <param name="database">The database to stop.</param>
+        /// <param name="writeErrorToConsole">Instructs the CLI library to
+        /// write out eventual error information to the console.</param>
+        /// <returns>Zero on success; an error code otherwise. If an error
+        /// is returned, a formatted error has already been written to the
+        /// console, depending on the value of </returns>
+        /// <param name="failIfMissing">Indicates if the method should
+        /// treat it as a failure if the database does not exist.</param>
+        public int StopDatabase(string database, bool writeErrorToConsole = false, bool failIfMissing = false) {
+            if (string.IsNullOrWhiteSpace(database)) {
+                throw new ArgumentNullException("database");
+            }
+
+            var response = node.DELETE(
+                adminAPI.FormatUri(adminAPI.Uris.Engine, database), (string)null, null);
+
+            if (!response.IsSuccessStatusCode) {
+                ErrorMessage msg = null;
+                try {
+                    var detail = new ErrorDetail();
+                    detail.PopulateFromJson(response.Body);
+                    
+                    if (writeErrorToConsole) {
+                        if (failIfMissing || detail.ServerCode != Error.SCERRDATABASENOTFOUND) {
+                            SharedCLI.ShowErrorAndSetExitCode(detail);
+                        }
+                    }
+                    return (int)detail.ServerCode;
+                }
+                catch {
+                    if (response.StatusCode == 503) {
+                        throw ErrorCode.ToException(Error.SCERRSERVERNOTRUNNING);
+                    }
+                    else {
+                        // Trigger final fallback
+                        response.FailIfNotSuccess();
+                    }
+                }
+                throw msg.ToException();
+            }
+
+            return 0;
+        }
+
+        /// <summary>
         /// Deletes the database specified in <paramref name="database"/>.
         /// </summary>
         /// <param name="database">The database to delete.</param>
@@ -141,8 +188,10 @@ namespace Starcounter.CLI {
         /// is returned, a formatted error has already been written to the
         /// console, depending on the value of </returns>
         /// <param name="failIfMissing">Indicates if the method should
-        /// treat it as a failure of the database does not exist.</param>
-        public int DeleteDatabase(string database, bool writeErrorToConsole = false, bool failIfMissing = false) {
+        /// treat it as a failure if the database does not exist.</param>
+        /// <param name="stopIfRunning">Stops the database before deleting it, 
+        /// if it's found to be running.</param>
+        public int DeleteDatabase(string database, bool writeErrorToConsole = false, bool failIfMissing = false, bool stopIfRunning = true) {
             if (string.IsNullOrWhiteSpace(database)) {
                 throw new ArgumentNullException("database");
             }
@@ -155,6 +204,18 @@ namespace Starcounter.CLI {
                 try {
                     var detail = new ErrorDetail();
                     detail.PopulateFromJson(response.Body);
+
+                    if (stopIfRunning && detail.ServerCode == Error.SCERRDATABASERUNNING) {
+                        int stopped = StopDatabase(database, writeErrorToConsole, failIfMissing);
+                        if (stopped != 0) {
+                            return stopped;
+                        }
+
+                        // Invoke ourselves recursively, but without instruction to stop to
+                        // make sure we don't acceidentaly end up in a loop we can't get out
+                        // of.
+                        return DeleteDatabase(database, writeErrorToConsole, failIfMissing, false);
+                    }
                     
                     if (writeErrorToConsole) {
                         if (failIfMissing || detail.ServerCode != Error.SCERRDATABASENOTFOUND) {
