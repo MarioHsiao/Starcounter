ee191a7bbef3a6ccb57bd0deff8f7bca955109ff
Christian Holmstrand <christian.holmstrand@starcounter.com>
2016-07-06 14:00:03 +0200

    Added parameter to exclude properties and fields that are marked with GeneratedCodeAttribute when searching for members for databinding to allow binding directly to code-behind properties.

--- src/Starcounter.XSON/ReflectionHelper.cs
+++ src/Starcounter.XSON/ReflectionHelper.cs
@@ -1,21 +1,23 @@
-﻿
-
-using System;
+﻿using System;
+using System.CodeDom.Compiler;
 using System.Reflection;
+
 namespace Starcounter.Internal.XSON {
     internal static class ReflectionHelper {
-
         /// <summary>
         /// Finds the first property with the specified name
         /// </summary>
         /// <param name="type">The type to search in</param>
         /// <param name="name">The name of the property</param>
         /// <returns>The reflected property</returns>
-        internal static MemberInfo FindPropertyOrField(Type type, string name) {
+        internal static MemberInfo FindPropertyOrField(Type type, string name, bool excludeCodegenMembers) {
             PropertyInfo[] pis = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
             PropertyInfo pi = null;
             foreach (var tmp in pis) {
                 if (tmp.Name == name) {
+                    if (excludeCodegenMembers && tmp.GetCustomAttribute<GeneratedCodeAttribute>() != null)
+                        continue;
+
                     pi = tmp;
                     break;
                 }
@@ -25,6 +27,9 @@ namespace Starcounter.Internal.XSON {
                 FieldInfo fi = null;
                 foreach (var tmp2 in fis) {
                     if (tmp2.Name == name) {
+                        if (excludeCodegenMembers && tmp2.GetCustomAttribute<GeneratedCodeAttribute>() != null)
+                            continue;
+
                         fi = tmp2;
                         break;
                     }
