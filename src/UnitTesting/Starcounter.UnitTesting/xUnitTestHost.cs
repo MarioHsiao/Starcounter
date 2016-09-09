553dcca488fc5c2f6779e8a833e8829e220e36d7
Per Samuelsson <per@starcounter.com>
2016-09-09 16:04:11 +0200

    Added xUnitTestHost, a simple implementation of TestHost, based on backing xUnit runner.

---
+++ src/UnitTesting/Starcounter.UnitTesting/xUnitTestHost.cs
@@ -0,0 +1,13 @@
+﻿
+using Starcounter.UnitTesting.xUnit;
+
+namespace Starcounter.UnitTesting
+{
+    internal class xUnitTestHost : TestHost
+    {
+        protected override void RunTestsInAssambly(string assemblyPath)
+        {
+            new HostedAssemblyTestRunner(assemblyPath).Run();
+        }
+    }
+}
