107b85f72e9fbedfd157ba8f08a38132935cee6a
Per Samuelsson <per@starcounter.com>
2016-09-09 16:00:05 +0200

    Added new trivial HostedAssemblyTestRunner, driven by underlying xUnit runner.

---
+++ src/UnitTesting/Starcounter.UnitTesting.xUnit/HostedAssemblyTestRunner.cs
@@ -0,0 +1,29 @@
+﻿
+using Xunit.Runners;
+
+namespace Starcounter.UnitTesting.xUnit
+{
+    public class HostedAssemblyTestRunner
+    {
+        readonly string assemblyPath;
+
+        public HostedAssemblyTestRunner(string path)
+        {
+            assemblyPath = path;
+        }
+
+        public void Run()
+        {
+            var runner = AssemblyRunner.WithoutAppDomain(assemblyPath);
+            runner.Start();
+        }
+
+        void WaitForRunnerCompletion(AssemblyRunner runner)
+        {
+            while (runner.Status != AssemblyRunnerStatus.Idle)
+            {
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}
