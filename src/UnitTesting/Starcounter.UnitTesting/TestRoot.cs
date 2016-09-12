d313889782747c9306bb6cb43d197e76e9635871
Per Samuelsson <per@starcounter.com>
2016-09-12 13:26:05 +0200

    Included default ITestRoot implementation: TestRoot

---
+++ src/UnitTesting/Starcounter.UnitTesting/TestRoot.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Starcounter.UnitTesting
+{
+    public class TestRoot : ITestRoot
+    {
+        public readonly string Database;
+
+        readonly Dictionary<string, TestHost> appHosts = new Dictionary<string, TestHost>();
+
+        public TestRoot(string database)
+        {
+            if (string.IsNullOrEmpty(database))
+            {
+                throw new ArgumentNullException(nameof(database));
+            }
+
+            Database = database;
+        }
+
+        public IEnumerable<TestHost> Hosts {
+            get {
+                return appHosts.Values;
+            }
+        }
+
+        public TestHost IncludeNewHost(string application)
+        {
+            if (string.IsNullOrEmpty(application))
+            {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            TestHost host;
+            var exist = appHosts.TryGetValue(application, out host);
+            if (exist)
+            {
+                return host;
+            }
+
+            host = TestHostFactory.CreateHost();
+            appHosts.Add(application, host);
+
+            return host;
+        }
+    }
+}
