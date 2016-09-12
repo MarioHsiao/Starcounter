be232fab4c419b28fcbe2bd1e2a14fd266cabdc8
Per Samuelsson <per@starcounter.com>
2016-09-12 15:29:47 +0200

    Implemented new basic xunit test discovery algorithm: TestDiscoverer.

---
+++ src/UnitTesting/Starcounter.UnitTesting.xUnit/TestDiscoverer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Starcounter.UnitTesting.xUnit
+{
+    internal class TestDiscoverer : TestMessageVisitor<IDiscoveryCompleteMessage>
+    {
+        readonly List<ITestCase> tests;
+
+        public TestDiscoverer()
+        {
+            tests = new List<ITestCase>();
+        }
+
+        public IEnumerable<ITestCase> TestCases {
+            get {
+                return tests;
+            }
+        }
+
+        public void Reset()
+        {
+            tests.Clear();
+        }
+
+        protected override bool Visit(ITestCaseDiscoveryMessage testCaseDiscovered)
+        {
+            tests.Add(testCaseDiscovered.TestCase);
+            return true;
+        }
+    }
+}
