e93547c3069c0c0d56c1c8baaa6423a6f3ff0dcc
Per Samuelsson <per@starcounter.com>
2016-09-12 14:23:15 +0200

    Defined notion of a TestAssembly, supporting iteration of hosted tests.

---
+++ src/UnitTesting/Starcounter.UnitTesting/TestAssembly.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Starcounter.UnitTesting
+{
+    /// <summary>
+    /// Define semantics of a test assembly.
+    /// </summary>
+    public abstract class TestAssembly
+    {
+        /// <summary>
+        /// The host the current assembly belong to.
+        /// </summary>
+        public TestHost Host { get; internal set; }
+
+        /// <summary>
+        /// Path to the assembly.
+        /// </summary>
+        public string AssemblyPath { get; private set; }
+
+        public TestAssembly(string assemblyPath)
+        {
+            AssemblyPath = assemblyPath;
+        }
+
+        /// <summary>
+        /// When implemented by a test framework, return the set of
+        /// tests contained in the current assembly.
+        /// </summary>
+        public abstract IEnumerable<IHostedTest> Tests { get; }
+    }
+}
