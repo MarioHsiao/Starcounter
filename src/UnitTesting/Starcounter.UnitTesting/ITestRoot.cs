a530e99470bcecba685c178782dcbf549089385c
Per Samuelsson <per@starcounter.com>
2016-09-12 13:25:08 +0200

    Added new ITestRoot abstraction. Defines a root for a set of hosted tests.

---
+++ src/UnitTesting/Starcounter.UnitTesting/ITestRoot.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+
+namespace Starcounter.UnitTesting
+{
+    /// <summary>
+    /// Define the semantics of a root node in a tree of tests.
+    /// </summary>
+    public interface ITestRoot
+    {
+        /// <summary>
+        /// Get all hosts under the current root.
+        /// </summary>
+        IEnumerable<TestHost> Hosts { get; }
+
+        /// <summary>
+        /// Include a new host for the specified application.
+        /// </summary>
+        /// <param name="application">Name of application.
+        /// </param>
+        /// <returns></returns>
+        TestHost IncludeNewHost(string application);
+    }
+}
