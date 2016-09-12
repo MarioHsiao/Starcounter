81cc7da6433214dd85e913ccb47851edf0a175fa
Per Samuelsson <per@starcounter.com>
2016-09-12 15:12:55 +0200

    Introduced TestLoader in Starcounter.Hosting, responsible for setting up and provide the root for, hosted tests.

---
+++ src/Hosting/Starcounter.Hosting/TestLoader.cs
@@ -0,0 +1,56 @@
+﻿using Starcounter.Internal;
+using Starcounter.UnitTesting;
+using Starcounter.UnitTesting.xUnit;
+using System;
+using System.Collections.Generic;
+
+namespace Starcounter.Hosting
+{
+    public static class TestLoader
+    {
+        internal class TestsNotSupported : ITestRoot
+        {
+            public IEnumerable<TestHost> Hosts {
+                get {
+                    return new TestHost[0];
+                }
+            }
+
+            public TestHost IncludeNewHost(string application, ITestHostFactory factory = null)
+            {
+                throw new InvalidOperationException("The database is not configured to run tests.");
+            }
+        }
+
+        static ITestRoot testRoot;
+
+        internal static ITestRoot TestRoot {
+            get {
+                return testRoot;
+            }
+        }
+
+        public static void Setup()
+        {
+            SetupTestRoot();
+        }
+        
+        static void SetupTestRoot()
+        {
+            // In this spot, we can put constraints if testing are to be
+            // allowed in the contextual database or not. If not, create a
+            // TestRoot that don't support hosts to be specified.
+            var db = StarcounterEnvironment.DatabaseNameLower;
+            var testsAreSupported = true;
+
+            if (testsAreSupported)
+            {
+                testRoot = new TestRoot(db, new xUnitTestHostFactory());
+            }
+            else
+            {
+                testRoot = new TestsNotSupported();
+            }
+        }
+    }
+}
