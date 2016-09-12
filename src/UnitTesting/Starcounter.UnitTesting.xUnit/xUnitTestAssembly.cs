7328fa6c8ab197bee6ce03e727543b6abeb7cc72
Per Samuelsson <per@starcounter.com>
2016-09-12 15:30:43 +0200

    Implemeted discovery of tests in xUnitTestAssembly

--- src/UnitTesting/Starcounter.UnitTesting.xUnit/xUnitTestAssembly.cs
+++ src/UnitTesting/Starcounter.UnitTesting.xUnit/xUnitTestAssembly.cs
@@ -1,23 +1,61 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using Xunit;
 
 namespace Starcounter.UnitTesting.xUnit
 {
     internal class xUnitTestAssembly : TestAssembly
     {
+        TestDiscoverer discoverer;
+        XunitFrontController front;
+
         public xUnitTestAssembly(string assemblyPath) : base(assemblyPath)
         {
-            // DiscoverAllTests()
-            //
-            // TODO:
-        }
+            discoverer = new TestDiscoverer();
+            front = new XunitFrontController(AppDomainSupport.Denied, AssemblyPath);
 
+            RediscoverTests();
+        }
+        
         public override IEnumerable<IHostedTest> Tests {
             get {
-                // Return all tests discovered.
+                // Return all tests discovered by converting each
+                // test case in discoverer to a hosted test.
                 // TODO:
                 throw new NotImplementedException();
+            }
+        }
+
+        void RediscoverTests()
+        {
+            discoverer.Reset();
+            
+            var configuration = ConfigReader.Load(AssemblyPath);
+            configuration.ShadowCopy = false;
+            configuration.ParallelizeAssembly = false;
+            configuration.MaxParallelThreads = 1;
+            configuration.AppDomain = AppDomainSupport.Denied;
+            configuration.ParallelizeTestCollections = false;
+            configuration.PreEnumerateTheories = false;
+
+            var discoveryOptions = TestFrameworkOptions.ForDiscovery(configuration);
+            discoveryOptions.SetSynchronousMessageReporting(true);
+
+            try
+            {
+                front.Find(false, discoverer, discoveryOptions);
+                discoverer.Finished.WaitOne();
+            }
+            catch (InvalidOperationException e)
+            {
+                // Check if it does not find execution path. If so, it's
+                // probably not meant to be tests (e.g. app.exe). Can we
+                // distinguish this from a poor setup (i.e. xunit not copied
+                // to exe-path, etc).
+                // TODO: 
+
+                Console.WriteLine(e.ToString());
             }
         }
     }
