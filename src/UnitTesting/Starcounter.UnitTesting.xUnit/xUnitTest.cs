e0627df02c65a25a1d9d75c06ed6efd1501df9e2
Per Samuelsson <per@starcounter.com>
2016-09-13 09:27:59 +0200

    Added new xUnitTest class, providing a xUnit implementation of IHostedTest.

---
+++ src/UnitTesting/Starcounter.UnitTesting.xUnit/xUnitTest.cs
@@ -0,0 +1,27 @@
+﻿
+using Xunit.Abstractions;
+
+namespace Starcounter.UnitTesting.xUnit
+{
+    internal class xUnitTest : IHostedTest
+    {
+        readonly xUnitTestAssembly assembly;
+        readonly ITestCase testCase;
+
+        public xUnitTest(xUnitTestAssembly asm, ITestCase t)
+        {
+            assembly = asm;
+            testCase = t;
+        }
+
+        public override string ToString()
+        {
+            return $"{testCase.DisplayName} ({testCase.UniqueID})";
+        }
+
+        public override int GetHashCode()
+        {
+            return testCase.UniqueID.GetHashCode();
+        }
+    }
+}
