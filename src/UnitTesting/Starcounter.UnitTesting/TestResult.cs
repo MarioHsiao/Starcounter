781cac6223724323a55bdb9fb33bd870e123656d
Per Samuelsson <per@starcounter.com>
2016-09-15 08:44:18 +0200

    Extended TestResult to include some summarizing statistics.

--- src/UnitTesting/Starcounter.UnitTesting/TestResult.cs
+++ src/UnitTesting/Starcounter.UnitTesting/TestResult.cs
@@ -1,36 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Starcounter.UnitTesting
 {
     /// <summary>
     /// Framework agnostic result of running a configured set of
     /// sets, targeting one application in a specified database, but
-    /// possibly expanding over several test assemblies.
+    /// possibly expanding over several test assemblies. Correspond
+    /// to a single invocation of running a given host.
     /// </summary>
     public class TestResult
     {
         /// <summary>
         /// The database tests executed in.
         /// </summary>
-        public string Database;
+        public string Database { get; internal set; }
 
         /// <summary>
         /// The target application of tests.
         /// </summary>
-        public string Application;
+        public string Application { get; internal set; }
 
         /// <summary>
-        /// The time the configured set of tests started.
+        /// The time the host was asked to start executing tests.
         /// </summary>
-        public DateTime Started;
+        public DateTime Started { get; internal set; }
 
         /// <summary>
-        /// The time tests finished.
+        /// The time the host responded with a result.
         /// </summary>
-        public DateTime Finished;
+        public DateTime Finished { get; internal set; }
+
+        /// <summary>
+        /// Number of succeded tests.
+        /// </summary>
+        public int TestsSucceeded { get; set; }
+
+        /// <summary>
+        /// Number of tests that failed.
+        /// </summary>
+        public int TestsFailed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of tests that were skipped.
+        /// </summary>
+        public int TestsSkipped { get; set; }
     }
 }
