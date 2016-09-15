aaf5d238732ec7caba4990026ba39f5504249fc3
Per Samuelsson <per@starcounter.com>
2016-09-15 08:39:11 +0200

    Introduced new ResultHeader type, produced by running a test suite.

---
+++ src/UnitTesting/Starcounter.UnitTesting/Runtime/ResultHeader.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Starcounter.UnitTesting.Runtime
+{
+    public class ResultHeader
+    {
+        public string UniqueId { get; private set; }
+        public string Database { get; private set; }
+        public DateTime Start { get; private set; }
+        
+        public static ResultHeader NewStartingNow(string database)
+        {
+            return new ResultHeader()
+            {
+                UniqueId = Guid.NewGuid().ToString(),
+                Database = database,
+                Start = DateTime.Now
+            };
+        }
+    }
+}
