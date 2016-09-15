d8145a67f91492c3938d1a4137fa933ab734c0e8
Per Samuelsson <per@starcounter.com>
2016-09-15 08:46:34 +0200

    Introduced TestRunner, the primary facade to run discovered tests in a framework-neutral manner.

---
+++ src/UnitTesting/Starcounter.UnitTesting/Runtime/TestRunner.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Starcounter.UnitTesting.Runtime
+{
+    public class TestRunner
+    {
+        public static TestResult Run(TestRoot root)
+        {
+            // Filter on hosts? Filter provided by caller?
+            // TODO:
+
+            return Run(root.Database, root.Hosts.ToArray());
+        }
+
+        public static TestResult Run(string database, TestHost host)
+        {
+            return Run(database, new[] { host });
+        }
+
+        public static TestResult Run(string database, IEnumerable<TestHost> hosts)
+        {
+            var header = ResultHeader.NewStartingNow(database);
+
+            var writer = new ResultWriter(header);
+
+            foreach (var host in hosts)
+            {
+                var result = new TestResult();
+                result.Database = database;
+                result.Application = string.Empty;  // TODO
+                result.Started = DateTime.Now;
+                
+                var hostWriter = writer.OpenHostWriter(host);
+                
+                host.Run(result, hostWriter.Writer);
+                result.Finished = DateTime.Now;
+
+                writer.CloseHostWriterWithResult(hostWriter,result);
+            }
+
+            // Emit footer, flush and produce a result for each host.
+            // TODO:
+
+            return null;
+        }
+    }
+}
