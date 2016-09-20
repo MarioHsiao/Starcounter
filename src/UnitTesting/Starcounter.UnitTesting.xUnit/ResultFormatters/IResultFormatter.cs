770a0cb5efabf50838325d90e8968716d7ce055d
Per Samuelsson <per@starcounter.com>
2016-09-20 12:39:09 +0200

    Introduced new xUnit IResultFormatter interface, abstracting a formatter that can emit test results in a specific format.

---
+++ src/UnitTesting/Starcounter.UnitTesting.xUnit/ResultFormatters/IResultFormatter.cs
@@ -0,0 +1,19 @@
+﻿
+using Xunit.Abstractions;
+
+namespace Starcounter.UnitTesting.xUnit.ResultFormatters
+{
+    internal interface IResultFormatter
+    {
+        void Open(xUnitTestAssembly[] assemblies);
+
+        void BeginAssembly(xUnitTestAssembly assembly);
+        void EndAssembly(xUnitTestAssembly assembly);
+
+        void TestPassed(xUnitTestAssembly assembly, ITestPassed passed);
+        void TestFailed(xUnitTestAssembly assembly, ITestFailed failed);
+        void TestSkipped(xUnitTestAssembly assembly, ITestSkipped skipped);
+
+        void Close();
+    }
+}
