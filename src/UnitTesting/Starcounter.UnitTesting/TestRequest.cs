bd38409007ce63f4e2dafe8d59a46e2a4a4f80cf
Per Samuelsson <per@starcounter.com>
2016-09-22 15:00:23 +0200

    New TestRequest type, encapsulating a request to run some unit tests in a host.

---
+++ src/UnitTesting/Starcounter.UnitTesting/TestRequest.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Starcounter.UnitTesting
+{
+    /// <summary>
+    /// A request to run (or just load) a set of assemblies containing
+    /// tests.
+    /// </summary>
+    [Serializable]
+    public class TestRequest
+    {
+        public string[] Assemblies { get; set; }
+
+        public byte[] ToBytes()
+        {
+            return SimpleSerializer.SerializeToByteArray(this);
+        }
+
+        public static TestRequest FromBytes(byte[] bytes)
+        {
+            return SimpleSerializer.DeserializeFromByteArray<TestRequest>(bytes);
+        }
+    }
+}
