70f1801ae1d8fe653629d5ad367e43434794a898
Per Samuelsson <per@starcounter.com>
2016-09-22 14:58:59 +0200

    Introduced simplest possible (binary) SimpleSerializer to drive unit test request and result propagation.

---
+++ src/UnitTesting/Starcounter.UnitTesting/SimpleSerializer.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Starcounter.UnitTesting
+{
+    /// <summary>
+    /// Simple as possible, but ok for our needs at this point.
+    /// </summary>
+    internal static class SimpleSerializer
+    {
+        public static byte[] SerializeToByteArray<T>(T target)
+        {
+            var f = new BinaryFormatter();
+            var bytes = new MemoryStream();
+
+            f.Serialize(bytes, target);
+
+            return bytes.ToArray();
+        }
+
+        public static T DeserializeFromByteArray<T>(byte[] content)
+        {
+            var f = new BinaryFormatter();
+            return (T)f.Deserialize(new MemoryStream(content));
+        }
+    }
+}
