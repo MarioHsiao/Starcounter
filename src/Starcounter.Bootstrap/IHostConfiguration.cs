a2e4dbe9f326a351d8c6b7a8b6c3cc8e80db9d3b
Per Samuelsson <per@starcounter.com>
2016-10-10 14:09:30 +0200

    Extracted new IHostConfiguration interface from bootstrap configuration.

---
+++ src/Starcounter.Bootstrap/IHostConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Starcounter.Bootstrap
+{
+    public interface IHostConfiguration
+    {
+        string AutoStartExePath { get; }
+        uint ChunksNumber { get; }
+        uint DefaultSessionTimeoutMinutes { get; }
+        ushort DefaultSystemHttpPort { get; }
+        ushort DefaultUserHttpPort { get; }
+        byte GatewayNumberOfWorkers { get; }
+        uint InstanceID { get; }
+        string Name { get; }
+        bool NoDb { get; }
+        bool NoNetworkGateway { get; }
+        string OutputDirectory { get; }
+        uint SchedulerCount { get; }
+        string ServerName { get; }
+        ushort SQLProcessPort { get; }
+        string TempDirectory { get; }
+    }
+}
