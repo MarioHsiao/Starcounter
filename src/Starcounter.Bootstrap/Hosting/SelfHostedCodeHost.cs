8cb6ad8f3f00de55e2643b83a9062ac61f47057f
Per Samuelsson <per@starcounter.com>
2016-10-10 15:46:36 +0200

    Introduced new class SelfHostedCodeHost, implementing ICodeHost.

---
+++ src/Starcounter.Bootstrap/Hosting/SelfHostedCodeHost.cs
@@ -0,0 +1,30 @@
+﻿
+using Starcounter.Bootstrap;
+using Starcounter.Ioc;
+using StarcounterInternal.Bootstrap;
+using System;
+
+namespace Starcounter.Hosting
+{
+    internal class SelfHostedCodeHost : ICodeHost
+    {
+        readonly IHostConfiguration configuration;
+
+        public SelfHostedCodeHost(IHostConfiguration config)
+        {
+            configuration = config;
+        }
+
+        public IServices Services {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Run(Action entrypoint)
+        {
+            var control = Control.CreateAndInitialize(LogSources.Hosting);
+            control.RunUntilExit(() => { return configuration; }, entrypoint);
+        }
+    }
+}
