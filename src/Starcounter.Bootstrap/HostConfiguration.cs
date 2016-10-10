a9f102fbf2f45694566c8fd4407050e000918744
Per Samuelsson <per@starcounter.com>
2016-10-10 14:30:07 +0200

    Introduced HostConfiguration, a standard POCO IHostConfiguration implementation.

---
+++ src/Starcounter.Bootstrap/HostConfiguration.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace Starcounter.Bootstrap
+{
+    public sealed class HostConfiguration : IHostConfiguration
+    {
+        public string AutoStartExePath { get; set; }
+
+        public string AutoStartUserArguments { get; set; }
+
+        public string AutoStartWorkingDirectory { get; set; }
+
+        public uint ChunksNumber { get; set; }
+
+        public uint DefaultSessionTimeoutMinutes { get; set; }
+
+        public ushort DefaultSystemHttpPort { get; set; }
+
+        public ushort DefaultUserHttpPort { get; set; }
+
+        public byte GatewayNumberOfWorkers { get; set; }
+
+        public uint InstanceID { get; set; }
+
+        public string Name { get; set; }
+
+        public bool NoDb { get; set; }
+
+        public bool NoNetworkGateway { get; set; }
+
+        public string OutputDirectory { get; set; }
+
+        public uint SchedulerCount { get; set; }
+
+        public string ServerName { get; set; }
+
+        public ushort SQLProcessPort { get; set; }
+
+        public string TempDirectory { get; set; }
+    }
+}
