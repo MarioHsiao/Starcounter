8b633a476563772599d72ea1ec6129857dcb9fb8
Per Samuelsson <per@starcounter.com>
2016-10-13 14:00:58 +0200

    Introduced new HostConfigurationFromServerConfiguration, enabling bootstrapping a self-hosted host using server side configuration.

---
+++ src/Starcounter.Bootstrap/Hosting/HostConfigurationFromServerConfiguration.cs
@@ -0,0 +1,80 @@
+﻿
+using Starcounter.Bootstrap;
+using Starcounter.Internal;
+using Starcounter.Server.Rest.Representations.JSON;
+using System;
+
+namespace Starcounter.Hosting
+{
+    using Server = Starcounter.Server.Rest.Representations.JSON.Server;
+
+    /// <summary>
+    /// Host configuration provider that leverage configuration from an
+    /// admin server based API (and it's underlying configuration).
+    /// </summary>
+    internal class HostConfigurationFromServerConfiguration
+    {
+        readonly string DatabaseName;
+        readonly int AdminPort;
+
+        public HostConfigurationFromServerConfiguration(string name) : this(name, GetResolvedSystemPort())
+        {
+        }
+
+        public HostConfigurationFromServerConfiguration(string name, int systemPort)
+        {
+            DatabaseName = name;
+            AdminPort = systemPort;
+        }
+        
+        public IHostConfiguration CreateConfiguration()
+        {
+            var endpoint = $"127.0.0.1:{AdminPort}";
+
+            var server = GET<Server>(endpoint, "api/server");
+            var database = GET<Database>(endpoint, $"api/databases/{DatabaseName}/config");
+
+            return CreateConfiguration(server, database);
+        }
+
+        static IHostConfiguration CreateConfiguration(Server server, Database database)
+        {
+            var config = new HostConfiguration();
+            config.ServerName = "PERSONAL";
+            config.Name = database.Name.ToUpper();
+            config.InstanceID = (uint)database.InstanceID;
+            config.OutputDirectory = server.Logs.LogDirectory;
+            config.TempDirectory = database.Configuration.TempDirectory;
+            config.DefaultSystemHttpPort = (ushort)server.Configuration.SystemHttpPort;
+            config.DefaultUserHttpPort = (ushort)database.Configuration.DefaultUserHttpPort;
+            config.SchedulerCount = (uint)database.Configuration.SchedulerCount;
+            config.NoDb = false;
+            config.NoNetworkGateway = false;
+
+            return config;
+        }
+
+        static T GET<T>(string endpoint, string uri) where T : Json, new()
+        {
+            var fullUri = $"http://{endpoint}/{uri}";
+
+            var response = Http.GET(fullUri);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"{response.StatusCode}-{response.StatusDescription}: {response.Body}");
+            }
+
+            var json = new T();
+            json.PopulateFromJson(response.Body);
+            return json;
+        }
+
+        static int GetResolvedSystemPort()
+        {
+            return EnvironmentExtensions.GetEnvironmentInteger(
+                StarcounterEnvironment.VariableNames.DefaultServerPersonalPort,
+                StarcounterConstants.NetworkPorts.DefaultPersonalServerSystemHttpPort
+                );
+        }
+    }
+}
