134f179fb6552af0392171e21f2cbd21975e264a
Per Samuelsson <per@starcounter.com>
2016-10-13 09:01:56 +0200

    Introduced new interface ILifetimeService, intended to factor out that part from monolithic RuntimeHost.

---
+++ src/Starcounter.Bootstrap/ILifetimeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Starcounter.Bootstrap
+{
+    /// <summary>
+    /// Defines the semantics of a service that are in charge of the
+    /// lifetime of a runtime host. The primary duty of such service
+    /// is to provide a mechanism (Run()) for the runtime host to use
+    /// to decide when it's time to shut down.
+    /// </summary>
+    public interface ILifetimeService
+    {
+        /// <summary>
+        /// Configure the current life-time service.
+        /// </summary>
+        /// <param name="configuration"></param>
+        void Configure(IHostConfiguration configuration);
+
+        /// <summary>
+        /// Announce to the lifetime service when the runtime host
+        /// moves into "started" mode.
+        /// </summary>
+        /// <param name="schedulerContext"></param>
+        void Start(IntPtr schedulerContext);
+
+        /// <summary>
+        /// Called by the runtime host, giving control to the lifetime
+        /// service to run until told to shut down.
+        /// </summary>
+        void Run();
+    }
+}
