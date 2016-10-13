0e25ea7b6ba9774d48a56fa11d1ec4e668f207ab
Per Samuelsson <per@starcounter.com>
2016-10-13 14:01:53 +0200

    Made AppHostBuilder support building a host using new HostConfigurationFromServerConfiguration provider.

--- src/Starcounter.Bootstrap/Hosting/AppHostBuilder.cs
+++ src/Starcounter.Bootstrap/Hosting/AppHostBuilder.cs
@@ -2,79 +2,61 @@
 using Starcounter.Bootstrap;
 using System;
 using System.IO;
-using System.Reflection;
 
 namespace Starcounter.Hosting
 {
-    /*
-     * Ideas:
-     * 
-var appHost =
-AppHostBuilder
-  UseDatabase("default").
-  UseDatabaseFileDirectory("").
-  UseConfiguration(DatabaseConfiguration).
-  UseAppHostSettings(new AppHostSettings() { WrapJSON, LoadExtensionLibraries, etc).
-  UseApplication("path/to/exe").
-  UseApplication(Assembly.Current).
-  UseCurrentAssemblyAsApplication().	// This should be the default
-  Build();
-
-appHost.Run(Action invokeThisAndShutDownProcessWhenItReturn);
-
-When the delegate is invoked, it can do the stuff a standard hosted
-application can do, like Application.Current.CodeHost, etc.
-
-
-IAppHost : ICodeHost
-or 
-IAppHost {
-  ICodeHost { get; }
-}
-
-Just have ICodeHost and support Run on that?
-And if you do that on the default implementation (the one used
-by the shared code host) it's either ignored or you get an exception.
-And likewise, if you do that on a process that is already implemented,
-you will get an error too.
-     */
+    /// <summary>
+    /// Semantic entrypoint for independent applications to build and
+    /// bootstrap self-hosted code hosts.
+    /// </summary>
+    /// <example>
+    /// // The idea:
+    /// <code>
+    /// var appHost = AppHostBuilder
+    ///   UseDatabase("default"). // or ...
+    ///   UseDatabaseFileDirectory(""). // and ...
+    ///   UseConfiguration(DatabaseConfiguration). // and ...
+    ///   UseAppHostSettings(new AppHostSettings() { WrapJSON, LoadExtensionLibraries, etc }). // and ...
+    ///   UseApplication("path/to/exe"). // or ..
+    ///   UseApplication(Assembly.Current). // or ...
+    ///   UseCurrentAssemblyAsApplication().	// This should be the default
+    ///   Build();
+    ///   
+    ///   appHost.Run(() => { Console.Write("Press ENTER to exit"); Console.ReadLine(); });
+    /// </code>
+    /// When Run(Action) is invoked, the delegate can do the stuff a standard hosted
+    /// application can do, like Application.Current.CodeHost, etc. Plus what standard
+    /// console/desktop applications can.
+    /// </example>
     public class AppHostBuilder
     {
+        Func<IHostConfiguration> configProvider;
+
         public AppHostBuilder UseDatabase(string name)
         {
-            // Return a builder based on a configured, named database.
-            // TODO:
-            throw new NotImplementedException();
+            var p = new HostConfigurationFromServerConfiguration(name);
+            configProvider = p.CreateConfiguration;
+            return this;
         }
 
         public AppHostBuilder UseDatabase(DirectoryInfo databaseDir)
         {
             // Return a builder based on a directory with database
             // files.
+            // var p = new DirectoryBasedConfigurationProvider(databaseDir);
+            // configProvider = p.CreateConfiguration;
             // TODO:
             throw new NotImplementedException();
         }
 
+        protected virtual IHostConfiguration CreateConfiguration()
+        {
+            return configProvider();
+        }
+
         public ICodeHost Build()
         {
-            // Prototyping
-            // TODO:
-
-            var config = new HostConfiguration();
-            config.InstanceID = 1;
-            config.Name = "DEFAULT";
-            config.ServerName = "PERSONAL";
-            config.OutputDirectory = @"C:\Users\Per\Documents\Starcounter\Personal\Logs";
-            config.TempDirectory = @"C:\Users\Per\Documents\Starcounter\Personal\Temp";
-            config.DefaultSystemHttpPort = 8181;
-            config.DefaultUserHttpPort = 8080;
-            config.SchedulerCount = (uint)Environment.ProcessorCount;
-            config.GatewayNumberOfWorkers = 2;
-            config.ChunksNumber = Starcounter.Internal.MixedCodeConstants.SHM_CHUNKS_DEFAULT_NUMBER;
-
-            config.AutoStartExePath = Assembly.GetEntryAssembly().Location;
-            config.AutoStartWorkingDirectory = Environment.CurrentDirectory;
-            
+            var config = CreateConfiguration();
             return new SelfHostedCodeHost(config); 
         }
     }
