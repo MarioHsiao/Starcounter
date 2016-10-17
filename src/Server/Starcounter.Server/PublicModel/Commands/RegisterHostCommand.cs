fc32d6198a54cac0a38122a34854dc67e60fa9e0
Per Samuelsson <per@starcounter.com>
2016-10-17 12:53:56 +0200

    New RegisterHostCommand class in Starcounter.Server.

---
+++ src/Server/Starcounter.Server/PublicModel/Commands/RegisterHostCommand.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace Starcounter.Server.PublicModel.Commands
+{
+    /// <summary>
+    /// Command issued when a host need to be registered with the server.
+    /// </summary>
+    public sealed class RegisterHostCommand : ServerCommand
+    {
+        /// <summary>
+        /// Name of the database the host run on top.
+        /// </summary>
+        public string DatabaseName { get; private set; }
+
+        /// <summary>
+        /// The process ID of the host.
+        /// </summary>
+        public int ProcessId { get; private set; }
+
+        /// <summary>
+        /// Name of a hosted application. If set, indicating the
+        /// host is a single-app self host.
+        /// </summary>
+        public string HostedApplicationName { get; set; }
+
+        /// <summary>
+        /// Initializes a new <see cref="RegisterHostCommand"/>.
+        /// </summary>
+        /// <param name="engine">The server engine</param>
+        /// <param name="databaseName">Name of the database the host run on top.</param>
+        public RegisterHostCommand(
+            ServerEngine engine, 
+            string databaseName,
+            int processId,
+            string applicationName) : base(engine, "Registering host {0}", databaseName)
+        {
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName));
+            }
+
+            ProcessId = processId;
+            HostedApplicationName = applicationName;
+        }
+    }
+}
