51f38bee5c097237cedf8ed7a1ab98947dd2371a
Per Samuelsson <per@starcounter.com>
2016-10-17 13:03:29 +0200

    Introduced new RegisterHostCommandProcessor stub type, server admin processor of RegisterHostCommand instances.

---
+++ src/Server/Starcounter.Server/Commands/Processors/RegisterHostCommandProcessor.cs
@@ -0,0 +1,23 @@
+﻿
+using Starcounter.Server.PublicModel.Commands;
+
+namespace Starcounter.Server.Commands
+{
+    [CommandProcessor(typeof(RegisterHostCommand))]
+    internal sealed class RegisterHostCommandProcessor : CommandProcessor
+    {
+        public RegisterHostCommandProcessor(ServerEngine server, ServerCommand command) : base(server, command) {
+        }
+
+        protected override void Execute()
+        {
+            Execute((RegisterHostCommand)Command);
+        }
+
+        void Execute(RegisterHostCommand command)
+        {
+            // Register the specified host.
+            // TODO:
+        }
+    }
+}
