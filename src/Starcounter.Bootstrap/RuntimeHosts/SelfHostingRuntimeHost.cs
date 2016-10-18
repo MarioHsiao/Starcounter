910336f7d7344f775d2cef23ef19c50db119060e
Per Samuelsson <per@starcounter.com>
2016-10-18 11:35:32 +0200

    Adapted SelfHostingRuntimeHost to register itself with admin server when bootstrapping the host.

--- src/Starcounter.Bootstrap/RuntimeHosts/SelfHostingRuntimeHost.cs
+++ src/Starcounter.Bootstrap/RuntimeHosts/SelfHostingRuntimeHost.cs
@@ -1,6 +1,8 @@
 ﻿
+using Starcounter.Bootstrap.Management.Representations.JSON;
 using Starcounter.Hosting;
 using System;
+using System.Diagnostics;
 
 namespace Starcounter.Bootstrap.RuntimeHosts
 {
@@ -55,6 +57,20 @@ namespace Starcounter.Bootstrap.RuntimeHosts
         protected override IExceptionManager CreateExceptionManager()
         {
             return new ExceptionManagerImpl();
+        }
+
+        protected override void RunLifetimeService(ILifetimeService lifetimeService)
+        {
+            var hostJson = new CodeHost();
+            hostJson.DatabaseName = configuration.Name;
+            hostJson.HostedApplicationName = Process.GetCurrentProcess().ProcessName;
+            hostJson.ProcessId = Process.GetCurrentProcess().Id;
+
+            var uri = $"http://localhost:{configuration.DefaultSystemHttpPort}/api/engines/{configuration.Name}/host";
+            var response = Http.PUT(uri, hostJson.ToJson(), null);
+
+            // Invoke base implementation, blocking on the service
+            base.RunLifetimeService(lifetimeService);
         }
     }
 }
