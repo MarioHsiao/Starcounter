b1ccbb77a5a0835227a294c0e0789bac8464cb90
Per Samuelsson <per@starcounter.com>
2016-10-18 10:37:26 +0200

    Introduced new CodeHost JSON representation, to be used when registering independent hosts with the admin server.

---
+++ src/Starcounter.Bootstrap/Management/Representations.JSON/CodeHost.json.cs
@@ -0,0 +1,13 @@
+﻿
+using Starcounter;
+
+namespace Starcounter.Bootstrap.Management.Representations.JSON {
+    /// <summary>
+    /// Defines the JSON representation of a code host, used to communicate
+	/// independent hosts of self-hosted apps for registration in the admin
+	/// server.
+    /// </summary>
+    [CodeHost_json]
+    partial class CodeHost : Json {
+    }
+}
