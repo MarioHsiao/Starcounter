3657fd5f620b0237797fe368f9c3ded5a9caf384
Per Samuelsson <per@starcounter.com>
2016-10-19 10:25:25 +0200

    Starcounter.Server: Introduced new (yet rough, and highly WIP) AppCompiler class and related AppCompilerException and AppCompilerError.

---
+++ src/Server/Starcounter.Server/Compiler/AppCompiler.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Starcounter.Server.Compiler
+{
+    /// <summary>
+    /// Compiler that allows programmatic compilation of Starcounter
+    /// apps.
+    /// </summary>
+    public sealed class AppCompiler {
+        readonly string tempPath;
+
+        /// <summary>
+        /// Gets or sets the name to use for the compiled application.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target assembly path. If specified before
+        /// the app is compiled, the compiler will emit the assembly to
+        /// this path, and remove any temporary artifacts.
+        /// </summary>
+        public string TargetPath { get; set; }
+
+        /// <summary>
+        /// Holds source files that should be included in the compilation.
+        /// </summary>
+        public List<string> SourceFiles = new List<string>();
+
+        /// <summary>
+        /// Holds a set of source code strings that should be included in
+        /// the compilation.
+        /// </summary>
+        public List<string> SourceCode = new List<string>();
+
+        /// <summary>
+        /// Initialize a new <see cref="AppCompiler"/> instance.
+        /// </summary>
+        public AppCompiler() {
+            var guid = Guid.NewGuid().ToString();
+            var tp = Path.GetTempPath();
+            tempPath = Path.Combine(tp, guid);
+            if (Directory.Exists(tempPath)) {
+                Directory.Delete(tempPath, true);
+            }
+        }
+
+        /// <summary>
+        /// Compiles the included source code into an application.
+        /// </summary>
+        public void Compile() {
+            if (SourceCode.Count == 0 && SourceFiles.Count == 0) {
+                throw new AppCompilerException(AppCompilerError.NoSourceSpecified);
+            }
+
+            var provider = CSharpCodeProvider.CreateProvider("CSharp");
+            var parameters = new CompilerParameters() {
+                GenerateExecutable = true,
+                GenerateInMemory = false,
+                IncludeDebugInformation = true
+            };
+
+            Directory.CreateDirectory(tempPath);
+
+            parameters.TempFiles = new TempFileCollection(tempPath, false);
+
+            // var temporaryDiskExePath = Path.GetFileNameWithoutExtension(sourceCode);
+            var temporaryDiskExePath = "Foo";
+            temporaryDiskExePath += ".exe";
+            temporaryDiskExePath = Path.Combine(parameters.TempFiles.TempDir, temporaryDiskExePath);
+            try {
+                temporaryDiskExePath = Path.GetFullPath(temporaryDiskExePath);
+            }
+            catch (PathTooLongException) {
+                // We should provide a nice error here already, saying that either
+                // the user can use another temp directory (shorter path), or have
+                // a shorter file name.
+                // TODO:
+                throw;
+            }
+
+            parameters.TempFiles.AddFile(temporaryDiskExePath, true);
+            parameters.TempFiles.AddFile(Path.ChangeExtension(temporaryDiskExePath, ".pdb"), true);
+
+            parameters.OutputAssembly = temporaryDiskExePath;
+        }
+    }
+}
---
+++ src/Server/Starcounter.Server/Compiler/AppCompilerException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Starcounter.Server.Compiler {
+
+    /// <summary>
+    /// Errors related to compiling with an <see cref="AppCompiler"/>.
+    /// </summary>
+    public enum AppCompilerError : uint {
+        /// <summary>
+        /// The compiler was given no source to compile.
+        /// </summary>
+        NoSourceSpecified
+    }
+
+    /// <summary>
+    /// Exception specific to <see cref="AppCompiler"/> errors.
+    /// </summary>
+    public sealed class AppCompilerException : Exception {
+        /// <summary>
+        /// Gets or sets the specific error.
+        /// </summary>
+        public readonly AppCompilerError Error;
+
+        /// <summary>
+        /// Initialize a new exception from an error.
+        /// </summary>
+        /// <param name="error">The specifici error</param>
+        public AppCompilerException(AppCompilerError error) : base(Enum.GetName(typeof(AppCompilerError), error)) {
+            Error = error;
+        }
+    }
+}
