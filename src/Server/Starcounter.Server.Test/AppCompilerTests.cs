13048fee0ffbbab128d7be78ee7542ebed636ae6
Per Samuelsson <per@starcounter.com>
2016-10-19 10:30:10 +0200

    Added new Starcounter.Server.Tests project with NUnit unit tests, initially targeting the upcoming AppCompiler.

---
+++ src/Server/Starcounter.Server.Test/AppCompilerTests.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+using Starcounter.Server.Compiler;
+
+namespace Starcounter.Server.Test
+{
+    public class AppCompilerTests {
+
+        [Test]
+        public void CompileNoSource() {
+            var c = new AppCompiler();
+            var e = Assert.Throws<AppCompilerException>(() => c.Compile());
+            Assert.True(e.Error == AppCompilerError.NoSourceSpecified);
+        }
+    }
+}
