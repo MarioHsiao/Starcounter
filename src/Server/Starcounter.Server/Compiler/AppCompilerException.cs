fefeeec8f73949b49c60899c579ff730d004029e
Per Samuelsson <per@starcounter.com>
2016-10-20 14:57:30 +0200

    Extended AppCompilerException to support error steaming from underlying DOM compiler.

--- src/Server/Starcounter.Server/Compiler/AppCompilerException.cs
+++ src/Server/Starcounter.Server/Compiler/AppCompilerException.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
 
 namespace Starcounter.Server.Compiler {
 
@@ -9,24 +11,57 @@ namespace Starcounter.Server.Compiler {
         /// <summary>
         /// The compiler was given no source to compile.
         /// </summary>
-        NoSourceSpecified
+        NoSourceSpecified,
+        /// <summary>
+        /// The underlying compiler failed to compile inputs.
+        /// </summary>
+        ErrorsCompiling
     }
 
     /// <summary>
     /// Exception specific to <see cref="AppCompiler"/> errors.
     /// </summary>
     public sealed class AppCompilerException : Exception {
+        readonly CompilerResults results;
+
         /// <summary>
         /// Gets or sets the specific error.
         /// </summary>
         public readonly AppCompilerError Error;
 
+        public IDictionary<string, string> CompilerErrors {
+            get {
+                if (results == null)
+                {
+                    return new Dictionary<string, string>();
+                }
+
+                var d = new Dictionary<string, string>(results.Errors.Count);
+                for (int i = 0; i < results.Errors.Count; i++)
+                {
+                    var error = results.Errors[i];
+                    d.Add(error.ErrorNumber, error.ErrorText);
+                }
+                return d;
+            }
+        }
+
         /// <summary>
         /// Initialize a new exception from an error.
         /// </summary>
-        /// <param name="error">The specifici error</param>
-        public AppCompilerException(AppCompilerError error) : base(Enum.GetName(typeof(AppCompilerError), error)) {
+        /// <param name="error">The specified error</param>
+        internal AppCompilerException(AppCompilerError error) : base(Enum.GetName(typeof(AppCompilerError), error)) {
             Error = error;
+        }
+
+        /// <summary>
+        /// Initialize a new exception from an error and a compiler
+        /// result.
+        /// </summary>
+        /// <param name="result">Results from underlying compiler.</param>
+        internal AppCompilerException(CompilerResults result) : this(AppCompilerError.ErrorsCompiling)
+        {
+            results = result;
         }
     }
 }
