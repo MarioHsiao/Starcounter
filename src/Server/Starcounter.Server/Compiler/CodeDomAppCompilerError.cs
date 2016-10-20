de7468674722b2327cb38ad8b48733d5c2fb0bae
Per Samuelsson <per@starcounter.com>
2016-10-20 17:29:39 +0200

    Introduced internal type CodeDomAppCompilerError, wrapping a CodeDom CompilerError as a IAppCompilerSourceError.

---
+++ src/Server/Starcounter.Server/Compiler/CodeDomAppCompilerError.cs
@@ -0,0 +1,43 @@
+﻿
+using System.CodeDom.Compiler;
+
+namespace Starcounter.Server.Compiler
+{
+    /// <summary>
+    /// Exposed a CodeDom based <c>CompilerError</c> as an
+    /// <c>IAppCompilerSourceError</c>.
+    /// </summary>
+    internal sealed class CodeDomAppCompilerError : IAppCompilerSourceError
+    {
+        readonly CompilerError inner;
+
+        public CodeDomAppCompilerError(CompilerError error)
+        {
+            inner = error;
+        }
+
+        int IAppCompilerSourceError.Column {
+            get {
+                return inner.Column;
+            }
+        }
+
+        string IAppCompilerSourceError.Description {
+            get {
+                return inner.ErrorText;
+            }
+        }
+
+        string IAppCompilerSourceError.File {
+            get {
+                return inner.FileName;
+            }
+        }
+
+        string IAppCompilerSourceError.Id {
+            get {
+                return inner.ErrorNumber;
+            }
+        }
+    }
+}
