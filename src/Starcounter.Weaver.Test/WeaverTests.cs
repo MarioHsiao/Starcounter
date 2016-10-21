c1082c63f7fcb269f393fd7c14fa57359afc5856
Per Samuelsson <per@starcounter.com>
2016-10-21 10:53:18 +0200

    Added new Starcounter.Weaver.Test project, using NUnit to test new Starcounter.Weaver library.

---
+++ src/Starcounter.Weaver.Test/WeaverTests.cs
@@ -0,0 +1,21 @@
+﻿
+using NUnit.Framework;
+using System;
+
+namespace Starcounter.Weaver.Test
+{
+    public class WeaverTests
+    {
+        [Test]
+        public void WeaverWithNoHostShouldRaiseException()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => new CodeWeaver(
+                null, 
+                Environment.CurrentDirectory, 
+                "Ignored.cs", 
+                Environment.CurrentDirectory, 
+                Environment.CurrentDirectory));
+            Assert.AreEqual(e.ParamName.ToLowerInvariant(), "host");
+        }
+    }
+}
