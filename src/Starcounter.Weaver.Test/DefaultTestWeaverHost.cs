229f4b397d821390f658f222855fdf28637cc5ea
Per Samuelsson <per@starcounter.com>
2016-10-24 12:45:47 +0200

    New DefaultTestWeaverHost to use in WeaverTests

---
+++ src/Starcounter.Weaver.Test/DefaultTestWeaverHost.cs
@@ -0,0 +1,25 @@
+﻿namespace Starcounter.Weaver.Test
+{
+    /// <summary>
+    /// Test host, just implementing empty stubs. Subclass if we need to
+    /// act on weaver output.
+    /// </summary>
+    internal class DefaultTestWeaverHost : IWeaverHost
+    {
+        public virtual void WriteDebug(string message, params object[] parameters)
+        {
+        }
+
+        public virtual void WriteError(uint code, string message, params object[] parameters)
+        {
+        }
+
+        public virtual void WriteInformation(string message, params object[] parameters)
+        {
+        }
+
+        public virtual void WriteWarning(string message, params object[] parameters)
+        {
+        }
+    }
+}
