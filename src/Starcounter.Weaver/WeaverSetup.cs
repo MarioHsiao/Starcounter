1a4e0c1ee789e069049b9516f0e73705e19c700b
Per Samuelsson <per@starcounter.com>
2016-10-25 13:16:48 +0200

    Further extended WeaverSetup, supporting a broader public API for drivers setting up the weaver.

--- src/Starcounter.Weaver/WeaverSetup.cs
+++ src/Starcounter.Weaver/WeaverSetup.cs
@@ -37,6 +37,26 @@ namespace Starcounter.Weaver
         /// binaries.
         /// </summary>
         public bool WeaveToCacheOnly { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a value dictating if outdated assemblies should be
+        /// weaved/transformed. Defaults to true. If this is set to false,
+        /// only analysis will be performed.
+        /// </summary>
+        public bool AnalyzeOnly { get; set; }
+
+        /// <summary>
+        /// Gets or sets the weaver runtime directory path. This path will be
+        /// consulted when the weaver needs to locate neccessary runtime files,
+        /// such as the PostSharp-related project- and plugin files.
+        /// </summary>
+        /// <remarks>
+        /// Weaver setup will try to assign this to the Starcounter binary
+        /// directory and, as a fallback, use the current directory if that
+        /// fails. Tools can override this funcationality by explicitly setting
+        /// it after the weaver component is created (and before executed).
+        /// </remarks>
+        public string WeaverRuntimeDirectory { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating if the weaver cache should be
@@ -45,5 +65,35 @@ namespace Starcounter.Weaver
         /// transformed on every run.
         /// </summary>
         public bool DisableWeaverCache { get; set; }
+
+        /// <summary>
+        /// More of an internal, experimental thing, allowing to instruct
+        /// weaver to emit a redirection layer over CRUD calls.
+        /// </summary>
+        public bool UseStateRedirect { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating if the current weaver should
+        /// exclude any possible edition libraries found when running.
+        /// </summary>
+        public bool DisableEditionLibraries { get; set; }
+
+        /// <summary>
+        /// Can be used by hosts that want to enable maximum tracing
+        /// and diagnostic output.
+        /// </summary>
+        public bool EnableTracing { get; set; }
+
+        /// <summary>
+        /// Indicates if the weaver should emit a detailed boot diagnostic message
+        /// before actual weaaving kicks in, and a similar message when finalizing.
+        /// </summary>
+        public bool EmitBootAndFinalizationDiagnostics { get; set; }
+
+        /// <summary>
+        /// Instruct the weaver to always include a location when writing errors to
+        /// the host.
+        /// </summary>
+        public bool IncludeLocationInErrorMessages { get; set; }
     }
 }
