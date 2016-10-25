ca67d515bd5b42fd0931902c39ff474a1c1f16e0
Per Samuelsson <per@starcounter.com>
2016-10-25 11:43:37 +0200

    Introduced new RemoteDomainWeaver, driver in a remote app domain, managing the real weaver setup, lifetime and execution inside the remote domain.

---
+++ src/Starcounter.Weaver/RemoteDomainWeaver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Starcounter.Weaver
+{
+    /// <summary>
+    /// Internal weaver proxy living in a remote domain from that of the
+    /// weaver initiator. 
+    /// </summary>
+    internal class RemoteDomainWeaver : MarshalByRefObject, IWeaver
+    {
+        IWeaver weaver;
+
+        WeaverSetup IWeaver.Setup {
+            get {
+                return weaver.Setup;
+            }
+        }
+
+        public void Setup(WeaverSetup weaverSetup, string weaverHostTypeName, string weaverHostTypeAssembly)
+        {
+            var hostAssembly = GetWeaverHostAssembly(weaverHostTypeAssembly);
+            if (hostAssembly == null)
+            {
+                // Return specific error, allowing local domain to raise error to
+                // the caller.
+                // TODO:
+            }
+
+            var host = hostAssembly.CreateInstance(weaverHostTypeAssembly) as IWeaverHost;
+            if (host == null)
+            {
+                // Return specific error, allowing local domain to raise error to
+                // the caller.
+                // TODO:
+            }
+
+            weaver = new CodeWeaver(weaverSetup, host);
+        }
+
+        void IWeaver.Execute()
+        {
+            weaver.Execute();
+        }
+
+        Assembly GetWeaverHostAssembly(string weaverHostTypeAssembly)
+        {
+            var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var hostAssembly = loadedAssemblies.FirstOrDefault((candidate) =>
+            {
+                return candidate.FullName.Equals(weaverHostTypeAssembly, StringComparison.InvariantCultureIgnoreCase);
+            });
+            if (hostAssembly == null)
+            {
+                hostAssembly = Assembly.Load(weaverHostTypeAssembly);
+            }
+
+            return hostAssembly;
+        }
+    }
+}
