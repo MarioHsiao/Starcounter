a9bd5accb5be23cedd127f0455c64d84f2da7ef2
Per Samuelsson <per@starcounter.com>
2016-10-25 11:36:03 +0200

    New IWeaver abstraction.

---
+++ src/Starcounter.Weaver/IWeaver.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Starcounter.Weaver
+{
+    /// <summary>
+    /// Abstraction of the weaver runtime, possible to execute
+    /// using <c>Execute()</c>.
+    /// </summary>
+    public interface IWeaver
+    {
+        /// <summary>
+        /// Gets the setup used to create the weaver.
+        /// </summary>
+        WeaverSetup Setup { get; }
+
+        /// <summary>
+        /// Executes the weaver.
+        /// </summary>
+        void Execute();
+    }
+}
