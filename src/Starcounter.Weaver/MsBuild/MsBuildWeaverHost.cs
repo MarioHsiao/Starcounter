093cb36b98052a9a842a4ce4528ded01308ed28a
Per Samuelsson <per@starcounter.com>
2016-10-31 18:01:05 +0100

    Introduced first (still unpolished, and unfinished) version of MsBuild-supported weaving.

---
+++ src/Starcounter.Weaver/MsBuild/MsBuildWeaverHost.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Starcounter.Weaver.MsBuild
+{
+    internal class MsBuildWeaverHost : IWeaverHost
+    {
+        const string exceptionSourceIdentity = "64B54F54-869E-4371-A4C1-B33D2AB73EF2";
+
+        List<string> errors = new List<string>();
+        int maxErrorCount = int.MaxValue;
+
+        // TODO: Move to Error/exception propagation helpers
+        public static bool IsOriginatorOf(Exception e)
+        {
+            return e.Source == exceptionSourceIdentity; 
+        }
+
+        // TODO: Move to Error/exception propagation helpers
+        public static IEnumerable<string> EnumerateErrorsFrom(Exception e)
+        {
+            var result = new List<string>(e.Data.Count);
+            foreach (DictionaryEntry de in e.Data)
+            {
+                result.Add(de.Value as string);
+            }
+
+            return result;
+        }
+
+        // TODO: Move to Error/exception propagation helpers
+        public static bool ContainOnlyWarnings(Exception e)
+        {
+            // Let's use this to output warnings but still allow
+            // weaver result to be true
+            // TODO:
+            return e.Source == exceptionSourceIdentity;
+        }
+
+        public void OnWeaverDone(bool result)
+        {
+            if (!result)
+            {
+                RaiseExceptionFromErrors();
+            }
+        }
+
+        public void OnWeaverSetup(WeaverSetup setup)
+        {
+            if (setup.HostProperties.ContainsKey("MsBuildWeaverHost.MaxErrors"))
+            {
+                maxErrorCount = int.Parse(setup.HostProperties["MsBuildWeaverHost.MaxErrors"]);
+            }
+        }
+
+        public void OnWeaverStart()
+        {
+        }
+
+        public void WriteDebug(string message, params object[] parameters)
+        {
+        }
+
+        public void WriteError(uint code, string message, params object[] parameters)
+        {
+            errors.Add($"{code} - {string.Format(message, parameters)}");
+
+            if (errors.Count >= maxErrorCount)
+            {
+                RaiseExceptionFromErrors();
+            }
+        }
+
+        public void WriteInformation(string message, params object[] parameters)
+        {
+        }
+
+        public void WriteWarning(string message, params object[] parameters)
+        {
+        }
+
+        void RaiseExceptionFromErrors()
+        {
+            var e = new Exception();
+            e.Source = exceptionSourceIdentity;
+
+            // TODO: Figure out a good storage of errors
+            foreach (var error in errors)
+            {
+                e.Data.Add(Guid.NewGuid().ToString(), error);
+            }
+
+            throw e;
+        }
+    }
+}
---
+++ src/Starcounter.Weaver/MsBuild/Weave.cs
@@ -0,0 +1,68 @@
+﻿
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections;
+using System.IO;
+
+namespace Starcounter.Weaver.MsBuild
+{
+    public class Weave : Task
+    {
+        [Required]
+        public string AssemblyFile { get; set; }
+
+        [Required]
+        public string CacheDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(AssemblyFile))
+            {
+                Log.LogError($"Unable to weave {AssemblyFile}: file does not exist");
+                return false;
+            }
+
+            if (!Directory.Exists(CacheDirectory))
+            {
+                Directory.CreateDirectory(CacheDirectory);
+            }
+
+            // We are weaving to cache only, so output directory should not really
+            // be in play here.
+
+            var ignoredOutputDirectory = Path.Combine(CacheDirectory, ".output_ignored");
+            if (!Directory.Exists(ignoredOutputDirectory))
+            {
+                Directory.CreateDirectory(ignoredOutputDirectory);
+            }
+            
+            var setup = new WeaverSetup()
+            {
+                InputDirectory = Path.GetDirectoryName(AssemblyFile),
+                AssemblyFile = AssemblyFile,
+                OutputDirectory = ignoredOutputDirectory,
+                CacheDirectory = CacheDirectory
+            };
+            setup.WeaveToCacheOnly = true;
+
+            Log.LogMessageFromText($"Weaving {AssemblyFile} -> {CacheDirectory}", MessageImportance.High);
+
+            var weaver = WeaverFactory.CreateWeaver(setup, typeof(MsBuildWeaverHost));
+            try
+            {
+                return weaver.Execute();
+            }
+            catch (Exception ex) when (MsBuildWeaverHost.IsOriginatorOf(ex))
+            {
+                foreach (var error in MsBuildWeaverHost.EnumerateErrorsFrom(ex))
+                {
+                    Log.LogError(error);
+                }
+
+                // Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
