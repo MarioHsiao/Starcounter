32e19d451c67185bfaede6e4cc7b13d307f6ca54
Per Samuelsson <per@starcounter.com>
2016-11-01 08:33:38 +0100

    New class Starcounter.Weaver.MsBuild.ErrorOrWarning

---
+++ src/Starcounter.Weaver/MsBuild/ErrorOrWarning.cs
@@ -0,0 +1,28 @@
+﻿namespace Starcounter.Weaver.MsBuild
+{
+    internal class ErrorOrWarning
+    {
+        public uint ErrorCode;
+        public string Message;
+
+        public bool IsWarning {
+            get {
+                return ErrorCode == 0;
+            }
+        }
+
+        public string Serialize()
+        {
+            return $"{ErrorCode}:{Message}";
+        }
+
+        public static ErrorOrWarning Deserialize(string serialized)
+        {
+            var e = new ErrorOrWarning();
+            var index = serialized.IndexOf(":");
+            e.ErrorCode = uint.Parse(serialized.Substring(0, index));
+            e.Message = serialized.Substring(index + 1);
+            return e;
+        }
+    }
+}
