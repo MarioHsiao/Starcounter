82e50d5e5e4e70bfa9e46ef603da54e046041567
Christian Holmstrand <christian.holmstrand@starcounter.com>
2016-11-03 10:18:50 +0100

    Added IExplicitBound interface and BoundAttribute that will be used to enable compile-time detection of invalid bindings in TypedJSON.

---
+++ src/Starcounter.XSON/Binding/BoundAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Starcounter.XSON.Binding {
+    /// <summary>
+    /// Attribute used to tag generated code for properties in TypedJSON that is explicitly
+    /// bound to enable compile-time errors and call-graphs from properties in dataobject.
+    /// <code>
+    /// // Generated code \\
+    /// ...
+    /// [Bound(nameof(Person.Name))] 
+    /// public string Name { get { ... } set {... } }
+    /// ...
+    /// </code>
+    /// </summary>
+    /// <remarks>
+    /// The attribute itself contains no logic and is not used runtime.
+    /// </remarks>
+    public class BoundAttribute : Attribute {
+        public BoundAttribute(string bindPath) { }
+    }
+}
