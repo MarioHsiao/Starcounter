b527b2f4a0a6b4c84aebbf97b11107a1ef8639d6
Per Samuelsson <per@starcounter.com>
2016-11-10 14:16:04 +0100

    Introduced new IAssemblyResolver abstraction.

---
+++ src/Starcounter.Bootstrap/Hosting/IAssemblyResolver.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Reflection;
+
+namespace Starcounter.Hosting
+{
+    /// <summary>
+    /// Define behavior of a custom assembly resolver.
+    /// </summary>
+    internal interface IAssemblyResolver
+    {
+        Assembly ResolveApplicationReference(ResolveEventArgs args);
+    }
+}
