f06acde5b50349606fbc2af9b92680b2678e832d
Per Samuelsson <per@starcounter.com>
2016-11-10 15:49:50 +0100

    Added self-hosted IAssemblyResolver implementation.

---
+++ src/Starcounter.Bootstrap/RuntimeHosts/SelfHosted/AssemblyResolver.cs
@@ -0,0 +1,40 @@
+﻿
+using Starcounter.Hosting;
+using Starcounter.Internal;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Starcounter.Bootstrap.RuntimeHosts.SelfHosted
+{
+    internal class AssemblyResolver : IAssemblyResolver
+    {
+        readonly string installationDirectory;
+
+        public AssemblyResolver()
+        {
+            installationDirectory = StarcounterEnvironment.InstallationDirectory;
+        }
+
+        Assembly IAssemblyResolver.RegisterApplication(string executablePath, ApplicationDirectory appDirectory)
+        {
+            // Not really interesting for us. But we could assert the call
+            // just reach us once.
+
+            return Assembly.GetEntryAssembly();
+        }
+
+        Assembly IAssemblyResolver.ResolveApplicationReference(ResolveEventArgs args)
+        {
+            var name = new AssemblyName(args.Name);
+            var assemblyPath = Path.Combine(installationDirectory, name + ".dll");
+
+            if (File.Exists(assemblyPath))
+            {
+                return Assembly.LoadFile(assemblyPath);
+            }
+
+            return null;
+        }
+    }
+}
