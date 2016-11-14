55920a052ab4df1104327e41eaa89198afcdd0c3
Per Samuelsson <per@starcounter.com>
2016-11-14 09:43:05 +0100

    New enum: CachedAssemblyArtifact.

---
+++ src/Starcounter.Weaver/Starcounter.Internal.Weaver/Cache/CachedAssemblyArtifact.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace Starcounter.Internal.Weaver.Cache
+{
+    [Flags]
+    public enum CachedAssemblyArtifact
+    {
+        Schema = 1,
+        Assembly = 2,
+        Symbols = 4,
+        Binaries = Assembly | Symbols,
+        All = Schema | Binaries
+    }
+}
