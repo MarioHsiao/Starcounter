570c87e04be5b55099c87fc458272251a6ee1956
Erlend <elandroe@icloud.com>
2016-11-16 10:30:28 +0100

    expand HTMLComposition API

--- src/Starcounter.Extensions/HTMLComposition.cs
+++ src/Starcounter.Extensions/HTMLComposition.cs
@@ -28,10 +28,23 @@ namespace Starcounter {
             }
         }
 
+        public static HTMLComposition GetAll() {
+            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c");
+        }
+
+        public static HTMLComposition GetUsingAppOrPartial(string app) {
+            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.Key LIKE ?", app).First;
+        }
+
+        public static HTMLComposition GetStandalonePartial(string partial) {
+            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.KEY=?", partial).First;
+        }
+
         public static void Register() {
 
             HTMLComposition.CreateIndex();
 
+            // Posts a composition to the database
             Handle.POST("/sc/partial/composition?key={?}&ver={?}", (Request request, string key, string version) => {
                 key = System.Uri.UnescapeDataString(key);
                 Db.Transact(() => {
@@ -47,19 +60,21 @@ namespace Starcounter {
                 return 204;
             }, new HandlerOptions() { SkipRequestFilters = true });
 
-            Handle.GET("/sc/partial/composition?key={?}&ver={?}", (string key, string version) => {
+            // Returns the wanted composition by inserting a key and/or version
+            Handle.GET("/sc/partial/composition?key={?}&ver={?}&returntype={?)", (string key, string version, string returnType) => {
                 key = System.Uri.UnescapeDataString(key);
                 var setup = HTMLComposition.GetUsingKeyAndVersion(key, version);
                 if (setup == null)
                     return 404;
 
                 Response response = new Response();
-                response.ContentType = "application/json";
+                response.ContentType = returnType == "html" ? "text/html" : "application/json";
                 response.Body = setup.Value;
                 response.StatusCode = 200;
                 return response;
             }, new HandlerOptions() { SkipRequestFilters = true });
 
+            // Deletes an html composition from the database based on the key and/or version
             Handle.DELETE("/sc/partial/composition?key={?}&ver={?}", (string key, string version) => {
                 key = System.Uri.UnescapeDataString(key);
                 Db.Transact(() => {
@@ -75,25 +90,80 @@ namespace Starcounter {
                 return 204;
             }, new HandlerOptions() { SkipRequestFilters = true });
 
-            Handle.GET("/sc/generate/partial/composition/{?}", (string app) => {
-                string sql = "SELECT i FROM Starcounter.HTMLComposition i WHERE i.Key LIKE ?";
-                StringBuilder sb = new StringBuilder();
-                int index = 0;
+            // Takes as input the name of an app an responds with all compositions connected to that app.
+            // Returns all if you set that as the app
+            Handle.GET("/sc/partial/composition?app={?}&returntype={?}", (string app) =>
+            {
+                var setup = app == "all" ? GetAll() : GetUsingAppOrPartial("%/" + app + "/%");
 
-                app = "/" + app + "/%";
-                sb.Append("INSERT INTO Starcounter.HTMLComposition(\"Key\",\"Value\") VALUES").Append(Environment.NewLine);
-
-                foreach (HTMLComposition item in Db.SQL<HTMLComposition>(sql, app)) {
-                    if (index > 0) {
-                        sb.Append(",").Append(Environment.NewLine);
-                    }
-
-                    sb.Append("    ('").Append(item.Key).Append("', '").Append(item.Value).Append("')");
-                    index++;
+                if (setup == null)
+                {
+                    return 404;
                 }
 
-                return sb.ToString();
+                Response response = new Response();
+                response.ContentType = returnType == "html" ? "text/html" : "application/json";
+                response.Body = setup.Value;
+                response.StatusCode = 200;
+                return response;
             }, new HandlerOptions() { SkipRequestFilters = true });
+
+            // returns compositions for given partials in any set of apps
+            Handle.GET("/sc/partial/composition?partial={?}&partial={?}&returntype={?}", (string partialOne, string partialTwo, string returnType) =>
+            {
+                string partials = "%/" + partialOne + "&" + partialTwo + "/%";
+                var setup = GetUsingAppOrPartial(partials);
+
+                if (setup == null)
+                {
+                    return 404;
+                }
+
+                Response response = new Response();
+                response.ContentType = returnType == "html" ? "text/html" : "application/json";
+                response.Body = setup.Value;
+                response.StatusCode = 200;
+                return response;
+
+            }, new HandlerOptions() { SkipRequestFilters = true });
+
+            Handle.GET("/sc/partial/composition?standalonepartial={?}&returntype={?}", (string partial, string returnType) =>
+            {
+                var setup = GetStandalonePartial(partial);
+
+                if (setup == null)
+                {
+                    return 404;
+                }
+
+                Response response = new Response();
+                response.ContentType = returnType == "html" ? "text/html" : "application/json";
+                response.Body = setup.Value;
+                response.StatusCode = 200;
+                return response;
+            });
+
+            //Handle.GET("/sc/generate/partial/composition/{?}", (string app) => {
+            //    string sql = "SELECT i FROM Starcounter.HTMLComposition i WHERE i.Key LIKE ?";
+            //    StringBuilder sb = new StringBuilder();
+            //    int index = 0;
+
+            //    app = "/" + app + "/%";
+            //    sb.Append("INSERT INTO Starcounter.HTMLComposition(\"Key\",\"Value\") VALUES").Append(Environment.NewLine);
+
+            //    foreach (HTMLComposition item in Db.SQL<HTMLComposition>(sql, app))
+            //    {
+            //        if (index > 0)
+            //        {
+            //            sb.Append(",").Append(Environment.NewLine);
+            //        }
+
+            //        sb.Append("    ('").Append(item.Key).Append("', '").Append(item.Value).Append("')");
+            //        index++;
+            //    }
+
+            //    return sb.ToString();
+            //}, new HandlerOptions() { SkipRequestFilters = true });
         }
     }
 }
