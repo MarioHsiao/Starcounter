12a95764e785057772202a360cac5198f396366c
Per Samuelsson <per@starcounter.com>
2016-11-16 15:55:49 +0100

    Added new neccessary PInvoke signatures for us to have more control over process creation and giving us options to attach the debugger.

---
+++ src/Development/Starcounter.VisualStudio/PInvoke/Kernel32.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Starcounter.VisualStudio.PInvoke
+{
+    internal static class Kernel32
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine,
+            ref SECURITY_ATTRIBUTES lpProcessAttributes,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint ResumeThread(IntPtr hThread);
+    }
+}
---
+++ src/Development/Starcounter.VisualStudio/PInvoke/SECURITY_ATTRIBUTES.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+namespace Starcounter.VisualStudio.PInvoke
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SECURITY_ATTRIBUTES
+    {
+        public int nLength;
+        public IntPtr lpSecurityDescriptor;
+        public int bInheritHandle;
+    }
+}
