fd18773a8ecdb9d4fa1da970279012f260dc806c
Per Samuelsson <per@starcounter.com>
2016-12-02 10:56:53 +0100

    New utility method class DirectoryExtensions.

---
+++ src/Starcounter.Internal/DirectoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Starcounter.Internal
+{
+    /// <summary>
+    /// Utility methods for directories.
+    /// </summary>
+    public static class DirectoryExtensions
+    {
+        /// <summary>
+        /// Compare two directory paths to see if the reference the same folder.
+        /// </summary>
+        /// <param name="dir1">First path</param>
+        /// <param name="dir2">Second path</param>
+        /// <returns>True if both paths reference the same folder</returns>
+        public static bool EqualDirectories(string dir1, string dir2)
+        {
+            Guard.NotNull(dir1, nameof(dir1));
+            Guard.NotNull(dir2, nameof(dir2));
+
+            return string.Compare(
+                Path.GetFullPath(dir1).TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar),
+                Path.GetFullPath(dir2).TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar),
+                StringComparison.CurrentCultureIgnoreCase) == 0;
+        }
+    }
+}
