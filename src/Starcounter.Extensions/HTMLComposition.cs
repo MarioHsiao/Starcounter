7efcc002c4e1768178d4ce025768f5749b73bab8
Erlend Landrö <elandroe@icloud.com>
2016-12-08 12:38:04 +0100

    response to Marcin review

--- src/Starcounter.Extensions/HTMLComposition.cs
+++ src/Starcounter.Extensions/HTMLComposition.cs
@@ -1,88 +1,165 @@
 ﻿using System;
-using System.Text;
 
-namespace Starcounter {
+namespace Starcounter
+{
 
     [Database]
-    public class HTMLComposition {
+    public class HTMLComposition
+    {
         public string Key;
         public string Value;
         public string Version;
 
-        public static void CreateIndex() {
+        public static void CreateIndex()
+        {
 
-            if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE i.Name = ?", "StarcounterHTMLCompositionIndex").First == null) {
+            if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE i.Name = ?", "StarcounterHTMLCompositionIndex").First == null)
+            {
                 Starcounter.Db.SQL("CREATE UNIQUE INDEX StarcounterHTMLCompositionIndex ON Starcounter.HTMLComposition (Key ASC)");
             }
         }
 
-        public static HTMLComposition GetUsingKey(string key) {
-            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.Key = ?", key).First;
+        public Response GetResponse(QueryResultRows setup, Request request, string version, string callSource)
+        {
+            List<string> body = new List<string>();
+
+            if (setup == null)
+            {
+                return 404;
+            }
+
+            if (String.IsNullOrEmpty(version))
+            {
+                foreach (HTMLComposition composition in setup)
+                {
+                    body.Add(composition.Value + Environment.NewLine);
+                }
+            }
+            else if (version == "latest")
+            {
+                body.Add(GetLatestVersion(setup).Value, callSource);
+            }
+            else
+            {
+                foreach (HTMLComposition composition in setup)
+                {
+                    if (composition.Version = version)
+                    {
+                        body.Add(composition.Value);
+                        break;
+                    }
+                }
+                if (body.Count == 0)
+                {
+                    return 404;
+                }
+            }
+            Response response = new Response();
+            response.ContentType = request.Headers["Accept"];
+            response.Body = body;
+            response.StatusCode = 200;
+            return response;
         }
 
-        public static HTMLComposition GetUsingKeyAndVersion(string key, string version) {
-            if (String.IsNullOrEmpty(version)) {
+        private HTMLComposition GetLatestVersion(QueryResultRows setup, string callSource)
+        {
+            if (callSource == "partial")
+            {
+                return Db.SQL<QueryResultRows>("SELECT c.Key, max(c.Version) FROM Starcounter.HTMLComposition c WHERE c.Key LIKE '%Page.html%' GROUP BY c.Key");
+            }
+
+            int highestVersion = 0;
+            HTMLComposition latestComposition;
+
+            foreach (HTMLComposition composition in setup)
+            {
+                int currentVersion = int.Parse(composition.Version);
+                HTMLComposition currentComposition = composition;
+                if (currentVersion > highestVersion)
+                {
+                    highestVersion = currentVersion;
+                    latestComposition = currentComposition;
+                }
+            }
+            return latestComposition;
+        }
+
+        public static HTMLComposition GetUsingKey(string key)
+        {
+            return Db.SQL<QueryResultRows>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.Key = ?", key);
+        }
+
+        public static HTMLComposition GetUsingKeyAndVersion(string key, string version)
+        {
+            if (String.IsNullOrEmpty(version))
+            {
                 return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.Key = ?", key).First;
-            } else {
+            }
+            else
+            {
                 return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.Key = ? AND c.Version = ?", key, version).First;
             }
         }
 
-        public static HTMLComposition GetAll() {
-            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c");
+        public static HTMLComposition GetAll()
+        {
+            return Db.SQL<QueryResultRows>("SELECT c FROM Starcounter.HTMLComposition c");
         }
 
-        public static HTMLComposition GetUsingAppOrPartial(string app) {
-            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.Key LIKE ?", app).First;
+        public static HTMLComposition GetUsingMergedPartials(string mergedPartials)
+        {
+            return Db.SQL<QueryResultRows>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.KEY=?", mergedPartials);
         }
 
-        public static HTMLComposition GetStandalonePartial(string partial) {
-            return Db.SQL<HTMLComposition>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.KEY=?", partial).First;
+        public static HTMLComposition GetUsingPartial(string partial)
+        {
+            return Db.SQL<QueryResultRows>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.KEY LIKE '%?%'", partial);
         }
 
-        public static void Register() {
+        public static HTMLComposition GetUsingStandalonePartial(string partial)
+        {
+            return Db.SQL<QueryResultRows>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.KEY LIKE '?%'", partial);
+        }
+
+        public static HTMLComposition GetUsingApp(string app)
+        {
+            return Db.SQL<QueryResultRows>("SELECT c FROM Starcounter.HTMLComposition c WHERE c.KEY LIKE '%?=%'", app);
+        }
+
+        public static void Register()
+        {
 
             HTMLComposition.CreateIndex();
 
-            // Posts a composition to the database
             Handle.POST("/sc/partial/composition?key={?}&ver={?}", (Request request, string key, string version) => {
                 key = System.Uri.UnescapeDataString(key);
                 Db.Transact(() => {
-                    var setup = HTMLComposition.GetUsingKeyAndVersion(key, version);
-                    if (setup == null) {
-                        setup = new HTMLComposition() {
+                    var setup = GetUsingKeyAndVersion(key, version);
+                    if (setup == null)
+                    {
+                        setup = new HTMLComposition()
+                        {
                             Key = key,
                             Version = version
                         };
                     }
                     setup.Value = request.Body;
                 });
-                return 204;
+                return 201;
             }, new HandlerOptions() { SkipRequestFilters = true });
 
-            // Returns the wanted composition by inserting a key and/or version
-            Handle.GET("/sc/partial/composition?key={?}&ver={?}&returntype={?)", (string key, string version, string returnType) => {
-                key = System.Uri.UnescapeDataString(key);
-                var setup = HTMLComposition.GetUsingKeyAndVersion(key, version);
-                if (setup == null)
-                    return 404;
-
-                Response response = new Response();
-                response.ContentType = returnType == "html" ? "text/html" : "application/json";
-                response.Body = setup.Value;
-                response.StatusCode = 200;
-                return response;
-            }, new HandlerOptions() { SkipRequestFilters = true });
-
-            // Deletes an html composition from the database based on the key and/or version
             Handle.DELETE("/sc/partial/composition?key={?}&ver={?}", (string key, string version) => {
                 key = System.Uri.UnescapeDataString(key);
                 Db.Transact(() => {
-                    if (key == "all") {
+                    if (key == "all")
+                    {
                         Db.SlowSQL("DELETE FROM Starcounter.HTMLComposition");
-                    } else {
-                        var setup = HTMLComposition.GetUsingKeyAndVersion(key, version);
-                        if (setup != null) {
+                    }
+                    else
+                    {
+                        var setup = GetUsingKeyAndVersion(key, version);
+                        if (setup != null)
+                        {
                             setup.Delete();
                         }
                     }
@@ -90,80 +167,35 @@ namespace Starcounter {
                 return 204;
             }, new HandlerOptions() { SkipRequestFilters = true });
 
-            // Takes as input the name of an app an responds with all compositions connected to that app.
-            // Returns all if you set that as the app
-            Handle.GET("/sc/partial/composition?app={?}&returntype={?}", (string app) =>
-            {
-                var setup = app == "all" ? GetAll() : GetUsingAppOrPartial("%/" + app + "/%");
-
-                if (setup == null)
-                {
-                    return 404;
-                }
-
-                Response response = new Response();
-                response.ContentType = returnType == "html" ? "text/html" : "application/json";
-                response.Body = setup.Value;
-                response.StatusCode = 200;
-                return response;
+            Handle.GET("/sc/partial/composition?key={?}&ver={?}", (Request request, string key, string version) => {
+                key = System.Uri.UnescapeDataString(key);
+                return GetResponse(GetUsingKey(key), request, version);
             }, new HandlerOptions() { SkipRequestFilters = true });
 
-            // returns compositions for given partials in any set of apps
-            Handle.GET("/sc/partial/composition?partial={?}&partial={?}&returntype={?}", (string partialOne, string partialTwo, string returnType) =>
+            Handle.GET("/sc/partial/composition/all", (Request request) =>
             {
-                string partials = "%/" + partialOne + "&" + partialTwo + "/%";
-                var setup = GetUsingAppOrPartial(partials);
-
-                if (setup == null)
-                {
-                    return 404;
-                }
-
-                Response response = new Response();
-                response.ContentType = returnType == "html" ? "text/html" : "application/json";
-                response.Body = setup.Value;
-                response.StatusCode = 200;
-                return response;
-
-            }, new HandlerOptions() { SkipRequestFilters = true });
-
-            Handle.GET("/sc/partial/composition?standalonepartial={?}&returntype={?}", (string partial, string returnType) =>
-            {
-                var setup = GetStandalonePartial(partial);
-
-                if (setup == null)
-                {
-                    return 404;
-                }
-
-                Response response = new Response();
-                response.ContentType = returnType == "html" ? "text/html" : "application/json";
-                response.Body = setup.Value;
-                response.StatusCode = 200;
-                return response;
+                return GetResponse(GetAll(), request, null, "all");
             });
 
-            //Handle.GET("/sc/generate/partial/composition/{?}", (string app) => {
-            //    string sql = "SELECT i FROM Starcounter.HTMLComposition i WHERE i.Key LIKE ?";
-            //    StringBuilder sb = new StringBuilder();
-            //    int index = 0;
+            Handle.GET("/sc/partial/composition?mergedpartial={?}&ver={?}", (string mergedPartial, string version) =>
+            {
+                return GetResponse(GetUsingMergedPartials(mergedPartial), request, version, "mergedpartial");
+            });
 
-            //    app = "/" + app + "/%";
-            //    sb.Append("INSERT INTO Starcounter.HTMLComposition(\"Key\",\"Value\") VALUES").Append(Environment.NewLine);
+            Handle.GET("/sc/partial/composition?partial={?}&ver={?}", (string partial, string version) =>
+            {
+                return GetResponse(GetUsingPartial(partial), request, version, "partial");
+            });
 
-            //    foreach (HTMLComposition item in Db.SQL<HTMLComposition>(sql, app))
-            //    {
-            //        if (index > 0)
-            //        {
-            //            sb.Append(",").Append(Environment.NewLine);
-            //        }
+            Handle.GET("/sc/partial/composition?standalonepartial={?}&ver={?}", (string standalonepartial, string version) =>
+            {
+                return GetResponse(GetUsingStandalonePartial(standalonepartial), request, version, "standalonepartial");
+            });
 
-            //        sb.Append("    ('").Append(item.Key).Append("', '").Append(item.Value).Append("')");
-            //        index++;
-            //    }
-
-            //    return sb.ToString();
-            //}, new HandlerOptions() { SkipRequestFilters = true });
+            Handle.GET("/sc/partial/composition?app={?}&ver={?}", (string app, string version) =>
+            {
+                return GetResponse(GetUsingApp(app), request, version, "app");
+            });
         }
     }
 }
